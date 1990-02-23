9a02c1fd6e0d52622d898991af40ef195b71e5e9
Marc Teitelbaum <marc>
1990-02-23 11:04:15 -0800

    add KERNTEXTOFF
    SCCS-vsn: 7.14

--- sys/vax/include/param.h
+++ sys/vax/include/param.h
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)param.h	7.13 (Berkeley) 12/30/89
+ *	@(#)param.h	7.14 (Berkeley) 02/23/90
  */
 
 /*
@@ -26,6 +26,7 @@
 #define	NPTEPG		(NBPG/(sizeof (struct pte)))
 
 #define	KERNBASE	0x80000000	/* start of kernel virtual */
+#define	KERNTEXTOFF	KERNBASE	/* start of kernel text */
 #define	BTOPKERNBASE	((u_long)KERNBASE >> PGSHIFT)
 
 #ifndef SECSIZE
