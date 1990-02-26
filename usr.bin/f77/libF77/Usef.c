936a3362bc7acda1ed5d77cf268a007920c00852
Donn Seeley <donn>
1990-02-25 18:55:59 -0800

    don't use 'cc -f' with compilers that don't do automatic double to float promotion.
    SCCS-vsn: 1.4

--- usr.bin/f77/libF77/Usef.c
+++ usr.bin/f77/libF77/Usef.c
@@ -3,7 +3,7 @@
  * All rights reserved.  The Berkeley software License Agreement
  * specifies the terms and conditions for redistribution.
  *
- *	@(#)Usef.c	1.3	09/14/87
+ *	@(#)Usef.c	1.4	02/25/90
  */
 
 /*		returns '-f' if need to use -f to bypass C bug		*/
