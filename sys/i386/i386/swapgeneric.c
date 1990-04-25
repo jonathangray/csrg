c1144d8c5ae011c48a5cd3259773c6046e53047e
William Jolitz <william>
1990-04-24 19:13:14 -0700

    1st Berkeley Release
    SCCS-vsn: 5.2

--- sys/i386/i386/swapgeneric.c
+++ sys/i386/i386/swapgeneric.c
@@ -1,4 +1,29 @@
-/*	swapgeneric.c	1.5	86/11/25	*/
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * William Jolitz.
+ *
+ * Copying or redistribution in any form is explicitly forbidden
+ * unless prior written permission is obtained from William Jolitz or an
+ * authorized representative of the University of California, Berkeley.
+ *
+ * Freely redistributable copies of this code will be available in
+ * the near future; for more information contact William Jolitz or
+ * the Computer Systems Research Group at the University of California,
+ * Berkeley.
+ *
+ * The name of the University may not be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.  THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ *
+ *	@(#)swapgeneric.c	5.2 (Berkeley) 04/24/90
+ */
+
 
 #include "../machine/pte.h"
 
