22f5d14e3462729ae23b2eaa12b69a989e3c8834
Kirk McKusick <mckusick>
1990-05-08 22:46:00 -0700

    merge in hp300 support from Utah
    SCCS-vsn: 7.1

---
+++ sys/hp300/stand/conf.c
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)conf.c	7.1 (Berkeley) 05/08/90
+ */
+
+#include "saio.h"
+
+devread(io)
+	register struct iob *io;
+{
+	int cc;
+
+	/* check for interrupt */
+	(void) peekchar();
+
+	io->i_flgs |= F_RDDATA;
+	io->i_error = 0;
+	cc = (*devsw[io->i_ino.i_dev].dv_strategy)(io, READ);
+	io->i_flgs &= ~F_TYPEMASK;
+	return (cc);
+}
+
+devwrite(io)
+	register struct iob *io;
+{
+	int cc;
+
+	io->i_flgs |= F_WRDATA;
+	io->i_error = 0;
+	cc = (*devsw[io->i_ino.i_dev].dv_strategy)(io, WRITE);
+	io->i_flgs &= ~F_TYPEMASK;
+	return (cc);
+}
+
+devopen(io)
+	register struct iob *io;
+{
+
+	(*devsw[io->i_ino.i_dev].dv_open)(io);
+}
+
+devclose(io)
+	register struct iob *io;
+{
+
+	(*devsw[io->i_ino.i_dev].dv_close)(io);
+}
+
+devioctl(io, cmd, arg)
+	register struct iob *io;
+	int cmd;
+	caddr_t arg;
+{
+
+	return ((*devsw[io->i_ino.i_dev].dv_ioctl)(io, cmd, arg));
+}
+
+/*ARGSUSED*/
+nullsys(io)
+	struct iob *io;
+{
+
+	;
+}
+
+/*ARGSUSED*/
+nullioctl(io, cmd, arg)
+	struct iob *io;
+	int cmd;
+	caddr_t arg;
+{
+
+	return (ECMD);
+}
+
+int	nullsys(), nullioctl();
+int	rdstrategy(), rdopen(), rdioctl();
+int	sdstrategy(), sdopen(), sdioctl();
+#ifndef BOOT
+int	ctstrategy(), ctopen(), ctclose();
+#endif
+
+struct devsw devsw[] = {
+	{ "rd",	rdstrategy,	rdopen,		nullsys,	nullioctl },
+	{ "sd",	sdstrategy,	sdopen,		nullsys,	nullioctl },
+#ifndef BOOT
+	{ "ct",	ctstrategy,	ctopen,		ctclose,	nullioctl },
+#endif
+	{ 0, 0, 0, 0, 0 },
+};
