fe007b107f72a1fc8cddb0fef43d0a12aa83a3fe
Keith Bostic <bostic>
1990-05-15 19:35:24 -0700

    date and time created 90/05/15 19:35:24 by bostic
    SCCS-vsn: 5.1

---
+++ include/stdlib.h
@@ -0,0 +1,129 @@
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)stdlib.h	5.1 (Berkeley) 05/15/90
+ */
+
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
+#include <machine/x3j11.h>
+
+#ifdef	_SIZE_T_
+typedef	_SIZE_T_	size_t;
+#undef	_SIZE_T_
+#endif
+
+#ifdef	_WCHAR_T_
+typedef	_WCHAR_T_	wchar_t;
+#undef	_WCHAR_T_
+#endif
+
+typedef struct {
+	int quot;		/* quotient */
+	int rem;		/* remainder */
+} div_t;
+typedef struct {
+	long quot;		/* quotient */
+	long rem;		/* remainder */
+} ldiv_t;
+
+#define	EXIT_FAILURE	1
+#define	EXIT_SUCCESS	0
+
+#define	RAND_MAX	0x7ffffffff
+
+#define	MB_CUR_MAX	1	/* XXX */
+
+#ifdef __STDC__
+
+void	 abort(void);
+int	 abs(int);
+int	 atexit(void (*_func)(void));
+double	 atof(const char *_nptr);
+int	 atoi(const char *_nptr);
+long	 atol(const char *_nptr);
+void	*bsearch(const void *_key, const void *_base, size_t _nmemb,
+	    size_t _size, int (*_compar)(const void *, const void *));
+void	*calloc(size_t _nmemb, size_t _size);
+div_t	 div(int _numer, int _denom);
+void	 exit(int _status);
+void	 free(void *_ptr);
+char	*getenv(const char *_string);
+long	 labs(long);
+ldiv_t	 ldiv(long _numer, long _denom);
+void	*malloc(size_t _size);
+void	 qsort(void *_base, size_t _nmemb, size_t _size,
+	    int (*_compar)(const void *, const void *));
+int	 rand(void);
+void	*realloc(void *_ptr, size_t _size);
+void	 srand(unsigned _seed);
+long	 strtol(const char *_nptr, char **_endptr, int _base);
+u_long	 strtoul(const char *_nptr, char **_endptr, int _base);
+int	 system(const char *_string);
+
+#else /* !__STDC__ */
+
+void	 abort();
+int	 abs();
+int	 atexit();
+double	 atof();
+int	 atoi();
+long	 atol();
+void	*bsearch();
+void	*calloc();
+div_t	 div();
+void	 exit();
+void	 free();
+char	*getenv();
+long	 labs();
+ldiv_t	 ldiv();
+void	*malloc();
+void	 qsort();
+int	 rand();
+void	*realloc();
+void	 srand();
+long	 strtol();
+u_long	 strtoul();
+int	 system();
+
+#endif /* __STDC__ */
+
+#ifdef NOT_YET_IMPLEMENTED
+int	mblen(const char *_s, size_t _n);
+size_t	mbstowcs(wchar_t *_pwcs, const char *_s, size_t _n);
+int	wctomb(char *_s, wchar_t _wchar);
+int	mbtowc(wchar_t *_pwc, const char *_s, size_t _n);
+double	strtod(const char *_nptr, char **_endptr);
+size_t	wcstombs(char *_s, const wchar_t *_pwcs, size_t _n);
+#endif
+
+#endif /* _STDLIB_H_ */
