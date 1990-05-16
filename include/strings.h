f30dcf95a9331650bbc60fcb0f98f4258c6b24da
Keith Bostic <bostic>
1990-05-15 19:03:05 -0700

    string.h is the official one
    SCCS-vsn: 5.8

--- include/strings.h
+++ include/strings.h
@@ -1,72 +1,36 @@
-/*
- * Copyright (c) 1988 The Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)strings.h	5.7 (Berkeley) 03/06/90
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)strings.h	5.8 (Berkeley) 05/15/90
  */
 
-#ifdef __STDC__
-extern int memcmp(const char *, const char *, int);
-extern int strcasecmp(const char *, const char *);
-extern int strcmp(const char *, const char *);
-extern int strcspn(const char *, const char *);
-extern int strlen(const char *);
-extern int strncasecmp(const char *, const char *, int);
-extern int strncmp(const char *, const char *, int);
-extern int strspn(const char *, const char *);
-extern char *index(const char *, int);
-extern char *memccpy(char *, const char *, int, int);
-extern char *memchr(const char *, int, int);
-extern char *memcpy(char *, const char *, int);
-extern char *memset(char *, int, int);
-extern char *rindex(const char *, int);
-extern char *strcat(char *, const char *);
-extern char *strchr(const char *, int);
-extern char *strcpy(char *, const char *);
-extern char *strdup(const char *);
-extern char *strerror(int);
-extern char *strncat(char *, const char *, int);
-extern char *strncpy(char *, const char *, int);
-extern char *strpbrk(const char *, const char *);
-extern char *strrchr(const char *, int);
-extern char *strsep(char *, const char *);
-extern char *strtok(char *, const char *);
-#else
-extern int memcmp();
-extern int strcasecmp();
-extern int strcmp();
-extern int strcspn();
-extern int strlen();
-extern int strncasecmp();
-extern int strncmp();
-extern int strspn();
-extern char *index();
-extern char *memccpy();
-extern char *memchr();
-extern char *memcpy();
-extern char *memset();
-extern char *rindex();
-extern char *strcat();
-extern char *strchr();
-extern char *strcpy();
-extern char *strdup();
-extern char *strerror();
-extern char *strncat();
-extern char *strncpy();
-extern char *strpbrk();
-extern char *strrchr();
-extern char *strsep();
-extern char *strtok();
-#endif
+#include <string.h>
