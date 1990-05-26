d1ce1f17b8724ec47199ad9de4351ce73c957db8
Keith Bostic <bostic>
1990-05-25 17:56:07 -0700

    make rewinddir a function, closedir returns int; POSIX 1003.1
    SCCS-vsn: 5.9

--- include/dirent.h
+++ include/dirent.h
@@ -1,51 +1,71 @@
-/*
+/*-
  * Copyright (c) 1989 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)dirent.h	5.8 (Berkeley) 03/06/90
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)dirent.h	5.9 (Berkeley) 05/25/90
  */
 
 #ifndef _DIRENT_
 #define _DIRENT_
 
 /*
- * A directory entry has a struct direct at the front of it,
- * containing its inode number, the length of the entry, and the
- * length of the name contained in the entry.  These are followed
- * by the name padded to a 4 byte boundary with null bytes.  All
- * names are guaranteed null terminated.  The maximum length of a
- * name in a directory is MAXNAMLEN.
+ * A directory entry has a struct direct at the front of it, containing its
+ * inode number, the length of the entry, and the length of the name
+ * contained in the entry.  These are followed by the name padded to a 4
+ * byte boundary with null bytes.  All names are guaranteed null terminated.
+ * The maximum length of a name in a directory is MAXNAMLEN.
  */
-#define	MAXNAMLEN	255
 
-struct	dirent {
+struct dirent {
 	u_long	d_fileno;		/* file number of entry */
 	u_short	d_reclen;		/* length of this record */
 	u_short	d_namlen;		/* length of string in d_name */
+#ifdef _POSIX_SOURCE
+	char	d_name[255 + 1];	/* name must be no longer than this */
+#else
+#define	MAXNAMLEN	255
 	char	d_name[MAXNAMLEN + 1];	/* name must be no longer than this */
+#endif
 };
-#define d_ino d_fileno		/* backward compatibility */
 
-/*
- * Definitions for library routines operating on directories.
- */
+#ifdef _POSIX_SOURCE
+typedef void *	DIR;
+#else
+
+#define	d_ino		d_fileno	/* backward compatibility */
+
+/* definitions for library routines operating on directories. */
 #define	DIRBLKSIZ	1024
 
-/*
- * This structure describes an open directory.
- */
+/* structure describing an open directory. */
 typedef struct _dirdesc {
 	int	dd_fd;		/* file descriptor associated with directory */
 	long	dd_loc;		/* offset in current buffer */
@@ -55,33 +75,36 @@ typedef struct _dirdesc {
 	long	dd_seek;	/* magic cookie returned by getdirentries */
 } DIR;
 
-#define dirfd(dirp)	((dirp)->dd_fd)
+#define	dirfd(dirp)	((dirp)->dd_fd)
 
 #ifndef NULL
-#define NULL 0
+#define	NULL	0
 #endif
 
-extern	long _rewinddir;
-#define rewinddir(dirp) \
-	_seekdir((dirp), _rewinddir), \
-	_rewinddir = telldir(dirp)
+#endif /* _POSIX_SOURCE */
 
 #ifdef __STDC__
 extern DIR *opendir(const char *);
 extern struct dirent *readdir(DIR *);
+extern void rewindir(DIR *);
+extern int closedir(DIR *);
+#ifndef _POSIX_SOURCE
 extern long telldir(const DIR *);
 extern void seekdir(DIR *, long);
-extern void closedir(DIR *);
 extern int scandir(const char *, struct direct *(*[]),
     int (* )(struct direct *), int (* )(char *, char *));
 extern int alphasort(const struct direct **, const struct direct **);
+#endif
 #else
 extern DIR *opendir();
 extern struct dirent *readdir();
+extern void rewinddir();
+extern int closedir();
+#ifndef _POSIX_SOURCE
 extern long telldir();
 extern void seekdir();
-extern void closedir();
 extern int scandir();
 extern int alphasort();
+#endif
 #endif
 #endif /* _DIRENT_ */
