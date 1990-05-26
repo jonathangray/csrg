c4d3aa620a1432dc3086776b61312c858d4e8602
Keith Bostic <bostic>
1990-05-25 18:21:51 -0700

    POSIX 1003.1
    SCCS-vsn: 5.4

--- include/pwd.h
+++ include/pwd.h
@@ -1,24 +1,41 @@
-/*
- * Copyright (c) 1988 The Regents of the University of California.
+/*-
+ * Copyright (c) 1989 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)pwd.h	5.3 (Berkeley) 03/13/90
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)pwd.h	5.4 (Berkeley) 05/25/90
  */
 
 #include <sys/types.h>
 
+#ifndef _POSIX_SOURCE
 #define	_PATH_PASSWD		"/etc/passwd"
 #define	_PATH_MASTERPASSWD	"/etc/master.passwd"
 #define	_PATH_MKPASSWD		"/usr/sbin/mkpasswd"
@@ -28,34 +45,39 @@
 #define	_PW_KEYBYUID		'1'
 
 #define	_PASSWORD_LEN		128
+#endif
 
 struct passwd {
-	char	*pw_name;			/* user name */
-	char	*pw_passwd;			/* encrypted password */
-	int	pw_uid;				/* user uid */
-	int	pw_gid;				/* user gid */
-	u_long	pw_change;			/* password change time */
-	char	*pw_class;			/* user access class */
-	char	*pw_gecos;			/* Honeywell login info */
-	char	*pw_dir;			/* home directory */
-	char	*pw_shell;			/* default shell */
-	u_long	pw_expire;			/* account expiration */
+	char	*pw_name;		/* user name */
+	char	*pw_passwd;		/* encrypted password */
+	int	pw_uid;			/* user uid */
+	int	pw_gid;			/* user gid */
+	time_t	pw_change;		/* password change time */
+	char	*pw_class;		/* user access class */
+	char	*pw_gecos;		/* Honeywell login info */
+	char	*pw_dir;		/* home directory */
+	char	*pw_shell;		/* default shell */
+	time_t	pw_expire;		/* account expiration */
 };
 
 #ifdef __STDC__
-struct passwd *getpwent(void);
-struct passwd *getpwuid(int);
+struct passwd *getpwuid(uid_t);
 struct passwd *getpwnam(const char *);
+#ifndef _POSIX_SOURCE
+struct passwd *getpwent(void);
 int setpwent(void);
 void endpwent(void);
 void setpwfile(const char *);
 int setpassent(int);
+#endif
 #else
-struct passwd *getpwent();
 struct passwd *getpwuid();
 struct passwd *getpwnam();
+#ifndef _POSIX_SOURCE
+struct passwd *getpwent();
 int setpwent();
 void endpwent();
 void setpwfile();
 int setpassent();
+#endif
 #endif
