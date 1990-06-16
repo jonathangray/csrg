df3e44be48036dae8d44d2c9708f99c01d983189
Edward Wang <edward>
1990-06-15 22:24:51 -0700

    show-rcpt added
    SCCS-vsn: 5.20

--- usr.bin/mail/cmd1.c
+++ usr.bin/mail/cmd1.c
@@ -1,22 +1,38 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmd1.c	5.19 (Berkeley) 05/11/89";
+static char sccsid[] = "@(#)cmd1.c	5.20 (Berkeley) 06/15/90";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -151,6 +167,7 @@ printhead(mesg)
 	char pbuf[BUFSIZ];
 	struct headline hl;
 	int subjlen;
+	char *name;
 
 	mp = &message[mesg-1];
 	(void) readline(setinput(mp), headline, LINESIZE);
@@ -172,14 +189,16 @@ printhead(mesg)
 	if (mp->m_flag & MBOX)
 		dispc = 'M';
 	parse(headline, &hl, pbuf);
-	sprintf(wcount, "%3d/%-4ld", mp->m_lines, mp->m_size);
+	sprintf(wcount, "%3d/%-5ld", mp->m_lines, mp->m_size);
 	subjlen = screenwidth - 50 - strlen(wcount);
+	name = value("show-rcpt") != NOSTR ?
+		skin(hfield("to", mp)) : nameof(mp, 0);
 	if (subjline == NOSTR || subjlen < 0)		/* pretty pathetic */
 		printf("%c%c%3d %-20.20s  %16.16s %s\n",
-			curind, dispc, mesg, nameof(mp, 0), hl.l_date, wcount);
+			curind, dispc, mesg, name, hl.l_date, wcount);
 	else
 		printf("%c%c%3d %-20.20s  %16.16s %s \"%.*s\"\n",
-			curind, dispc, mesg, nameof(mp, 0), hl.l_date, wcount,
+			curind, dispc, mesg, name, hl.l_date, wcount,
 			subjlen, subjline);
 }
 
