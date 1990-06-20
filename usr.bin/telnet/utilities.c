29c0f3765bae546552506437e9289f46471a4808
Kevin Fall <kfall>
1990-06-20 16:44:37 -0700

    towards Kerberized telnet
    SCCS-vsn: 1.18

--- usr.bin/telnet/utilities.c
+++ usr.bin/telnet/utilities.c
@@ -2,37 +2,23 @@
  * Copyright (c) 1988 Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * Redistribution and use in source and binary forms are permitted
+ * provided that: (1) source distributions retain this entire copyright
+ * notice and comment, and (2) distributions including binaries display
+ * the following acknowledgement:  ``This product includes software
+ * developed by the University of California, Berkeley and its contributors''
+ * in the documentation or other materials provided with the distribution
+ * and in all advertising materials mentioning features or use of this
+ * software. Neither the name of the University nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)utilities.c	1.17 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)utilities.c	1.17 (Berkeley) 6/1/90";
 #endif /* not lint */
 
 #define	TELOPTS
@@ -277,6 +263,12 @@ optionstatus()
 
 char *slcnames[] = { SLC_NAMES };
 
+#ifdef	KERBEROS
+static char *authtypes[3] = { "NONE", "PRIVATE", "KERBEROS" };
+#else
+static char *authtypes[2] = { "NONE", "PRIVATE" };
+#endif
+
 void
 printsub(direction, pointer, length)
 char	direction;		/* '<' or '>' */
@@ -397,6 +389,51 @@ int	length;			/* length of suboption data */
 	    for (i = 5; i < length; i++)
 		fprintf(NetTrace, " ?%d?", pointer[i]);
 	    break;
+
+#ifdef	KERBEROS
+	case TELOPT_AUTHENTICATION:
+	    fprintf(NetTrace, "Authentication information ");
+	    switch (pointer[1]) {
+	    case TELQUAL_IS:
+		switch (pointer[2]) {
+		case TELQUAL_AUTHTYPE_NONE:
+		case TELQUAL_AUTHTYPE_PRIVATE:
+		case TELQUAL_AUTHTYPE_KERBEROS:
+
+			fprintf(NetTrace, "is type %s\r\n", authtypes[pointer[2]]);
+			break;
+		default:
+			fprintf(NetTrace, "is type unknown\r\n");
+			break;
+		}
+
+	    case TELQUAL_SEND:
+	    {
+		int	idx = 2;
+		fprintf(NetTrace, "- request to send, types");
+		for (idx = 2; idx < length - 1; idx++)
+			switch (pointer[idx]) {
+			case TELQUAL_AUTHTYPE_NONE:
+			case TELQUAL_AUTHTYPE_PRIVATE:
+			case TELQUAL_AUTHTYPE_KERBEROS:
+				fprintf(NetTrace, " %s",
+					authtypes[pointer[idx]]);
+					break;
+			default:
+				fprintf(NetTrace, " <unknown %u>",
+					pointer[idx]);
+				break;
+			}
+		fprintf(NetTrace, "\r\n");
+	    }
+		break;
+
+	    default:
+		fprintf(NetTrace, " - unknown qualifier %d (0x%x).\r\n",
+			pointer[1], pointer[1]);
+	    }
+	    break;
+#endif /* KERBEROS */
 
 	case TELOPT_LINEMODE:
 	    fprintf(NetTrace, "LINEMODE ");
