40f228ebed724ff28e913a65e74dce3c1046cbd8
Kirk McKusick <mckusick>
1990-06-21 17:51:30 -0700

    new system call convention
    SCCS-vsn: 7.12

--- sys/vm/vm_swap.c
+++ sys/vm/vm_swap.c
@@ -14,7 +14,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)vm_swap.c	7.11 (Berkeley) 03/27/90
+ *	@(#)vm_swap.c	7.12 (Berkeley) 06/21/90
  */
 
 #include "param.h"
