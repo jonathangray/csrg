c4e2979f3b944dbece23c9222a4f3d89c47af250
Kirk McKusick <mckusick>
1990-06-22 17:18:36 -0700

    have to pass error to ktrace
    SCCS-vsn: 7.17

--- sys/kern/uipc_syscalls.c
+++ sys/kern/uipc_syscalls.c
@@ -12,7 +12,7 @@
  * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
  * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- *	@(#)uipc_syscalls.c	7.17 (Berkeley) 06/05/90
+ *	@(#)uipc_syscalls.c	7.17 (Berkeley) 06/22/90
  */
 
 #include "param.h"
@@ -560,7 +560,7 @@ sendit(s, mp, flags, retsize)
 	if (ktriov != NULL) {
 		if (error == 0)
 			ktrgenio(u.u_procp->p_tracep, s, UIO_WRITE,
-				ktriov, *retsize);
+				ktriov, *retsize, error);
 		FREE(ktriov, M_TEMP);
 	}
 #endif
@@ -790,7 +790,7 @@ recvit(s, mp, namelenp, retsize)
 	if (ktriov != NULL) {
 		if (error == 0)
 			ktrgenio(u.u_procp->p_tracep, s, UIO_READ,
-				ktriov, len - auio.uio_resid);
+				ktriov, len - auio.uio_resid, error);
 		FREE(ktriov, M_TEMP);
 	}
 #endif
