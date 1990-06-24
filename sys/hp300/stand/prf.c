ece9ba5fbcb33148dee9184adb6088579d17c00f
Mike Karels <karels>
1990-06-24 14:58:26 -0700

    update includes
    SCCS-vsn: 7.2

--- sys/hp300/stand/prf.c
+++ sys/hp300/stand/prf.c
@@ -30,10 +30,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)prf.c	7.1 (Berkeley) 05/08/90
+ *	@(#)prf.c	7.2 (Berkeley) 06/24/90
  */
 
-#include "../h/param.h"
+#include "param.h"
 
 /*
  * Scaled down version of C Library printf.
