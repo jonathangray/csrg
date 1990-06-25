50375831b87588ab04ba3ba6386bb39e58d5c8fe
Keith Bostic <bostic>
1990-06-24 23:33:44 -0700

    move _PATH_MAILDIR into <paths.h>
    SCCS-vsn: 5.16

--- old/sysline/sysline.c
+++ old/sysline/sysline.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)sysline.c	5.15 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)sysline.c	5.16 (Berkeley) 06/24/90";
 #endif /* not lint */
 
 /*
@@ -435,7 +435,7 @@ main(argc,argv)
 		if ((username = getenv("USER")) == 0)
 			mailcheck = 0;
 		else {
-			chdir(_PATH_MBOX);
+			chdir(_PATH_MAILDIR);
 			if (stat(username, &mstbuf) >= 0)
 				mailsize = mstbuf.st_size;
 			else
