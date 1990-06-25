adfed109fa4dc0e5400f4587197d8ef68c2eac75
Edward Wang <edward>
1990-06-25 00:01:25 -0700

    remove call of _fwalk
    SCCS-vsn: 5.14

--- usr.bin/mail/cmd2.c
+++ usr.bin/mail/cmd2.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmd2.c	5.13 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)cmd2.c	5.14 (Berkeley) 06/25/90";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -181,7 +181,7 @@ save1(str, mark, cmd, ignore)
 		disp = "[Appended]";
 	else
 		disp = "[New file]";
-	if ((obuf = fopen(file, "a")) == NULL) {
+	if ((obuf = Fopen(file, "a")) == NULL) {
 		perror(NOSTR);
 		return(1);
 	}
@@ -190,7 +190,7 @@ save1(str, mark, cmd, ignore)
 		touch(mp);
 		if (send(mp, obuf, ignore, NOSTR) < 0) {
 			perror(file);
-			fclose(obuf);
+			Fclose(obuf);
 			return(1);
 		}
 		if (mark)
@@ -199,7 +199,7 @@ save1(str, mark, cmd, ignore)
 	fflush(obuf);
 	if (ferror(obuf))
 		perror(file);
-	fclose(obuf);
+	Fclose(obuf);
 	printf("%s\n", disp);
 	return(0);
 }
