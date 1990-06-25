6d72df14d67213bc98bf683e7066f3c209f13300
Keith Bostic <bostic>
1990-06-24 23:29:12 -0700

    move _PATH_MAILDIR into <paths.h>
    SCCS-vsn: 5.2

--- libexec/mail.local/pathnames.h
+++ libexec/mail.local/pathnames.h
@@ -30,11 +30,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	5.1 (Berkeley) 04/29/90
+ *	@(#)pathnames.h	5.2 (Berkeley) 06/24/90
  */
 
 #include <paths.h>
 
 #undef _PATH_TMP
 #define _PATH_TMP	"/tmp/maXXXXX"
-#define _PATH_MAILDIR	"/var/mail/"
