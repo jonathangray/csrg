dd991941c1528187111b21f005ca91110bcbfb19
Marc Teitelbaum <marc>
1990-06-26 14:22:27 -0700

    new copyright, STDC prototypes, new flags
    SCCS-vsn: 5.4

--- include/vis.h
+++ include/vis.h
@@ -1,59 +1,79 @@
-/*
- * Copyright (c) 1989 The Regents of the University of California.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)vis.h	5.3 (Berkeley) 05/11/90
- */
-
-
-/*
- * vis flags
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)vis.h	5.4 (Berkeley) 06/26/90
  */
 
 /*
  * to select alternate encoding format
  */
 #define	VIS_OCTAL	0x01	/* use octal \ddd format */
-#define	VIS_CSTYLE	0x02	/* use \[nrft...] where appropiate */
+#define	VIS_CSTYLE	0x02	/* use \[nrft0..] where appropiate */
+
 /*
- * to alter set of characters encoded
- * (default is to encode all non-graphic except space, tab, and newline)
+ * to alter set of characters encoded (default is to encode all
+ * non-graphic except space, tab, and newline).
  */
-#define	VIS_TAB		0x04	/* also encode tab */
-#define	VIS_WHITE	0x08	/* also encode space, tab, and newline */
-#define	VIS_SAFE	0x10	/* only encode "unsafe" characters */
+#define	VIS_SP		0x04	/* also encode space */
+#define	VIS_TAB		0x08	/* also encode tab */
+#define	VIS_NL		0x10	/* also encode newline */
+#define	VIS_WHITE	(VIS_SP | VIS_TAB | VIS_NL)
+#define	VIS_SAFE	0x20	/* only encode "unsafe" characters */
+
 /*
  * other
  */
-#define	VIS_NOSLASH	0x20	/* inhibit printing '\' */
-#define	VIS_NEXTC	0x40	/* nextc character is valid */
+#define	VIS_NOSLASH	0x40	/* inhibit printing '\' */
 
-extern char *vis();
+/*
+ * unvis return codes
+ */
+#define	UNVIS_VALID	 1	/* character valid */
+#define	UNVIS_VALIDPUSH	 2	/* character valid, push back passed char */
+#define	UNVIS_NOCHAR	 3	/* valid sequence, no character produced */
+#define	UNVIS_SYNBAD	-1	/* unrecognized escape sequence */
+#define	UNVIS_ERROR	-2	/* decoder in unknown state (unrecoverable) */
 
 /*
  * unvis flags
  */
+#define	UNVIS_END	1	/* no more characters */
 
-#define	UNVIS_HAT	0x01	/* treat ^ as special */
-#define	UNVIS_END	0x02	/* reset decode machine */
-
-/*
- * return codes
- */
-#define	UNVIS_NEEDMORE	0	/* not done, feed another char */
-#define	UNVIS_OK		1	/* done, character ready */
-#define	UNVIS_OKPUSH	2	/* done, push back passed char */
-#define	UNVIS_NOCHAR	3	/* done, no character ready (hidden newline) */
-#define	UNVIS_SYNBAD	-1	/* unrecognized escape sequence */
+#if __STDC__ || c_plusplus
+char *vis(char *, char, char, int);
+int   strvis(char *, const char *, int);
+int   strvisx(char *, const char *, size_t, int);
+#else
+char *vis();
+int   strvis();
+int   strvisx();
+#endif
