a2f5e7c5125a6908109959c6ba4ceb780476f470
Mike Karels <karels>
1990-06-27 17:07:57 -0700

    add IP type-of-service without gettos (at least for now)
    SCCS-vsn: 5.7

--- libexec/telnetd/defs.h
+++ libexec/telnetd/defs.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)defs.h	5.6 (Berkeley) 06/01/90
+ *	@(#)defs.h	5.7 (Berkeley) 06/27/90
  */
 
 /*
@@ -68,6 +68,8 @@
 #include <sys/ioctl.h>
 
 #include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
 
 #include <arpa/telnet.h>
 
