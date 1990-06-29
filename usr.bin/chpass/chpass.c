9f59a0b930a9692108f5e763072697a5e4976ff0
Marc Teitelbaum <marc>
1990-06-29 00:17:56 -0700

    new interface to strsep
    SCCS-vsn: 5.15

--- usr.bin/chpass/chpass.c
+++ usr.bin/chpass/chpass.c
@@ -1,18 +1,34 @@
-/*
+/*-
  * Copyright (c) 1988 The Regents of the University of California.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms are permitted
- * provided that the above copyright notice and this paragraph are
- * duplicated in all such forms and that any documentation,
- * advertising materials, and other materials related to such
- * distribution and use acknowledge that the software was developed
- * by the University of California, Berkeley.  The name of the
- * University may not be used to endorse or promote products derived
- * from this software without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef lint
@@ -22,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)chpass.c	5.14 (Berkeley) 04/04/90";
+static char sccsid[] = "@(#)chpass.c	5.15 (Berkeley) 06/29/90";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -35,7 +51,7 @@ static char sccsid[] = "@(#)chpass.c	5.14 (Berkeley) 04/04/90";
 #include <errno.h>
 #include <stdio.h>
 #include <ctype.h>
-#include <strings.h>
+#include <string.h>
 #include "chpass.h"
 #include "pathnames.h"
 
@@ -444,28 +460,29 @@ loadpw(arg, pw)
 	register struct passwd *pw;
 {
 	register char *cp;
+	char *bp = arg;
 	long atol();
 	char *strsep();
 
-	pw->pw_name = strsep(arg, ":");
-	pw->pw_passwd = strsep((char *)NULL, ":");
-	if (!(cp = strsep((char *)NULL, ":")))
+	pw->pw_name = strsep(&bp, ":");
+	pw->pw_passwd = strsep(&bp, ":");
+	if (!(cp = strsep(&bp, ":")))
 		goto bad;
 	pw->pw_uid = atoi(cp);
-	if (!(cp = strsep((char *)NULL, ":")))
+	if (!(cp = strsep(&bp, ":")))
 		goto bad;
 	pw->pw_gid = atoi(cp);
-	pw->pw_class = strsep((char *)NULL, ":");
-	if (!(cp = strsep((char *)NULL, ":")))
+	pw->pw_class = strsep(&bp, ":");
+	if (!(cp = strsep(&bp, ":")))
 		goto bad;
 	pw->pw_change = atol(cp);
-	if (!(cp = strsep((char *)NULL, ":")))
+	if (!(cp = strsep(&bp, ":")))
 		goto bad;
 	pw->pw_expire = atol(cp);
-	pw->pw_gecos = strsep((char *)NULL, ":");
-	pw->pw_dir = strsep((char *)NULL, ":");
-	pw->pw_shell = strsep((char *)NULL, ":");
-	if (!pw->pw_shell || strsep((char *)NULL, ":")) {
+	pw->pw_gecos = strsep(&bp, ":");
+	pw->pw_dir = strsep(&bp, ":");
+	pw->pw_shell = strsep(&bp, ":");
+	if (!pw->pw_shell || strsep(&bp, ":")) {
 bad:		(void)fprintf(stderr, "chpass: bad password list.\n");
 		exit(1);
 	}
