92e4851db000e340a3dc2b418c3f714531b1b5c0
Keith Sklower <sklower>
1990-08-30 18:48:28 -0700

    this version compiles; completely untested
    SCCS-vsn: 7.3

--- sys/netccitt/pk.h
+++ sys/netccitt/pk.h
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pk.h	7.2 (Berkeley) 05/15/90
+ *	@(#)pk.h	7.3 (Berkeley) 08/30/90
  */
 
 /*
@@ -115,7 +115,7 @@ typedef u_char octet;
 struct x25_calladdr {
 	octet ORDER2(called_addrlen:4, calling_addrlen:4);
 	octet address_field[MAXADDRLN];
-}
+};
 
 struct x25_packet {
 	octet ORDER3(lc_group_number:4, fmt_identifier:3, q_bit:1);
