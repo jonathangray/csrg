46a71cae89333486a819451778d9200483158da2
Kirk McKusick <mckusick>
1990-09-11 11:16:31 -0700

    protect with _QUOTA_
    SCCS-vsn: 7.7

--- sys/ufs/ffs/quota.h
+++ sys/ufs/ffs/quota.h
@@ -33,8 +33,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)quota.h	7.6 (Berkeley) 06/28/90
+ *	@(#)quota.h	7.7 (Berkeley) 09/11/90
  */
+
+#ifndef _QUOTA_
+#define _QUOTA_
 
 /*
  * Definitions for disk quotas imposed on the average user
@@ -170,3 +173,4 @@ struct	dquot {
 #define	DQREF(dq)	dqref(dq)
 #endif /* DIAGNOSTIC */
 #endif /* KERNEL */
+#endif /* _QUOTA_ */
