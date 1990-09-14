3670d453886706f41b7041c8997b39aead7c0e3d
Dave Borman <borman>
1990-09-14 10:41:58 -0700

    Bump the sccs id to 5.1
    SCCS-vsn: 5.1

--- usr.bin/telnet/defines.h
+++ usr.bin/telnet/defines.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)defines.h	1.10 (Berkeley) 06/28/90
+ *	@(#)defines.h	5.1 (Berkeley) 09/14/90
  */
 
 #define	settimer(x)	clocks.x = clocks.system++
