175241d2d70f3c521343dda04e10b8022a6cc646
Dave Borman <borman>
1990-09-14 10:42:07 -0700

    Bump the sccs id to 5.1
    SCCS-vsn: 5.1

--- usr.bin/telnet/ring.h
+++ usr.bin/telnet/ring.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ring.h	1.10 (Berkeley) 06/28/90
+ *	@(#)ring.h	5.1 (Berkeley) 09/14/90
  */
 
 /*
