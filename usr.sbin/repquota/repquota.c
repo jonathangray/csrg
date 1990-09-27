6d7da04b3ef222b6841af26230ab8e36b9baa8fe
Kirk McKusick <mckusick>
1990-09-27 14:04:40 -0700

    have to initialize quota variables here rather than in quota.h
    SCCS-vsn: 5.11

--- usr.sbin/repquota/repquota.c
+++ usr.sbin/repquota/repquota.c
@@ -41,7 +41,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)repquota.c	5.10 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)repquota.c	5.11 (Berkeley) 09/27/90";
 #endif /* not lint */
 
 /*
@@ -55,6 +55,9 @@ static char sccsid[] = "@(#)repquota.c	5.10 (Berkeley) 06/01/90";
 #include <grp.h>
 #include <stdio.h>
 #include <errno.h>
+
+char *qfname = QUOTAFILENAME;
+char *qfextension[] = INITQFNAMES;
 
 struct fileusage {
 	struct	fileusage *fu_next;
