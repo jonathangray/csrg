50b0bcd5dbf7fa2458cbf14e28d61808ae4a89db
Keith Bostic <bostic>
1990-10-09 18:16:18 -0700

    new copyright notice
    SCCS-vsn: 5.5

--- lib/libm/vax/support.s
+++ lib/libm/vax/support.s
@@ -30,17 +30,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * All recipients should regard themselves as participants in an ongoing
- * research project and hence should feel obligated to report their
- * experiences (good or bad) with these elementary function codes, using
- * the sendbug(8) program, to the authors.
- *
- *	@(#)support.s	5.4 (Berkeley) 06/01/90
+ *	@(#)support.s	5.5 (Berkeley) 10/09/90
  */
 	.data
 	.align	2
 _sccsid:
-.asciz	"@(#)support.s	1.3 (Berkeley) 8/21/85; 5.4 (ucb.elefunt) 06/01/90"
+.asciz	"@(#)support.s	1.3 (Berkeley) 8/21/85; 5.5 (ucb.elefunt) 10/09/90"
 
 /*
  * copysign(x,y),
