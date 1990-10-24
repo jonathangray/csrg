730cd28bcf86f620d6a3d5faf276fccb1a305f10
Kirk McKusick <mckusick>
1990-10-24 15:09:43 -0700

    update Utah source identifier
    SCCS-vsn: 7.3

--- sys/hp/hpux/hpux_termio.h
+++ sys/hp/hpux/hpux_termio.h
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: hpux.h 1.15 89/09/25$
+ * from: Utah $Hdr: hpux_termio.h 1.1 90/07/09$
  *
- *	@(#)hpux_termio.h	7.2 (Berkeley) 06/27/90
+ *	@(#)hpux_termio.h	7.3 (Berkeley) 10/24/90
  */
 
 /* HP-UX termio stuff */
