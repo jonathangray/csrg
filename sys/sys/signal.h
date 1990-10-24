b68bc761b2cd34b12adea0fbe6c310ac52370177
Kirk McKusick <mckusick>
1990-10-23 22:53:42 -0700

    need types for function prototypes
    SCCS-vsn: 7.12

--- sys/sys/signal.h
+++ sys/sys/signal.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)signal.h	7.11 (Berkeley) 07/01/90
+ *	@(#)signal.h	7.12 (Berkeley) 10/23/90
  */
 
 #ifndef	NSIG
@@ -219,6 +219,11 @@ struct	sigcontext {
 #endif /* KERNEL */
 
 #if __STDC__ || c_plusplus
+#ifdef KERNEL
+#include "types.h"
+#else
+#include <sys/types.h>
+#endif
 int kill(pid_t, int);
 int sigaction(int, const struct sigaction *, struct sigaction *);
 int sigprocmask(int, const sigset_t *, sigset_t *);
