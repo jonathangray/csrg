e31f450493170c73d23fdf6d0ba6450c96931cb9
Keith Bostic <bostic>
1990-10-31 11:17:09 -0800

    don't include function prototypes in the kernel
    SCCS-vsn: 7.13

--- sys/sys/signal.h
+++ sys/sys/signal.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)signal.h	7.12 (Berkeley) 10/23/90
+ *	@(#)signal.h	7.13 (Berkeley) 10/31/90
  */
 
 #ifndef	NSIG
@@ -218,12 +218,9 @@ struct	sigcontext {
 
 #endif /* KERNEL */
 
+#ifndef KERNEL
 #if __STDC__ || c_plusplus
-#ifdef KERNEL
-#include "types.h"
-#else
 #include <sys/types.h>
-#endif
 int kill(pid_t, int);
 int sigaction(int, const struct sigaction *, struct sigaction *);
 int sigprocmask(int, const sigset_t *, sigset_t *);
@@ -235,5 +232,6 @@ int sigaction();
 int sigprocmask();
 int sigpending();
 int sigsuspend();
+#endif
 #endif
 #endif /* NSIG */
