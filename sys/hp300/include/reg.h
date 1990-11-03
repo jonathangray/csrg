817bd6b9f17a7f3084480f64987325981a56dc51
Kirk McKusick <mckusick>
1990-11-03 12:53:55 -0800

    update identifier from Utah
    SCCS-vsn: 7.2

--- sys/hp300/include/reg.h
+++ sys/hp300/include/reg.h
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: reg.h 1.5 89/04/11$
+ * from: Utah $Hdr: reg.h 1.1 90/07/09$
  *
- *	@(#)reg.h	7.1 (Berkeley) 05/08/90
+ *	@(#)reg.h	7.2 (Berkeley) 11/03/90
  */
 
 /*
