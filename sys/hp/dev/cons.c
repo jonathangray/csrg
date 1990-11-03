b0f00aba5a589d1f511729838f250398e217cfaf
Kirk McKusick <mckusick>
1990-11-02 16:25:57 -0800

    update identifier from Utah
    SCCS-vsn: 7.4

--- sys/hp/dev/cons.c
+++ sys/hp/dev/cons.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: cons.c 1.4 88/12/03$
+ * from: Utah $Hdr: cons.c 1.1 90/07/09$
  *
- *	@(#)cons.c	7.3 (Berkeley) 06/06/90
+ *	@(#)cons.c	7.4 (Berkeley) 11/02/90
  */
 
 #include "param.h"
