161dd154fd574b14cfc6a3f27a051a4b162b0e5a
Kirk McKusick <mckusick>
1990-11-03 11:42:23 -0800

    update identifier from Utah
    SCCS-vsn: 7.2

--- sys/hp300/hp300/in_cksum.c
+++ sys/hp300/hp300/in_cksum.c
@@ -30,9 +30,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: in_cksum.c 1.6 89/08/24$
+ * from: Utah $Hdr: in_cksum.c 1.1 90/07/09$
  *
- *	@(#)in_cksum.c	7.1 (Berkeley) 05/08/90
+ *	@(#)in_cksum.c	7.2 (Berkeley) 11/03/90
  */
 
 /*
