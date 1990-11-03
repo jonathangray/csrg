ed4bdbf2aff3963ade32b0fdf63105864a2a566b
Kirk McKusick <mckusick>
1990-11-03 12:48:11 -0800

    update identifier from Utah
    SCCS-vsn: 7.2

--- sys/hp300/include/mtpr.h
+++ sys/hp300/include/mtpr.h
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: mtpr.h 1.5 89/04/11$
+ * from: Utah $Hdr: mtpr.h 1.1 90/07/09$
  *
- *	@(#)mtpr.h	7.1 (Berkeley) 05/08/90
+ *	@(#)mtpr.h	7.2 (Berkeley) 11/03/90
  */
 
 /*
