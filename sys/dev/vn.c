e551756f257b598461ee5f54705f9005b3a7e2d4
Kirk McKusick <mckusick>
1990-11-04 19:21:57 -0800

    update from Mike Hibler
    SCCS-vsn: 7.2

--- sys/dev/vn.c
+++ sys/dev/vn.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: fd.c 1.3 89/12/03$
+ * from: Utah $Hdr: fd.c 1.1 90/07/09$
  *
- *	@(#)vn.c	7.1 (Berkeley) 05/08/90
+ *	@(#)vn.c	7.2 (Berkeley) 11/04/90
  */
 
 /*
