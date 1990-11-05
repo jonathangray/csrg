d38c1868344583ce1c006e960912110a2854a3b9
Kirk McKusick <mckusick>
1990-11-04 19:31:46 -0800

    update identifier from Utah
    SCCS-vsn: 7.2

--- sys/hp/dev/hilreg.h
+++ sys/hp/dev/hilreg.h
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: hilreg.h 1.8 89/08/24$
+ * from: Utah $Hdr: hilreg.h 1.1 90/07/09$
  *
- *	@(#)hilreg.h	7.1 (Berkeley) 05/08/90
+ *	@(#)hilreg.h	7.2 (Berkeley) 11/04/90
  */
 
 struct	hil_dev {
