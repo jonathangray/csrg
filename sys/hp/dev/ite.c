85747cca065946f6ad45ecbad8225f599f0f3886
Kirk McKusick <mckusick>
1990-11-04 19:33:27 -0800

    update identifier from Utah
    SCCS-vsn: 7.3

--- sys/hp/dev/ite.c
+++ sys/hp/dev/ite.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: ite.c 1.22 89/08/17$
+ * from: Utah $Hdr: ite.c 1.1 90/07/09$
  *
- *	@(#)ite.c	7.2 (Berkeley) 06/22/90
+ *	@(#)ite.c	7.3 (Berkeley) 11/04/90
  */
 
 /*
