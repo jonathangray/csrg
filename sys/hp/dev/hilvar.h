9076923b6d56ecb08bf02ba74f2c98a2aa1abc83
Kirk McKusick <mckusick>
1990-11-04 19:32:14 -0800

    update identifier from Utah
    SCCS-vsn: 7.2

--- sys/hp/dev/hilvar.h
+++ sys/hp/dev/hilvar.h
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: hilvar.h 1.1 89/08/22$
+ * from: Utah $Hdr: hilvar.h 1.1 90/07/09$
  *
- *	@(#)hilvar.h	7.1 (Berkeley) 05/08/90
+ *	@(#)hilvar.h	7.2 (Berkeley) 11/04/90
  */
 
 #ifndef TRUE
