2d5209e4e0cbaf83cc0cadb385aabf219deaf595
Keith Bostic <bostic>
1990-12-05 15:57:06 -0800

    kernel reorganization
    SCCS-vsn: 7.14

--- sys/sys/user.h
+++ sys/sys/user.h
@@ -30,24 +30,15 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)user.h	7.13 (Berkeley) 06/28/90
+ *	@(#)user.h	7.14 (Berkeley) 12/05/90
  */
 
-#ifdef KERNEL
-#include "machine/pcb.h"
-#include "dmap.h"
-#include "time.h"
-#include "resource.h"
-#include "namei.h"
-#include "ucred.h"
-#else
 #include <machine/pcb.h>
 #include <sys/dmap.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/namei.h>
 #include <sys/ucred.h>
-#endif
 
 /*
  * Per process structure containing data that
