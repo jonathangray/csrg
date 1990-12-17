ba5a4bd04f0cbdd1e658c7057bd35e214037738a
Keith Bostic <bostic>
1990-12-16 16:39:22 -0800

    kernel reorg
    SCCS-vsn: 7.2

--- sys/hp300/stand/installboot.c
+++ sys/hp300/stand/installboot.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)installboot.c	7.1 (Berkeley) 05/08/90
+ *	@(#)installboot.c	7.2 (Berkeley) 12/16/90
  */
 
 #ifndef lint
@@ -40,11 +40,11 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)installboot.c	7.1 (Berkeley) 05/08/90";
+static char sccsid[] = "@(#)installboot.c	7.2 (Berkeley) 12/16/90";
 #endif /* not lint */
 
-#include "../sys/param.h"
-#include "../ufs/fs.h"
+#include "sys/param.h"
+#include "ufs/fs.h"
 
 char block[1024];
 int maxbootsize = 16 * 7 * 512;		/* XXX */
