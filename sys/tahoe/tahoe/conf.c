e047ea7c03516967cf4cacece79321de4c50b496
Keith Bostic <bostic>
1990-12-16 16:45:59 -0800

    kernel reorg
    SCCS-vsn: 7.7

--- sys/tahoe/tahoe/conf.c
+++ sys/tahoe/tahoe/conf.c
@@ -30,15 +30,15 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)conf.c	7.6 (Berkeley) 06/28/90
+ *	@(#)conf.c	7.7 (Berkeley) 12/16/90
  */
 
-#include "param.h"
-#include "systm.h"
-#include "buf.h"
-#include "ioctl.h"
-#include "tty.h"
-#include "conf.h"
+#include "sys/param.h"
+#include "sys/systm.h"
+#include "sys/buf.h"
+#include "sys/ioctl.h"
+#include "sys/tty.h"
+#include "sys/conf.h"
 
 int nulldev(), nodev(), rawread(), rawwrite(), swstrategy();
 
