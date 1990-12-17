7f456cee6db928a7d98867f692e91c188e1953a8
Keith Bostic <bostic>
1990-12-16 16:39:36 -0800

    kernel reorg
    SCCS-vsn: 7.2

--- sys/hp300/stand/mkboot.c
+++ sys/hp300/stand/mkboot.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mkboot.c	7.1 (Berkeley) 05/08/90
+ *	@(#)mkboot.c	7.2 (Berkeley) 12/16/90
  */
 
 #ifndef lint
@@ -40,10 +40,10 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkboot.c	7.1 (Berkeley) 05/08/90";
+static char sccsid[] = "@(#)mkboot.c	7.2 (Berkeley) 12/16/90";
 #endif /* not lint */
 
-#include "machine/machparam.h"
+#include "../include/param.h"
 #include "volhdr.h"
 #include <sys/exec.h>
 #include <sys/file.h>
