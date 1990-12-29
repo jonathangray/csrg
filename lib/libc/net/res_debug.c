00a9e00421e23aea64c01d014c7e4bad9267654f
William Jolitz <william>
1990-12-28 16:00:53 -0800

    use param.h instead of types.h to pick up BYTE_ORDER transparently
    SCCS-vsn: 5.31

--- lib/libc/net/res_debug.c
+++ lib/libc/net/res_debug.c
@@ -30,14 +30,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)res_debug.c	5.30 (Berkeley) 06/27/90
+ *	@(#)res_debug.c	5.31 (Berkeley) 12/28/90
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)res_debug.c	5.30 (Berkeley) 06/27/90";
+static char sccsid[] = "@(#)res_debug.c	5.31 (Berkeley) 12/28/90";
 #endif /* LIBC_SCCS and not lint */
 
-#include <sys/types.h>
+#include <sys/param.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <arpa/nameser.h>
