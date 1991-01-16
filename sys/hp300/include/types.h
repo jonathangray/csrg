9b5e03011a54b1ddad62fec0ec3286cd23fcceb1
Keith Bostic <bostic>
1991-01-15 20:57:22 -0800

    stdio has to include this file, don't require types.h
    SCCS-vsn: 7.6

--- sys/hp300/include/types.h
+++ sys/hp300/include/types.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)types.h	7.5 (Berkeley) 12/05/90
+ *	@(#)types.h	7.6 (Berkeley) 01/15/91
  */
 
 #ifndef	_MACHTYPES_H_
@@ -61,7 +61,7 @@ typedef struct _physadr {
 typedef struct label_t {		/* consistent with HP-UX */
 	int val[15];
 } label_t;
-typedef	u_long	vm_offset_t;
-typedef	u_long	vm_size_t;
+typedef	unsigned long	vm_offset_t;
+typedef	unsigned long	vm_size_t;
 #endif
 #endif	/* _MACHTYPES_H_ */
