a63076e01d01ba9ba30a0504205d8befece2e8ab
Keith Bostic <bostic>
1991-01-18 18:31:28 -0800

    the position of new files depends on the [-abi] flags
    SCCS-vsn: 5.2

--- usr.bin/ar/replace.c
+++ usr.bin/ar/replace.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)replace.c	5.1 (Berkeley) 01/17/91";
+static char sccsid[] = "@(#)replace.c	5.2 (Berkeley) 01/18/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -86,8 +86,6 @@ replace(argv)
 
 	tfd1 = tmp();			/* Files before key file. */
 	tfd2 = tmp();			/* Files after key file. */
-	
-	mods = (options & (AR_A|AR_B));
 
 	/*
 	 * Break archive into two parts -- entries before and after the key
@@ -96,6 +94,7 @@ replace(argv)
 	 * key, place the key at the end of the before key entries.  Put it
 	 * all back together at the end.
 	 */
+	mods = (options & (AR_A|AR_B));
 	for (err = 0, curfd = tfd1; get_header(afd);) {
 		if ((file = *argv) && files(argv)) {
 			if ((sfd = open(file, O_RDONLY)) < 0) {
@@ -153,7 +152,8 @@ append:	while (file = *argv++) {
 			continue;
 		}
 		(void)fstat(sfd, &sb);
-		SETCF(sfd, file, tfd2, tname, WPAD);
+		SETCF(sfd, file,
+		    options & (AR_A|AR_B) ? tfd1 : tfd2, tname, WPAD);
 		put_header(&cf, &sb);
 		copyfile(&cf, sb.st_size);
 		(void)close(sfd);
