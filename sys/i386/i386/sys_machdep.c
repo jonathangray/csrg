c4456b3b47a829212aa52161364cbe33581fa9b1
William Jolitz <william>
1991-01-19 13:16:22 -0800

    track TRACE changes
    SCCS-vsn: 5.5

--- sys/i386/i386/sys_machdep.c
+++ sys/i386/i386/sys_machdep.c
@@ -1,3 +1,4 @@
+
 /*-
  * Copyright (c) 1990 The Regents of the University of California.
  * All rights reserved.
@@ -5,72 +6,90 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
-%sccs.include.noredist.c%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)sys_machdep.c	5.4 (Berkeley) 01/15/91
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)sys_machdep.c	5.5 (Berkeley) 01/19/91
  */
 
 #include "param.h"
 #include "systm.h"
-#include "dir.h"
-#include "user.h"
 #include "ioctl.h"
 #include "file.h"
+#include "time.h"
 #include "proc.h"
 #include "uio.h"
 #include "kernel.h"
 #include "mtio.h"
 #include "buf.h"
-
-#include "machine/dkio.h"
-#include "machine/pte.h"
+#include "trace.h"
 
 #ifdef TRACE
 int	nvualarm;
 
-vtrace()
-{
-	register struct a {
+vtrace(p, uap, retval)
+	struct proc *p;
+	register struct args {
 		int	request;
 		int	value;
 	} *uap;
+	int *retval;
+{
 	int vdoualarm();
 
-	uap = (struct a *)u.u_ap;
 	switch (uap->request) {
 
 	case VTR_DISABLE:		/* disable a trace point */
 	case VTR_ENABLE:		/* enable a trace point */
 		if (uap->value < 0 || uap->value >= TR_NFLAGS)
-			u.u_error = EINVAL;
-		else {
-			u.u_r.r_val1 = traceflags[uap->value];
-			traceflags[uap->value] = uap->request;
-		}
+			return (EINVAL);
+		*retval = traceflags[uap->value];
+		traceflags[uap->value] = uap->request;
 		break;
 
 	case VTR_VALUE:		/* return a trace point setting */
 		if (uap->value < 0 || uap->value >= TR_NFLAGS)
-			u.u_error = EINVAL;
-		else
-			u.u_r.r_val1 = traceflags[uap->value];
+			return (EINVAL);
+		*retval = traceflags[uap->value];
 		break;
 
 	case VTR_UALARM:	/* set a real-time ualarm, less than 1 min */
-		if (uap->value <= 0 || uap->value > 60 * hz ||
-		    nvualarm > 5)
-			u.u_error = EINVAL;
-		else {
-			nvualarm++;
-			timeout(vdoualarm, (caddr_t)u.u_procp->p_pid,
-			    uap->value);
-		}
+		if (uap->value <= 0 || uap->value > 60 * hz || nvualarm > 5)
+			return (EINVAL);
+		nvualarm++;
+		timeout(vdoualarm, (caddr_t)p->p_pid, uap->value);
 		break;
 
 	case VTR_STAMP:
-		trace(TR_STAMP, uap->value, u.u_procp->p_pid);
+		trace(TR_STAMP, uap->value, p->p_pid);
 		break;
 	}
+	return (0);
 }
 
 vdoualarm(arg)
@@ -82,61 +101,5 @@ vdoualarm(arg)
 	if (p)
 		psignal(p, 16);
 	nvualarm--;
-}
-#endif
-
-#ifdef COMPAT
-/*
- * Note: these tables are sorted by
- * ioctl "code" (in ascending order).
- */
-int dctls[] = { DKIOCHDR, 0 };
-int fctls[] = { FIOCLEX, FIONCLEX, FIOASYNC, FIONBIO, FIONREAD, 0 };
-int mctls[] = { MTIOCTOP, MTIOCGET, 0 };
-int tctls[] = {
-	TIOCGETD, TIOCSETD, TIOCHPCL, TIOCMODG, TIOCMODS,
-	TIOCGETP, TIOCSETP, TIOCSETN, TIOCEXCL, TIOCNXCL,
-	TIOCFLUSH,TIOCSETC, TIOCGETC, TIOCREMOTE,TIOCMGET,
-	TIOCMBIC, TIOCMBIS, TIOCMSET, TIOCSTART,TIOCSTOP,
-	TIOCPKT,  TIOCNOTTY,TIOCSTI,  TIOCOUTQ, TIOCGLTC,
-	TIOCSLTC, TIOCSPGRP,TIOCGPGRP,TIOCCDTR, TIOCSDTR,
-	TIOCCBRK, TIOCSBRK, TIOCLGET, TIOCLSET, TIOCLBIC,
-	TIOCLBIS, 0
-};
-
-/*
- * Map an old style ioctl command to new.
- */
-mapioctl(cmd)
-	int cmd;
-{
-	register int *map, c;
-
-	switch ((cmd >> 8) & 0xff) {
-
-	case 'd':
-		map = dctls;
-		break;
-
-	case 'f':
-		map = fctls;
-		break;
-
-	case 'm':
-		map = mctls;
-		break;
-
-	case 't':
-		map = tctls;
-		break;
-
-	default:
-		return (0);
-	}
-	while ((c = *map) && (c&0xff) < (cmd&0xff))
-		map++;
-	if (c && (c&0xff) == (cmd&0xff))
-		return (c);
-	return (0);
 }
 #endif
