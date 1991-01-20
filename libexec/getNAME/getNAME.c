f3bf31b9d35c5524b0603837c78eaed0a7f74b74
Keith Bostic <bostic>
1991-01-20 12:43:47 -0800

    written at Berkeley, use BSD copyright; lint, getopt(3)
    SCCS-vsn: 5.4

--- libexec/getNAME/getNAME.c
+++ libexec/getNAME/getNAME.c
@@ -1,12 +1,45 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)getNAME.c	5.3 (Berkeley) 05/15/90";
-#endif not lint
+char copyright[] =
+"@(#) Copyright (c) 1980 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)getNAME.c	5.4 (Berkeley) 01/20/91";
+#endif /* not lint */
 
 /*
  * Get name sections from manual pages.
@@ -22,26 +55,40 @@ int intro;
 
 main(argc, argv)
 	int argc;
-	char *argv[];
+	char **argv;
 {
+	extern int optind;
+	int ch;
 
-	argc--, argv++;
-	if (!strcmp(argv[0], "-t"))
-		argc--, argv++, tocrc++;
-	if (!strcmp(argv[0], "-i"))
-		argc--, argv++, intro++;
-	while (argc > 0)
-		getfrom(*argv++), argc--;
+	while ((ch = getopt(argc, argv, "it")) != EOF)
+		switch(ch) {
+		case 'i':
+			intro = 1;
+			break;
+		case 't':
+			tocrc = 1;
+			break;
+		case '?':
+		default:
+			usage();
+		}
+	argc -= optind;
+	argv += optind;
+
+	if (!*argv)
+		usage();
+
+	for (; *argv; ++argv)
+		getfrom(*argv);
 	exit(0);
 }
 
 getfrom(name)
 	char *name;
 {
+	int i = 0;
 	char headbuf[BUFSIZ];
 	char linbuf[BUFSIZ];
-	register char *cp;
-	int i = 0;
 
 	if (freopen(name, "r", stdin) == 0) {
 		perror(name);
@@ -178,4 +225,10 @@ again:
 		dp++;
 	while (*dp)
 		putchar (*dp++);
+}
+
+usage()
+{
+	(void)fprintf(stderr, "usage: getNAME [-it] file ...\n");
+	exit(1);
 }
