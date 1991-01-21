12ab2007b909abd0b526e033824faea32e349470
Bob Corbett <corbett>
1991-01-20 20:01:57 -0800

    I added code to implement the -r option.  -rpc
    SCCS-vsn: 5.6

--- usr.bin/yacc/output.c
+++ usr.bin/yacc/output.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)output.c	5.5 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)output.c	5.6 (Berkeley) 01/20/91";
 #endif /* not lint */
 
 #include "defs.h"
