9d5911f314ef874225eadd95e5edbeaae498a4fb
Keith Bostic <bostic>
1991-01-26 15:32:07 -0800

    stdc.h -> cdefs.h
    SCCS-vsn: 5.4

--- lib/libc/string/strsep.c
+++ lib/libc/string/strsep.c
@@ -31,12 +31,12 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/stdc.h>
+#include <sys/cdefs.h>
 #include <string.h>
 #include <stdio.h>
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static const char sccsid[] = "@(#)strsep.c	5.3 (Berkeley) 05/18/90";
+static const char sccsid[] = "@(#)strsep.c	5.4 (Berkeley) 01/26/91";
 #endif /* LIBC_SCCS and not lint */
 
 /*
