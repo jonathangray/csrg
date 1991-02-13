bea6938ae2fd34ed7f723c199c747d30dd1df20b
Keith Bostic <bostic>
1991-02-12 22:38:00 -0800

    rework the password subsystem
    SCCS-vsn: 5.3

--- usr.bin/chpass/chpass.h
+++ usr.bin/chpass/chpass.h
@@ -30,13 +30,21 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)chpass.h	5.2 (Berkeley) 06/01/90
+ *	@(#)chpass.h	5.3 (Berkeley) 02/12/91
  */
 
-struct entry {
+typedef struct _entry {
 	char *prompt;
 	int (*func)(), restricted, len;
 	char *except, *save;
-};
+} ENTRY;
 
+/* Field numbers. */
+#define	E_BPHONE	8
+#define	E_HPHONE	9
+#define	E_LOCATE	10
+#define	E_NAME		7
+#define	E_SHELL		12
+
+extern ENTRY list[];
 extern uid_t uid;
