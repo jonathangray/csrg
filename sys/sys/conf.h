977c9c5c99f18c00b1c10d2f393da1a534596007
Keith Bostic <bostic>
1991-02-15 14:27:19 -0800

    add Berkeley specific copyright notice
    SCCS-vsn: 7.6

--- sys/sys/conf.h
+++ sys/sys/conf.h
@@ -1,94 +1,102 @@
-/*
- * Copyright (c) 1982, 1986, 1990 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1990 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)conf.h	7.5 (Berkeley) 04/04/90
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)conf.h	7.6 (Berkeley) 02/15/91
  */
 
-/*
- * Declaration of block device
- * switch. Each entry (row) is
- * the only link between the
- * main unix code and the driver.
- * The initialization of the
- * device switches is in the
- * file conf.c.
- */
-struct bdevsw
-{
-	int	(*d_open)();
-	int	(*d_close)();
-	int	(*d_strategy)();
-	int	(*d_ioctl)();
-	int	(*d_ioctl)();
-	int	(*d_dump)();
-	int	(*d_psize)();
-	int	d_flags;
+struct bdevsw {
+	int (*d_open)();
+	int (*d_close)();
+	int (*d_strategy)();
+	int (*d_ioctl)();
+	int (*d_dump)();
+	int (*d_psize)();
+	int d_flags;
 };
+
 #ifdef KERNEL
-struct	bdevsw bdevsw[];
+struct bdevsw bdevsw[];
 #endif
 
-/*
- * Character device switch.
- */
-struct cdevsw
-{
-	int	(*d_open)();
-	int	(*d_close)();
-	int	(*d_read)();
-	int	(*d_write)();
-	int	(*d_ioctl)();
-	int	(*d_stop)();
-	int	(*d_reset)();
+struct cdevsw {
+	int (*d_open)();
+	int (*d_close)();
+	int (*d_read)();
+	int (*d_write)();
+	int (*d_ioctl)();
+	int (*d_stop)();
+	int (*d_reset)();
 	struct tty *d_ttys;
-	int	(*d_select)();
-	int	(*d_mmap)();
-	int	(*d_strategy)();
+	int (*d_select)();
+	int (*d_mmap)();
+	int (*d_strategy)();
 };
+
 #ifdef KERNEL
-struct	cdevsw cdevsw[];
+struct cdevsw cdevsw[];
 
 /* symbolic sleep message strings */
-extern	 char devopn[], devio[], devwait[];
-extern	 char devin[], devout[], devioc[], devcls[];
+extern char devopn[], devio[], devwait[], devin[], devout[];
+extern char devioc[], devcls[];
 #endif
 
-/*
- * tty line control switch.
- */
-struct linesw
-{
-	int	(*l_open)();
-	int	(*l_close)();
-	int	(*l_read)();
-	int	(*l_write)();
-	int	(*l_ioctl)();
-	int	(*l_rint)();
-	int	(*l_rend)();
-	int	(*l_meta)();
-	int	(*l_start)();
-	int	(*l_modem)();
+struct linesw {
+	int (*l_open)();
+	int (*l_close)();
+	int (*l_read)();
+	int (*l_write)();
+	int (*l_ioctl)();
+	int (*l_rint)();
+	int (*l_rend)();
+	int (*l_meta)();
+	int (*l_start)();
+	int (*l_modem)();
 };
+
 #ifdef KERNEL
-struct	linesw linesw[];
+struct linesw linesw[];
 #endif
 
-/*
- * Swap device information
- */
-struct swdevt
-{
-	dev_t	sw_dev;
-	int	sw_freed;
-	int	sw_nblks;
+struct swdevt {
+	dev_t sw_dev;
+	int sw_freed;
+	int sw_nblks;
 	struct vnode *sw_vp;
 #ifdef SECSIZE
 	int	sw_blksize;
 	int	sw_bshift;
 #endif SECSIZE
 };
+
 #ifdef KERNEL
-struct	swdevt swdevt[];
+struct swdevt swdevt[];
 #endif
