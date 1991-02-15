d8baaa2c24f0310f2942a5e535f36385024e3286
Keith Bostic <bostic>
1991-02-15 14:27:39 -0800

    add Berkeley specific copyright notice
    SCCS-vsn: 7.3

--- sys/sys/map.h
+++ sys/sys/map.h
@@ -1,13 +1,40 @@
-/*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986, The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)map.h	7.2 (Berkeley) 06/06/87
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)map.h	7.3 (Berkeley) 02/15/91
  */
 
 /*
- * Resource Allocation Maps.
+ * Resource allocation maps.
  *
  * Associated routines manage sub-allocation of an address space using
  * an array of segment descriptors.  The first element of this array
@@ -30,22 +57,16 @@
  * as it is used internally by the resource map routines.
  */
 struct map {
-	struct	mapent *m_limit;	/* address of last slot in map */
-	char	*m_name;		/* name of resource */
-/* we use m_name when the map overflows, in warning messages */
+	struct mapent *m_limit;	/* address of last slot in map */
+	char *m_name;		/* name of resource, for messages */
 };
-struct mapent
-{
-	int	m_size;		/* size of this segment of the map */
-	int	m_addr;		/* resource-space addr of start of segment */
+struct mapent {
+	int m_size;		/* size of this segment of the map */
+	int m_addr;		/* resource-space addr of start of segment */
 };
 
 #ifdef KERNEL
-struct	map *swapmap;
-int	nswapmap;
-struct	map *argmap;
 #define	ARGMAPSIZE	16
-struct	map *kernelmap;
-struct	map *mbmap;
-struct	map *kmemmap;
+struct map *argmap, *kernelmap, *kmemmap, *mbmap, *swapmap;
+int nswapmap;
 #endif
