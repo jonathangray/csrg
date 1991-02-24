a415f3a68ae60e24262a1f2be071b4b00227e02d
Keith Bostic <bostic>
1991-02-24 15:26:18 -0800

    move insque/remque to libcompat, no longer need structure/prototypes
    SCCS-vsn: 5.6

--- include/unistd.h
+++ include/unistd.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)unistd.h	5.5 (Berkeley) 02/24/91
+ *	@(#)unistd.h	5.6 (Berkeley) 02/24/91
  */
 
 #include <sys/cdefs.h>
@@ -106,13 +106,6 @@ ssize_t		 write __P((int, const void *, size_t));
 /* structure timeval required for select() */
 #include <sys/time.h>
 
-/* structure qelem required for insque() and remque() */
-struct qelem {
-	struct	qelem *q_forw;
-	struct	qelem *q_back;
-	char	q_data[1];	/* arbitrary amount of data */
-};
-
 int	 acct __P((const char *));
 int	 async_daemon __P((void));
 char	*brk __P((const char *));
@@ -138,7 +131,6 @@ char	*getpass __P((const char *));
 char	*getusershell __P((void));
 char	*getwd __P((char *));			/* obsoleted by getcwd() */
 int	 initgroups __P((const char *, int));
-void	 insque __P((struct qelem *, struct qelem *));
 int	 mknod __P((const char *, mode_t, dev_t));
 int	 mkstemp __P((char *));
 char	*mktemp __P((char *));
@@ -153,7 +145,6 @@ char	*re_comp __P((const char *));
 int	 re_exec __P((const char *));
 int	 readlink __P((const char *, char *, int));
 int	 reboot __P((int));
-void	 remque __P((struct qelem *));
 int	 revoke __P((const char *));
 int	 rresvport __P((int *));
 int	 ruserok __P((const char *, int, const char *, const char *));
