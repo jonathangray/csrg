99e46544c8bfc64e26e64269e9a135b92994a1ad
Keith Bostic <bostic>
1991-02-26 13:54:54 -0800

    ANSI fixes
    SCCS-vsn: 5.11

--- usr.bin/indent/indent_globs.h
+++ usr.bin/indent/indent_globs.h
@@ -32,10 +32,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)indent_globs.h	5.10 (Berkeley) 06/01/90
+ *	@(#)indent_globs.h	5.11 (Berkeley) 02/26/91
  */
-
-#include <stdio.h>
 
 #define BACKSLASH '\\'
 #define bufsize 200		/* size of internal buffers */
