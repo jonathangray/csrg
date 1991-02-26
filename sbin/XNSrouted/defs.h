edd46bd329f63bdd85f8287440f9b40c76817279
Keith Bostic <bostic>
1991-02-26 15:29:56 -0800

    ANSI fixes
    SCCS-vsn: 5.9

--- sbin/XNSrouted/defs.h
+++ sbin/XNSrouted/defs.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)defs.h	5.8 (Berkeley) 06/01/90
+ *	@(#)defs.h	5.9 (Berkeley) 02/26/91
  */
 
 #include <sys/types.h>
@@ -83,11 +83,6 @@ struct	rip *msg;
 
 char	**argv0;
 
-extern	int errno;
-
-char	*malloc();
-int	exit();
-int	sendmsg();
+int	sndmsg();
 int	supply();
-int	timer();
 int	cleanup();
