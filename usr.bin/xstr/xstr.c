427cf9e05d3279aca2512028b321825abb0113b9
Keith Bostic <bostic>
1991-02-26 14:57:15 -0800

    ANSI fixes
    SCCS-vsn: 5.7

--- usr.bin/xstr/xstr.c
+++ usr.bin/xstr/xstr.c
@@ -38,13 +38,16 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)xstr.c	5.6 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)xstr.c	5.7 (Berkeley) 02/26/91";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <signal.h>
+#include <errno.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include "pathnames.h"
 
 /*
@@ -56,14 +59,10 @@ static char sccsid[] = "@(#)xstr.c	5.6 (Berkeley) 06/01/90";
 
 #define	ignore(a)	((void) a)
 
-char	*calloc();
 off_t	tellpt;
 off_t	hashit();
-char	*mktemp();
-int	onintr();
+void	onintr();
 char	*savestr();
-char	*strcat();
-char	*strcpy();
 off_t	yankstr();
 
 off_t	mesgpt;
@@ -106,7 +105,7 @@ main(argc, argv)
 	if (cflg || argc == 0 && !readstd)
 		inithash();
 	else
-		strings = mktemp(savestr(_PATH_TMP));
+		strings = mktemp(strdup(_PATH_TMP));
 	while (readstd || argc > 0) {
 		if (freopen("x.c", "w", stdout) == NULL)
 			perror("x.c"), exit(1);
@@ -332,7 +331,10 @@ hashit(str, new)
 		exit(8);
 	}
 	hp->hpt = mesgpt;
-	hp->hstr = savestr(str);
+	if (!(hp->hstr = strdup(str))) {
+		(void)fprintf(stderr, "xstr: %s\n", strerror(errno));
+		exit(1);
+	}
 	mesgpt += strlen(hp->hstr) + 1;
 	hp->hnext = hp0->hnext;
 	hp->hnew = new;
@@ -437,19 +439,6 @@ out:
 	ignore(fclose(strf));
 }
 
-char *
-savestr(cp)
-	register char *cp;
-{
-	register char *dp;
-
-	if ((dp = (char *) calloc(1, strlen(cp) + 1)) == NULL) {
-		perror("xstr");
-		exit(8);
-	}
-	return (strcpy(dp, cp));
-}
-
 lastchr(cp)
 	register char *cp;
 {
@@ -469,6 +458,7 @@ istail(str, of)
 	return (d);
 }
 
+void
 onintr()
 {
 
