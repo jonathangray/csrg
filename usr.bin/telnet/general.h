0f902ebfd3e7bea7afa29fea76adb0ebf9bbe0d1
Dave Borman <dab>
1991-03-01 01:31:55 -0800

    Telnet AUTHENTICATION option Telnet ENCRYPT option (test version) Function prototypes
    SCCS-vsn: 5.2

--- usr.bin/telnet/general.h
+++ usr.bin/telnet/general.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)general.h	5.1 (Berkeley) 09/14/90
+ *	@(#)general.h	5.2 (Berkeley) 03/01/91
  */
 
 /*
@@ -41,26 +41,5 @@
 #define	numberof(x)	(sizeof x/sizeof x[0])
 #define	highestof(x)	(numberof(x)-1)
 
-#if	defined(unix)
-#define	ClearElement(x)		bzero((char *)&x, sizeof x)
-#define	ClearArray(x)		bzero((char *)x, sizeof x)
-#else	/* defined(unix) */
 #define	ClearElement(x)		memset((char *)&x, 0, sizeof x)
 #define	ClearArray(x)		memset((char *)x, 0, sizeof x)
-#endif	/* defined(unix) */
-
-#if	defined(unix)		/* Define BSD equivalent mem* functions */
-#define	memcpy(dest,src,n)	bcopy(src,dest,n)
-#define	memmove(dest,src,n)	bcopy(src,dest,n)
-#define	memset(s,c,n)		if (c == 0) { \
-				    bzero(s,n); \
-				} else { \
-				    register char *src = s; \
-				    register int count = n; \
-					\
-				    while (count--) { \
-					*src++ = c; \
-				    } \
-				}
-#define	memcmp(s1,s2,n)		bcmp(s1,s2,n)
-#endif	/* defined(unix) */
