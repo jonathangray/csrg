6daa69d5c0ad237b83e47c38271647694dead151
Keith Bostic <bostic>
1991-03-01 14:56:31 -0800

    ANSI
    SCCS-vsn: 5.66

--- usr.bin/login/login.c
+++ usr.bin/login/login.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)login.c	5.65 (Berkeley) 02/05/91";
+static char sccsid[] = "@(#)login.c	5.66 (Berkeley) 03/01/91";
 #endif /* not lint */
 
 /*
@@ -485,7 +485,7 @@ motd()
 {
 	register int fd, nchars;
 	sig_t oldint;
-	int sigint();
+	void sigint();
 	char tbuf[8192];
 
 	if ((fd = open(_PATH_MOTDFILE, O_RDONLY, 0)) < 0)
@@ -498,6 +498,7 @@ motd()
 	(void)close(fd);
 }
 
+void
 sigint()
 {
 	longjmp(motdinterrupt, 1);
