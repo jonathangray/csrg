d559a57c61e0b47c1c73d8165e53970cf3ff4f7a
Keith Bostic <bostic>
1991-03-02 17:15:18 -0800

    ANSI
    SCCS-vsn: 5.39

--- usr.sbin/sendmail/src/deliver.c
+++ usr.sbin/sendmail/src/deliver.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	5.38 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)deliver.c	5.39 (Berkeley) 03/02/91";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -446,6 +446,8 @@ deliver(e, firstto)
 	else
 #endif /* SMTP */
 	{
+		static int sendoff();
+
 		message(Arpa_Info, "Connecting to %s (%s)...", host, m->m_name);
 		rcode = sendoff(e, m, pv, ctladdr);
 	}
