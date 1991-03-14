7ac06a0992cdb48084940a62676135106c455f4f
Marc Teitelbaum <marc>
1991-03-13 18:46:50 -0800

    wrong source
    SCCS-vsn: 5.2

--- bin/sh/miscbltin.c
+++ bin/sh/miscbltin.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)miscbltin.c	5.1 (Berkeley) 03/07/91";
+static char sccsid[] = "@(#)miscbltin.c	5.2 (Berkeley) 03/13/91";
 #endif /* not lint */
 
 /*
@@ -76,7 +76,7 @@ readcmd(argc, argv)  char **argv; {
 
 	eflag = 0;
 	prompt = NULL;
-	while ((i = nextopt("ep:")) >= 0) {
+	while ((i = nextopt("ep:")) != '\0') {
 		if (i == 'p')
 			prompt = optarg;
 		else
