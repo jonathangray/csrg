7b8ef58f185e76afbef32f9b25392f271d07570e
Mike Karels <karels>
1991-03-17 15:42:05 -0800

    more-or-less working with new proc & user structs
    SCCS-vsn: 7.9

--- sys/kern/subr_xxx.c
+++ sys/kern/subr_xxx.c
@@ -1,27 +1,77 @@
 /*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+ * Copyright (c) 1982, 1986, 1991 Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)subr_xxx.c	7.8 (Berkeley) 01/08/91
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)subr_xxx.c	7.9 (Berkeley) 03/17/91
  */
 
+/*
+ * Miscellaneous trivial functions, including many
+ * that are often inline-expanded or done in assembler.
+ */
+#include "types.h"
+#include "machine/cpu.h"
 #include "errno.h"
 
 /*
- * Routine placed in illegal entries in the bdevsw and cdevsw tables.
+ * Unsupported device function (e.g. writing to read-only device).
  */
-nodev()
+enodev()
 {
 
 	return (ENODEV);
 }
 
 /*
- * Null routine; placed in insignificant entries
- * in the bdevsw and cdevsw tables.
+ * Unconfigured device function; driver not configured.
  */
-nulldev()
+enxio()
+{
+
+	return (ENXIO);
+}
+
+/*
+ * Return error for unsupported operation.
+ */
+eopnotsupp()
+{
+
+	return (EOPNOTSUPP);
+}
+
+/*
+ * Generic null operation, always returns success.
+ */
+nullop()
 {
 
 	return (0);
@@ -33,28 +83,62 @@ nulldev()
  */
 #if !defined(vax) && !defined(tahoe)
 imin(a, b)
+	int a, b;
 {
 
 	return (a < b ? a : b);
 }
 
 imax(a, b)
+	int a, b;
 {
 
 	return (a > b ? a : b);
 }
 
-unsigned
+unsigned int
 min(a, b)
-	unsigned a, b;
+	unsigned int a, b;
 {
 
 	return (a < b ? a : b);
 }
 
-unsigned
+unsigned int
 max(a, b)
-	unsigned a, b;
+	unsigned int a, b;
+{
+
+	return (a > b ? a : b);
+}
+
+long
+lmin(a, b)
+	long a, b;
+{
+
+	return (a < b ? a : b);
+}
+
+long
+lmax(a, b)
+	long a, b;
+{
+
+	return (a > b ? a : b);
+}
+
+unsigned long
+ulmin(a, b)
+	unsigned long a, b;
+{
+
+	return (a < b ? a : b);
+}
+
+unsigned long
+ulmax(a, b)
+	unsigned long a, b;
 {
 
 	return (a > b ? a : b);
@@ -78,10 +162,11 @@ ffs(mask)
 #endif
 
 #if !defined(vax) && !defined(hp300)
-bcmp(s1, s2, len)
-	register char *s1, *s2;
+bcmp(v1, v2, len)
+	void *v1, *v2;
 	register unsigned len;
 {
+	register u_char *s1 = v1, *s2 = v2;
 
 	while (len--)
 		if (*s1++ != *s2++)
@@ -95,7 +180,7 @@ strlen(s1)
 	register int len;
 
 	for (len = 0; *s1++ != '\0'; len++)
-		/* void */;
+		;
 	return (len);
 }
 #endif
