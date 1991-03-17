7b35cba3a7ea6b28c15c038aca9001dcda18a072
Jan-Simon Pendry <pendry>
1991-03-17 13:50:01 -0800

    from amd5.3 alpha11
    SCCS-vsn: 5.2

--- usr.sbin/amd/config/os-utx32.h
+++ usr.sbin/amd/config/os-utx32.h
@@ -1,4 +1,4 @@
-/* $Id: os-utx32.h,v 5.2 90/06/23 22:21:00 jsp Rel $ */
+/* $Id: os-utx32.h,v 5.2.1.1 90/10/21 22:31:11 jsp Exp $ */
 
 /*
  * Gould UTX/32 definitions for Amd (automounter)
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)os-utx32.h	5.1 (Berkeley) 06/29/90
+ *	@(#)os-utx32.h	5.2 (Berkeley) 03/17/91
  */
 
 /*
