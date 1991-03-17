b8fcc4a246d717c4875f2a5a8eb7009dfcf6d0d1
Keith Sklower <sklower>
1991-03-16 17:07:39 -0800

    allow the ifa to be specified as well as the ifp on a RTM_CHANGE\;
    SCCS-vsn: 7.16

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rtsock.c	7.15 (Berkeley) 11/29/90
+ *	@(#)rtsock.c	7.16 (Berkeley) 03/16/91
  */
 
 #include "param.h"
