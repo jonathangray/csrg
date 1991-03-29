53e195952e3e17faaf410029707ed068267c6e2b
Marc Teitelbaum <marc>
1991-03-29 14:27:48 -0800

    add exit code
    SCCS-vsn: 5.8

--- usr.bin/gcore/gcore.c
+++ usr.bin/gcore/gcore.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)gcore.c	5.7 (Berkeley) 02/06/91";
+static char sccsid[] = "@(#)gcore.c	5.8 (Berkeley) 03/29/91";
 #endif /* not lint */
 
 /*
@@ -195,6 +195,7 @@ main(argc, argv)
 		close(cor);
 		printf("%s dumped\n", coref);
 	}
+	exit(0);
 }
 
 getword(loc)
