dc0b3cd100f7d13c0bce29051a5e0d8c9fa6c14c
Keith Bostic <bostic>
1991-04-04 13:44:37 -0800

    from scratch, add Berkeley copyright; change to be included by a.out.h
    SCCS-vsn: 5.6

--- include/nlist.h
+++ include/nlist.h
@@ -1,52 +1,80 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)nlist.h	5.5 (Berkeley) 02/05/91
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)nlist.h	5.6 (Berkeley) 04/04/91
  */
+
+#ifndef _NLIST_H_
+#define	_NLIST_H_
 
 /*
- * Format of a symbol table entry; this file is included by <a.out.h>
- * and should be used if you aren't interested the a.out header
- * or relocation information.
+ * Symbol table entry format.  The #ifdef's are so that programs including
+ * nlist.h can initialize nlist structures statically.
  */
-struct	nlist {
-	char	*n_name;	/* for use when in-core */
-	unsigned char n_type;	/* type flag, i.e. N_TEXT etc; see below */
-	char	n_other;	/* unused */
-	short	n_desc;		/* see <stab.h> */
-	unsigned long n_value;	/* value of this symbol (or sdb offset) */
-};
-#define	n_hash	n_desc		/* used internally by ld */
+struct nlist {
+#ifdef _AOUT_INCLUDE_
+	union {
+		char *n_name;	/* symbol name (in memory) */
+		long n_strx;	/* file string table offset (on disk) */
+	} n_un;
+#else
+	char *n_name;		/* symbol name (in memory) */
+#endif
 
-/*
- * Simple values for n_type.
- */
-#define	N_UNDF	0x0		/* undefined */
-#define	N_ABS	0x2		/* absolute */
-#define	N_TEXT	0x4		/* text */
-#define	N_DATA	0x6		/* data */
-#define	N_BSS	0x8		/* bss */
-#define	N_COMM	0x12		/* common (internal to ld) */
-#define	N_FN	0x1e		/* file name symbol */
+#define	N_UNDF	0x00		/* undefined */
+#define	N_ABS	0x02		/* absolute address */
+#define	N_TEXT	0x04		/* text segment */
+#define	N_DATA	0x06		/* data segment */
+#define	N_BSS	0x08		/* bss segment */
+#define	N_COMM	0x12		/* common reference */
+#define	N_FN	0x1e		/* file name */
 
-#define	N_EXT	01		/* external bit, or'ed in */
+#define	N_EXT	0x01		/* external (global) bit, OR'ed in */
 #define	N_TYPE	0x1e		/* mask for all the type bits */
+	unsigned char n_type;	/* type defines */
 
-/*
- * Sdb entries have some of the N_STAB bits set.
- * These are given in <stab.h>
- */
-#define	N_STAB	0xe0		/* if any of these bits set, a SDB entry */
+	char n_other;		/* spare */
+#define	n_hash	n_desc		/* used internally by ld(1); XXX */
+	short n_desc;		/* used by stab entries */
+	unsigned long n_value;	/* address/value of the symbol */
+};
 
-/*
- * Format for namelist values.
- */
-#define	N_FORMAT	"%08x"
+#define	N_FORMAT	"%08x"	/* namelist value format; XXX */
+#define	N_STAB		0x0e0	/* mask for debugger symbols -- stab(5) */
 
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
 int nlist __P((const char *, struct nlist *));
 __END_DECLS
+
+#endif /* !_NLIST_H_ */
