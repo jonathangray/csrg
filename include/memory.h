a5265a8917dcd313b0c8a05386f8a7909e097e70
Keith Bostic <bostic>
1991-04-03 16:13:41 -0800

    protect all user-level include files against reinclusion
    SCCS-vsn: 5.6

--- include/memory.h
+++ include/memory.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)memory.h	5.5 (Berkeley) 06/01/90
+ *	@(#)memory.h	5.6 (Berkeley) 04/03/91
  */
 
 #include <string.h>
