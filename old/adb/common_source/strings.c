a733319985db5fae6e169eeafae8222f0ea919a9
Keith Bostic <bostic>
1991-04-04 18:02:00 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- old/adb/common_source/strings.c
+++ old/adb/common_source/strings.c
@@ -1,13 +1,22 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)strings.c	5.1 (Berkeley) 01/16/89";
-#endif
+static char sccsid[] = "@(#)strings.c	5.2 (Berkeley) 04/04/91";
+#endif /* not lint */
 
 /*
  * adb - readonly data
  */
 
 #ifndef lint
-char VERSION[] = "\nVERSION VM/VAX4.3	DATE 01/16/89\n";
+char VERSION[] = "\nVERSION VM/VAX4.3	DATE 04/04/91\n";
 #endif
 
 char	BADMOD[] = "bad modifier";
