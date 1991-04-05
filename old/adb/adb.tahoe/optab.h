ba29330834b7b7cb9d00c9196093e32f6eb0b0b3
Keith Bostic <bostic>
1991-04-04 17:52:25 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.1

--- old/adb/adb.tahoe/optab.h
+++ old/adb/adb.tahoe/optab.h
@@ -1,4 +1,13 @@
-/*	@(#)optab.h	1.2 (Berkeley) 01/16/89	*/
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)optab.h	5.1 (Berkeley) 04/04/91
+ */
 
 /*
  * Argument access types
