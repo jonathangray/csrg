f3f04eb05b5f76a15a767e402991dc225f78fe83
Keith Bostic <bostic>
1991-04-04 18:20:34 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- bin/dd/dd.c
+++ bin/dd/dd.c
@@ -1,6 +1,21 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)dd.c	5.2 (Berkeley) 02/25/91";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)dd.c	5.3 (Berkeley) 04/04/91";
+#endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
