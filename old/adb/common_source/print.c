c75fcc9cd6dc0bfbc08b5a272244309e77ecf43e
Keith Bostic <bostic>
1991-04-04 18:01:58 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.4

--- old/adb/common_source/print.c
+++ old/adb/common_source/print.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)print.c	5.3 (Berkeley) 04/09/89";
-#endif
+static char sccsid[] = "@(#)print.c	5.4 (Berkeley) 04/04/91";
+#endif /* not lint */
 
 /*
  * adb - printing routines
