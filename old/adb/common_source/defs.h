27ef10a4a1beb47967af6d7aaaa6993781ee17ce
Keith Bostic <bostic>
1991-04-04 18:01:52 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.5

--- old/adb/common_source/defs.h
+++ old/adb/common_source/defs.h
@@ -1,4 +1,13 @@
-/*	@(#)defs.h	5.4 (Berkeley) 02/20/90	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)defs.h	5.5 (Berkeley) 04/04/91
+ */
 
 /*
  * adb: common definitions
