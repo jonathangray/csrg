4a559d37ac899cbc48d2b63f174580d44021afd1
Keith Bostic <bostic>
1991-04-04 17:56:42 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- old/adb/adb.vax/machdep.h
+++ old/adb/adb.vax/machdep.h
@@ -1,8 +1,12 @@
-/*
+/*-
  * Copyright (c) 1988 The Regents of the University of California.
  * All rights reserved.
  *
- *	@(#)machdep.h	5.2 (Berkeley) 02/23/90
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)machdep.h	5.3 (Berkeley) 04/04/91
  */
 
 /*
