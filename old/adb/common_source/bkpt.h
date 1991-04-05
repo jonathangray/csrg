49cd714998a1e59338d5c1bbf24f96b90070c3ab
Keith Bostic <bostic>
1991-04-04 18:01:50 -0800

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- old/adb/common_source/bkpt.h
+++ old/adb/common_source/bkpt.h
@@ -1,7 +1,12 @@
-/*
- * adb: breakpoints.
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)bkpt.h	5.1 (Berkeley) 01/16/89
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)bkpt.h	5.2 (Berkeley) 04/04/91
  */
 
 #define	MAX_BKPTCOM	64	/* maximum length of command at bkpt */
