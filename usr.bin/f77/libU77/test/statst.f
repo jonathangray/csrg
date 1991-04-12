9fbb085d2dcceca2a687ef23b14f98983d60587f
Keith Bostic <bostic>
1991-04-12 15:41:32 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/libU77/test/statst.f
+++ usr.bin/f77/libU77/test/statst.f
@@ -1,3 +1,14 @@
+C
+C Copyright (c) 1980 The Regents of the University of California.
+C All rights reserved.
+C
+C This module is believed to contain source code proprietary to AT&T.
+C Use and redistribution is subject to the Berkeley Software License
+C Agreement and your Software Agreement with AT&T (Western Electric).
+C
+C	@(#)statst.f	5.2 (Berkeley) 04/12/91
+C
+
 	character*24 ctime
 	integer stata(11), statb(11), i1, i2
 	open(1, file='statst.f')
