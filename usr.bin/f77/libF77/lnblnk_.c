f5bb45f22c5c461172cf3929d730dcfe870ac763
Keith Bostic <bostic>
1991-04-12 14:59:36 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/libF77/lnblnk_.c
+++ usr.bin/f77/libF77/lnblnk_.c
@@ -1,10 +1,17 @@
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)lnblnk_.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
+
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
- *
- *	@(#)lnblnk_.c	5.2	06/07/85
- *
  * find last occurrence of a non-blank character in string
  *
  * calling sequence:
