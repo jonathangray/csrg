61385217d63c49f236c4390112254eefcb3aa012
Keith Bostic <bostic>
1991-04-12 13:55:05 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.5

--- lib/libcompat/4.1/vax/reset.s
+++ lib/libcompat/4.1/vax/reset.s
@@ -1,11 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
-#ifdef LIBC_SCCS
-_sccsid:.asciz	"@(#)reset.s	5.4 (Berkeley) 03/09/86"
-#endif LIBC_SCCS
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)reset.s	5.5 (Berkeley) 04/12/91"
+#endif /* LIBC_SCCS and not lint */
 
 /*
  * C library -- reset, setexit
