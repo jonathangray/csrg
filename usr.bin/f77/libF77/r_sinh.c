247e3309256a7379d66bb20d792e56028bee5641
Keith Bostic <bostic>
1991-04-12 15:00:05 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/libF77/r_sinh.c
+++ usr.bin/f77/libF77/r_sinh.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)r_sinh.c	5.2	07/08/85
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)r_sinh.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 float r_sinh(x)
 float *x;
