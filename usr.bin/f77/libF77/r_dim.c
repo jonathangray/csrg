154ced8e76528dab468d8e684950336a0991316a
Keith Bostic <bostic>
1991-04-12 14:59:56 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.4

--- usr.bin/f77/libF77/r_dim.c
+++ usr.bin/f77/libF77/r_dim.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)r_dim.c	5.3	07/09/85
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)r_dim.c	5.4 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 float flt_retval;
 
