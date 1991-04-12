76b8d3c3be6a6d9e5f4f2aa72aa211e72d3a72ae
Keith Bostic <bostic>
1991-04-12 15:33:17 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/libU77/perror_.c
+++ usr.bin/f77/libU77/perror_.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)perror_.c	5.2	05/28/90
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)perror_.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /*
  * write a standard error message to the standard error output
