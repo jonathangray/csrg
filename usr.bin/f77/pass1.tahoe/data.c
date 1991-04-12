3b92a90ec05f2ed8faeb941d72c188fd7aea9183
Keith Bostic <bostic>
1991-04-12 16:00:32 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/pass1.tahoe/data.c
+++ usr.bin/f77/pass1.tahoe/data.c
@@ -1,12 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)data.c	5.1 (Berkeley) 6/7/85";
-#endif not lint
+static char sccsid[] = "@(#)data.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /*
  * data.c
