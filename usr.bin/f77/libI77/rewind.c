790dcc99cefdc462875dfd9be81247d3b932a4af
Keith Bostic <bostic>
1991-04-12 15:23:29 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/libI77/rewind.c
+++ usr.bin/f77/libI77/rewind.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)rewind.c	5.2	07/30/85
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)rewind.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /*
  * rewind.c  -  f77 file rewind
