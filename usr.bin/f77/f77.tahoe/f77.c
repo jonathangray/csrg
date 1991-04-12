dfd0ef5fd4f991d45375c568a50ddb9051e3245e
Keith Bostic <bostic>
1991-04-12 14:32:21 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.5

--- usr.bin/f77/f77.tahoe/f77.c
+++ usr.bin/f77/f77.tahoe/f77.c
@@ -1,3 +1,22 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)f77.c	5.5 (Berkeley) 04/12/91";
+#endif /* not lint */
+
 /*
  * f77.c
  *
