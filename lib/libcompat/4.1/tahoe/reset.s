9fd208cb1a885404b6f62d3b5e9dc609434db3f0
Keith Bostic <bostic>
1991-04-12 13:55:58 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 1.3

--- lib/libcompat/4.1/tahoe/reset.s
+++ lib/libcompat/4.1/tahoe/reset.s
@@ -1,11 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1986 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
-#ifdef LIBC_SCCS
-_sccsid:.asciz	"@(#)reset.s	1.2 (Berkeley/CCI) 08/01/86"
-#endif LIBC_SCCS
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	.asciz "@(#)reset.s	1.3 (Berkeley) 04/12/91"
+#endif /* LIBC_SCCS and not lint */
 
 /*
  * C library -- reset, setexit
