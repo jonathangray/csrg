73964063dce56428fbb9c5e96159cf806d69374f
Keith Bostic <bostic>
1991-04-12 14:59:33 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/libF77/l_gt.c
+++ usr.bin/f77/libF77/l_gt.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)l_gt.c	5.1	06/07/85
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)l_gt.c	5.2 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 long int l_gt(a,b,la,lb)
 char *a, *b;
