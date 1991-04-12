137f56edecde9aa237d12b8548af827d3c5520a4
Keith Bostic <bostic>
1991-04-12 15:16:00 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/libF77/CCI/mclock_.c
+++ usr.bin/f77/libF77/CCI/mclock_.c
@@ -1,6 +1,18 @@
-/*
- *	@(#)mclock_.c	5.1 (Berkeley) 11/03/86
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)mclock_.c	5.2 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 long int  mclock_()
   {
