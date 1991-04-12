a21efefcf0f167c3c46a43e2bccf9ab44d552d27
Keith Bostic <bostic>
1991-04-12 15:00:09 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.5

--- usr.bin/f77/libF77/random_.c
+++ usr.bin/f77/libF77/random_.c
@@ -1,10 +1,17 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)random_.c	5.4	05/13/90
- * 
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)random_.c	5.5 (Berkeley) 04/12/91";
+#endif /* not lint */
+
+/* 
  * Routines to return random values
  *
  * calling sequence:
