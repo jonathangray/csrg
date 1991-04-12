c7f1fa0318c2075d65903a59346616d7b5628048
Keith Bostic <bostic>
1991-04-12 15:42:43 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/libU77/test/sigfpe.s
+++ usr.bin/f77/libU77/test/sigfpe.s
@@ -1,3 +1,16 @@
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+	.asciz "@(#)sigfpe.s	5.2 (Berkeley) 04/12/91"
+#endif /* not lint */
+
 	.data
 	.even
 L16:
