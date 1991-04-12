8561a3ad44d7e78419023ea5fb0673685afc8cc2
Keith Bostic <bostic>
1991-04-12 14:59:40 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/libF77/opcodes.h
+++ usr.bin/f77/libF77/opcodes.h
@@ -1,10 +1,15 @@
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)opcodes.h	5.2 (Berkeley) 04/12/91
+ */
+
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
- *
- *	@(#)opcodes.h	5.1 (Berkeley) 06/07/85
- *
  * We just define the floating-point opcodes, to ease our minds.
  */
 
