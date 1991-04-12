aac830661408fc5ed474a4c5a81086f1f6b9383d
Keith Bostic <bostic>
1991-04-12 14:58:31 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 1.5

--- usr.bin/f77/libF77/Usef.c
+++ usr.bin/f77/libF77/Usef.c
@@ -1,10 +1,21 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)Usef.c	1.4	02/25/90
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1980 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)Usef.c	1.5 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /*		returns '-f' if need to use -f to bypass C bug		*/
 
