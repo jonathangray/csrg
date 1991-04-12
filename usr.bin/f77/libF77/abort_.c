16029f644e1e217385b07631661e30904cc1a42f
Keith Bostic <bostic>
1991-04-12 14:58:32 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/libF77/abort_.c
+++ usr.bin/f77/libF77/abort_.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)abort_.c	5.2	11/03/86
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)abort_.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 #include <stdio.h>
 
