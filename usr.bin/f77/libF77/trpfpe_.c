ea89d00790204c550b71c293256c9ed2f3661574
Keith Bostic <bostic>
1991-04-12 15:00:25 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.7

--- usr.bin/f77/libF77/trpfpe_.c
+++ usr.bin/f77/libF77/trpfpe_.c
@@ -1,12 +1,19 @@
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)trpfpe_.c	5.7 (Berkeley) 04/12/91";
+#endif /* not lint */
+
 /* #define OLD_BSD if you're running < 4.2 bsd */
+
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
- *
- *	@(#)trpfpe_.c	5.6	02/14/90
- *
- *
  *	Fortran floating-point error handler
  *
  *	Synopsis:
