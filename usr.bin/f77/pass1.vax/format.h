b890f83b515780a9f427ee9e007fb2871ce30c5b
Keith Bostic <bostic>
1991-04-12 16:08:49 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/pass1.vax/format.h
+++ usr.bin/f77/pass1.vax/format.h
@@ -1,9 +1,12 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)format.h	5.1 (Berkeley) 06/07/85
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)format.h	5.2 (Berkeley) 04/12/91
  */
 
 /*
