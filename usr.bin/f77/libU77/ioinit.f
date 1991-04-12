9c3a8f04a5e948fe5e234c52431c8748fb1dff1b
Keith Bostic <bostic>
1991-04-12 15:36:10 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- usr.bin/f77/libU77/ioinit.f
+++ usr.bin/f77/libU77/ioinit.f
@@ -1,11 +1,14 @@
 C
-C Copyright (c) 1980 Regents of the University of California.
-C All rights reserved.  The Berkeley software License Agreement
-C specifies the terms and conditions for redistribution.
+C Copyright (c) 1980 The Regents of the University of California.
+C All rights reserved.
 C
-C	@(#)ioinit.f	5.1 (Berkeley) 06/08/85
+C This module is believed to contain source code proprietary to AT&T.
+C Use and redistribution is subject to the Berkeley Software License
+C Agreement and your Software Agreement with AT&T (Western Electric).
 C
+C	@(#)ioinit.f	5.2 (Berkeley) 04/12/91
 C
+
 C ioinit - initialize the I/O system
 C
 C synopsis:
