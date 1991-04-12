70fd6ed821edfaab4b0c3842112814f0c53a8286
Keith Bostic <bostic>
1991-04-12 16:09:01 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.5

--- usr.bin/f77/pass1.vax/optim.c
+++ usr.bin/f77/pass1.vax/optim.c
@@ -1,12 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)optim.c	5.4 (Berkeley) 01/03/88";
-#endif not lint
+static char sccsid[] = "@(#)optim.c	5.5 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 /*
  * optim.c
