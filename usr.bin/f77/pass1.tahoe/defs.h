169248331006bb2668bdd0a256377607c277e8aa
Keith Bostic <bostic>
1991-04-12 16:00:35 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/pass1.tahoe/defs.h
+++ usr.bin/f77/pass1.tahoe/defs.h
@@ -1,9 +1,12 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)defs.h	5.1 (Berkeley) 6/7/85
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)defs.h	5.3 (Berkeley) 04/12/91
  */
 
 /*
