ec49a046a6cc291f6222eab9b866974f203cdd8b
Keith Bostic <bostic>
1991-04-12 15:00:01 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.6

--- usr.bin/f77/libF77/r_mod.c
+++ usr.bin/f77/libF77/r_mod.c
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)r_mod.c	5.5	01/15/91
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)r_mod.c	5.6 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 #ifndef tahoe
 float flt_retval;
