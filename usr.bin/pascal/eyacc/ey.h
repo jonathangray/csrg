4199951cfa060ff891169dca560c8fb9af38ee44
Keith Bostic <bostic>
1991-04-12 16:47:47 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/pascal/eyacc/ey.h
+++ usr.bin/pascal/eyacc/ey.h
@@ -1,9 +1,12 @@
-/*
- * Copyright (c) 1979 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1979 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)ey.h	5.2 (Berkeley) 09/22/88
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)ey.h	5.3 (Berkeley) 04/12/91
  */
 
 #include <stdio.h>
