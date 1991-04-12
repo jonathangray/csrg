13c86d58a9722a07997cd49f7e8d1159a494092d
Keith Bostic <bostic>
1991-04-12 15:00:32 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/f77/libF77/zabs.c
+++ usr.bin/f77/libF77/zabs.c
@@ -1,6 +1,15 @@
-/*
- *	@(#)zabs.c	5.2	11/03/86
+/*-
+ * Copyright (c) 1980 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
+
+#ifndef lint
+static char sccsid[] = "@(#)zabs.c	5.3 (Berkeley) 04/12/91";
+#endif /* not lint */
 
 #ifdef tahoe
 /* THIS IS BASED ON TAHOE FP REPRESENTATION */
