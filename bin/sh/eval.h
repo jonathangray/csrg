2bacdc2893ca895a564cae0fd2beb5d5b1c3326b
Marc Teitelbaum <marc>
1991-04-12 18:20:49 -0700

    add BLETCH comment to 'union node;'
    SCCS-vsn: 5.2

--- bin/sh/eval.h
+++ bin/sh/eval.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)eval.h	5.1 (Berkeley) 03/07/91
+ *	@(#)eval.h	5.2 (Berkeley) 04/12/91
  */
 
 extern char *commandname;	/* currently executing command */
@@ -51,7 +51,7 @@ struct backcmd {		/* result of evalbackcmd */
 
 #ifdef __STDC__
 void evalstring(char *);
-union node;
+union node;	/* BLETCH for ansi C */
 void evaltree(union node *, int);
 void evalbackcmd(union node *, struct backcmd *);
 #else
