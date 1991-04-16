a8b2d417e942e378f2e645272c7612b8be2b0d1b
Keith Bostic <bostic>
1991-04-16 16:48:50 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/struct/struct/4.def.h
+++ usr.bin/struct/struct/4.def.h
@@ -1,4 +1,10 @@
-/*	4.def.h	4.2	83/08/11	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)4.def.h	4.3 (Berkeley) 04/16/91
+ */
 
 #define YESTAB	TRUE
 #define NOTAB	FALSE
