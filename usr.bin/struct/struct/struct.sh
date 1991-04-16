b67e33766a94c7454594317e588180b9ff750daf
Keith Bostic <bostic>
1991-04-16 16:45:16 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- usr.bin/struct/struct/struct.sh
+++ usr.bin/struct/struct/struct.sh
@@ -1,7 +1,12 @@
-#! /bin/sh
+#!/bin/sh -
 #
-#	@(#)struct.sh	4.3	(Berkeley)	89/05/10
+# This module is believed to contain source code proprietary to AT&T.
+# Use and redistribution is subject to the Berkeley Software License
+# Agreement and your Software Agreement with AT&T (Western Electric).
 #
+#	@(#)struct.sh	4.4 (Berkeley) 04/16/91
+#
+
 trap "rm -f /tmp/struct*$$" 0 1 2 3 13 15
 files=no
 for i
