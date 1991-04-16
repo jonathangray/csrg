9f7e1b5598bb7155d6302cf253ad4f646627269f
Keith Bostic <bostic>
1991-04-16 16:48:44 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/struct/struct/3.def.h
+++ usr.bin/struct/struct/3.def.h
@@ -1,4 +1,10 @@
-/*	3.def.h	4.2	83/08/11	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)3.def.h	4.3 (Berkeley) 04/16/91
+ */
 
 #define RECURSE(p,v,r)	{ for (r = 0; r < CHILDNUM(v); ++r) if (DEFINED(LCHILD(v,r))) p(LCHILD(v,r)); if (DEFINED(RSIB(v))) p(RSIB(v)); }
 
