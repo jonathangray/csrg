ddcb2d4fb1b6a0db161887fb93cca50bd17038fc
Keith Bostic <bostic>
1991-04-16 14:45:34 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 1.3

--- old/ratfor/rlook.c
+++ old/ratfor/rlook.c
@@ -1,7 +1,13 @@
 Original BTL Ratfor System for 4.2
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)rlook.c	1.2 (Berkeley) 08/11/83";
-#endif
+static char sccsid[] = "@(#)rlook.c	1.3 (Berkeley) 04/16/91";
+#endif /* not lint */
 
 #define NULL 0
 #define EOS 0
