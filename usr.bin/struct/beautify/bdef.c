e9ae516ab780eec8eaba96acac5ac74350795566
Keith Bostic <bostic>
1991-04-16 16:44:12 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.1

--- usr.bin/struct/beautify/bdef.c
+++ usr.bin/struct/beautify/bdef.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)bdef.c	4.1	(Berkeley)	02/11/83";
-#endif not lint
+static char sccsid[] = "@(#)bdef.c	5.1 (Berkeley) 04/16/91";
+#endif /* not lint */
 
 #define xxtop	100		/* max size of xxstack */
 int xxindent, xxval, newflag, xxmaxchars, xxbpertab;
