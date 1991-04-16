ae790b1c410127fb290cacad147ad60e8b57f6a7
Kirk McKusick <mckusick>
1991-04-16 14:50:30 -0700

    extensive checking of directories only when DIIAGNOSTIC set
    SCCS-vsn: 7.31

--- sys/ufs/ffs/ufs_lookup.c
+++ sys/ufs/ffs/ufs_lookup.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ufs_lookup.c	7.30 (Berkeley) 04/16/91
+ *	@(#)ufs_lookup.c	7.31 (Berkeley) 04/16/91
  */
 
 #include "param.h"
@@ -44,7 +44,11 @@
 #include "fs.h"
 
 struct	nchstats nchstats;
+#ifdef DIAGNOSTIC
 int	dirchk = 1;
+#else
+int	dirchk = 0;
+#endif
 
 /*
  * Convert a component of a pathname into a pointer to a locked inode.
