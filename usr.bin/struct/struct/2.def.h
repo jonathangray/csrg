ed698f02bb5c18621fe0d992d14610f0c4c24c76
Keith Bostic <bostic>
1991-04-16 16:48:38 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/struct/struct/2.def.h
+++ usr.bin/struct/struct/2.def.h
@@ -1,4 +1,10 @@
-/*	2.def.h	4.2	83/08/11	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)2.def.h	4.3 (Berkeley) 04/16/91
+ */
 
 extern int accessnum;		/* number of nodes accessible from START */
 extern VERT *after;		/* node numbers associated with after numbers of depth first search */
