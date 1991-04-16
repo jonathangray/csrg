9cad9378403e0d12624263b136973361338d9116
Keith Bostic <bostic>
1991-04-16 16:44:14 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.1

--- usr.bin/struct/beautify/tree.c
+++ usr.bin/struct/beautify/tree.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)tree.c	4.4	(Berkeley)	02/05/91";
-#endif not lint
+static char sccsid[] = "@(#)tree.c	5.1 (Berkeley) 04/16/91";
+#endif /* not lint */
 
 # include "y.tab.h"
 #include "b.h"
