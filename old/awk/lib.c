2d40dce8c4a51783ebc6cd31f382e36e57b4db76
Keith Bostic <bostic>
1991-04-17 16:54:59 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.9

--- old/awk/lib.c
+++ old/awk/lib.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)lib.c	4.8 (Berkeley) 03/01/91";
-#endif
+static char sccsid[] = "@(#)lib.c	4.9 (Berkeley) 04/17/91";
+#endif /* not lint */
 
 #include <stdarg.h>
 #include "stdio.h"
