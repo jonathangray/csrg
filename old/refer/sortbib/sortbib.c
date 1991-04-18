961775bba85dca65ddca964ab0a34b822ace9e17
Keith Bostic <bostic>
1991-04-18 16:44:44 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.6

--- old/refer/sortbib/sortbib.c
+++ old/refer/sortbib/sortbib.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)sortbib.c	4.5 (Berkeley) 03/07/91";
-#endif
+static char sccsid[] = "@(#)sortbib.c	4.6 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include <stdio.h>
 #include <signal.h>
