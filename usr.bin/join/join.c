02dc2122780a0e9b92b4032ca421486f6fd4eda3
Keith Bostic <bostic>
1991-04-17 17:54:46 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/join/join.c
+++ usr.bin/join/join.c
@@ -1,4 +1,22 @@
-static char *sccsid = "@(#)join.c	4.2 (Berkeley) 06/30/83";
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)join.c	4.3 (Berkeley) 04/17/91";
+#endif /* not lint */
+
 /*	join F1 F2 on stuff */
 
 #include	<stdio.h>
