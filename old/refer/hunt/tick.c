0b0d34952a958d0914d6c6f26a7c3027651231d6
Keith Bostic <bostic>
1991-04-18 16:39:05 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- old/refer/hunt/tick.c
+++ old/refer/hunt/tick.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)tick.c	4.3 (Berkeley) 05/11/89";
-#endif
+static char sccsid[] = "@(#)tick.c	4.4 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 /* time programs */
 # include <stdio.h>
