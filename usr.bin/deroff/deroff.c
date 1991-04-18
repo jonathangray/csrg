b6a5faa0df3c5a65886b167308db559194b0f31f
Keith Bostic <bostic>
1991-04-17 17:10:03 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.8

--- usr.bin/deroff/deroff.c
+++ usr.bin/deroff/deroff.c
@@ -1,6 +1,21 @@
+/*-
+ * Copyright (c) 1988 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)deroff.c	4.7	(Berkeley)	88/04/24";
-#endif not lint
+char copyright[] =
+"@(#) Copyright (c) 1988 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)deroff.c	4.8 (Berkeley) 04/17/91";
+#endif /* not lint */
 
 #include <stdio.h>
 
