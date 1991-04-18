86c4a8f851b02f94be4026ee861297acb5397100
Keith Bostic <bostic>
1991-04-18 16:50:53 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- old/roff/nroff/n6.c
+++ old/roff/nroff/n6.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)n6.c	4.2 11/26/82";
-#endif lint
+static char sccsid[] = "@(#)n6.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include "tdef.h"
 extern
