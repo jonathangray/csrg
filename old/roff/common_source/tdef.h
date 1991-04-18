95aa0de61accf60f3199be7c11fe773ea95fa642
Keith Bostic <bostic>
1991-04-18 16:49:25 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.9

--- old/roff/common_source/tdef.h
+++ old/roff/common_source/tdef.h
@@ -1,4 +1,13 @@
-/*	tdef.h	4.8	87/04/30	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)tdef.h	4.9 (Berkeley) 04/18/91
+ */
 
 #include <sys/param.h>
 #undef CMASK			/* XXX */
