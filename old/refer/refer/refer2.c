4e20e724485c4739ea42d31730d2e7f1b6181ac2
Keith Bostic <bostic>
1991-04-18 16:44:10 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- old/refer/refer/refer2.c
+++ old/refer/refer/refer2.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)refer2.c	4.3 (Berkeley) 07/29/85";
-#endif
+static char sccsid[] = "@(#)refer2.c	4.4 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include "refer..c"
 #define NFLD 30
