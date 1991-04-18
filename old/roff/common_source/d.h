9dbab9498e3c59031e55c173270881fffd9695bf
Keith Bostic <bostic>
1991-04-18 16:49:11 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- old/roff/common_source/d.h
+++ old/roff/common_source/d.h
@@ -1,4 +1,13 @@
-/*	d.h	4.2	83/08/11	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)d.h	4.3 (Berkeley) 04/18/91
+ */
 
 struct d {filep op; int dnl,dimac,ditrap,ditf,alss,blss,nls,mkline,
 		maxl,hnl,curd;} d[NDI], *dip;
