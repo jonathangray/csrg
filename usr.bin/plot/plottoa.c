801e4e2f9cf71a6f8d6d00103e0db71c4afc2484
Keith Bostic <bostic>
1991-04-18 16:28:22 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/plot/plottoa.c
+++ usr.bin/plot/plottoa.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)plottoa.c	4.2 (Berkeley) 01/09/85";
-#endif
+static char sccsid[] = "@(#)plottoa.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 /*
  * Convert the standard plot input into a readable form for debugging.
