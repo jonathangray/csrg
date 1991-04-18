d3d59b9925f68a900048f9572dde41cf8ea1fcd9
Keith Bostic <bostic>
1991-04-18 16:44:06 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- old/refer/refer/glue4.c
+++ old/refer/refer/glue4.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)glue4.c	4.3 (Berkeley) 05/11/89";
-#endif
+static char sccsid[] = "@(#)glue4.c	4.4 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include <stdio.h>
 #include <ctype.h>
