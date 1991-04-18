3aac1cda245cab7dd4f92b33940e73dd91a7d396
Keith Bostic <bostic>
1991-04-17 17:25:27 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.6

--- old/eqn/common_source/io.c
+++ old/eqn/common_source/io.c
@@ -1,6 +1,21 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)io.c	4.5 07/22/88";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)io.c	4.6 (Berkeley) 04/17/91";
+#endif /* not lint */
 
 # include "e.h"
 #define	MAXLINE	1200	/* maximum input line */
