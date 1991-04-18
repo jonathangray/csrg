f6f6ff019093b1a220b642f60267e3fd1692833a
Keith Bostic <bostic>
1991-04-17 17:17:43 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/diction/style3/pscan.c
+++ usr.bin/diction/style3/pscan.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)pscan.c	4.2	(Berkeley)	82/11/06";
-#endif not lint
+static char sccsid[] = "@(#)pscan.c	4.3 (Berkeley) 04/17/91";
+#endif /* not lint */
 
 #include "names.h"
 #include "conp.h"
