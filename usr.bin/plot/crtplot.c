e8c6a141160280a936c4c54f5ff88fc335589371
Keith Bostic <bostic>
1991-04-18 16:28:20 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/plot/crtplot.c
+++ usr.bin/plot/crtplot.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)crtplot.c	4.2 (Berkeley) 03/01/91";
-#endif
+static char sccsid[] = "@(#)crtplot.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 /*
 This plotting routine interprets plot commands and outputs them onto
