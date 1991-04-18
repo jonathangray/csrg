b4d35e339185165c00542d9c023d5b711937eaab
Keith Bostic <bostic>
1991-04-17 17:51:57 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.9

--- usr.bin/grep/old.bin.grep/old.bin.grep.c
+++ usr.bin/grep/old.bin.grep/old.bin.grep.c
@@ -1,6 +1,21 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)old.bin.grep.c	4.8 (Berkeley) 01/21/88";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)old.bin.grep.c	4.9 (Berkeley) 04/17/91";
+#endif /* not lint */
 
 /*
  * grep -- print lines matching (or not matching) a pattern
