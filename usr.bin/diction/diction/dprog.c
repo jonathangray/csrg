70ac4f4bb7a78affdd08a4a8e5f30aad1df0c1f7
Keith Bostic <bostic>
1991-04-17 17:12:33 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.5

--- usr.bin/diction/diction/dprog.c
+++ usr.bin/diction/diction/dprog.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)dprog.c	4.4	(Berkeley)	91/03/01";
-#endif not lint
+static char sccsid[] = "@(#)dprog.c	4.5 (Berkeley) 04/17/91";
+#endif /* not lint */
 
 /*
  * diction -- print all sentences containing one of default phrases
