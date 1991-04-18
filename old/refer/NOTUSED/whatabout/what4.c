d386552a83f0b2353c2b916ac0352fcd820cf840
Keith Bostic <bostic>
1991-04-18 16:34:55 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- old/refer/NOTUSED/whatabout/what4.c
+++ old/refer/NOTUSED/whatabout/what4.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)what4.c	4.2 (Berkeley) 09/11/87";
-#endif
+static char sccsid[] = "@(#)what4.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include "what..c"
 #define NW 5
