8cf35362e04194c533e772da0baf91b7aa594f79
Keith Bostic <bostic>
1991-04-18 16:38:58 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.6

--- old/refer/hunt/hunt2.c
+++ old/refer/hunt/hunt2.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)hunt2.c	4.5 (Berkeley) 09/28/87";
-#endif
+static char sccsid[] = "@(#)hunt2.c	4.6 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include "refer..c"
 
