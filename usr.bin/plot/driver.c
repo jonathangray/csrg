4f7ed2541d7ac18409f7f41ca135fafa3f0dc880
Keith Bostic <bostic>
1991-04-18 16:28:22 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.5

--- usr.bin/plot/driver.c
+++ usr.bin/plot/driver.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)driver.c	4.4 (Berkeley) 09/21/85";
-#endif
+static char sccsid[] = "@(#)driver.c	4.5 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include <stdio.h>
 
