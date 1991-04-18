07ee7904ad9b8f476c4f088a30268666b23148fd
Keith Bostic <bostic>
1991-04-18 16:44:08 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.5

--- old/refer/refer/refer..c
+++ old/refer/refer/refer..c
@@ -1,4 +1,10 @@
-/*	refer..c	4.4	87/10/22	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)refer..c	4.5 (Berkeley) 04/18/91
+ */
 
 #include <stdio.h>
 #include <ctype.h>
