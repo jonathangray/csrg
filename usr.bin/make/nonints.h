4fb0ef991529eb1393e9c4ab8b004f587d5caf8c
Keith Bostic <bostic>
1991-04-18 16:11:22 -0700

    add correct copyright notice...
    SCCS-vsn: 5.6

--- usr.bin/make/nonints.h
+++ usr.bin/make/nonints.h
@@ -34,6 +34,8 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ *
+ *	@(#)nonints.h	5.6 (Berkeley) 04/18/91
  */
 
 char **brk_string(), *emalloc(), *str_concat();
