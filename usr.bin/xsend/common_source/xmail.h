3da3ac6c7af3e283fbd1703b0ecf2a93d1245fd0
Keith Bostic <bostic>
1991-04-18 18:59:56 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.2

--- usr.bin/xsend/common_source/xmail.h
+++ usr.bin/xsend/common_source/xmail.h
@@ -1,4 +1,10 @@
-/*	xmail.h	4.1	83/03/30	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)xmail.h	4.2 (Berkeley) 04/18/91
+ */
 
 #include <stdio.h>
 #include <mp.h>
