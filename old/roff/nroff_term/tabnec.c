b6f9e445af1293beb66ebd86ea238db7649f59d5
Keith Bostic <bostic>
1991-04-18 17:52:58 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- old/roff/nroff_term/tabnec.c
+++ old/roff/nroff_term/tabnec.c
@@ -1,4 +1,11 @@
-/*	tabnec.c	4.2	85/02/14	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)tabnec.c	4.3 (Berkeley) 04/18/91
+ */
+
 #define INCH 240
 /*
  * NEC Spinwriter 5500 or 7700 10 Pitch
