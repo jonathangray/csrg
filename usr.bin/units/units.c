49cd4f78da1c047dcaf15850e3c53f27cc397783
Keith Bostic <bostic>
1991-04-18 18:36:57 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/units/units.c
+++ usr.bin/units/units.c
@@ -1,4 +1,13 @@
-static char *sccsid = "@(#)units.c	4.2 (Berkeley) 05/11/89";
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)units.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
+
 #include <stdio.h>
 #include "pathnames.h"
 
