deb661ff8c7e2625b6c4a4221700fb5fe95dfde7
Keith Bostic <bostic>
1991-04-18 18:17:41 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- old/tbl/ti.c
+++ old/tbl/ti.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)ti.c	4.2 08/11/83";
-#endif
+static char sccsid[] = "@(#)ti.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
  /* ti.c: classify line intersections */
 # include "t..c"
