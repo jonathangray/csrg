50369a26096bfdae8679b17322faca288d132cc8
Keith Bostic <bostic>
1991-04-18 18:00:55 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- old/sed/sed.h
+++ old/sed/sed.h
@@ -1,4 +1,10 @@
-/*	sed.h	4.3	87/12/21	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)sed.h	4.4 (Berkeley) 04/18/91
+ */
 
 /*
  * sed -- stream  editor
