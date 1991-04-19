7423096f8c4a19d7a95b7fa49b583bf486e76f12
Keith Bostic <bostic>
1991-04-18 21:12:03 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.10

--- usr.sbin/ac/ac.c
+++ usr.sbin/ac/ac.c
@@ -1,6 +1,22 @@
+/*-
+ * Copyright (c) 1982 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char *sccsid = "@(#)ac.c	4.9 (Berkeley) 04/02/89";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1982 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)ac.c	4.10 (Berkeley) 04/18/91";
+#endif /* not lint */
+
 /*
  * ac [ -w wtmp ] [ -d ] [ -p ] [ people ]
  */
