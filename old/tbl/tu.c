f3d6ff8eac2956fc64e795a3bbfe91811c75d01b
Keith Bostic <bostic>
1991-04-18 18:17:44 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- old/tbl/tu.c
+++ old/tbl/tu.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)tu.c	4.3 07/22/88";
-#endif
+static char sccsid[] = "@(#)tu.c	4.4 (Berkeley) 04/18/91";
+#endif /* not lint */
 
  /* tu.c: draws horizontal lines */
 # include "t..c"
