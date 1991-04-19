013253974b3681e636c8c82dc9acbc592e79a629
Keith Bostic <bostic>
1991-04-18 18:07:55 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 1.6

--- usr.bin/spell/spell.sh
+++ usr.bin/spell/spell.sh
@@ -1,7 +1,15 @@
-#! /bin/sh
+#!/bin/sh -
 #
-#	@(#)spell.sh	1.5	(Berkeley)	89/08/21
+# Copyright (c) 1983 The Regents of the University of California.
+# All rights reserved.
 #
+# This module is believed to contain source code proprietary to AT&T.
+# Use and redistribution is subject to the Berkeley Software License
+# Agreement and your Software Agreement with AT&T (Western Electric).
+#
+#	@(#)spell.sh	1.6 (Berkeley) 04/18/91
+#
+
 : V data for -v, B flags, D dictionary, S stop, H history, F files, T temp
 : R for deroff program
 V=/dev/null		B=			F= 
