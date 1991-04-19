3ba40d7c5f78a4cd06fb19c44aaf04c742db148b
Keith Bostic <bostic>
1991-04-18 18:16:08 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.5

--- old/tbl/t1.c
+++ old/tbl/t1.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)t1.c	4.4 03/01/91";
-#endif
+static char sccsid[] = "@(#)t1.c	4.5 (Berkeley) 04/18/91";
+#endif /* not lint */
 
  /* t1.c: main control and input switching */
 #
