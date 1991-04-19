085d7e88303a1e81f41b53118814604b61727ae1
Keith Bostic <bostic>
1991-04-18 17:55:05 -0700

    | sed '/ATT (NEW)/d'
    SCCS-vsn: 4.3

--- old/roff/troff/svt6.c
+++ old/roff/troff/svt6.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)svt6.c	4.2 05/11/89";
-#endif lint
+static char sccsid[] = "@(#)svt6.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include "tdef.h"
 extern
