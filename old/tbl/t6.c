2b3c9cc7e3a25556f9414adb6bbea1b3bb21c0eb
Keith Bostic <bostic>
1991-04-18 18:17:34 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.5

--- old/tbl/t6.c
+++ old/tbl/t6.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)t6.c	4.4 07/22/88";
-#endif
+static char sccsid[] = "@(#)t6.c	4.5 (Berkeley) 04/18/91";
+#endif /* not lint */
 
  /* t6.c: compute tab stops */
 
