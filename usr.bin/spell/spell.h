fdc80eff1969af78542fcdfd6c86e9c72e8f45d4
Keith Bostic <bostic>
1991-04-18 18:07:55 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/spell/spell.h
+++ usr.bin/spell/spell.h
@@ -1,4 +1,13 @@
-/*	@(#)spell.h	4.2	02/02/91	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)spell.h	4.3 (Berkeley) 04/18/91
+ */
 
 #include <stdio.h>
 #include <ctype.h>
