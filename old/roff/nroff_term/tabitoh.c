939ebe332919c85c615e2c4182c85b13daf3875c
Keith Bostic <bostic>
1991-04-18 17:52:55 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.2

--- old/roff/nroff_term/tabitoh.c
+++ old/roff/nroff_term/tabitoh.c
@@ -1,4 +1,11 @@
-/*	tabitoh.c	4.1	83/08/05	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)tabitoh.c	4.2 (Berkeley) 04/18/91
+ */
+
 #define INCH 240
 /*
  * C:Itoh Prowriter (dot matrix) 10 pitch
