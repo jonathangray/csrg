80ff3a969192e2c40630721f13e8b3aadb308562
Keith Bostic <bostic>
1991-04-19 15:49:03 -0700

    remove last vestiges of AT&T code; fix for ANSI; Berkeley copyright
    SCCS-vsn: 4.7

--- usr.sbin/update/update.c
+++ usr.sbin/update/update.c
@@ -1,41 +1,84 @@
-/*
- * Copyright (c) 1987, 1990 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1987, 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)update.c	4.6 (Berkeley) 03/02/91";
-#endif
+char copyright[] =
+"@(#) Copyright (c) 1987, 1990 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
 
-/*
- * Update the file system every 30 seconds.
- * For cache benefit, open certain system directories.
- */
+#ifndef lint
+static char sccsid[] = "@(#)update.c	4.7 (Berkeley) 04/19/91";
+#endif /* not lint */
 
 #include <sys/time.h>
-#include <sys/file.h>
-#include <sys/signal.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include "pathnames.h"
 
 main()
 {
 	struct itimerval value;
-	register char **f;
-	void sync();
+	void mysync();
 
 	daemon(0, 0);
-	for (f = fillst; *f; f++)
-		(void)open(*f, O_RDONLY, 0);
-	(void)signal(SIGALRM, sync);
+
+	(void)open(_PATH_BIN, O_RDONLY, 0);
+	(void)open(_PATH_USR, O_RDONLY, 0);
+	(void)open(_PATH_USRBIN, O_RDONLY, 0);
+	(void)open(_PATH_USRLIB, O_RDONLY, 0);
+
+	(void)signal(SIGALRM, mysync);
+
 	value.it_interval.tv_sec = 30;
 	value.it_interval.tv_usec = 0;
 	value.it_value = value.it_interval;
-	if (setitimer(ITIMER_REAL, &value, (struct itimerval *)NULL)) {
+	if (setitimer(ITIMER_REAL, &value, NULL)) {
 		perror("update: setitimer");
 		exit(1);
 	}
 	for (;;)
 		pause();
 	/*NOTREACHED*/
+}
+
+/* VARARGS */
+void
+mysync(i)
+	int i;
+{
+	(void)sync();
 }
