f9bae0cdcf1f0e100d0e92ea3f53a0d6f0814cd1
Keith Bostic <bostic>
1991-04-18 18:17:40 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- old/tbl/tf.c
+++ old/tbl/tf.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)tf.c	4.2 08/11/83";
-#endif
+static char sccsid[] = "@(#)tf.c	4.3 (Berkeley) 04/18/91";
+#endif /* not lint */
 
  /* tf.c: save and restore fill mode around table */
 # include "t..c"
