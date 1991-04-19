285070f7bd0492ec2d078e95bf1261b7b82692cd
Keith Bostic <bostic>
1991-04-18 18:00:57 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- old/sed/sed1.c
+++ old/sed/sed1.c
@@ -1,4 +1,12 @@
-/*	sed1.c	4.3	87/12/21	*/
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)sed1.c	4.4 (Berkeley) 04/18/91";
+#endif /* not lint */
 
 #include	<stdio.h>
 #include "sed.h"
