05780d461af2322a0a5bd29ef941df41884722d5
Mike Karels <karels>
1991-04-20 15:11:05 -0700

    it's contaminated; ansify
    SCCS-vsn: 7.18

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,41 +1,13 @@
 /*
  * Copyright (c) 1982, 1986, 1990 Regents of the University of California.
- * All rights reserved.
+ * All rights reserved.  The Berkeley software License Agreement
+ * specifies the terms and conditions for redistribution.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- *	@(#)kern_physio.c	7.17 (Berkeley) 03/17/91
+ *	@(#)kern_physio.c	7.18 (Berkeley) 04/20/91
  */
 
 #include "param.h"
 #include "systm.h"
-#include "user.h"
 #include "buf.h"
 #include "conf.h"
 #include "proc.h"
@@ -44,6 +16,13 @@
 #include "map.h"
 #include "vnode.h"
 #include "specdev.h"
+
+#ifdef HPUXCOMPAT
+#include "user.h"
+#endif
+
+static	struct buf *getswbuf();
+static	freeswbuf();
 
 /*
  * Raw I/O. The arguments are
@@ -73,7 +52,6 @@ physio(strat, bp, dev, rw, mincnt, uio)
 	register struct proc *p = curproc;
 	char *a;
 	int s, allocbuf = 0, error = 0;
-	struct buf *getswbuf();
 #ifdef SECSIZE
 	int bsize;
 	struct partinfo dpart;
