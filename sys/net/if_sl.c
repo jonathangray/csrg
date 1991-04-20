c2e518b05a08edf60bf7ced9543c43581515f416
Mike Karels <karels>
1991-04-20 16:38:44 -0700

    new proc, user; rm user.h; remove TIOCGETD hack returning unit
    SCCS-vsn: 7.22

--- sys/net/if_sl.c
+++ sys/net/if_sl.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_sl.c	7.21 (Berkeley) 06/28/90
+ *	@(#)if_sl.c	7.22 (Berkeley) 04/20/91
  */
 
 /*
@@ -71,7 +71,7 @@
 #if NSL > 0
 
 #include "param.h"
-#include "user.h"
+#include "proc.h"
 #include "mbuf.h"
 #include "buf.h"
 #include "dk.h"
@@ -81,17 +81,18 @@
 #include "tty.h"
 #include "kernel.h"
 #include "conf.h"
-#include "errno.h"
 
 #include "if.h"
 #include "if_types.h"
 #include "netisr.h"
 #include "route.h"
 #if INET
-#include "../netinet/in.h"
-#include "../netinet/in_systm.h"
-#include "../netinet/in_var.h"
-#include "../netinet/ip.h"
+#include "netinet/in.h"
+#include "netinet/in_systm.h"
+#include "netinet/in_var.h"
+#include "netinet/ip.h"
+#else
+Huh? Slip without inet?
 #endif
 
 #include "machine/mtpr.h"
@@ -236,11 +237,12 @@ slopen(dev, tp)
 	dev_t dev;
 	register struct tty *tp;
 {
+	struct proc *p = curproc;		/* XXX */
 	register struct sl_softc *sc;
 	register int nsl;
 	int error;
 
-	if (error = suser(u.u_cred, &u.u_acflag))
+	if (error = suser(p->p_ucred, &p->p_acflag))
 		return (error);
 
 	if (tp->t_line == SLIPDISC)
@@ -299,8 +301,7 @@ sltioctl(tp, cmd, data, flag)
 	int s;
 
 	switch (cmd) {
-	case TIOCGETD:				/* XXX */
-	case SLIOGUNIT:
+	case SLIOCGUNIT:
 		*(int *)data = sc->sc_if.if_unit;
 		break;
 
