832798b124328fd93c46e9f765a760f033f21792
Keith Bostic <bostic>
1991-04-19 19:12:31 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- lib/libmp/msqrt.c
+++ lib/libmp/msqrt.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)msqrt.c	5.2 (Berkeley) 03/02/87";
-#endif not lint
+static char sccsid[] = "@(#)msqrt.c	5.3 (Berkeley) 04/19/91";
+#endif /* not lint */
 
 #include <mp.h>
 msqrt(a,b,r) MINT *a,*b,*r;
