b2dbf0fd1f69640e3613c33ba4db68334ef0c2c4
Keith Bostic <bostic>
1991-04-20 15:30:52 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- share/tmac/tmac.r
+++ share/tmac/tmac.r
@@ -1,3 +1,13 @@
+.\" Copyright (c) 1979 The Regents of the University of California.
+.\" All rights reserved.
+.\"
+.\" This module is believed to contain source code proprietary to AT&T.
+.\" Use and redistribution is subject to the Berkeley Software License
+.\" Agreement and your Software Agreement with AT&T (Western Electric).
+.\"
+.\"	@(#)tmac.r	5.2 (Berkeley) 04/20/91
+.\"
+
 .de HD
 .ps 10
 .ft 1
