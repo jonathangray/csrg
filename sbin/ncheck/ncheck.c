827ae025de309da85281a6e650ee9a20666a6a9d
Keith Bostic <bostic>
1991-04-20 14:44:28 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.13

--- sbin/ncheck/ncheck.c
+++ sbin/ncheck/ncheck.c
@@ -1,18 +1,21 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1988, 1990 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1980 Regents of the University of California.\n\
+"@(#) Copyright (c) 1988, 1990 The Regents of the University of California.\n\
  All rights reserved.\n";
-#endif not lint
+#endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)ncheck.c	5.12 (Berkeley) 05/04/90";
-#endif not lint
+static char sccsid[] = "@(#)ncheck.c	5.13 (Berkeley) 04/20/91";
+#endif /* not lint */
 
 /*
  * ncheck -- obtain file names from reading filesystem
