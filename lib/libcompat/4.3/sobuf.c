b56b02e01b65e7a23dc04e5c762dc5481c7ee0be
Keith Bostic <bostic>
1991-04-20 14:12:53 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- lib/libcompat/4.3/sobuf.c
+++ lib/libcompat/4.3/sobuf.c
@@ -1,6 +1,12 @@
+/*-
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)sobuf.c	5.2 (Berkeley) 03/09/86";
-#endif LIBC_SCCS and not lint
+static char sccsid[] = "@(#)sobuf.c	5.3 (Berkeley) 04/20/91";
+#endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
 
