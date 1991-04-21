4112a3bd6b415da3a78734d424af56ea40d2810f
Keith Bostic <bostic>
1991-04-21 14:28:38 -0700

    from Tom Truscott -- encrypted password of "*" always succeeded
    SCCS-vsn: 5.6

--- lib/libc/gen/crypt.c
+++ lib/libc/gen/crypt.c
@@ -19,7 +19,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)crypt.c	5.5 (Berkeley) 03/06/91";
+static char sccsid[] = "@(#)crypt.c	5.6 (Berkeley) 04/21/91";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/cdefs.h>
