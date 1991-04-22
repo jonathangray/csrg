dfbcba4039cd79a06dabb95ad3a062b33c4bc537
Keith Bostic <bostic>
1991-04-22 16:09:33 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- lib/libplot/vt0/open.c
+++ lib/libplot/vt0/open.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)open.c	4.2 (Berkeley) 11/10/83";
-#endif
+static char sccsid[] = "@(#)open.c	4.3 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 int xnow;
 int ynow;
