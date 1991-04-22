965ac991d6a5018ca48a7b82e675baebd02d0422
Keith Bostic <bostic>
1991-04-22 16:05:56 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- lib/libplot/t4014/arc.c
+++ lib/libplot/t4014/arc.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)arc.c	4.3 (Berkeley) 06/23/85";
-#endif
+static char sccsid[] = "@(#)arc.c	4.4 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 int del = 20;
 step(d){
