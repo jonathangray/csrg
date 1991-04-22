e7f0d11a2ccb49b4acaeec4210042c573e2fc453
Keith Bostic <bostic>
1991-04-22 14:54:45 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- lib/libplot/aed/line.c
+++ lib/libplot/aed/line.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)line.c	5.2 (Berkeley) 06/06/85";
-#endif not lint
+static char sccsid[] = "@(#)line.c	5.3 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 #include "aed.h"
 
