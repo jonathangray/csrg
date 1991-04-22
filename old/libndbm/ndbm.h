c4ba95d214cf4c8e797650dd3f67b8dd5db91026
Keith Bostic <bostic>
1991-04-22 14:48:54 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- old/libndbm/ndbm.h
+++ old/libndbm/ndbm.h
@@ -1,7 +1,16 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)ndbm.h	5.2 (Berkeley) 04/22/91
+ */
+
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+ * Derived from:
  *
  *	@(#)ndbm.h	5.3 (Berkeley) 5/29/90
  */
