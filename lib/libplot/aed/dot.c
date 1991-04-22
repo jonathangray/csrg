7673fa358e9d9b8f7b22fcf4ef48c4d20af8a6af
Keith Bostic <bostic>
1991-04-22 14:54:43 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.2

--- lib/libplot/aed/dot.c
+++ lib/libplot/aed/dot.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1983 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)dot.c	4.1 (Berkeley) 11/11/83";
-#endif
+static char sccsid[] = "@(#)dot.c	5.2 (Berkeley) 04/22/91";
+#endif /* not lint */
 
 dot(){
 }
