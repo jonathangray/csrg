5bf640d0400e97a776da34e108a6108119787564
Keith Bostic <bostic>
1991-04-24 17:33:59 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.5

--- usr.bin/uucp/libuu/xqt.c
+++ usr.bin/uucp/libuu/xqt.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)xqt.c	5.4	(Berkeley) 05/04/88";
-#endif
+static char sccsid[] = "@(#)xqt.c	5.5 (Berkeley) 04/24/91";
+#endif /* not lint */
 
 #include <signal.h>
 #include "uucp.h"
