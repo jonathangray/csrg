4b8d3d701339275e2ea768e21f4a8dab5e53f272
Keith Bostic <bostic>
1991-04-24 17:36:53 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.6

--- usr.bin/uucp/port/getwd.c
+++ usr.bin/uucp/port/getwd.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)getwd.c	5.5 (Berkeley) 06/29/90";
-#endif
+static char sccsid[] = "@(#)getwd.c	5.6 (Berkeley) 04/24/91";
+#endif /* not lint */
 
 #include "uucp.h"
 
