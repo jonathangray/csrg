98040fc54501108ddf38090e431d46de0c3d27f8
Keith Bostic <bostic>
1991-04-24 17:42:46 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.3

--- usr.bin/uucp/uucico/chksum.c
+++ usr.bin/uucp/uucico/chksum.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)chksum.c	4.2 (Berkeley) 06/19/85";
-#endif
+static char sccsid[] = "@(#)chksum.c	4.3 (Berkeley) 04/24/91";
+#endif /* not lint */
 
 #ifndef pdp11
 chksum (s, n)
