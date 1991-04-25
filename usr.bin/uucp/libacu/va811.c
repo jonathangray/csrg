543240bf72298a01bd2f139b350f0dde6442a6e0
Keith Bostic <bostic>
1991-04-24 17:30:43 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.4

--- usr.bin/uucp/libacu/va811.c
+++ usr.bin/uucp/libacu/va811.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)va811.c	4.3 (Berkeley) 03/02/91";
-#endif
+static char sccsid[] = "@(#)va811.c	4.4 (Berkeley) 04/24/91";
+#endif /* not lint */
 
 #include "condevs.h"
 
