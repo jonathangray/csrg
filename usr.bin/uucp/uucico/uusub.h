f525c0ca202d1bb94388c440ef26f035670c1494
Keith Bostic <bostic>
1991-04-24 17:43:01 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.2

--- usr.bin/uucp/uucico/uusub.h
+++ usr.bin/uucp/uucico/uusub.h
@@ -1,4 +1,12 @@
-/*	uusub.h	4.1	85/01/22	*/
-/*	uusub is not privided with 4.2bsd  */
- 
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)uusub.h	4.2 (Berkeley) 04/24/91
+ */
+
 #define UB_SST(a)	 
