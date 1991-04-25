a2d9bc2121ed5773bcb081857c7c2219e1bd854a
Keith Bostic <bostic>
1991-04-24 17:28:35 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.9

--- usr.bin/uucp/includes/condevs.h
+++ usr.bin/uucp/includes/condevs.h
@@ -1,4 +1,13 @@
-/*	@(#)condevs.h	4.8	(Berkeley)	03/02/91	*/
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)condevs.h	4.9 (Berkeley) 04/24/91
+ */
 
 #include "uucp.h"
 #include <errno.h>
