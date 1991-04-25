443a251b8b9fd8311a90b2bb96d25c958a683bdf
Keith Bostic <bostic>
1991-04-24 17:36:55 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 5.3

--- usr.bin/uucp/port/ioctl.c
+++ usr.bin/uucp/port/ioctl.c
@@ -1,6 +1,15 @@
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ */
+
 #ifndef lint
-static char sccsid[] = "@(#)ioctl.c	5.2 (Berkeley) 01/22/85";
-#endif
+static char sccsid[] = "@(#)ioctl.c	5.3 (Berkeley) 04/24/91";
+#endif /* not lint */
 
 #include "uucp.h"
 #include <sgtty.h>
