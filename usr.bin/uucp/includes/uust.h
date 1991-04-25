bf4475489f7056e7d2cff6baecf8b540c8e38620
Keith Bostic <bostic>
1991-04-24 17:28:37 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 4.2

--- usr.bin/uucp/includes/uust.h
+++ usr.bin/uucp/includes/uust.h
@@ -1,8 +1,15 @@
-/*	uust.h	4.1	85/01/22	*/
-/*	uustat is not provided with 4.2bsd */
+/*-
+ * Copyright (c) 1985 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)uust.h	4.2 (Berkeley) 04/24/91
+ */
 
-#define US_RRS(a,b)	 
-#define US_CRS(a)	 
-#define US_SST(a)	 
+#define	US_RRS(a,b)	 
+#define	US_CRS(a)	 
+#define	US_SST(a)	 
 #define	USRF(flag)
- 
