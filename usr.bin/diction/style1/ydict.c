aca7873bad87fd80e6d3b67a8f81b92ceea6c376
Keith Bostic <bostic>
1991-04-27 16:33:18 -0700

    all of the .c's are really .h's...
    SCCS-vsn: 4.4

--- usr.bin/diction/style1/ydict.c
+++ usr.bin/diction/style1/ydict.c
@@ -2,11 +2,9 @@
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)ydict.c	4.4 (Berkeley) 04/27/91
  */
-
-#ifndef lint
-static char sccsid[] = "@(#)ydict.c	4.3 (Berkeley) 04/17/91";
-#endif /* not lint */
 
 struct dict ary_d[] = {
 "auxili",'Y',
