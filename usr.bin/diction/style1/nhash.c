ec23f2f71b6798760f0ed815178c5e7767df5fbe
Keith Bostic <bostic>
1991-04-27 16:33:17 -0700

    all of the .c's are really .h's...
    SCCS-vsn: 4.5

--- usr.bin/diction/style1/nhash.c
+++ usr.bin/diction/style1/nhash.c
@@ -2,11 +2,9 @@
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)nhash.c	4.5 (Berkeley) 04/27/91
  */
-
-#ifndef lint
-static char sccsid[] = "@(#)nhash.c	4.4 (Berkeley) 04/17/91";
-#endif /* not lint */
 
 struct dict {
 	char *entry;
