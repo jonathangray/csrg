67ba5a3f59976e3b33cb336c1c9d17467ab6f0b4
Keith Bostic <bostic>
1991-04-27 16:33:15 -0700

    all of the .c's are really .h's...
    SCCS-vsn: 4.4

--- usr.bin/diction/style1/abbrev.c
+++ usr.bin/diction/style1/abbrev.c
@@ -2,11 +2,9 @@
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)abbrev.c	4.4 (Berkeley) 04/27/91
  */
-
-#ifndef lint
-static char sccsid[] = "@(#)abbrev.c	4.3 (Berkeley) 04/17/91";
-#endif /* not lint */
 
 struct dict abbrev_d[] = {
 "St",'N',
