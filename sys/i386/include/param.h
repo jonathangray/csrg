1f3569f1e9ecae3f58f1086c46b46ae9fef97c11
Keith Bostic <bostic>
1991-04-28 12:03:30 -0700

    move SSIZE comment from <sys/param.h>
    SCCS-vsn: 5.5

--- sys/i386/include/param.h
+++ sys/i386/include/param.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)param.h	5.4 (Berkeley) 04/15/91
+ *	@(#)param.h	5.5 (Berkeley) 04/28/91
  */
 
 /*
@@ -62,6 +62,7 @@
 #define	CLSIZE		1
 #define	CLSIZELOG2	0
 
+/* NOTE: SSIZE, SINCR and UPAGES must be multiples of CLSIZE */
 #define	SSIZE	1		/* initial stack size/NBPG */
 #define	SINCR	1		/* increment of stack/NBPG */
 
