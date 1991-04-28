283c683005b236a3372dd4d4a51bb25c74d4b6e5
Donn Seeley <donn>
1991-04-28 12:13:42 -0700

    Cleanups; take advantage of gcc asm() features.
    SCCS-vsn: 5.4

--- lib/csu/i386/crt0.c
+++ lib/csu/i386/crt0.c
@@ -8,7 +8,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)crt0.c	5.3 (Berkeley) 04/12/91";
+static char sccsid[] = "@(#)crt0.c	5.4 (Berkeley) 04/28/91";
 #endif /* LIBC_SCCS and not lint */
 
 /*
