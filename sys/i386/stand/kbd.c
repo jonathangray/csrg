8d109cdea7ed2ddfc19b37d1e86d32277a3821d1
William Jolitz <william>
1991-04-28 17:16:44 -0700

    cleanup
    SCCS-vsn: 7.3

--- sys/i386/stand/kbd.c
+++ sys/i386/stand/kbd.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kbd.c	7.2 (Berkeley) 04/28/91
+ *	@(#)kbd.c	7.3 (Berkeley) 04/28/91
  */
 
 #define	L		0x01	/* locking function */
