5e4310cf958bb8cf4474ec8ac91ecd112df6c1d5
William Jolitz <william>
1991-04-28 17:03:24 -0700

    merge for common sys.c
    SCCS-vsn: 7.2

--- sys/i386/stand/confxx.c
+++ sys/i386/stand/confxx.c
@@ -5,16 +5,38 @@
  * This code is derived from software contributed to Berkeley by
  * William Jolitz.
  *
-%sccs.include.noredist.c%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- *	@(#)confxx.c	7.1 (Berkeley) 04/24/90
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)confxx.c	7.2 (Berkeley) 04/28/91
  */
 
-#include "../machine/pte.h"
-
-#include "../h/param.h"
-#include "../h/inode.h"
-#include "../h/fs.h"
+#include "param.h"
 #include "saio.h"
 
 devread(io)
@@ -86,12 +108,6 @@ int	nullsys(), nullioctl();
 int	xxstrategy(), xxopen()/*, xxioctl()*/;
 
 struct devsw devsw[] = {
-	{ "XX",	xxstrategy,	xxopen,		nullsys,	/*xxioctl*/ },
-	{ 0, 0, 0, 0, 0 }
+	{ "XX",	xxstrategy,	xxopen,		nullsys,	/*xxioctl*/ }
 };
-#ifdef	TP
-char	bootprog[] = "XX0b:" ;
-#else
-char	bootprog[] = "XX0a:/boot" ;
-#endif
 int ndevs = 1 ;
