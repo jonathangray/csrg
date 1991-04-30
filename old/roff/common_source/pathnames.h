2c411c202e417d21a8805943a31b6136414fa178
Keith Bostic <bostic>
1991-04-30 14:32:59 -0700

    move term/tab into /usr/old
    SCCS-vsn: 5.5

--- old/roff/common_source/pathnames.h
+++ old/roff/common_source/pathnames.h
@@ -30,10 +30,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	5.4 (Berkeley) 04/18/91
+ *	@(#)pathnames.h	5.5 (Berkeley) 04/30/91
  */
 
 #define	_PATH_TMAC		"/usr/share/tmac/tmac."
 #define	_PATH_LOCAL_TMAC	"/usr/local/lib/tmac/tmac."
-#define	_PATH_TERM		"/usr/libdata/term/tab"
+#define	_PATH_TERM		"/usr/old/libdata/term/tab"
 #define	_PATH_TRACCT		"/var/account/tracct"
