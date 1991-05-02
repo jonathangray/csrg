3d9232044dd2ec76e1fc2e031389cd21a857a031
Keith Sklower <sklower>
1991-05-01 18:11:25 -0700

    borrow two flags from isode (nodnic & prepnd0) for convering AFI37 to  X.121
    SCCS-vsn: 7.7

--- sys/netccitt/x25.h
+++ sys/netccitt/x25.h
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)x25.h	7.6 (Berkeley) 01/09/91
+ *	@(#)x25.h	7.7 (Berkeley) 05/01/91
  */
 
 #ifdef KERNEL
@@ -132,7 +132,9 @@ struct	x25config {
 #define X25_DDN		3
 #define X25_BASIC	4
 		xc_ptrace:1,	/* packet level tracing flag */
-		xc_rsvd2:5;
+		xc_nodnic:1,	/* remove our dnic when calling on net */
+		xc_prepnd0:1,	/* prepend 0 when making offnet calls */
+		xc_rsvd2:3;
 	u_short	xc_maxlcn;	/* max logical channels */
 	u_short	xc_dg_idletimo;	/* timeout for idle datagram circuits. */
 };
