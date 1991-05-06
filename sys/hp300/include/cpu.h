35c6df14016a87ce265ecfdcd2cd1bb1ccdd1b0f
Mike Karels <karels>
1991-05-06 11:29:02 -0700

    psl.h is already included by machine/param.h; add signotify(); do ast's here, global rather than per-process
    SCCS-vsn: 7.5

--- sys/hp300/include/cpu.h
+++ sys/hp300/include/cpu.h
@@ -37,14 +37,8 @@
  *
  * from: Utah $Hdr: cpu.h 1.13 89/06/23$
  *
- *	@(#)cpu.h	7.4 (Berkeley) 04/20/91
+ *	@(#)cpu.h	7.5 (Berkeley) 05/06/91
  */
-
-#ifdef notyet
-#include "../hp300/psl.h"
-#else
-#include "machine/psl.h"
-#endif
 
 /*
  * Exported definitions unique to hp300/68k cpu support.
@@ -79,8 +73,8 @@ typedef struct intrframe {
 	int	ps;
 } clockframe;
 
-#define	CLKF_USERMODE(framep)	USERMODE((framep)->ps)
-#define	CLKF_BASEPRI(framep)	BASEPRI((framep)->ps)
+#define	CLKF_USERMODE(framep)	(((framep)->ps & PSL_S) == 0)
+#define	CLKF_BASEPRI(framep)	(((framep)->ps & PSL_IPL7) == 0)
 #define	CLKF_PC(framep)		((framep)->pc)
 
 
@@ -90,7 +84,6 @@ typedef struct intrframe {
  */
 #define	need_resched()	{ want_resched++; aston(); }
 
-
 /*
  * Give a profiling tick to the current process from the softclock
  * interrupt.  On hp300, request an ast to send us through trap(),
@@ -98,6 +91,15 @@ typedef struct intrframe {
  */
 #define	profile_tick(p, framep)	{ (p)->p_flag |= SOWEUPC; aston(); }
 
+/*
+ * Notify the current process (p) that it has a signal pending,
+ * process as soon as possible.
+ */
+#define	signotify(p)	aston()
+
+#define aston() (astpending++)
+
+int	astpending;		/* need to trap before returning to user mode */
 int	want_resched;		/* resched() was called */
 
 
