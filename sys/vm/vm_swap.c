dbe53c0e500991efea41dd34146b5af2371eceff
Mike Karels <karels>
1991-05-06 13:06:06 -0700

    define nswdev, nswap
    SCCS-vsn: 7.18

--- sys/vm/vm_swap.c
+++ sys/vm/vm_swap.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_swap.c	7.17 (Berkeley) 04/16/91
+ *	@(#)vm_swap.c	7.18 (Berkeley) 05/06/91
  */
 
 #include "param.h"
@@ -49,6 +49,8 @@
 /*
  * Indirect driver for multi-controller paging.
  */
+
+int	nswap, nswdev;
 
 /*
  * Set up swap devices.
