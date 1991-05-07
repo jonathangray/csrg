f83ad9a850cf33b3281568bf02fb6d49e4558762
Keith Bostic <bostic>
1991-05-06 19:24:20 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.8

--- sys/netiso/tp_inet.c
+++ sys/netiso/tp_inet.c
@@ -1,3 +1,38 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)tp_inet.c	7.8 (Berkeley) 05/06/91
+ */
+
 /***********************************************************
 		Copyright IBM Corporation 1987
 
@@ -28,7 +63,6 @@ SOFTWARE.
  * ARGO TP
  * $Header: tp_inet.c,v 5.3 88/11/18 17:27:29 nhall Exp $ 
  * $Source: /usr/argo/sys/netiso/RCS/tp_inet.c,v $
- *	@(#)tp_inet.c	7.7 (Berkeley) 11/20/90 *
  *
  * Here is where you find the inet-dependent code.  We've tried
  * keep all net-level and (primarily) address-family-dependent stuff
@@ -36,21 +70,17 @@ SOFTWARE.
  * through a switch table (struct nl_protosw *) tpcb->tp_nlproto 
  * (see tp_pcb.c). 
  * The routines here are:
- * 		in_getsufx: gets transport suffix out of an inpcb structure.
- * 		in_putsufx: put transport suffix into an inpcb structure.
- *		in_putnetaddr: put a whole net addr into an inpcb.
- *		in_getnetaddr: get a whole net addr from an inpcb.
- *		in_cmpnetaddr: compare a whole net addr from an isopcb.
- *		in_recycle_suffix: clear suffix for reuse in inpcb
- *		tpip_mtu: figure out what size tpdu to use
- *		tpip_input: take a pkt from ip, strip off its ip header, give to tp
- *		tpip_output_dg: package a pkt for ip given 2 addresses & some data
- *		tpip_output: package a pkt for ip given an inpcb & some data
+ * 	in_getsufx: gets transport suffix out of an inpcb structure.
+ * 	in_putsufx: put transport suffix into an inpcb structure.
+ *	in_putnetaddr: put a whole net addr into an inpcb.
+ *	in_getnetaddr: get a whole net addr from an inpcb.
+ *	in_cmpnetaddr: compare a whole net addr from an isopcb.
+ *	in_recycle_suffix: clear suffix for reuse in inpcb
+ *	tpip_mtu: figure out what size tpdu to use
+ *	tpip_input: take a pkt from ip, strip off its ip header, give to tp
+ *	tpip_output_dg: package a pkt for ip given 2 addresses & some data
+ *	tpip_output: package a pkt for ip given an inpcb & some data
  */
-
-#ifndef lint
-static char *rcsid = "$Header: tp_inet.c,v 5.3 88/11/18 17:27:29 nhall Exp $";
-#endif lint
 
 #ifdef INET
 
