323ae73eb3281a59374ee46a27a58c4040b5d55e
Mike Hibler <hibler>
1991-05-07 09:54:08 -0700

    DIO-II support
    SCCS-vsn: 7.5

--- sys/hp300/dev/ite_rb.c
+++ sys/hp300/dev/ite_rb.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: ite_rb.c 1.1 90/07/09$
+ * from: Utah $Hdr: ite_rb.c 1.16 91/01/21$
  *
- *	@(#)ite_rb.c	7.4 (Berkeley) 05/04/91
+ *	@(#)ite_rb.c	7.5 (Berkeley) 05/07/91
  */
 
 #include "ite.h"
@@ -70,9 +70,9 @@ rbox_init(ip)
 
 	/* XXX */
 	if (ip->regbase == 0) {
-		struct grfinfo *gi = &grf_softc[ip - ite_softc].g_display;
-		ip->regbase = IOV(gi->gd_regaddr);
-		ip->fbbase = IOV(gi->gd_fbaddr);
+		struct grf_softc *gp = &grf_softc[ip - ite_softc];
+		ip->regbase = gp->g_regkva;
+		ip->fbbase = gp->g_fbkva;
 	}
 
 	rb_waitbusy(REGADDR);
