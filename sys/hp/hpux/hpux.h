0f0d8e4dacede5c998972edeac46458583e6cef8
Mike Hibler <hibler>
1991-05-07 10:03:48 -0700

    update from utah
    SCCS-vsn: 7.4

--- sys/hp/hpux/hpux.h
+++ sys/hp/hpux/hpux.h
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * from: Utah $Hdr: hpux.h 1.1 90/07/09$
+ * from: Utah $Hdr: hpux.h 1.18 91/02/09$
  *
- *	@(#)hpux.h	7.3 (Berkeley) 12/16/90
+ *	@(#)hpux.h	7.4 (Berkeley) 05/07/91
  */
 
 #include "hpux_exec.h"
@@ -82,6 +82,9 @@ struct hpuxsgttyb {
 #define	HPUXTIOCSPGRP	_IOW('T', 29, int)
 #define	HPUXTIOCGPGRP	_IOR('T', 30, int)
 #define HPUXTIOCCONS	_IO('t', 104)
+
+/* non-blocking IO--doesn't interfere with O_NDELAY */
+#define HPUXFIOSNBIO	_IOW('f', 126, int)
 
 /* HP-UX stat structure */
 
