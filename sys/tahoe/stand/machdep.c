990ed8db1ef2e22182c5418f483d13cc6461b72b
Keith Bostic <bostic>
1991-05-08 14:26:52 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 1.6

--- sys/tahoe/stand/machdep.c
+++ sys/tahoe/stand/machdep.c
@@ -1,4 +1,16 @@
-/*	machdep.c	1.5	90/12/16	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)machdep.c	1.6 (Berkeley) 05/08/91
+ */
 
 #include "../tahoe/mem.h"
 #include "../include/mtpr.h"
