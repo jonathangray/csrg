daee2c5654bc05a043832004ec5a1b9084b7300b
Keith Bostic <bostic>
1991-05-08 14:23:31 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.4

--- sys/tahoe/include/pte.h
+++ sys/tahoe/include/pte.h
@@ -1,9 +1,15 @@
-/*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1988 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)pte.h	7.3 (Berkeley) 08/31/89
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)pte.h	7.4 (Berkeley) 05/08/91
  */
 
 /*
