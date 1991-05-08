6911e0aad2f8eefa8777b82c1e45cc84d15d1d58
Keith Bostic <bostic>
1991-05-08 14:23:33 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.3

--- sys/tahoe/include/vmparam.h
+++ sys/tahoe/include/vmparam.h
@@ -1,12 +1,18 @@
-/*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1988 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)vmparam.h	7.2 (Berkeley) 05/10/90
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)vmparam.h	7.3 (Berkeley) 05/08/91
  */
 
-/*
+/*-
  * Machine dependent constants for tahoe.
  */
 
