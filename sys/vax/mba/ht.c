9976f891b1d19418c1e4206387247363d1fd5352
Keith Bostic <bostic>
1991-05-08 14:45:02 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.14

--- sys/vax/mba/ht.c
+++ sys/vax/mba/ht.c
@@ -1,9 +1,12 @@
-/*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)ht.c	7.13 (Berkeley) 12/16/90
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)ht.c	7.14 (Berkeley) 05/08/91
  */
 
 #include "tu.h"
