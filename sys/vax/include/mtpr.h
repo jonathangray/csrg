d4516e8c260a93e731a45614b97746f854125b63
Keith Bostic <bostic>
1991-05-08 14:40:52 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.6

--- sys/vax/include/mtpr.h
+++ sys/vax/include/mtpr.h
@@ -1,9 +1,12 @@
-/*
- * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1982, 1986, 1988 The Regents of the University of California.
+ * All rights reserved.
  *
- *	@(#)mtpr.h	7.5 (Berkeley) 11/09/88
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)mtpr.h	7.6 (Berkeley) 05/08/91
  */
 
 /*
