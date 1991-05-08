d12db34ebe6370cb4f1c8fd377b6ba5cea226e75
Keith Bostic <bostic>
1991-05-08 14:26:50 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.13

--- sys/tahoe/stand/cy.c
+++ sys/tahoe/stand/cy.c
@@ -1,4 +1,16 @@
-/*	cy.c	7.12	91/05/04	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)cy.c	7.13 (Berkeley) 05/08/91
+ */
 
 /*
  * Cypher tape driver. Stand alone version.
