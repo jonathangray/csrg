4b8bea4d1a7a650a1e75b360baa479c54b131fdd
Keith Bostic <bostic>
1991-05-08 14:26:53 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 7.2

--- sys/tahoe/stand/srt0.c
+++ sys/tahoe/stand/srt0.c
@@ -1,4 +1,16 @@
-/*	srt0.c	7.1	91/05/05	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)srt0.c	7.2 (Berkeley) 05/08/91
+ */
 
 #include "../include/mtpr.h"
 #define	LOCORE
