4a9ed8b789d5f1adea41f3e16e565edf9e645bbf
Keith Bostic <bostic>
1991-05-08 14:26:51 -0700

    new copyright; att/bsd/shared
    SCCS-vsn: 1.3

--- sys/tahoe/stand/cyvar.h
+++ sys/tahoe/stand/cyvar.h
@@ -1,4 +1,16 @@
-/*	cyvar.h	1.2	86/12/18	*/
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Computer Consoles Inc.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
+ *
+ *	@(#)cyvar.h	1.3 (Berkeley) 05/08/91
+ */
 
 #define TM_ATTENTION(addr,x) 	movob(addr,x)	/* also known as: GO */
 
