38a5ab2462dc6f7fc658d042a8fa6509cc692592
Jan-Simon Pendry <pendry>
1991-05-12 15:59:48 -0700

    checkpoint for network tape
    SCCS-vsn: 5.3

--- usr.sbin/amd/fsinfo/fsinfo.c
+++ usr.sbin/amd/fsinfo/fsinfo.c
@@ -1,6 +1,4 @@
 /*
- * $Id: fsinfo.c,v 5.2.1.2 90/12/21 16:46:47 jsp Alpha $
- *
  * Copyright (c) 1989 Jan-Simon Pendry
  * Copyright (c) 1989 Imperial College of Science, Technology & Medicine
  * Copyright (c) 1989 The Regents of the University of California.
@@ -37,7 +35,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fsinfo.c	5.2 (Berkeley) 03/17/91
+ *	@(#)fsinfo.c	5.3 (Berkeley) 05/12/91
+ *
+ * $Id: fsinfo.c,v 5.2.1.3 91/05/07 22:19:01 jsp Alpha $
+ *
  */
 
 /*
