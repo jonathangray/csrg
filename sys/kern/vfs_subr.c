72c8b6e77d637226be0cf59008ae3c942e802954
Kirk McKusick <mckusick>
1991-05-12 16:51:48 -0700

    add code for printing locked vnodes
    SCCS-vsn: 7.56

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vfs_subr.c	7.55 (Berkeley) 05/08/91
+ *	@(#)vfs_subr.c	7.56 (Berkeley) 05/12/91
  */
 
 /*
@@ -1139,6 +1139,27 @@ vprint(label, vp)
 	printf("\n\t");
 	VOP_PRINT(vp);
 }
+
+#ifdef DEBUG
+/*
+ * List all of the locked vnodes in the system.
+ * Called when debugging the kernel.
+ */
+printlockedvnodes()
+{
+	register struct mount *mp;
+	register struct vnode *vp;
+
+	printf("Locked vnodes\n");
+	mp = rootfs;
+	do {
+		for (vp = mp->mnt_mounth; vp; vp = vp->v_mountf)
+			if (VOP_ISLOCKED(vp))
+				vprint((char *)0, vp);
+		mp = mp->mnt_next;
+	} while (mp != rootfs);
+}
+#endif
 
 int kinfo_vdebug = 1;
 int kinfo_vgetfailed;
