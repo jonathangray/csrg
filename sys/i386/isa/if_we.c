6c8beffc5b332bba02ca93b482025fefca1ed66a
Keith Bostic <bostic>
1991-05-21 14:19:02 -0700

    format police
    SCCS-vsn: 7.3

--- sys/i386/isa/if_we.c
+++ sys/i386/isa/if_we.c
@@ -3,7 +3,7 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Tim L. Tucker
+ * Tim L. Tucker.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_we.c	7.2 (Berkeley) 05/12/91
+ *	@(#)if_we.c	7.3 (Berkeley) 05/21/91
  */
 
 /*
