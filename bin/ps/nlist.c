bda7cf46b546385e7dfcb1177bc27565f9898871
Keith Bostic <bostic>
1991-06-03 16:55:39 -0700

    fix err routine, make everyone use it; minor ANSI cleanups
    SCCS-vsn: 5.2

--- bin/ps/nlist.c
+++ bin/ps/nlist.c
@@ -32,13 +32,14 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)nlist.c	5.1 (Berkeley) 02/08/91";
+static char sccsid[] = "@(#)nlist.c	5.2 (Berkeley) 06/03/91";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <nlist.h>
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 struct	nlist psnl[] = {
 	{"_ecmx"},
@@ -84,4 +85,3 @@ donlist()
 	}
 	return(rval);
 }
-
