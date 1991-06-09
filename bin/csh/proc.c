038875b079f23f371427ba2798da6ff7abd3451b
Christos Zoulas <christos>
1991-06-08 17:09:00 -0700

    More prototype fixes, Removed TERMIOS define
    SCCS-vsn: 5.20

--- bin/csh/proc.c
+++ bin/csh/proc.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)proc.c	5.19 (Berkeley) 06/08/91";
+static char sccsid[] = "@(#)proc.c	5.20 (Berkeley) 06/08/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -41,6 +41,12 @@ static char sccsid[] = "@(#)proc.c	5.19 (Berkeley) 06/08/91";
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#if __STDC__
+# include <stdarg.h>
+#else
+# include <varargs.h>
+#endif
+
 #include "csh.h"
 #include "dir.h"
 #include "proc.h"
