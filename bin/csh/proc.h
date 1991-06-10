eeea98b4393edce0faec8c37504fa1529e43d25c
Christos Zoulas <christos>
1991-06-09 18:39:56 -0700

    replaced PNULL def with NULL
    SCCS-vsn: 5.5

--- bin/csh/proc.h
+++ bin/csh/proc.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)proc.h	5.4 (Berkeley) 06/04/91
+ *	@(#)proc.h	5.5 (Berkeley) 06/09/91
  */
 
 /*
@@ -77,7 +77,6 @@ struct process {
 #define	PPTIME		(1<<14)	/* time individual process */
 #define	PNEEDNOTE	(1<<15)	/* notify as soon as practical */
 
-#define	PNULL		(struct process *)0
 #define	PMAXLEN		80
 
 /* defines for arguments to pprint */
