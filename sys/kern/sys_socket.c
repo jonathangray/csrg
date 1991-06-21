a0c1f070f78b54934edf9e9cf2b870ff96920aa1
Kirk McKusick <mckusick>
1991-06-21 10:42:53 -0700

    soo_close now gets pointer to struct socket instead of file pointer
    SCCS-vsn: 7.12

--- sys/kern/sys_socket.c
+++ sys/kern/sys_socket.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sys_socket.c	7.11 (Berkeley) 04/16/91
+ *	@(#)sys_socket.c	7.12 (Berkeley) 06/21/91
  */
 
 #include "param.h"
