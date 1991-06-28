42e0f31cc1c013c3e147865c369b4e084e8b8cc5
Keith Sklower <sklower>
1991-06-27 23:49:14 -0700

    make compile under newvm system (change types.h to param.h)
    SCCS-vsn: 7.3

--- sys/hp300/hp300/ns_cksum.c
+++ sys/hp300/hp300/ns_cksum.c
@@ -30,11 +30,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ns_cksum.c	7.2 (Berkeley) 12/16/90
+ *	@(#)ns_cksum.c	7.3 (Berkeley) 06/27/91
  */
 
-#include "sys/types.h"
-#include "sys/mbuf.h"
+#include "param.h"
+#include "mbuf.h"
 
 
 /*
