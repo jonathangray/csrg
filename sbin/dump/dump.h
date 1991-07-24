f673e08e9e9ff5a1846ed9897c4024ee74c8f41f
Kirk McKusick <mckusick>
1991-07-23 21:09:16 -0700

    use POSIX constant NAME_MAX rather than MAXNAMLEN from dir.h
    SCCS-vsn: 5.17

--- sbin/dump/dump.h
+++ sbin/dump/dump.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)dump.h	5.16 (Berkeley) 05/29/91
+ *	@(#)dump.h	5.17 (Berkeley) 07/23/91
  */
 
 #define MAXINOPB	(MAXBSIZE / sizeof(struct dinode))
@@ -142,7 +142,7 @@ struct	fstab	*fstabsearch();	/* search in fs_file and fs_spec */
  *	a linked list, and then (eventually) arrayified.
  */
 struct dumpdates {
-	char	dd_name[MAXNAMLEN+3];
+	char	dd_name[NAME_MAX+3];
 	char	dd_level;
 	time_t	dd_ddate;
 };
