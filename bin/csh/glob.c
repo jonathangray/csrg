3e6f44f1b9fb5a1801267212a8ff59417c79b879
Christos Zoulas <christos>
1991-07-26 14:12:42 -0700

    Bugfix for echo * `echo`, fglag would get overwritten
    SCCS-vsn: 5.24

--- bin/csh/glob.c
+++ bin/csh/glob.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)glob.c	5.23 (Berkeley) 07/19/91";
+static char sccsid[] = "@(#)glob.c	5.24 (Berkeley) 07/26/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -397,23 +397,24 @@ globone(str, action)
     Char   *str;
     int     action;
 {
-
     Char   *v[2], **vl, **vo;
+    int    gflg;
 
     noglob = adrof(STRnoglob) != 0;
     gflag = 0;
     v[0] = str;
     v[1] = 0;
     tglob(v);
-    if (gflag == G_NONE)
+    gflg = gflag;
+    if (gflg == G_NONE)
 	return (strip(Strsave(str)));
 
-    if (gflag & G_CSH) {
+    if (gflg & G_CSH) {
 	/*
 	 * Expand back-quote, tilde and brace
 	 */
 	vo = globexpand(v);
-	if (noglob || (gflag & G_GLOB) == 0) {
+	if (noglob || (gflg & G_GLOB) == 0) {
 	    if (vo[0] == NULL) {
 		xfree((ptr_t) vo);
 		return (Strsave(STRNULL));
@@ -427,13 +428,13 @@ globone(str, action)
 	    }
 	}
     }
-    else if (noglob || (gflag & G_GLOB) == 0)
+    else if (noglob || (gflg & G_GLOB) == 0)
 	return (strip(Strsave(str)));
     else
 	vo = v;
 
     vl = libglob(vo);
-    if (gflag & G_CSH)
+    if (gflg & G_CSH)
 	blkfree(vo);
     if (vl == NULL) {
 	setname(short2str(str));
@@ -457,6 +458,7 @@ globall(v)
     Char  **v;
 {
     Char  **vl, **vo;
+    int   gflg = gflag;
 
     if (!v || !v[0]) {
 	gargv = saveblk(v);
@@ -466,7 +468,7 @@ globall(v)
 
     noglob = adrof(STRnoglob) != 0;
 
-    if (gflag & G_CSH)
+    if (gflg & G_CSH)
 	/*
 	 * Expand back-quote, tilde and brace
 	 */
@@ -474,9 +476,9 @@ globall(v)
     else
 	vl = vo = saveblk(v);
 
-    if (!noglob && (gflag & G_GLOB)) {
+    if (!noglob && (gflg & G_GLOB)) {
 	vl = libglob(vo);
-	if (gflag & G_CSH)
+	if (gflg & G_CSH)
 	    blkfree(vo);
     }
 
