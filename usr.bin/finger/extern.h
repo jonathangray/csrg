eb11f08d4b89dfdc0ae33ee6ccb983125de7dbd8
Keith Bostic <bostic>
1991-07-27 17:59:12 -0700

    finger really didn't need its own hashing scheme
    SCCS-vsn: 5.2

--- usr.bin/finger/extern.h
+++ usr.bin/finger/extern.h
@@ -30,13 +30,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	5.1 (Berkeley) 07/27/91
+ *	@(#)extern.h	5.2 (Berkeley) 07/27/91
  */
 
-extern PERSON *htab[HSIZE];		/* The buckets. */
-extern PERSON *phead, *ptail;		/* The linked list of all people. */
 extern char tbuf[1024];			/* Temp buffer for anybody. */
 extern int entries;			/* Number of people. */
+extern DB *db;				/* Database. */
 
 void	 enter_lastlog __P((PERSON *));
 PERSON	*enter_person __P((struct passwd *));
