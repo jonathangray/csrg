15cc07693aa71298aa4fb8816eeece2d7ad35f43
Christos Zoulas <christos>
1991-07-28 14:14:21 -0700

    Added dowhich() and doprintf() builtins.
    SCCS-vsn: 5.22

--- bin/csh/func.c
+++ bin/csh/func.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)func.c	5.21 (Berkeley) 07/19/91";
+static char sccsid[] = "@(#)func.c	5.22 (Berkeley) 07/28/91";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -1434,3 +1434,21 @@ doeval(v, t)
     if (my_reenter)
 	stderror(ERR_SILENT);
 }
+
+void
+/*ARGSUSED*/
+doprintf(v, t)
+    Char **v;
+    struct command *t;
+{
+    char **c;
+    extern int progprintf __P((int, char **));
+    int ret;
+    
+    ret = progprintf(blklen(v), c = short2blk(v));
+
+    blkfree((Char **) c);
+    if (ret)
+	stderror(ERR_SILENT);
+}
+
