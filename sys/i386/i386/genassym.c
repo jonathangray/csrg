71dfaba5e1bfae9017cd6f4406f649b4be68e0b0
William Jolitz <william>
1991-08-15 17:37:35 -0700

    forget about cmap.h
    SCCS-vsn: 5.12

--- sys/i386/i386/genassym.c
+++ sys/i386/i386/genassym.c
@@ -33,17 +33,16 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)genassym.c	5.11 (Berkeley) 05/10/91
+ *	@(#)genassym.c	5.12 (Berkeley) 08/15/91
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)genassym.c	5.11 (Berkeley) 05/10/91";
+static char sccsid[] = "@(#)genassym.c	5.12 (Berkeley) 08/15/91";
 #endif /* not lint */
 
 #include "sys/param.h"
 #include "sys/buf.h"
 #include "sys/vmmeter.h"
-#include "sys/cmap.h"
 #include "sys/map.h"
 #include "sys/proc.h"
 #include "sys/user.h"
