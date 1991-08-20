1b06ba7528724cd7e020f2a70854cbbaa35ad150
Kirk McKusick <mckusick>
1991-08-20 16:56:14 -0700

    delete data-stream encryption for the foreign sites (Network, Release 2)
    SCCS-vsn: 5.33.1.1

--- usr.bin/rlogin/rlogin.c
+++ usr.bin/rlogin/rlogin.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogin.c	5.33 (Berkeley) 03/01/91";
+static char sccsid[] = "@(#)rlogin.c	5.33.1.1 (Berkeley) 08/20/91";
 #endif /* not lint */
 
 /*
