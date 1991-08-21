9a3aeb17378db38da5c26a3d6e900df82d76a660
Kirk McKusick <mckusick>
1991-08-20 17:10:59 -0700

    delete data-stream encryption for the foreign sites (Network, Release 2)
    SCCS-vsn: 5.53.1.1

--- libexec/rlogind/rlogind.c
+++ libexec/rlogind/rlogind.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogind.c	5.53 (Berkeley) 04/20/91";
+static char sccsid[] = "@(#)rlogind.c	5.53.1.1 (Berkeley) 08/20/91";
 #endif /* not lint */
 
 #ifdef KERBEROS
