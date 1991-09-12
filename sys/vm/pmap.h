3bdc9cdd1b93bb37de92a723094c95023c9661e2
Marc Teitelbaum <marc>
1991-09-12 15:09:38 -0700

    whoopsey daisey, pamp_enter() has another arg ralphie
    SCCS-vsn: 7.8

--- sys/vm/pmap.h
+++ sys/vm/pmap.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pmap.h	7.7 (Berkeley) 09/07/91
+ *	@(#)pmap.h	7.8 (Berkeley) 09/12/91
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -97,7 +97,7 @@ void		pmap_reference __P((pmap_t));
 void		pmap_remove __P((pmap_t, vm_offset_t, vm_offset_t));
 void		pmap_page_protect __P((vm_offset_t, vm_prot_t));
 void		pmap_protect __P((pmap_t, vm_offset_t, vm_offset_t, vm_prot_t));
-void		pmap_enter __P((pmap_t, vm_offset_t, vm_prot_t, boolean_t));
+void		pmap_enter __P((pmap_t, vm_offset_t, vm_offset_t, vm_prot_t, boolean_t));
 void		pmap_change_wiring __P((pmap_t, vm_offset_t, boolean_t));
 vm_offset_t	pmap_extract __P((pmap_t, vm_offset_t));
 void		pmap_copy __P((pmap_t, pmap_t, vm_offset_t, vm_size_t,
