094670ee3d4bdc21748af6fa9cc9a1f14db2197a
Keith Sklower <sklower>
1991-09-17 15:51:16 -0700

    fix copyright date
    SCCS-vsn: 7.2

--- sys/i386/isa/if_ec.c
+++ sys/i386/isa/if_ec.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1982, 1990 The Regents of the University of California.
+ * Copyright (c) 1991 The Regents of the University of California.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_ec.c	7.1 (Berkeley) 09/17/91
+ *	@(#)if_ec.c	7.2 (Berkeley) 09/17/91
  */
 
 #include "ec.h"
