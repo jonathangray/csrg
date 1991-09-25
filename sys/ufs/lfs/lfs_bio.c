ce5c8eb9870b67bafd92f502c824612b9d6dec80
Keith Bostic <bostic>
1991-09-25 14:31:19 -0700

    date and time created 91/09/25 14:31:19 by bostic
    SCCS-vsn: 5.1

---
+++ sys/ufs/lfs/lfs_bio.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)lfs_bio.c	5.1 (Berkeley) 09/25/91
+ */
+
+#include "param.h"
+#include "proc.h"
+#include "buf.h"
+#include "vnode.h"
+#include "specdev.h"
+#include "mount.h"
+#include "trace.h"
+#include "resourcevar.h"
+#include "lfs.h"
+
+/*
+ * lfs_bwrite --
+ *	LFS version of bawrite, bdwrite, bwrite.  Set the delayed write flag
+ *	and use reassignbuf to move the buffer from the clean list to the
+ *	dirty one.  Then unlock the buffer.
+ */
+lfs_bwrite(bp)
+	register BUF *bp;
+{
+	curproc->p_stats->p_ru.ru_oublock++;	/* XXX: no one paid yet */
+	bp->b_flags &= ~(B_READ | B_DONE | B_ERROR | B_DELWRI);
+	bp->b_flags |= B_DELWRI;
+	reassignbuf(bp, bp->b_vp);		/* XXX: do this inline */
+	brelse(bp);
+}
