879b6703c379dee3df806acbf27827343a83d1a9
Eric Allman <eric>
1991-10-02 13:21:19 -0700

    check to see if hostname has changed in thaw(); if it has, ignore the frozen config (with the wrong hostname)
    SCCS-vsn: 5.34

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.33 (Berkeley) 07/26/91";
+static char sccsid[] = "@(#)main.c	5.34 (Berkeley) 10/02/91";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -131,7 +131,7 @@ main(argc, argv, envp)
 	bool queuemode = FALSE;		/* process queue requests */
 	bool nothaw;
 	static bool reenter = FALSE;
-	char jbuf[30];			/* holds MyHostName */
+	char jbuf[60];			/* holds MyHostName */
 	extern bool safefile();
 	extern time_t convtime();
 	extern putheader(), putbody();
@@ -958,10 +958,14 @@ thaw(freezefile)
 	char *freezefile;
 {
 	int f;
+	register char *p;
 	union frz fhdr;
+	char hbuf[60];
 	extern char edata, end;
 	extern char Version[];
 	extern caddr_t brk();
+	extern char **myhostname();
+	extern char *macvalue();
 
 	if (freezefile == NULL)
 		return (FALSE);
@@ -1011,7 +1015,17 @@ thaw(freezefile)
 	}
 
 	(void) close(f);
-	return (TRUE);
+
+	/* verify that the host name was correct on the freeze */
+	(void) myhostname(hbuf, sizeof hbuf);
+	p = macvalue('w', CurEnv);
+	if (p == NULL)
+		p = "";
+	if (strcmp(hbuf, macvalue('w', CurEnv)) == 0)
+		return (TRUE);
+	syslog(LOG_WARNING, "Hostname changed since freeze (%s => %s)",
+		p, hbuf);
+	return (FALSE);
 }
 /*
 **  DISCONNECT -- remove our connection with any foreground process
