f4e73fbb87125a15270e241e577fa9abea12f5f7
Charlie Root <root>
1991-10-09 11:24:15 -0700

    lint, use UNASSIGNED, not -1
    SCCS-vsn: 7.17

--- sys/ufs/lfs/lfs_balloc.c
+++ sys/ufs/lfs/lfs_balloc.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs_balloc.c	7.16 (Berkeley) 10/02/91
+ *	@(#)lfs_balloc.c	7.17 (Berkeley) 10/09/91
  */
 
 #ifdef LOGFS
@@ -59,6 +59,7 @@
  * is done by using the logical block number to index into
  * the array of block pointers described by the dinode.
  */
+int
 lfs_bmap(ip, bn, bnp)
 	register struct inode *ip;
 	register daddr_t bn;
@@ -70,7 +71,7 @@ lfs_bmap(ip, bn, bnp)
 	struct buf *bp;
 	daddr_t *bap, daddr;
 	daddr_t lbn_ind;
-	int i, j, off, sh;
+	int j, off, sh;
 	int error;
 
 printf("lfs_bmap: block number %d, inode %d\n", bn, ip->i_number);
@@ -97,7 +98,7 @@ printf("lfs_bmap: block number %d, inode %d\n", bn, ip->i_number);
 	if (bn < NDADDR) {
 		nb = ip->i_db[bn];
 		if (nb == 0) {
-			*bnp = (daddr_t)-1;
+			*bnp = UNASSIGNED;
 			return (0);
 		}
 		*bnp = nb;
@@ -126,7 +127,7 @@ printf("lfs_bmap: block number %d, inode %d\n", bn, ip->i_number);
 	devvp = VFSTOUFS(vp->v_mount)->um_devvp;
 	for (off = NIADDR - j, bap = ip->i_ib; j <= NIADDR; j++) {
 		if((daddr = bap[off]) == 0) {
-			daddr = (daddr_t)-1;
+			daddr = UNASSIGNED;
 			break;
 		}
 		if (bp)
