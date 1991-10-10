b7b6ef734c395f30389b92de85b89becc5e4431a
Kirk McKusick <mckusick>
1991-10-09 18:34:38 -0700

    keep the data structures consistent
    SCCS-vsn: 5.28

--- sbin/fsck/main.c
+++ sbin/fsck/main.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	5.27 (Berkeley) 08/07/90";
+static char sccsid[] = "@(#)main.c	5.28 (Berkeley) 10/09/91";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -262,6 +262,7 @@ checkfilesys(filesys, mntpt, auxdata, child)
 	}
 	zlnhead = (struct zlncnt *)0;
 	duplist = (struct dups *)0;
+	muldup = (struct dups *)0;
 	inocleanup();
 	if (fsmodified) {
 		(void)time(&sblock.fs_time);
