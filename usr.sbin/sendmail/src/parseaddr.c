66220a7d0583cfa173e40ce93e8ee9744e965946
Eric Allman <eric>
1991-10-20 01:50:03 -0700

    handle backslashes in addresses properly
    SCCS-vsn: 5.15

--- usr.sbin/sendmail/src/parseaddr.c
+++ usr.sbin/sendmail/src/parseaddr.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	5.14 (Berkeley) 10/05/91";
+static char sccsid[] = "@(#)parseaddr.c	5.15 (Berkeley) 10/20/91";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -367,7 +367,8 @@ prescan(addr, delim, pvpbuf)
 					c |= 0200;
 				bslashmode = FALSE;
 			}
-			else if (c == '\\')
+
+			if (c == '\\')
 			{
 				bslashmode = TRUE;
 				c = NOCHAR;
