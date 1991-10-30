4e4a6a221dcc0696bdb2a0c682e2a4d170e4aa15
Steven McCanne <mccanne>
1991-10-29 19:58:55 -0800

    restore sccs keywords
    SCCS-vsn: 7.3

--- sys/net/bpfdesc.h
+++ sys/net/bpfdesc.h
@@ -1,10 +1,10 @@
-/*-
- * Copyright (c) 1991 The Regents of the University of California.
+/*
+ * Copyright (c) 1990, 1991 Regents of the University of California.
  * All rights reserved.
  *
  * This code is derived from the Stanford/CMU enet packet filter,
  * (net/enet.c) distributed as part of 4.3BSD, and code contributed
- * to Berkeley by Steven McCanne and Van Jacobson both of Lawrence 
+ * to Berkeley by Steven McCanne and Van Jacobson both of Lawrence
  * Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)bpfdesc.h	7.1 (Berkeley) 5/7/91
+ *      @(#)bpfdesc.h	7.3 (Berkeley) 10/29/91
  *
  * @(#) $Header: bpfdesc.h,v 1.9 91/10/27 21:22:38 mccanne Exp $ (LBL)
  */
