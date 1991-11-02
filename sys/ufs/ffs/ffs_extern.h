53d52ef6a8b0132f0102fd387db3675fc26b5dfc
Keith Bostic <bostic>
1991-11-01 17:09:38 -0800

    UFS/FFS split for LFS version 1
    SCCS-vsn: 7.1

---
+++ sys/ufs/ffs/ffs_extern.h
@@ -0,0 +1,89 @@
+/*-
+ * Copyright (c) 1991 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)ffs_extern.h	7.1 (Berkeley) 11/01/91
+ */
+
+struct buf;
+struct fs;
+struct inode;
+struct mount;
+struct nameidata;
+struct proc;
+struct statfs;
+struct timeval;
+struct ucred;
+struct uio;
+struct vnode;
+
+__BEGIN_DECLS
+int	ffs_alloc __P((struct inode *, daddr_t, daddr_t, int, daddr_t *));
+int	ffs_balloc __P((struct inode *, daddr_t, int, struct buf **, int));
+int	ffs_blkatoff __P((struct inode *, off_t, char **, struct buf **));
+int	ffs_blkfree __P((struct inode *, daddr_t, off_t));
+daddr_t	ffs_blkpref __P((struct inode *, daddr_t, int, daddr_t *));
+int	ffs_bmap __P((struct inode *, daddr_t, daddr_t *));
+void	ffs_clrblock __P((struct fs *, u_char *, daddr_t));
+void	ffs_fragacct __P((struct fs *, int, long [], int));
+int	ffs_fsync
+	    __P((struct vnode *, int, struct ucred *, int, struct proc *));
+int	ffs_ialloc __P((struct inode *, int, struct ucred *, struct inode **));
+void	ffs_ifree __P((struct inode *, ino_t, int));
+int	ffs_iget __P((struct inode *, ino_t, struct inode **));
+int	ffs_init __P((void));
+int	ffs_isblock __P((struct fs *, u_char *, daddr_t));
+int	ffs_itrunc __P((struct inode *, u_long, int));
+int	ffs_iupdat
+	    __P((struct inode *, struct timeval *, struct timeval *, int));
+int	ffs_mount __P((struct mount *,
+	    char *, caddr_t, struct nameidata *, struct proc *));
+int	ffs_mountfs __P((struct vnode *, struct mount *, struct proc *));
+int	ffs_mountroot __P((void));
+int	ffs_read __P((struct vnode *, struct uio *, int, struct ucred *));
+int	ffs_realloccg
+	    __P((struct inode *, off_t, daddr_t, int, int, struct buf **));
+int	ffs_reclaim __P((struct vnode *));
+void	ffs_setblock __P((struct fs *, u_char *, daddr_t));
+int	ffs_statfs __P((struct mount *, struct statfs *, struct proc *));
+int	ffs_sync __P((struct mount *, int));
+int	ffs_unmount __P((struct mount *, int, struct proc *));
+int	ffs_write __P((struct vnode *, struct uio *, int, struct ucred *));
+
+int	bwrite();		/* FFS needs a bwrite routine.  XXX */
+
+#ifdef DIAGNOSTIC
+void	ffs_checkoverlap __P((struct buf *, struct inode *));
+#endif
+__END_DECLS
+
+extern int inside[], around[];
+extern u_char *fragtbl[];
