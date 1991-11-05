45e638b51503375f4e6a600e3a3b9beac0bbcaef
Kirk McKusick <mckusick>
1991-11-05 14:29:38 -0800

    reorganization to move ufsmount ops to be vnode ops
    SCCS-vsn: 7.15

--- sys/ufs/ffs/ffs_balloc.c
+++ sys/ufs/ffs/ffs_balloc.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ffs_balloc.c	7.14 (Berkeley) 11/01/91
+ *	@(#)ffs_balloc.c	7.15 (Berkeley) 11/05/91
  */
 
 #include <sys/param.h>
@@ -52,11 +52,14 @@
  * is done by using the logical block number to index into
  * the array of block pointers described by the dinode.
  */
-ffs_bmap(ip, bn, bnp)
-	register struct inode *ip;
+int
+ffs_bmap(vp, bn, vpp, bnp)
+	struct vnode *vp;
 	register daddr_t bn;
-	daddr_t	*bnp;
+	struct vnode **vpp;
+	daddr_t *bnp;
 {
+	register struct inode *ip;
 	register struct fs *fs;
 	register daddr_t nb;
 	struct buf *bp;
@@ -64,6 +67,15 @@ ffs_bmap(ip, bn, bnp)
 	int i, j, sh;
 	int error;
 
+	/*
+	 * Check for underlying vnode requests and ensure that logical
+	 * to physical mapping is requested.
+	 */
+	ip = VTOI(vp);
+	if (vpp != NULL)
+		*vpp = ip->i_devvp;
+	if (bnp == NULL)
+		return (0);
 	if (bn < 0)
 		return (EFBIG);
 	fs = ip->i_fs;
