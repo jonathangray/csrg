8dfef5b084d59ce3f11e8f600d7641b3b0aead2b
Chris Torek <torek>
1991-11-19 23:49:06 -0800

    purge seg.h
    SCCS-vsn: 7.23

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)sys_process.c	7.22 (Berkeley) 05/11/91
+ *	@(#)sys_process.c	7.23 (Berkeley) 11/19/91
  */
 
 #define IPCREG
