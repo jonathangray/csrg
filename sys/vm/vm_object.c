d5a1c5f759cd7f7e989c33739cc72f86fdb2d47e
Marc Teitelbaum <marc>
1991-11-20 16:35:01 -0800

    merge in 7.7.1.1 (ralphs 3100 changes)
    SCCS-vsn: 7.9

--- sys/vm/vm_object.c
+++ sys/vm/vm_object.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_object.c	7.7.1.1 (Berkeley) 11/20/91
+ *	@(#)vm_object.c	7.9 (Berkeley) 11/20/91
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
