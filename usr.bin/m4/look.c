83776d29c05b6f93529e74d477675828a45af92d
Keith Bostic <bostic>
1991-11-20 16:27:10 -0800

    add affiliation for Oz
    SCCS-vsn: 5.5

--- usr.bin/m4/look.c
+++ usr.bin/m4/look.c
@@ -3,7 +3,7 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Ozan Yigit.
+ * Ozan Yigit at York University.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)look.c	5.4 (Berkeley) 09/03/91";
+static char sccsid[] = "@(#)look.c	5.5 (Berkeley) 11/20/91";
 #endif /* not lint */
 
 /*
