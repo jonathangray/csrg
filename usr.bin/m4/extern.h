c94ef8a73350570ba4398b70bf0fdccff796d40c
Keith Bostic <bostic>
1991-11-20 16:27:09 -0800

    add affiliation for Oz
    SCCS-vsn: 5.3

--- usr.bin/m4/extern.h
+++ usr.bin/m4/extern.h
@@ -3,7 +3,7 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Ozan Yigit.
+ * Ozan Yigit at York University.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	5.2 (Berkeley) 10/02/91
+ *	@(#)extern.h	5.3 (Berkeley) 11/20/91
  */
 
 char	*basename __P((char *));
