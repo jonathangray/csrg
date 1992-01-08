37d0e54d47e69f66ca7ee7676988fa49287f8d24
Keith Bostic <bostic>
1992-01-08 14:33:02 -0800

    initialize p_augment, p_path
    SCCS-vsn: 5.7

--- usr.bin/whatis/whatis.c
+++ usr.bin/whatis/whatis.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)whatis.c	5.6 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)whatis.c	5.7 (Berkeley) 01/08/92";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -65,6 +65,7 @@ main(argc, argv)
 	char *p_augment, *p_path, **getdb();
 
 	progname = "whatis";
+	p_augment = p_path = NULL;
 	while ((ch = getopt(argc, argv, "M:m:P:")) != EOF)
 		switch((char)ch) {
 		case 'M':
