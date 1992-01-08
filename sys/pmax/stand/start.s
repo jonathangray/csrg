d9f6bf7be9fdb1689cee137406f24a47c30af1a6
Kirk McKusick <mckusick>
1992-01-07 20:45:21 -0800

    contributed by Ralph Campbell
    SCCS-vsn: 7.1

---
+++ sys/pmax/stand/start.s
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 1992 Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Ralph Campbell.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)start.s	7.1 (Berkeley) 01/07/92
+ *
+ * start.s -
+ *
+ *     Contains code that is the first executed at boot time.
+ *
+ *	Copyright (C) 1989 Digital Equipment Corporation.
+ *	Permission to use, copy, modify, and distribute this software and
+ *	its documentation for any purpose and without fee is hereby granted,
+ *	provided that the above copyright notice appears in all copies.  
+ *	Digital Equipment Corporation makes no representations about the
+ *	suitability of this software for any purpose.  It is provided "as is"
+ *	without express or implied warranty.
+ *
+ * from: $Header: /sprite/src/boot/decprom/ds3100.md/RCS/start.s,
+ *	v 1.1 90/02/16 16:19:39 shirriff Exp $ SPRITE (DECWRL)
+ */
+
+#include <regdef.h>
+#include "../include/machConst.h"
+
+/*
+ * Amount to take off of the stack for the benefit of the debugger.
+ */
+#define START_FRAME	((4 * 4) + 4 + 4)
+#define Init	0xbfc00018
+
+    .globl	start
+start:
+    .set noreorder
+    mtc0	zero, MACH_COP_0_STATUS_REG	# Disable interrupts
+    li		sp, MACH_CODE_START - START_FRAME
+    la		gp, _gp
+    sw		zero, START_FRAME - 4(sp)	# Zero out old ra for debugger
+    sw		zero, START_FRAME - 8(sp)	# Zero out old fp for debugger
+    jal		main				# main(argc, argv, envp)
+    nop
+    li		a0, Init			# done, so call prom
+    j		a0
+
+    .globl	Boot_Transfer
+Boot_Transfer:
+    mtc0	zero, MACH_COP_0_STATUS_REG	# Disable interrupts
+    li		sp, MACH_CODE_START - START_FRAME
+    la		gp, _gp
+    sw		zero, START_FRAME - 4(sp)	# Zero out old ra for debugger
+    sw		zero, START_FRAME - 8(sp)	# Zero out old fp for debugger
+    jal		a3				# Jump to routine
+    nop
