4636c663dc60917d01c454e3cbbc5ea55ef60151
Keith Bostic <bostic>
1992-01-08 14:19:17 -0800

    add Keith Gabryelski to contrib listing
    SCCS-vsn: 5.2

--- games/quiz/quiz.h
+++ games/quiz/quiz.h
@@ -3,7 +3,8 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Jim R. Oldroyd at The Instruction Set.
+ * Jim R. Oldroyd at The Instruction Set and Keith Gabryelski at
+ * Commodore Business Machines.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)quiz.h	5.1 (Berkeley) 11/10/91
+ *	@(#)quiz.h	5.2 (Berkeley) 01/08/92
  */
 
 #define	TRUE		1
