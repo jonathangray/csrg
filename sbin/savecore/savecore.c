bc7640b8838b0e08709c9ba779855bbeeb853d75
Keith Bostic <bostic>
1992-01-27 11:22:11 -0800

    statfs.f_fsize =>statfs.f_bsize
    SCCS-vsn: 5.27

--- sbin/savecore/savecore.c
+++ sbin/savecore/savecore.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)savecore.c	5.26 (Berkeley) 04/08/91";
+static char sccsid[] = "@(#)savecore.c	5.27 (Berkeley) 01/27/92";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -354,7 +354,7 @@ check_space()
 		Perror(LOG_ERR, "%s: %m\n", dirname);
 		exit(1);
 	}
- 	spacefree = fsbuf.f_bavail * fsbuf.f_fsize / 1024;
+ 	spacefree = fsbuf.f_bavail * fsbuf.f_bsize / 1024;
 	minfree = read_number("minfree");
  	if (minfree > 0 && spacefree - dumpsize < minfree) {
 		log(LOG_WARNING, "Dump omitted, not enough space on device\n");
