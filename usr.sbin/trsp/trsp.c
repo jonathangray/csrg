eec7c83ea5366478590d741804d1c214d089e4f7
Keith Bostic <bostic>
1992-01-28 14:01:58 -0800

    don't let users create their own symbol table for the running kernel
    SCCS-vsn: 6.9

--- usr.sbin/trsp/trsp.c
+++ usr.sbin/trsp/trsp.c
@@ -41,7 +41,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)trsp.c	6.8 (Berkeley) 03/02/91";
+static char sccsid[] = "@(#)trsp.c	6.9 (Berkeley) 01/28/92";
 #endif /* not lint */
 
 #include <sys/cdefs.h>
@@ -141,11 +141,17 @@ again:
 		system = *argv;
 		argc--, argv++;
 		mask++;
+		/*
+		 * Discard setgid privileges if not the running kernel so that
+		 * bad guys can't print interesting stuff from kernel memory.
+		 */
+		setgid(getgid());
 	}
 	if (argc > 0) {
 		core = *argv;
 		argc--, argv++;
 		mask++;
+		setgid(getgid());
 	}
 	(void) nlist(system, nl);
 	if (nl[0].n_value == 0) {
