43dac1e1a12f208a3e483bafa081c7d625b439dc
Kirk McKusick <mckusick>
1992-02-04 14:29:33 -0800

    new namei calling convention
    SCCS-vsn: 7.62

--- sys/ufs/ffs/ffs_vfsops.c
+++ sys/ufs/ffs/ffs_vfsops.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ffs_vfsops.c	7.61 (Berkeley) 01/13/92
+ *	@(#)ffs_vfsops.c	7.62 (Berkeley) 02/04/92
  */
 
 #include <sys/param.h>
@@ -176,10 +176,8 @@ ffs_mount(mp, path, data, ndp, p)
 	 * Not an update, or updating the name: look up the name
 	 * and verify that it refers to a sensible block device.
 	 */
-	ndp->ni_nameiop = LOOKUP | FOLLOW;
-	ndp->ni_segflg = UIO_USERSPACE;
-	ndp->ni_dirp = args.fspec;
-	if (error = namei(ndp, p))
+	NDINIT(ndp, LOOKUP, FOLLOW, UIO_USERSPACE, args.fspec, p);
+	if (error = namei(ndp))
 		return (error);
 	devvp = ndp->ni_vp;
 
