f6b72aa05560cacf2ce4593b567edd4a1c838fd3
Kirk McKusick <mckusick>
1992-02-05 16:30:18 -0800

    p_regs moves to p_md.md_regs
    SCCS-vsn: 7.2

--- sys/hp300/include/proc.h
+++ sys/hp300/include/proc.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)proc.h	7.1 (Berkeley) 05/15/91
+ *	@(#)proc.h	7.2 (Berkeley) 02/05/92
  */
 
 /*
@@ -38,9 +38,7 @@
  */
 struct mdproc {
 	int	md_flags;		/* machine-dependent flags */
-#ifdef notyet
-	int	*p_regs;		/* registers on current frame */
-#endif
+	int	*md_regs;		/* registers on current frame */
 };
 
 /* md_flags */
