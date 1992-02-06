0e5779877407c84dee5716fabe9f0d3a15480d54
Kirk McKusick <mckusick>
1992-02-05 23:04:15 -0800

    lint
    SCCS-vsn: 7.11

--- sys/ufs/lfs/lfs_segment.c
+++ sys/ufs/lfs/lfs_segment.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs_segment.c	7.10 (Berkeley) 02/04/92
+ *	@(#)lfs_segment.c	7.11 (Berkeley) 02/05/92
  */
 
 #include <sys/param.h>
@@ -168,7 +168,7 @@ lfs_vflush(vp)
 	 */
 	s = splbio();
 	if (--fs->lfs_iocount && (error =
-	    tsleep(&fs->lfs_iocount, PRIBIO + 1, "lfs vflush", 0)))
+	    tsleep((caddr_t)&fs->lfs_iocount, PRIBIO + 1, "lfs vflush", 0)))
 		return (error);
 	splx(s);
 	vfs_unbusy(mp);
@@ -277,8 +277,9 @@ printf("lfs_segment: failed to get vnode (tell Keith)!\n");
 	 */
 	if (do_ckp) {
 		s = splbio();
-		if (--fs->lfs_iocount && (error =
-		    tsleep(&fs->lfs_iocount, PRIBIO + 1, "lfs sync", 0)))
+		if (--fs->lfs_iocount &&
+		    (error = tsleep((caddr_t)&fs->lfs_iocount, PRIBIO + 1,
+		      "lfs sync", 0)))
 			return (error);
 		splx(s);
 		lfs_writesuper(fs, sp);
@@ -288,8 +289,8 @@ printf("lfs_segment: failed to get vnode (tell Keith)!\n");
 	free(sp, M_SEGMENT);
 
 	/* Wake up any cleaning processes waiting on this file system. */
-	wakeup(&fs->lfs_nextseg);
-	wakeup(&lfs_allclean_wakeup);
+	wakeup((caddr_t)&fs->lfs_nextseg);
+	wakeup((caddr_t)&lfs_allclean_wakeup);
 
 	return (0);
 }
@@ -869,7 +870,7 @@ lfs_callback(bp)
 		panic("lfs_callback: zero iocount\n");
 #endif
 	if (--fs->lfs_iocount == 0)
-		wakeup(&fs->lfs_iocount);
+		wakeup((caddr_t)&fs->lfs_iocount);
 
 	brelse(bp);
 }
