6fa8aecb139cdf5da36c1de7ba17dc7b9fd9cbd7
Jan-Simon Pendry <pendry>
1992-02-09 08:48:43 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/amd/nfs_start.c
+++ usr.sbin/amd/amd/nfs_start.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_start.c	5.3 (Berkeley) 05/12/91
+ *	@(#)nfs_start.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: nfs_start.c,v 5.2.1.3 91/05/07 22:18:21 jsp Alpha $
+ * $Id: nfs_start.c,v 5.2.2.1 1992/02/09 15:08:51 jsp beta $
  *
  */
 
