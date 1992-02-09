83a6b2f96a6d5e7e88e4b0c37780347bd1b66a92
Jan-Simon Pendry <pendry>
1992-02-09 08:44:10 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/fsinfo/wr_exportfs.c
+++ usr.sbin/amd/fsinfo/wr_exportfs.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)wr_exportfs.c	5.3 (Berkeley) 05/12/91
+ *	@(#)wr_exportfs.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: wr_exportfs.c,v 5.2.1.3 91/05/07 22:19:13 jsp Alpha $
+ * $Id: wr_exportfs.c,v 5.2.2.1 1992/02/09 15:09:48 jsp beta $
  *
  */
 
