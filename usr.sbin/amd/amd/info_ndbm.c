ab368685b1b57756565805364f42ee056e1ace78
Jan-Simon Pendry <pendry>
1992-02-09 08:48:17 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/amd/info_ndbm.c
+++ usr.sbin/amd/amd/info_ndbm.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)info_ndbm.c	5.3 (Berkeley) 05/12/91
+ *	@(#)info_ndbm.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: info_ndbm.c,v 5.2.1.3 91/05/07 22:18:00 jsp Alpha $
+ * $Id: info_ndbm.c,v 5.2.2.1 1992/02/09 15:08:31 jsp beta $
  *
  */
 
