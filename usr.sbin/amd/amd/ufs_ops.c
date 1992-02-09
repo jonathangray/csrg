d0d4b6fb1f7198a3c417e8d5fe6f2ece473b6cc4
Jan-Simon Pendry <pendry>
1992-02-09 08:49:08 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/amd/ufs_ops.c
+++ usr.sbin/amd/amd/ufs_ops.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ufs_ops.c	5.3 (Berkeley) 05/12/91
+ *	@(#)ufs_ops.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: ufs_ops.c,v 5.2.1.2 91/05/07 22:18:38 jsp Alpha $
+ * $Id: ufs_ops.c,v 5.2.2.1 1992/02/09 15:09:08 jsp beta $
  *
  */
 
