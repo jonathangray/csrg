cdb0321565b57c6d858aeae5f58c9f301fc8ec43
Jan-Simon Pendry <pendry>
1992-02-09 08:48:01 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/amd/clock.c
+++ usr.sbin/amd/amd/clock.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)clock.c	5.3 (Berkeley) 05/12/91
+ *	@(#)clock.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: clock.c,v 5.2.1.5 91/05/07 22:17:49 jsp Alpha $
+ * $Id: clock.c,v 5.2.2.1 1992/02/09 15:08:20 jsp beta $
  *
  */
 
