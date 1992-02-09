a4df97453bb262b54b668caf3a063d7385991e88
Jan-Simon Pendry <pendry>
1992-02-09 08:48:21 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/amd/info_passwd.c
+++ usr.sbin/amd/amd/info_passwd.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)info_passwd.c	5.3 (Berkeley) 05/12/91
+ *	@(#)info_passwd.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: info_passwd.c,v 5.2.1.3 91/05/07 22:18:02 jsp Alpha $
+ * $Id: info_passwd.c,v 5.2.2.1 1992/02/09 15:08:33 jsp beta $
  *
  */
 
