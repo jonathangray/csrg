de59b16b7bd71ff77d8cf8275426f741eb3b7923
Jan-Simon Pendry <pendry>
1992-02-09 08:44:00 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/fsinfo/fsinfo.c
+++ usr.sbin/amd/fsinfo/fsinfo.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fsinfo.c	5.3 (Berkeley) 05/12/91
+ *	@(#)fsinfo.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: fsinfo.c,v 5.2.1.3 91/05/07 22:19:01 jsp Alpha $
+ * $Id: fsinfo.c,v 5.2.2.1 1992/02/09 15:09:33 jsp beta $
  *
  */
 
