660beb6e2c40efb5db9dc0f601306ff6099f7213
Jan-Simon Pendry <pendry>
1992-02-09 08:45:12 -0800

    merge in new release
    SCCS-vsn: 5.4

--- usr.sbin/amd/rpcx/amq_clnt.c
+++ usr.sbin/amd/rpcx/amq_clnt.c
@@ -35,9 +35,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)amq_clnt.c	5.3 (Berkeley) 05/12/91
+ *	@(#)amq_clnt.c	5.4 (Berkeley) 02/09/92
  *
- * $Id: amq_clnt.c,v 5.2.1.3 91/05/07 22:18:52 jsp Alpha $
+ * $Id: amq_clnt.c,v 5.2.2.1 1992/02/09 15:09:24 jsp beta $
  *
  */
 
