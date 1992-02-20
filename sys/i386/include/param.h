7b0a22e12e26e2d4ff75eefd6b98d8a2de07f54e
Keith Bostic <bostic>
1992-02-19 17:45:51 -0800

    rip away illusion of portability in ALIGN
    SCCS-vsn: 5.9

--- sys/i386/include/param.h
+++ sys/i386/include/param.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)param.h	5.8 (Berkeley) 06/28/91
+ *	@(#)param.h	5.9 (Berkeley) 02/19/92
  */
 
 /*
@@ -43,11 +43,11 @@
 #define MACHINE "i386"
 
 /*
- * Round p (pointer or byte index) up to a correctly-aligned value
- * for all data types (int, long, ...).   The result is u_int and
- * must be cast to any desired pointer type.
+ * Round p (pointer or byte index) up to a correctly-aligned value for all
+ * data types (int, long, ...).   The result is u_int and must be cast to
+ * any desired pointer type.
  */
-#define	ALIGN(p)	(((u_int)(p) + (sizeof(int) - 1)) &~ (sizeof(int) - 1))
+#define	ALIGN(p)	(((u_int)(p) + 3) &~ 3)
 
 #define	NBPG		4096		/* bytes/page */
 #define	PGOFSET		(NBPG-1)	/* byte offset into page */
