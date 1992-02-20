a303a67c252fd3564c3fe98252d4758966e0a378
Keith Bostic <bostic>
1992-02-19 17:43:49 -0800

    rip away illusion of portability in ALIGN
    SCCS-vsn: 7.21

--- sys/vax/include/param.h
+++ sys/vax/include/param.h
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)param.h	7.20 (Berkeley) 06/08/91
+ *	@(#)param.h	7.21 (Berkeley) 02/19/92
  */
 
 /*
@@ -15,11 +15,11 @@
 #define	MACHINE	"vax"
 
 /*
- * Round p (pointer or byte index) up to a correctly-aligned value
- * for all data types (int, long, ...).   The result is u_int and
- * must be cast to any desired pointer type.
+ * Round p (pointer or byte index) up to a correctly-aligned value for all
+ * data types (int, long, ...).   The result is u_int and must be cast to
+ * any desired pointer type.
  */
-#define	ALIGN(p)	(((u_int)(p) + (sizeof(int) - 1)) &~ (sizeof(int) - 1))
+#define	ALIGN(p)	(((u_int)(p) + 3) &~ 3)
 
 #define	KERNBASE	0x80000000	/* start of kernel virtual */
 #define	BTOPKERNBASE	((u_long)KERNBASE >> PGSHIFT)
