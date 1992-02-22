baeeedf4a3fc1c2cf06fcf72ef95bd5a23d8f60a
Eric Allman <eric>
1992-02-21 20:57:47 -0800

    from Bryan Costales: several smallish changes, mostly to clean up debugging output.  The major change is that test mode (-bt) doesn't automatically run through ruleset three any more.
    SCCS-vsn: 5.17

--- usr.sbin/sendmail/src/parseaddr.c
+++ usr.sbin/sendmail/src/parseaddr.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	5.16 (Berkeley) 11/15/91";
+static char sccsid[] = "@(#)parseaddr.c	5.17 (Berkeley) 02/21/92";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -577,6 +577,8 @@ rewrite(pvp, ruleset)
 
 	for (rwr = RewriteRules[ruleset]; rwr != NULL; )
 	{
+		int loopcount = 0;
+
 		if (tTd(21, 12))
 		{
 			printf("-----trying rule:");
@@ -589,12 +591,17 @@ rewrite(pvp, ruleset)
 		avp = pvp;
 		while ((ap = *avp) != NULL || *rvp != NULL)
 		{
+			if (++loopcount > 100)
+			{
+				syserr("Infinite loop in ruleset %d", ruleset);
+				break;
+			}
 			rp = *rvp;
 			if (tTd(21, 35))
 			{
-				printf("ap=");
+				printf("operator=");
 				xputs(ap);
-				printf(", rp=");
+				printf(", token=");
 				xputs(rp);
 				printf("\n");
 			}
