7ac50c2cae6ead60e1c663559261c260cd73f1a3
Keith Bostic <bostic>
1992-02-29 12:57:08 -0800

    DEFS.h needed for ASMSTR macro
    SCCS-vsn: 5.2

--- lib/libc/mips/string/bcmp.s
+++ lib/libc/mips/string/bcmp.s
@@ -34,11 +34,11 @@
  * SUCH DAMAGE.
  */
 
-#if defined(LIBC_SCCS) && !defined(lint)
-	ASMSTR("@(#)bcmp.s	5.1 (Berkeley) 02/29/92")
-#endif /* LIBC_SCCS and not lint */
-
 #include "DEFS.h"
+
+#if defined(LIBC_SCCS) && !defined(lint)
+	ASMSTR("@(#)bcmp.s	5.2 (Berkeley) 02/29/92")
+#endif /* LIBC_SCCS and not lint */
 
 /* bcmp(s1, s2, n) */
 
