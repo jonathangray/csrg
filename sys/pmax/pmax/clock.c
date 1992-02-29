44ae5c98080ec7647795088463f31f9f0ea2e57e
Ralph Campbell <ralph>
1992-02-29 11:30:03 -0800

    block interrupts when updating the clock chip.
    SCCS-vsn: 7.2

--- sys/pmax/pmax/clock.c
+++ sys/pmax/pmax/clock.c
@@ -37,7 +37,7 @@
  *
  * from: Utah $Hdr: clock.c 1.18 91/01/21$
  *
- *	@(#)clock.c	7.1 (Berkeley) 01/07/92
+ *	@(#)clock.c	7.2 (Berkeley) 02/29/92
  */
 
 #include "param.h"
@@ -104,9 +104,9 @@ inittodr(base)
 		badbase = 1;
 	}
 
-	s = splclock();
 	c = (volatile struct chiptime *)MACH_CLOCK_ADDR;
 	/* don't read clock registers while they are being updated */
+	s = splclock();
 	while ((c->rega & REGA_UIP) == 1)
 		;
 	sec = c->sec;
@@ -116,6 +116,7 @@ inittodr(base)
 	mon = c->mon;
 	year = c->year + 19;
 	splx(s);
+
 	/* simple sanity checks */
 	if (year < 70 || mon < 1 || mon > 12 || day < 1 || day > 31 ||
 	    hour > 23 || min > 59 || sec > 59) {
@@ -165,6 +166,7 @@ resettodr()
 {
 	register volatile struct chiptime *c;
 	register int t, t2, t3;
+	int s;
 
 	/* compute the year */
 	t2 = time.tv_sec / SECDAY;
@@ -174,7 +176,9 @@ resettodr()
 		t++;
 		t2 -= LEAPYEAR(t) ? 366 : 365;
 	}
+
 	c = (volatile struct chiptime *)MACH_CLOCK_ADDR;
+	s = splclock();
 	c->regb |= REGB_SET_TIME;
 	c->year = t - 19;
 
@@ -198,4 +202,6 @@ resettodr()
 	c->min = t / 60;
 	c->sec = t % 60;
 	c->regb &= ~REGB_SET_TIME;
+	MachEmptyWriteBuffer();
+	splx(s);
 }
