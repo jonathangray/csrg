ea30823bfc91f6a9cc7a20267bc75d19cb4d9811
Ralph Campbell <ralph>
1992-02-29 13:49:34 -0800

    fix test for correct address protection
    SCCS-vsn: 7.2

--- sys/pmax/pmax/mem.c
+++ sys/pmax/pmax/mem.c
@@ -37,7 +37,7 @@
  *
  * from: Utah $Hdr: mem.c 1.14 90/10/12$
  *
- *	@(#)mem.c	7.1 (Berkeley) 01/07/92
+ *	@(#)mem.c	7.2 (Berkeley) 02/29/92
  */
 
 /*
@@ -68,7 +68,7 @@ mmrw(dev, uio, flags)
 	register struct iovec *iov;
 	int error = 0;
 	caddr_t zbuf = NULL;
-	extern u_int lowram;
+	extern vm_offset_t avail_end;
 
 	while (uio->uio_resid > 0 && error == 0) {
 		iov = uio->uio_iov;
@@ -93,12 +93,18 @@ mmrw(dev, uio, flags)
 
 /* minor device 1 is kernel memory */
 		case 1:
-			c = iov->iov_len;
-			if (!kernacc((caddr_t)uio->uio_offset, c,
-			    uio->uio_rw == UIO_READ ? B_READ : B_WRITE))
+			if (uio->uio_offset < MACH_CACHED_MEMORY_ADDR)
 				return (EFAULT);
-			error = uiomove((caddr_t)uio->uio_offset, (int)c, uio);
-			continue;
+			c = iov->iov_len;
+			if (uio->uio_offset + c <= avail_end ||
+			    uio->uio_offset >= MACH_KSEG2_ADDR &&
+			    kernacc((caddr_t)uio->uio_offset, c,
+			    uio->uio_rw == UIO_READ ? B_READ : B_WRITE)) {
+				error = uiomove((caddr_t)uio->uio_offset,
+					(int)c, uio);
+				continue;
+			}
+			return (EFAULT);
 
 /* minor device 2 is EOF/RATHOLE */
 		case 2:
