782974db495bb03e5c51fec18c02db71102c778d
Keith Bostic <bostic>
1992-03-04 09:14:16 -0800

    use stdin instead of another file descriptor; make -[cnb] -r work on pipes
    SCCS-vsn: 5.5

--- usr.bin/tail/reverse.c
+++ usr.bin/tail/reverse.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)reverse.c	5.4 (Berkeley) 03/03/92";
+static char sccsid[] = "@(#)reverse.c	5.5 (Berkeley) 03/04/92";
 #endif /* not lint */
 
 #include <sys/param.h>
