530a601918b48e9c0f935d8a3beb2709561ffd38
Chris Torek <torek>
1992-03-15 00:36:10 -0800

    gcc lint
    SCCS-vsn: 7.21

--- sys/netinet/if_ether.c
+++ sys/netinet/if_ether.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_ether.c	7.20 (Berkeley) 02/25/92
+ *	@(#)if_ether.c	7.21 (Berkeley) 03/15/92
  */
 
 /*
@@ -538,6 +538,7 @@ int arpdebug = 0;
 struct llinfo_arp *
 arplookup(addr, create, proxy)
 	u_long addr;
+	int create, proxy;
 {
 	register struct rtentry *rt;
 	static struct sockaddr_inarp sin = {sizeof(sin), AF_INET };
