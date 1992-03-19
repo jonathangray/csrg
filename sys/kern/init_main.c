fd59c48c60c00720d9a7f5a99e5dcc27b47b2040
Keith Sklower <sklower>
1992-03-18 18:05:41 -0800

    define boottime
    SCCS-vsn: 7.47

--- sys/kern/init_main.c
+++ sys/kern/init_main.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)init_main.c	7.46 (Berkeley) 02/14/92
+ *	@(#)init_main.c	7.47 (Berkeley) 03/18/92
  */
 
 #include "param.h"
@@ -79,6 +79,7 @@ extern	int (*mountroot)();
 
 struct	vnode *rootvp, *swapdev_vp;
 int	boothowto;
+struct	timeval boottime;
 
 /*
  * System startup; initialize the world, create process 0,
