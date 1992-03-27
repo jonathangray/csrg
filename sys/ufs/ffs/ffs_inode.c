07da2939944052d222748cf83d0cf2495a032f61
Kirk McKusick <mckusick>
1992-03-26 18:39:06 -0800

    ensure proper types for local variables and parameters
    SCCS-vsn: 7.47

--- sys/ufs/ffs/ffs_inode.c
+++ sys/ufs/ffs/ffs_inode.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ffs_inode.c	7.46 (Berkeley) 03/22/92
+ *	@(#)ffs_inode.c	7.47 (Berkeley) 03/26/92
  */
 
 #include <sys/param.h>
