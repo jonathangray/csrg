a6f1cdfcb43a758452ffa63b3c273dbdb67d4615
Kirk McKusick <mckusick>
1992-03-26 18:39:09 -0800

    ensure proper types for local variables and parameters
    SCCS-vsn: 7.5

--- sys/ufs/ffs/ffs_extern.h
+++ sys/ufs/ffs/ffs_extern.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ffs_extern.h	7.4 (Berkeley) 01/13/92
+ *	@(#)ffs_extern.h	7.5 (Berkeley) 03/26/92
  */
 
 struct buf;
