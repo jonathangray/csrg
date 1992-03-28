c3f6c1a3b83208d71e378cf7b39bd1f581cc475f
Kirk McKusick <mckusick>
1992-03-27 17:39:19 -0800

    define doff_t for size of directory offsets
    SCCS-vsn: 7.4

--- sys/ufs/ufs/ufs_extern.h
+++ sys/ufs/ufs/ufs_extern.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ufs_extern.h	7.3 (Berkeley) 01/22/92
+ *	@(#)ufs_extern.h	7.4 (Berkeley) 03/27/92
  */
 
 struct buf;
@@ -66,7 +66,7 @@ int	 ufs_chown __P((struct vnode *, u_int, u_int, struct proc *));
 int	 ufs_close __P((struct vnode *, int, struct ucred *, struct proc *));
 int	 ufs_create __P((struct vnode *, struct vnode **,
 			 struct componentname *, struct vattr *));
-void	 ufs_dirbad __P((struct inode *, off_t, char *));
+void	 ufs_dirbad __P((struct inode *, doff_t, char *));
 int	 ufs_dirbadentry __P((struct direct *, int));
 int	 ufs_dirempty __P((struct inode *, ino_t, struct ucred *));
 int	 ufs_direnter __P((struct inode *, struct vnode *,struct componentname *));
