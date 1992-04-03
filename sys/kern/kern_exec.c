0bae4accafdb53ade6e4d22f6fa23227feba53f0
Kirk McKusick <mckusick>
1992-04-03 14:57:21 -0800

    update from Chris Torek to add argument pointer at known location
    SCCS-vsn: 7.54

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)kern_exec.c	7.53 (Berkeley) 03/03/92
+ *	@(#)kern_exec.c	7.54 (Berkeley) 04/03/92
  */
 
 #include "param.h"
@@ -86,6 +86,7 @@ execve(p, uap, retval)
 	struct hpux_exec hhead;
 #endif
 	struct nameidata nd;
+	struct ps_strings ps;
 #ifdef SECSIZE
 	extern long argdbsize;			/* XXX */
 #endif SECSIZE
@@ -377,8 +378,11 @@ execve(p, uap, retval)
 	 * and character strings.  `nc' is currently just the number
 	 * of characters of arg and env strings.
 	 *
-	 * nc = size of signal code + 4 bytes of NULL pointer + nc,
-	 *	rounded to nearest integer;
+	 * nc = size of ps_strings structure +
+	 *	size of signal code +
+	 *	4 bytes of NULL pointer +
+	 *	nc,
+	 * rounded to nearest integer;
 	 * ucp = USRSTACK - nc;		[user characters pointer]
 	 * apsize = padding (if any) +
 	 *	4 bytes of NULL pointer +
@@ -390,7 +394,7 @@ execve(p, uap, retval)
 	 * ap = ucp - apsize;	[user address of argc]
 	 * ssize = ssize + nc + machine-dependent space;
 	 */
-	nc = (szsigcode + 4 + nc + NBPW-1) & ~(NBPW - 1);
+	nc = (sizeof(ps) + szsigcode + 4 + nc + NBPW-1) & ~(NBPW - 1);
 #ifdef sparc
 	ucp = USRSTACK;
 	ssize = (nc + (na + 3) * NBPW + 7) & ~7;
@@ -427,11 +431,15 @@ execve(p, uap, retval)
 	nc = 0;
 	cp = (char *) execargs;
 	cc = NCARGS;
+	ps.ps_argvstr = (char *)ucp;	/* first argv string */
+	ps.ps_nargvstr = na - ne;	/* argc */
 	for (;;) {
 		ap += NBPW;
 		if (na == ne) {
 			(void) suword((caddr_t)ap, 0);
 			ap += NBPW;
+			ps.ps_envstr = (char *)ucp;
+			ps.ps_nenvstr = ne;
 		}
 		if (--na < 0)
 			break;
@@ -448,6 +456,7 @@ execve(p, uap, retval)
 			panic("exec: EFAULT");
 	}
 	(void) suword((caddr_t)ap, 0);
+	(void) copyout((caddr_t)&ps, (caddr_t)PS_STRINGS, sizeof(ps));
 
 	execsigs(p);
 
@@ -472,7 +481,7 @@ execve(p, uap, retval)
 	/*
 	 * Install sigcode at top of user stack.
 	 */
-	copyout((caddr_t)sigcode, (caddr_t)(USRSTACK - szsigcode), szsigcode);
+	copyout((caddr_t)sigcode, (caddr_t)PS_STRINGS - szsigcode, szsigcode);
 #endif
 	/*
 	 * Remember file name for accounting.
