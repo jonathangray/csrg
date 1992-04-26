791e42732bc101d086189d2f3854a6f1297bb42d
Ralph Campbell <ralph>
1992-04-25 20:22:54 -0700

    deleted sys/cdefs.h since its included by db.h
    SCCS-vsn: 5.8

--- include/ndbm.h
+++ include/ndbm.h
@@ -33,13 +33,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ndbm.h	5.7 (Berkeley) 09/11/91
+ *	@(#)ndbm.h	5.8 (Berkeley) 04/25/92
  */
 
 #ifndef _NDBM_H_
 #define	_NDBM_H_
 
-#include <sys/cdefs.h>
 #include <db.h>
 
 /* Map dbm interface onto db(3). */
