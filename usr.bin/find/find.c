4daf2806a6522485814fc593891a091795df211e
Keith Bostic <bostic>
1992-05-05 10:55:09 -0700

    flush stdout so error messages in the right place
    SCCS-vsn: 5.5

--- usr.bin/find/find.c
+++ usr.bin/find/find.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)find.c	5.4 (Berkeley) 12/27/91";
+static char sccsid[] = "@(#)find.c	5.5 (Berkeley) 05/05/92";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -161,6 +161,7 @@ find_execute(plan, paths)
 		case FTS_DNR:
 		case FTS_ERR:
 		case FTS_NS:
+			(void)fflush(stdout);
 			(void)fprintf(stderr, "find: %s: %s\n", 
 			    entry->fts_path, strerror(errno));
 			continue;
@@ -174,6 +175,7 @@ find_execute(plan, paths)
 
 #define	BADCH	" \t\n\\'\""
 		if (isxargs && strpbrk(entry->fts_path, BADCH)) {
+			(void)fflush(stdout);
 			(void)fprintf(stderr,
 			    "find: illegal path: %s\n", entry->fts_path);
 			continue;
