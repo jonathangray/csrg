30a9408ffa7992e8285544b1ce30a751aa3481da
Kirk McKusick <mckusick>
1992-05-06 11:08:14 -0700

    AIX interoperability
    SCCS-vsn: 7.31

--- sys/nfs/nfs_syscalls.c
+++ sys/nfs/nfs_syscalls.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_syscalls.c	7.30.1.1 (Berkeley) 03/16/92
+ *	@(#)nfs_syscalls.c	7.31 (Berkeley) 05/06/92
  */
 
 #include "param.h"
@@ -448,7 +448,6 @@ nfssvc_nfsd(nsd, argp, p)
 			solockp = &slp->ns_solock;
 		else
 			solockp = (int *)0;
-		nd->nd_repstat = 0;
 		/*
 		 * nam == nam2 for connectionless protocols such as UDP
 		 * nam2 == NULL for connection based protocols to disable
