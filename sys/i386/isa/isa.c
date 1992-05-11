c88a167f6427caefc9345ff6b04c4b57c588ce1f
Keith Bostic <bostic>
1992-05-11 16:39:57 -0700

    pmap_kernel() -> kernel_pmap; from Pace Willison
    SCCS-vsn: 7.3

--- sys/i386/isa/isa.c
+++ sys/i386/isa/isa.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)isa.c	7.2 (Berkeley) 05/13/91
+ *	@(#)isa.c	7.3 (Berkeley) 05/11/92
  */
 
 /*
@@ -50,6 +50,7 @@
 #include "machine/segments.h"
 #include "i386/isa/isa_device.h"
 #include "i386/isa/icu.h"
+#include "vm/vm.h"
 
 /*
  * Configure all ISA devices
@@ -209,7 +210,7 @@ int nbytes;
 	if (raw) phys = (unsigned long) bounce;
 	else phys = addr;
 	/* translate to physical */
-	phys = pmap_extract(pmap_kernel(), (vm_offset_t)phys);
+	phys = pmap_extract(kernel_pmap, (vm_offset_t)phys);
 	outb(0x4,phys & 0xFF);
 	outb(0x4,(phys>>8) & 0xFF);
 	outb(0x81,(phys>>16) & 0xFF);
