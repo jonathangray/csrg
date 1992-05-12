7a6ed1cf5b4aa42a42768e5de3ec9ca317918083
Keith Bostic <bostic>
1992-05-12 15:19:57 -0700

    put on a Berkeley copyright notice -- original didn't have a GNU copyright, I'll add one as soon as I find out which one to add date and time created 92/05/12 15:19:57 by bostic
    SCCS-vsn: 5.1

---
+++ lib/libc/quad/ashrdi3.c
@@ -0,0 +1,70 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)ashrdi3.c	5.1 (Berkeley) 05/12/92";
+#endif /* LIBC_SCCS and not lint */
+
+#include "longlong.h"
+
+long long
+__ashrdi3 (u, b1)
+     long long u;
+     long long b1;
+{
+  long_long w;
+  unsigned long carries;
+  int bm;
+  long_long uu;
+  int b = b1;
+
+  if (b == 0)
+    return u;
+
+  uu.ll = u;
+
+  bm = (sizeof (int) * BITS_PER_UNIT) - b;
+  if (bm <= 0)
+    {
+      w.s.high = uu.s.high >> 31; /* just to make w.s.high 1..1 or 0..0 */
+      w.s.low = uu.s.high >> -bm;
+    }
+  else
+    {
+      carries = (unsigned long)uu.s.high << bm;
+      w.s.high = uu.s.high >> b;
+      w.s.low = ((unsigned long)uu.s.low >> b) | carries;
+    }
+
+  return w.ll;
+}
