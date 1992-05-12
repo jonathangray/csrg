0e64b68218efe23c7d889ddf727f6bffdb099034
Keith Bostic <bostic>
1992-05-12 15:20:06 -0700

    put on a Berkeley copyright notice -- original didn't have a GNU copyright, I'll add one as soon as I find out which one to add date and time created 92/05/12 15:20:06 by bostic
    SCCS-vsn: 5.1

---
+++ lib/libc/quad/negdi2.c
@@ -0,0 +1,80 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)negdi2.c	5.1 (Berkeley) 05/12/92";
+#endif /* LIBC_SCCS and not lint */
+
+#include "longlong.h"
+
+static int bneg ();
+
+long long 
+__negdi2 (u)
+     long long u;
+{
+  unsigned long a[2], b[2];
+  long_long w;
+  long_long uu;
+
+  uu.ll = u;
+
+  a[HIGH] = uu.s.high;
+  a[LOW] = uu.s.low;
+
+  bneg (a, b, sizeof b);
+
+  w.s.high = b[HIGH];
+  w.s.low = b[LOW];
+  return w.ll;
+}
+
+static int
+bneg (a, b, n)
+     unsigned short *a, *b;
+     size_t n;
+{
+  signed long acc;
+  int i;
+
+  n /= sizeof (short);
+
+  acc = 0;
+  for (i = little_end (n); is_not_msd (i, n); i = next_msd (i))
+    {
+      acc -= a[i];
+      b[i] = acc & low16;
+      acc = acc >> 16;
+    }
+  return acc;
+}
