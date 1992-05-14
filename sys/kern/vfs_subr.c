accb5ae8ad17419e8cbd54eec3131460c3e6f849
John Heidemann <heideman>
1992-05-14 10:22:06 -0700

    vfs initialization code moved to vfs_init.c
    SCCS-vsn: 7.73

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vfs_subr.c	7.73 (Berkeley) 05/13/92
+ *	@(#)vfs_subr.c	7.73 (Berkeley) 05/14/92
  */
 
 /*
@@ -189,34 +189,12 @@ void vattr_null(vap)
 /*
  * Routines having to do with the management of the vnode table.
  */
-struct vnode *vfreeh, **vfreet;
+extern struct vnode *vfreeh, **vfreet;
 extern struct vnodeops dead_vnodeops;
 extern struct vnodeops spec_vnodeops;
 extern void vclean();
 long numvnodes;
-struct vattr va_null;
-
-/*
- * Initialize the vnode structures and initialize each file system type.
- */
-vfsinit()
-{
-	struct vfsops **vfsp;
-
-	/*
-	 * Initialize the vnode name cache
-	 */
-	nchinit();
-	/*
-	 * Initialize each file system type.
-	 */
-	vattr_null(&va_null);
-	for (vfsp = &vfssw[0]; vfsp <= &vfssw[MOUNT_MAXTYPE]; vfsp++) {
-		if (*vfsp == NULL)
-			continue;
-		(*(*vfsp)->vfs_init)();
-	}
-}
+extern struct vattr va_null;
 
 /*
  * Return the next vnode from the free list.
