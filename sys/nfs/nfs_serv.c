f2a303767ad8edc8a3768941e21515711c058675
John Heidemann <heideman>
1992-05-14 11:23:40 -0700

    vnode interface conversion
    SCCS-vsn: 7.51

--- sys/nfs/nfs_serv.c
+++ sys/nfs/nfs_serv.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_serv.c	7.51 (Berkeley) 05/13/92
+ *	@(#)nfs_serv.c	7.51 (Berkeley) 05/14/92
  */
 
 /*
