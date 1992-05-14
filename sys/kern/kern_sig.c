4cbb3abe82a66d1f44c1530d8ce71b111925ce45
John Heidemann <heideman>
1992-05-14 10:34:07 -0700

    vnode interface conversion
    SCCS-vsn: 7.45

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kern_sig.c	7.45 (Berkeley) 05/13/92
+ *	@(#)kern_sig.c	7.45 (Berkeley) 05/14/92
  */
 
 #define	SIGPROP		/* include signal properties table */
