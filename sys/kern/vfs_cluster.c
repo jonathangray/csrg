9ecfeca240f7a8deac6fe01458ca8c2be69f8e13
John Heidemann <heideman>
1992-05-13 18:32:53 -0700

    vnode interface conversion
    SCCS-vsn: 7.47

--- sys/kern/vfs_cluster.c
+++ sys/kern/vfs_cluster.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)vfs_cluster.c	7.46 (Berkeley) 02/05/92
+ *	@(#)vfs_cluster.c	7.47 (Berkeley) 05/13/92
  */
 
 #include <sys/param.h>
