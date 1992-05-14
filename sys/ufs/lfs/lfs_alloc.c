96d74f0c87f72eb7fedb1a8cc54ad2491703bef4
John Heidemann <heideman>
1992-05-14 10:28:55 -0700

    vnode interface conversion
    SCCS-vsn: 7.43

--- sys/ufs/lfs/lfs_alloc.c
+++ sys/ufs/lfs/lfs_alloc.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs_alloc.c	7.43 (Berkeley) 05/13/92
+ *	@(#)lfs_alloc.c	7.43 (Berkeley) 05/14/92
  */
 
 #include <sys/param.h>
