fff6ade88d931094be12d439f73f00fd2b26208e
John Heidemann <heideman>
1992-05-14 10:29:29 -0700

    vnode interface conversion
    SCCS-vsn: 7.63

--- sys/ufs/lfs/lfs_inode.c
+++ sys/ufs/lfs/lfs_inode.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs_inode.c	7.63 (Berkeley) 05/13/92
+ *	@(#)lfs_inode.c	7.63 (Berkeley) 05/14/92
  */
 
 #include <sys/param.h>
