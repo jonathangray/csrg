44080f45f30d54baced9e38f298e44d27232db8e
John Heidemann <heideman>
1992-05-14 10:31:59 -0700

    vnode interface conversion
    SCCS-vsn: 7.13

--- sys/miscfs/fifofs/fifo_vnops.c
+++ sys/miscfs/fifofs/fifo_vnops.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fifo_vnops.c	7.13 (Berkeley) 05/13/92
+ *	@(#)fifo_vnops.c	7.13 (Berkeley) 05/14/92
  */
 
 #include "param.h"
