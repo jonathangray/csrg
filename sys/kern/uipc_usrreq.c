8255121ba198c5debe564a5ba9eb426ea92e0873
John Heidemann <heideman>
1992-05-13 18:32:35 -0700

    vnode interface conversion
    SCCS-vsn: 7.34

--- sys/kern/uipc_usrreq.c
+++ sys/kern/uipc_usrreq.c
@@ -28,7 +28,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)uipc_usrreq.c	7.33 (Berkeley) 05/13/92
+ *	@(#)uipc_usrreq.c	7.34 (Berkeley) 05/13/92
  */
 
 #include "param.h"
