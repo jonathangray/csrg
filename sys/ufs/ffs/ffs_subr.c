64e04fa381fd3da8aabbfd20ee815cf38fb40710
John Heidemann <heideman>
1992-05-14 10:26:05 -0700

    vnode interface conversion
    SCCS-vsn: 7.18

--- sys/ufs/ffs/ffs_subr.c
+++ sys/ufs/ffs/ffs_subr.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ffs_subr.c	7.18 (Berkeley) 05/13/92
+ *	@(#)ffs_subr.c	7.18 (Berkeley) 05/14/92
  */
 
 #include <sys/param.h>
