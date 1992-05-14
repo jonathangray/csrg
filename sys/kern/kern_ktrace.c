9a75d6b66ffb0a857835646498365fba454a1dbc
John Heidemann <heideman>
1992-05-14 10:33:35 -0700

    vnode interface conversion
    SCCS-vsn: 7.17

--- sys/kern/kern_ktrace.c
+++ sys/kern/kern_ktrace.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kern_ktrace.c	7.17 (Berkeley) 05/13/92
+ *	@(#)kern_ktrace.c	7.17 (Berkeley) 05/14/92
  */
 
 #ifdef KTRACE
