158301eb4908ee38156c742f402085b6c6702428
John Heidemann <heideman>
1992-05-14 10:38:50 -0700

    vnode interface conversion
    SCCS-vsn: 7.24

--- sys/nfs/nfs_bio.c
+++ sys/nfs/nfs_bio.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_bio.c	7.24 (Berkeley) 05/13/92
+ *	@(#)nfs_bio.c	7.24 (Berkeley) 05/14/92
  */
 
 #include <sys/param.h>
