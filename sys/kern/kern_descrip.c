d5c0f77e5dc18d61d6aaa5065bc8dafc0130d392
John Heidemann <heideman>
1992-05-14 10:32:39 -0700

    vnode interface conversion
    SCCS-vsn: 7.32

--- sys/kern/kern_descrip.c
+++ sys/kern/kern_descrip.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kern_descrip.c	7.32 (Berkeley) 05/13/92
+ *	@(#)kern_descrip.c	7.32 (Berkeley) 05/14/92
  */
 
 #include "param.h"
