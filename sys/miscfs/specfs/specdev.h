261bb5d7422457209fef5927d05f8d27defdce3d
John Heidemann <heideman>
1992-05-14 11:27:32 -0700

    vnode interface conversion
    SCCS-vsn: 7.9

--- sys/miscfs/specfs/specdev.h
+++ sys/miscfs/specfs/specdev.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)specdev.h	7.9 (Berkeley) 05/13/92
+ *	@(#)specdev.h	7.9 (Berkeley) 05/14/92
  */
 
 /*
