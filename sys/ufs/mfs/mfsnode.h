3635876c68ec796bfa4dfd884feb0f8ced54a9eb
John Heidemann <heideman>
1992-05-14 11:28:00 -0700

    vnode interface conversion
    SCCS-vsn: 7.10

--- sys/ufs/mfs/mfsnode.h
+++ sys/ufs/mfs/mfsnode.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mfsnode.h	7.10 (Berkeley) 05/13/92
+ *	@(#)mfsnode.h	7.10 (Berkeley) 05/14/92
  */
 
 /*
