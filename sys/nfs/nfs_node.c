1447abb530d417d37bfcf23deca5395e42f4683d
John Heidemann <heideman>
1992-05-14 10:38:59 -0700

    vnode interface conversion
    SCCS-vsn: 7.38

--- sys/nfs/nfs_node.c
+++ sys/nfs/nfs_node.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_node.c	7.38 (Berkeley) 05/13/92
+ *	@(#)nfs_node.c	7.38 (Berkeley) 05/14/92
  */
 
 #include "param.h"
