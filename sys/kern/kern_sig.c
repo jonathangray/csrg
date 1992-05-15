399c1c2fad2848d5969784d0cc1511156881e3f7
John Heidemann <heideman>
1992-05-14 17:33:13 -0700

    vnode interface conversion
    SCCS-vsn: 7.45

--- sys/kern/kern_sig.c
+++ sys/kern/kern_sig.c
@@ -1080,6 +1080,9 @@ sigexit(p, sig)
 coredump(p)
 	register struct proc *p;
 {
+	USES_VOP_GETATTR;
+	USES_VOP_SETATTR;
+	USES_VOP_UNLOCK;
 	register struct vnode *vp;
 	register struct pcred *pcred = p->p_cred;
 	register struct ucred *cred = pcred->pc_ucred;
