25f30b2743c1213719946231b7c3136d3f3ccdaf
Keith Bostic <bostic>
1992-05-15 15:23:55 -0700

    reformat to KNF, redo tracing interface, new version for vi
    SCCS-vsn: 5.5

--- lib/libcurses/fullname.c
+++ lib/libcurses/fullname.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)fullname.c	5.4 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)fullname.c	5.5 (Berkeley) 05/15/92";
 #endif /* not lint */
 
 # define	reg	register
