8dc3591edfe58bd6376a31e77c9c84c6ded8a632
Kirk McKusick <mckusick>
1992-05-27 16:07:45 -0700

    need real bwrite routine
    SCCS-vsn: 7.11

--- sys/ufs/mfs/mfsnode.h
+++ sys/ufs/mfs/mfsnode.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mfsnode.h	7.10 (Berkeley) 05/14/92
+ *	@(#)mfsnode.h	7.11 (Berkeley) 05/27/92
  */
 
 /*
