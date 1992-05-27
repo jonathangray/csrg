f486b102ea94a3806b810c4c9af1922673c88454
Keith Sklower <sklower>
1992-05-27 16:10:22 -0700

    more informative debugging for tp_slowtimo
    SCCS-vsn: 7.14

--- sys/netiso/tp_timer.c
+++ sys/netiso/tp_timer.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tp_timer.c	7.13 (Berkeley) 10/08/91
+ *	@(#)tp_timer.c	7.14 (Berkeley) 05/27/92
  */
 
 /***********************************************************
@@ -198,7 +198,6 @@ tp_slowtimo()
 	for (rp = tp_ref + tp_refinfo.tpr_maxopen; rp > tp_ref; rp--) {
 		if ((tpcb = rp->tpr_pcb) == 0 || tpcb->tp_refstate < REF_OPEN) 
 			continue;
-		t = TM_NTIMERS;
 		/* check the timers */
 		for (t = 0; t < TM_NTIMERS; t++) {
 			cp = tpcb->tp_timer + t;
@@ -206,7 +205,8 @@ tp_slowtimo()
 				*cp = 0;
 				E.ev_number = t;
 				IFDEBUG(D_TIMER)
-					printf("C expired! type 0x%x\n", t);
+					printf("tp_slowtimo: pcb 0x%x t %d\n",
+							tpcb, t);
 				ENDDEBUG
 				IncStat(ts_Cexpired);
 				tp_driver(tpcb, &E);
