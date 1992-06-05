0d2560130c2cac1ecc6fa75f642f45c834fd6c50
Mike Hibler <hibler>
1992-06-04 17:08:10 -0700

    change include path for HP code reorg
    SCCS-vsn: 7.17

--- sys/kern/sysv_shm.c
+++ sys/kern/sysv_shm.c
@@ -37,7 +37,7 @@
  *
  * from: Utah $Hdr: uipc_shm.c 1.9 89/08/14$
  *
- *	@(#)sysv_shm.c	7.16 (Berkeley) 05/04/92
+ *	@(#)sysv_shm.c	7.17 (Berkeley) 06/04/92
  */
 
 /*
@@ -61,7 +61,7 @@
 #include "vm/vm_pager.h"
 
 #ifdef HPUXCOMPAT
-#include "hp300/hpux/hpux.h"
+#include "hp/hpux/hpux.h"
 #endif
 
 int	shmat(), shmctl(), shmdt(), shmget();
