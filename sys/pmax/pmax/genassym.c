57987a9c83d12619ab6b4f30c6771e61bdabbb24
Ralph Campbell <ralph>
1992-06-20 12:04:05 -0700

    fix pmap cache changes.
    SCCS-vsn: 7.2

--- sys/pmax/pmax/genassym.c
+++ sys/pmax/pmax/genassym.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)genassym.c	7.1 (Berkeley) 01/07/92
+ *	@(#)genassym.c	7.2 (Berkeley) 06/20/92
  */
 
 #define KERNEL
@@ -64,8 +64,8 @@ main()
 	printf("#define\tU_PCB_FPREGS %d\n", &up->u_pcb.pcb_regs[F0]);
 	printf("#define\tU_PCB_CONTEXT %d\n", &up->u_pcb.pcb_context);
 	printf("#define\tU_PCB_ONFAULT %d\n", &up->u_pcb.pcb_onfault);
-	printf("#define\tPMAP_HASH_LOW_OFFSET 0x%x\n", &hp->low);
-	printf("#define\tPMAP_HASH_HIGH_OFFSET 0x%x\n", &hp->high);
+	printf("#define\tPMAP_HASH_LOW_OFFSET 0x%x\n", &hp->pmh_pte[0].low);
+	printf("#define\tPMAP_HASH_HIGH_OFFSET 0x%x\n", &hp->pmh_pte[0].high);
 	printf("#define\tPMAP_HASH_KPAGES %d\n", PMAP_HASH_KPAGES);
 	printf("#define\tPMAP_HASH_KADDR 0x%x\n", PMAP_HASH_KADDR);
 	printf("#define\tPMAP_HASH_SIZE_SHIFT %d\n", PMAP_HASH_SIZE_SHIFT);
