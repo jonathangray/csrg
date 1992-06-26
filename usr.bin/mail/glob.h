4ba04d64a106c733f234894933cede9c7bc3eb13
Keith Bostic <bostic>
1992-06-26 12:03:13 -0700

    quad's broke it -- prototyped it rather than figure out the problem
    SCCS-vsn: 5.21

--- usr.bin/mail/glob.h
+++ usr.bin/mail/glob.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)glob.h	5.20 (Berkeley) 06/25/90
+ *	@(#)glob.h	5.21 (Berkeley) 06/26/92
  */
 
 /*
