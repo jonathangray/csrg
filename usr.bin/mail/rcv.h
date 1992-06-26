f9aa71b84d5f7d16df2bd0b55501be0b5136174e
Keith Bostic <bostic>
1992-06-26 12:03:24 -0700

    quad's broke it -- prototyped it rather than figure out the problem
    SCCS-vsn: 5.6

--- usr.bin/mail/rcv.h
+++ usr.bin/mail/rcv.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rcv.h	5.5 (Berkeley) 06/01/90
+ *	@(#)rcv.h	5.6 (Berkeley) 06/26/92
  */
 
 /*
