43e74575d1c3dfab9c3229e527123642e3d061f3
Chris Torek <torek>
1992-07-10 21:05:46 -0700

    ANSIfy syscall args; readv does not need ARGSUSED; no longer need ugly __STDC__ hack
    SCCS-vsn: 7.36

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sys_generic.c	7.35 (Berkeley) 07/07/92
+ *	@(#)sys_generic.c	7.36 (Berkeley) 07/10/92
  */
 
 #include "param.h"
@@ -51,14 +51,15 @@
 /*
  * Read system call.
  */
+struct read_args {
+	int	fdes;
+	char	*cbuf;
+	unsigned count;
+};
 /* ARGSUSED */
 read(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		int	fdes;
-		char	*cbuf;
-		unsigned count;
-	} *uap;
+	register struct read_args *uap;
 	int *retval;
 {
 	register struct file *fp;
@@ -106,14 +107,14 @@ read(p, uap, retval)
 /*
  * Scatter read system call.
  */
-/* ARGSUSED */
+struct readv_args {
+	int	fdes;
+	struct	iovec *iovp;
+	unsigned iovcnt;
+};
 readv(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		int	fdes;
-		struct	iovec *iovp;
-		unsigned iovcnt;
-	} *uap;
+	register struct readv_args *uap;
 	int *retval;
 {
 	register struct file *fp;
@@ -196,13 +197,14 @@ done:
 /*
  * Write system call
  */
+struct write_args {
+	int	fdes;
+	char	*cbuf;
+	unsigned count;
+};
 write(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		int	fdes;
-		char	*cbuf;
-		unsigned count;
-	} *uap;
+	register struct write_args *uap;
 	int *retval;
 {
 	register struct file *fp;
@@ -254,13 +256,14 @@ write(p, uap, retval)
 /*
  * Gather write system call
  */
+struct writev_args {
+	int	fdes;
+	struct	iovec *iovp;
+	unsigned iovcnt;
+};
 writev(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		int	fdes;
-		struct	iovec *iovp;
-		unsigned iovcnt;
-	} *uap;
+	register struct writev_args *uap;
 	int *retval;
 {
 	register struct file *fp;
@@ -346,14 +349,15 @@ done:
 /*
  * Ioctl system call
  */
+struct ioctl_args {
+	int	fdes;
+	int	cmd;
+	caddr_t	cmarg;
+};
 /* ARGSUSED */
 ioctl(p, uap, retval)
 	struct proc *p;
-	register struct args {
-		int	fdes;
-		int	cmd;
-		caddr_t	cmarg;
-	} *uap;
+	register struct ioctl_args *uap;
 	int *retval;
 {
 	register struct file *fp;
@@ -482,13 +486,14 @@ int	selwait, nselcoll;
 /*
  * Select system call.
  */
+struct select_args {
+	int	nd;
+	fd_set	*in, *ou, *ex;
+	struct	timeval *tv;
+};
 select(p, uap, retval)
 	register struct proc *p;
-	register struct args {
-		int	nd;
-		fd_set	*in, *ou, *ex;
-		struct	timeval *tv;
-	} *uap;
+	register struct select_args *uap;
 	int *retval;
 {
 	fd_set ibits[3], obits[3];
@@ -605,14 +610,10 @@ selscan(p, ibits, obits, nfd, retval)
 }
 
 /*ARGSUSED*/
-#ifdef __STDC__
-seltrue(dev_t dev, int which, struct proc *p)
-#else
 seltrue(dev, flag, p)
 	dev_t dev;
 	int flag;
 	struct proc *p;
-#endif
 {
 
 	return (1);
