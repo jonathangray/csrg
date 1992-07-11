f7f1db698fc074ed726617cef015888acfafde95
Chris Torek <torek>
1992-07-10 21:06:54 -0700

    ANSIfy syscall args
    SCCS-vsn: 7.30

--- sys/kern/sys_process.c
+++ sys/kern/sys_process.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)sys_process.c	7.29 (Berkeley) 07/08/92
+ *	@(#)sys_process.c	7.30 (Berkeley) 07/10/92
  */
 
 #define IPCREG
@@ -46,14 +46,15 @@ struct {
 /*
  * Process debugging system call.
  */
+struct ptrace_args {
+	int	req;
+	int	pid;
+	int	*addr;
+	int	data;
+};
 ptrace(curp, uap, retval)
 	struct proc *curp;
-	register struct args {
-		int	req;
-		int	pid;
-		int	*addr;
-		int	data;
-	} *uap;
+	register struct ptrace_args *uap;
 	int *retval;
 {
 	register struct proc *p;
