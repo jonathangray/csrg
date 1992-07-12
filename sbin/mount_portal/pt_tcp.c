028231076da6bd18520f09a4ffeb9a36ebe03890
Jan-Simon Pendry <pendry>
1992-07-12 09:03:55 -0700

    checkpoint
    SCCS-vsn: 1.2

--- sbin/mount_portal/pt_tcp.c
+++ sbin/mount_portal/pt_tcp.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pt_tcp.c	1.1 (Berkeley) 07/09/92
+ *	@(#)pt_tcp.c	1.2 (Berkeley) 07/12/92
  *
  * $Id: pt_tcp.c,v 1.1 1992/05/25 21:43:09 jsp Exp jsp $
  */
@@ -43,11 +43,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <strings.h>
 #include <sys/types.h>
+#include <sys/param.h>
 #include <sys/syslog.h>
 
 #include "portald.h"
 
+/*
+ * Key will be tcp/host/port[/"priv"]
+ * Create a TCP socket connected to the
+ * requested host and port.
+ * Some trailing suffix values have special meanings.
+ * An unrecognised suffix is an error.
+ */
 int portal_tcp(pcr, key, v, so, fdp)
 struct portal_cred *pcr;
 char *key;
@@ -55,5 +64,24 @@ char **v;
 int so;
 int *fdp;
 {
+	char host[MAXHOSTNAMELEN];
+	char port[MAXHOSTNAMELEN];
+	char *p = key + (v[1] ? strlen(v[1]) : 0);
+	char *q;
+
+	q = strchr(p, '/');
+	if (q == 0 || q - p >= sizeof(host))
+		return (EINVAL);
+	*q = '\0';
+	strcpy(host, p);
+	p = q++;
+
+	q = strchr(p, '/');
+	if (q == 0 || q - p >= sizeof(port))
+		return (EINVAL);
+	*q = '\0';
+	strcpy(port, p);
+	p = q++;
+
 	return (EHOSTUNREACH);
 }
