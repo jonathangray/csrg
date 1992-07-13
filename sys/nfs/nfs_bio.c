c5d8f7437e7088e416b88a03cece2ee1962715c4
Jan-Simon Pendry <pendry>
1992-07-12 17:34:54 -0700

    need to include <sys/systm.h>
    SCCS-vsn: 7.31

--- sys/nfs/nfs_bio.c
+++ sys/nfs/nfs_bio.c
@@ -33,10 +33,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_bio.c	7.30 (Berkeley) 07/12/92
+ *	@(#)nfs_bio.c	7.31 (Berkeley) 07/12/92
  */
 
 #include <sys/param.h>
+#include <sys/systm.h>
 #include <sys/resourcevar.h>
 #include <sys/proc.h>
 #include <sys/buf.h>
