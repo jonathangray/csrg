07639bcdb12d938886d319719be32b0aaa83af1d
Chris Torek <torek>
1992-07-13 00:44:40 -0700

    date and time created 92/07/13 00:44:40 by torek
    SCCS-vsn: 7.1

---
+++ sys/sparc/sparc/memreg.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This software was developed by the Computer Systems Engineering group
+ * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
+ * contributed to Berkeley.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)memreg.c	7.1 (Berkeley) 07/13/92
+ *
+ * from: $Header: memreg.c,v 1.4 92/06/17 05:22:17 torek Exp $ (LBL)
+ */
+
+#include "sys/param.h"
+#include "sys/device.h"
+
+#include "machine/autoconf.h"
+
+#include "ctlreg.h"
+#include "memreg.h"
+
+static void memregattach __P((struct device *, struct device *, void *));
+struct cfdriver memregcd =
+    { NULL, "memory-error", matchbyname, memregattach,
+      DV_DULL, sizeof(struct device) };
+
+/* ARGSUSED */
+static void
+memregattach(parent, self, aux)
+	struct device *parent, *self;
+	void *aux;
+{
+	struct romaux *ra = aux;
+
+	par_err_reg = ra->ra_vaddr ? (volatile int *)ra->ra_vaddr :
+	    (volatile int *)mapiodev(ra->ra_paddr, sizeof(int));
+	printf("\n");
+}
+
+/*
+ * Synchronous and asynchronous memory error handler.
+ * (This is the level 15 interrupt, which is not vectored.)
+ * Should kill the process that got its bits clobbered,
+ * and take the page out of the page pool, but for now...
+ */
+void
+memerr(issync, ser, sva, aer, ava)
+	int issync, ser, sva, aer, ava;
+{
+
+	printf("%ssync mem err: ser=%b sva=%x aer=%b ava=%x\n",
+	    issync ? "" : "a", ser, SER_BITS, sva, aer & 0xff, AER_BITS, ava);
+	if (par_err_reg)
+		printf("parity error register = %b\n", *par_err_reg, PER_BITS);
+#ifdef DEBUG
+	callrom();
+#else
+	panic("memory error");		/* XXX */
+#endif
+}
