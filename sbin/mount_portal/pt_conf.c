7c88f94d086e9871c98ae7549a9e83e10684f656
Jan-Simon Pendry <pendry>
1992-07-13 15:47:29 -0700

    move to rev 5
    SCCS-vsn: 5.1

--- sbin/mount_portal/pt_conf.c
+++ sbin/mount_portal/pt_conf.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pt_conf.c	1.2 (Berkeley) 07/12/92
+ *	@(#)pt_conf.c	5.1 (Berkeley) 07/13/92
  *
  * $Id: pt_conf.c,v 1.2 1992/05/27 07:09:27 jsp Exp jsp $
  */
