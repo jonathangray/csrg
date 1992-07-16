351a2349c61724158aee1182b2fcd50630b12a60
Marc Teitelbaum <marc>
1992-07-16 13:58:19 -0700

    import tty line editing chars (actually, this version isn't quite right yet, but keith needs a checked in copy).
    SCCS-vsn: 5.4

--- lib/libedit/term.h
+++ lib/libedit/term.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)term.h	5.3 (Berkeley) 07/03/92
+ *	@(#)term.h	5.4 (Berkeley) 07/16/92
  */
 
 /*
