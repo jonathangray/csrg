a3ecac038adf4eab9a5ab37dd86c795e9722025d
Keith Bostic <bostic>
1992-07-21 16:50:39 -0700

    update the contribution notice; LBL requires acknowledgement
    SCCS-vsn: 7.2

--- sys/sparc/rcons/rcons_kern.c
+++ sys/sparc/rcons/rcons_kern.c
@@ -6,6 +6,11 @@
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
  * contributed to Berkeley.
  *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratories.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -34,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rcons_kern.c	7.1 (Berkeley) 07/13/92
+ *	@(#)rcons_kern.c	7.2 (Berkeley) 07/21/92
  *
  * from: $Header: rcons_kern.c,v 1.26 92/07/09 08:01:28 torek Exp $
  */
