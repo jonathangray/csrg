7be74a67b2ad1fca14e114e8d5a063ed76922788
Keith Bostic <bostic>
1992-07-21 16:37:56 -0700

    update the contribution notice; LBL requires acknowledgement
    SCCS-vsn: 7.2

--- sys/sparc/dev/event.c
+++ sys/sparc/dev/event.c
@@ -6,6 +6,11 @@
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
  * contributed to Berkeley.
  *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratories.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -34,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)event.c	7.1 (Berkeley) 07/13/92
+ *	@(#)event.c	7.2 (Berkeley) 07/21/92
  *
  * from: $Header: event.c,v 1.4 92/06/17 05:35:45 torek Exp $ (LBL)
  */
