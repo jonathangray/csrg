da2248b0c1711a725efe3daec009a9eecd3dd923
Keith Bostic <bostic>
1992-07-21 16:41:31 -0700

    update the contribution notice; LBL requires acknowledgement
    SCCS-vsn: 7.2

--- sys/sparc/include/ieee.h
+++ sys/sparc/include/ieee.h
@@ -6,6 +6,11 @@
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
  * contributed to Berkeley.
  *
+ * All advertising materials mentioning features or use of this software
+ * must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Lawrence Berkeley Laboratories.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -34,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ieee.h	7.1 (Berkeley) 07/13/92
+ *	@(#)ieee.h	7.2 (Berkeley) 07/21/92
  *
  * from: $Header: ieee.h,v 1.6 92/06/19 23:04:29 torek Exp $
  */
