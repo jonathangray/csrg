8841a893adc7b248d5d69b17f2a26239cbe11c4e
Keith Bostic <bostic>
1992-07-23 17:58:20 -0700

    Elan's new kvm version; fix copyright/header notice
    SCCS-vsn: 5.6

--- usr.bin/systat/fetch.c
+++ usr.bin/systat/fetch.c
@@ -1,22 +1,26 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980, 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)fetch.c	5.5 (Berkeley) 06/26/91";
-#endif not lint
+static char sccsid[] = "@(#)fetch.c	5.6 (Berkeley) 07/23/92";
+#endif /* not lint */
 
+#include <sys/types.h>
 #include "systat.h"
+#include "extern.h"
 
+int
 kvm_ckread(a, b, l)
 	void *a, *b;
 	int l;
 {
-	extern int verbose;
-
-	if (kvm_read(a, b, l) != l) {
+	if (kvm_read(kd, (u_long)a, b, l) != l) {
 		if (verbose)
 			error("error reading kmem at %x\n", a);
 		return (0);
