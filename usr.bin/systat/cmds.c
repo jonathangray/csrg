89128d55f584d4ccb7070d1f8bde75ba95ed6fa1
Keith Bostic <bostic>
1992-07-23 17:56:40 -0700

    update copyright/header
    SCCS-vsn: 5.10

--- usr.bin/systat/cmds.c
+++ usr.bin/systat/cmds.c
@@ -1,16 +1,15 @@
-/*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.  The Berkeley software License Agreement
- * specifies the terms and conditions for redistribution.
+/*-
+ * Copyright (c) 1980, 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This module is believed to contain source code proprietary to AT&T.
+ * Use and redistribution is subject to the Berkeley Software License
+ * Agreement and your Software Agreement with AT&T (Western Electric).
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	5.9 (Berkeley) 07/23/92";
-#endif not lint
-
-/*
- * Command support.
- */
+static char sccsid[] = "@(#)cmds.c	5.10 (Berkeley) 07/23/92";
+#endif /* not lint */
 
 #include <stdlib.h>
 #include <unistd.h>
