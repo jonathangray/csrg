e23ae362c39d03490e8d8b994cbe9065d7afc706
Keith Bostic <bostic>
1992-08-23 11:22:09 -0700

    KNF
    SCCS-vsn: 5.6

--- lib/libcurses/mvscanw.c
+++ lib/libcurses/mvscanw.c
@@ -32,26 +32,27 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mvscanw.c	5.6 (Berkeley) 05/15/92";
-#endif /* not lint */
+static char sccsid[] = "@(#)mvscanw.c	5.6 (Berkeley) 08/23/92";
+#endif	/* not lint */
+
+#include <curses.h>
 
 #if __STDC__
 #include <stdarg.h>
 #else
 #include <varargs.h>
 #endif
-#include "curses.ext"
 
 /*
- * implement the mvscanw commands.  Due to the variable number of
- * arguments, they cannot be macros.  Another sigh....
+ * mvscanw, mvwscanw -- 
+ *	Implement the mvscanw commands.  Due to the variable number of
+ *	arguments, they cannot be macros.  Another sigh....
  */
-
 #if __STDC__
-mvscanw(reg int y, reg int x, const char *fmt, ...)
+mvscanw(register int y, register int x, const char *fmt,...)
 #else
 mvscanw(y, x, fmt, va_alist)
-	reg int y, x;
+	register int y, x;
 	char *fmt;
 	va_dcl
 #endif
@@ -60,23 +61,24 @@ mvscanw(y, x, fmt, va_alist)
 	int ret;
 
 	if (move(y, x) != OK)
-		return ERR;
+		return (ERR);
 #if __STDC__
 	va_start(ap, fmt);
 #else
 	va_start(ap);
 #endif
-	ret = _sscans(stdscr, fmt, ap);
+	ret = __sscans(stdscr, fmt, ap);
 	va_end(ap);
-	return ret;
+	return (ret);
 }
 
 #if __STDC__
-mvwscanw(reg WINDOW *win, reg int y, reg int x, const char *fmt, ...)
+mvwscanw(register WINDOW * win, register int y, register int x,
+    const char *fmt, ...)
 #else
 mvwscanw(win, y, x, fmt, va_alist)
-	reg WINDOW *win;
-	reg int y, x;
+	register WINDOW *win;
+	register int y, x;
 	char *fmt;
 	va_dcl
 #endif
@@ -85,7 +87,7 @@ mvwscanw(win, y, x, fmt, va_alist)
 	int ret;
 
 	if (move(y, x) != OK)
-		return ERR;
+		return (ERR);
 #if __STDC__
 	va_start(ap, fmt);
 #else
@@ -93,5 +95,5 @@ mvwscanw(win, y, x, fmt, va_alist)
 #endif
 	ret = _sscans(win, fmt, ap);
 	va_end(ap);
-	return ret;
+	return (ret);
 }
