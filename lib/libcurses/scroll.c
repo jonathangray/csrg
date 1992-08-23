da966c4a988e7620cd3686f6a073e6b184b50603
Keith Bostic <bostic>
1992-08-23 12:13:44 -0700

    KNF
    SCCS-vsn: 5.5

--- lib/libcurses/scroll.c
+++ lib/libcurses/scroll.c
@@ -32,26 +32,27 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)scroll.c	5.5 (Berkeley) 05/15/92";
+static char sccsid[] = "@(#)scroll.c	5.5 (Berkeley) 08/23/92";
 #endif /* not lint */
 
-# include	"curses.ext"
+#include <curses.h>
 
 /*
- *	This routine scrolls the window up a line.
- *
+ * scroll --
+ *	Scroll the window up a line.
  */
+int
 scroll(win)
-register  WINDOW	*win;
+	register WINDOW *win;
 {
-	register int	oy, ox;
+	register int oy, ox;
 
-# ifdef DEBUG
-	fprintf(outf, "SCROLL(%0.2o)\n", win);
-# endif
+#ifdef DEBUG
+	__TRACE("scroll: (%0.2o)\n", win);
+#endif
 
 	if (!win->_scroll)
-		return ERR;
+		return (ERR);
 
 	getyx(win, oy, ox);
 	wmove(win, 0, 0);
@@ -59,11 +60,12 @@ register  WINDOW	*win;
 	wmove(win, oy, ox);
 
 	if (win == curscr) {
-		_putchar('\n');
+		putchar('\n');
 		if (!NONL)
 			win->_curx = 0;
-# ifdef DEBUG
-		fprintf(outf, "SCROLL: win == curscr\n");
-# endif
+#ifdef DEBUG
+		__TRACE("scroll: win == curscr\n");
+#endif
 	}
+	return (OK);
 }
