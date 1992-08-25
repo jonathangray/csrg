a6625ec3c1b8c0b4d1b1ee4d815f075a80d3b7da
Edward Wang <edward>
1992-08-24 19:17:43 -0700

    use close-on-exec
    SCCS-vsn: 3.13

--- usr.bin/window/context.c
+++ usr.bin/window/context.c
@@ -35,12 +35,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)context.c	3.12 (Berkeley) 06/06/90";
+static char sccsid[] = "@(#)context.c	3.13 (Berkeley) 08/24/92";
 #endif /* not lint */
 
 #include "value.h"
 #include "string.h"
 #include "context.h"
+#include <fcntl.h>
 
 /*
  * Context push/pop for nested command files.
@@ -89,6 +90,7 @@ char *filename;
 	cx.x_fp = fopen(filename, "r");
 	if (cx.x_fp == 0)
 		goto bad;
+	(void) fcntl(fileno(cx.x_fp), F_SETFD, 1);
 	cx.x_bol = 1;
 	cx.x_lineno = 0;
 	cx.x_errwin = 0;
