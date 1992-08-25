40adf70280b6ea264a34922b8e82bad45fbca4f8
Keith Bostic <bostic>
1992-08-25 14:31:40 -0700

    Add vnode pointer to segment structure so that updatemeta knows what file to finish off when it runs out of space in a partial or segment.
    SCCS-vsn: 7.22

--- sys/ufs/lfs/lfs.h
+++ sys/ufs/lfs/lfs.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs.h	7.21 (Berkeley) 08/21/92
+ *	@(#)lfs.h	7.22 (Berkeley) 08/25/92
  */
 
 #define	LFS_LABELPAD	8192		/* LFS label size */
@@ -323,6 +323,7 @@ struct segment {
 	struct buf	**start_bpp;	/* pointer to first bp in this set */
 	struct buf	*ibp;		/* buffer pointer to inode page */
 	struct finfo	*fip;		/* current fileinfo pointer */
+	struct vnode	*vp;		/* vnode being gathered */
 	void	*segsum;		/* segment summary info */
 	u_long	ninodes;		/* number of inodes in this segment */
 	u_long	seg_bytes_left;		/* bytes left in segment */
