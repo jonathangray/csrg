c15b898f73dc1604a16e4d92eeac16c17bdf8186
Elan Amir <elan>
1992-09-01 11:08:03 -0700

    Rewrote to use new getcap(3) library.  Major prototyping and cleanup.
    SCCS-vsn: 5.5

--- usr.sbin/lpr/common_source/pathnames.h
+++ usr.sbin/lpr/common_source/pathnames.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	5.4 (Berkeley) 06/01/90
+ *	@(#)pathnames.h	5.5 (Berkeley) 8/6/92
  */
 
 #include <paths.h>
