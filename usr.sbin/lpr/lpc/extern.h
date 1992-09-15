22e267a9a73adf640b834b618ecfb3f44f6780ab
Elan Amir <elan>
1992-09-15 10:48:20 -0700

    Added SCCS keywords.
    SCCS-vsn: 5.3

--- usr.sbin/lpr/lpc/extern.h
+++ usr.sbin/lpr/lpc/extern.h
@@ -1,6 +1,7 @@
-/*-
- * Copyright (c) 1992 The Regents of the University of California.
+/*
+ * Copyright (c) 1989 The Regents of the University of California.
  * All rights reserved.
+ *
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,8 +31,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	5.3 (Berkeley) 8/6/92
+ *      @(#)extern.h	5.3 (Berkeley) 09/15/92
  */
+
 
 #include <sys/cdefs.h>
 
