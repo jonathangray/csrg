ba796b75589f94da47c2e41ea17e52f1304ecd10
Elan Amir <elan>
1992-09-15 10:56:20 -0700

    Added SCCS keywords.
    SCCS-vsn: 5.9

--- usr.sbin/lpr/lprm/lprm.c
+++ usr.sbin/lpr/lprm/lprm.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
+ * Copyright (c) 1983 The Regents of the University of California.
  * All rights reserved.
+ *
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,12 +34,12 @@
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1983 Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1983 The Regents of the University of California.\n\
+ All rights reserved.";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lprm.c	5.8 (Berkeley) 8/6/92";
+static char sccsid[] = "@(#)lprm.c	5.9 (Berkeley) 09/15/92";
 #endif /* not lint */
 
 /*
