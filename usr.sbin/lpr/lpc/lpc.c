35b5565d29f3c9f49e711433c9b8dd744074c173
Elan Amir <elan>
1992-09-15 10:48:17 -0700

    Added SCCS keywords.
    SCCS-vsn: 5.14

--- usr.sbin/lpr/lpc/lpc.c
+++ usr.sbin/lpr/lpc/lpc.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
+ * Copyright (c) 1983 The Regents of the University of California.
  * All rights reserved.
+ *
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,12 +34,12 @@
 
 #ifndef lint
 char copyright[] =
-"@(#) Copyright (c) 1983 Regents of the University of California.\n\
+"@(#) Copyright (c) 1983 The Regents of the University of California.\n\
  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lpc.c	5.13 (Berkeley) 8/6/92";
+static char sccsid[] = "@(#)lpc.c	5.14 (Berkeley) 09/15/92";
 #endif /* not lint */
 
 #include <sys/param.h>
