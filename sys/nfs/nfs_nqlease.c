a67b31108614e0e3d22c70f119220550348b1eaa
Kirk McKusick <mckusick>
1992-09-30 16:02:58 -0700

    September update from Rick Macklem to add accept protocol to NQMFS
    SCCS-vsn: 7.12

--- sys/nfs/nfs_nqlease.c
+++ sys/nfs/nfs_nqlease.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_nqlease.c	7.11 (Berkeley) 09/16/92
+ *	@(#)nfs_nqlease.c	7.12 (Berkeley) 09/30/92
  */
 
 /*
@@ -116,6 +116,7 @@ int nqnfs_piggy[NFS_NPROCS] = {
 	NQL_READ,
 	0,
 	NQL_READ,
+	0,
 	0,
 	0,
 	0,
@@ -418,11 +419,9 @@ nqsrv_cmpnam(slp, nam, lph)
 	else
 		addr = slp->ns_nam;
 	if (lph->lph_flag & LC_UDP)
-		ret = netaddr_match(AF_INET, &lph->lph_haddr,
-			(union nethostaddr *)0, addr);
+		ret = netaddr_match(AF_INET, &lph->lph_haddr, addr);
 	else if (lph->lph_flag & LC_CLTP)
-		ret = netaddr_match(AF_ISO, &lph->lph_claddr,
-			(union nethostaddr *)0, addr);
+		ret = netaddr_match(AF_ISO, &lph->lph_claddr, addr);
 	else {
 		if ((lph->lph_slp->ns_flag & SLP_VALID) == 0)
 			return (0);
@@ -431,8 +430,7 @@ nqsrv_cmpnam(slp, nam, lph)
 			lhaddr.had_inetaddr = saddr->sin_addr.s_addr;
 		else
 			lhaddr.had_nam = lph->lph_slp->ns_nam;
-		ret = netaddr_match(saddr->sin_family, &lhaddr,
-			(union nethostaddr *)0, addr);
+		ret = netaddr_match(saddr->sin_family, &lhaddr, addr);
 	}
 	return (ret);
 }
