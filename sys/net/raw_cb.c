6e3385228aa32321203730f9e4bb3b9ce46efcac
Keith Bostic <bostic>
1992-10-11 11:49:04 -0700

    make kernel includes standard
    SCCS-vsn: 7.13

--- sys/net/raw_cb.c
+++ sys/net/raw_cb.c
@@ -30,22 +30,22 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)raw_cb.c	7.13 (Berkeley) 02/15/92
+ *	@(#)raw_cb.c	7.13 (Berkeley) 10/11/92
  */
 
-#include "param.h"
-#include "systm.h"
-#include "mbuf.h"
-#include "socket.h"
-#include "socketvar.h"
-#include "domain.h"
-#include "protosw.h"
-#include "errno.h"
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/mbuf.h>
+#include <sys/socket.h>
+#include <sys/socketvar.h>
+#include <sys/domain.h>
+#include <sys/protosw.h>
+#include <sys/errno.h>
 
-#include "if.h"
-#include "route.h"
-#include "raw_cb.h"
-#include "../netinet/in.h"
+#include <net/if.h>
+#include <net/route.h>
+#include <net/raw_cb.h>
+#include <netinet/in.h>
 
 /*
  * Routines to manage the raw protocol control blocks. 
