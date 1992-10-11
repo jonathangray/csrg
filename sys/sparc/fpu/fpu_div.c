ffc4cc69e5414f7908636bc4cde8018cf4024630
Keith Bostic <bostic>
1992-10-11 12:41:19 -0700

    make kernel includes standard
    SCCS-vsn: 7.3

--- sys/sparc/fpu/fpu_div.c
+++ sys/sparc/fpu/fpu_div.c
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fpu_div.c	7.2 (Berkeley) 07/21/92
+ *	@(#)fpu_div.c	7.3 (Berkeley) 10/11/92
  *
  * from: $Header: fpu_div.c,v 1.2 92/06/17 05:41:30 torek Exp $
  */
@@ -48,12 +48,12 @@
  * Perform an FPU divide (return x / y).
  */
 
-#include "sys/types.h"
+#include <sys/types.h>
 
-#include "machine/reg.h"
+#include <machine/reg.h>
 
-#include "fpu_arith.h"
-#include "fpu_emu.h"
+#include <sparc/fpu/fpu_arith.h>
+#include <sparc/fpu/fpu_emu.h>
 
 /*
  * Division of normal numbers is done as follows:
