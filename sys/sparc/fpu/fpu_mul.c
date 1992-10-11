415c9e92c3df08023dc4bce21d440ecbbbf42717
Keith Bostic <bostic>
1992-10-11 12:41:21 -0700

    make kernel includes standard
    SCCS-vsn: 7.3

--- sys/sparc/fpu/fpu_mul.c
+++ sys/sparc/fpu/fpu_mul.c
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fpu_mul.c	7.2 (Berkeley) 07/21/92
+ *	@(#)fpu_mul.c	7.3 (Berkeley) 10/11/92
  *
  * from: $Header: fpu_mul.c,v 1.2 92/06/17 05:41:34 torek Exp $
  */
@@ -48,12 +48,12 @@
  * Perform an FPU multiply (return x * y).
  */
 
-#include "sys/types.h"
+#include <sys/types.h>
 
-#include "machine/reg.h"
+#include <machine/reg.h>
 
-#include "fpu_arith.h"
-#include "fpu_emu.h"
+#include <sparc/fpu/fpu_arith.h>
+#include <sparc/fpu/fpu_emu.h>
 
 /*
  * The multiplication algorithm for normal numbers is as follows:
