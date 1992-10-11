2e0891eea582f136ad3f343bfabe21ff9d8a9d7c
Keith Bostic <bostic>
1992-10-11 10:01:03 -0700

    make kernel includes standard
    SCCS-vsn: 7.2

--- sys/stand.att/ioctl.c
+++ sys/stand.att/ioctl.c
@@ -6,11 +6,11 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)ioctl.c	7.1 (Berkeley) 05/05/91
+ *	@(#)ioctl.c	7.2 (Berkeley) 10/11/92
  */
 
 #include <sys/param.h>
-#include "saio.h"
+#include <stand/saio.h>
 
 #ifndef SMALL
 ioctl(fdesc, cmd, arg)
