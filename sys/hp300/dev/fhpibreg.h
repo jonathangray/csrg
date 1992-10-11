805f805788f894713bc435a46b9ec068788c09b8
Keith Bostic <bostic>
1992-10-11 09:23:46 -0700

    make kernel includes standard
    SCCS-vsn: 7.3

--- sys/hp300/dev/fhpibreg.h
+++ sys/hp300/dev/fhpibreg.h
@@ -30,14 +30,10 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fhpibreg.h	7.2 (Berkeley) 06/05/92
+ *	@(#)fhpibreg.h	7.3 (Berkeley) 10/11/92
  */
 
-#ifdef KERNEL
-#include "hp/dev/iotypes.h"	/* XXX */
-#else
 #include <hp/dev/iotypes.h>	/* XXX */
-#endif
 
 struct	fhpibdevice {
 	u_char	hpib_pad0;
