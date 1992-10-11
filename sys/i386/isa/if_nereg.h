0c51d3f9c188d88f26a25e83d6fadb1604df4fc8
Keith Bostic <bostic>
1992-10-11 10:20:09 -0700

    make kernel includes standard
    SCCS-vsn: 7.2

--- sys/i386/isa/if_nereg.h
+++ sys/i386/isa/if_nereg.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_nereg.h	7.1 (Berkeley) 05/09/91
+ *	@(#)if_nereg.h	7.2 (Berkeley) 10/11/92
  */
 
 /*
@@ -39,7 +39,7 @@
 
 /* The NE2000 uses a DS8390 Ethernet controller in at the beginning of
    its i/o space */
-#include "ic/ds8390.h"
+#include <i386/isa/ic/ds8390.h>
 
 #define ne_data		0x10	/* Data Transfer port */
 #define ne_reset	0x1f	/* Card Reset port */
