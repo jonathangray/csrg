6737e7504962a1140c0fdff27dd9f40ee98f60aa
Keith Bostic <bostic>
1992-10-11 10:01:07 -0700

    make kernel includes standard
    SCCS-vsn: 7.19

--- sys/stand.att/saio.h
+++ sys/stand.att/saio.h
@@ -30,14 +30,16 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)saio.h	7.18 (Berkeley) 06/18/92
+ *	@(#)saio.h	7.19 (Berkeley) 10/11/92
  */
 
 #include <sys/time.h>
+
 #include <ufs/ffs/fs.h>
 #include <ufs/ufs/dinode.h>
-#include "saioctl.h"
-#include "saerrno.h"
+
+#include <stand/saioctl.h>
+#include <stand/saerrno.h>
 
 #define	UNIX	"/vmunix"
 
