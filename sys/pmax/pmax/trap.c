789f28cf552e192ed972c21279106c20eadd3a11
Ralph Campbell <ralph>
1992-10-24 14:14:45 -0700

    vm_flags changes, changed sigreturn code to be on user stack.
    SCCS-vsn: 7.10

--- sys/pmax/pmax/trap.c
+++ sys/pmax/pmax/trap.c
@@ -37,7 +37,7 @@
  *
  * from: Utah $Hdr: trap.c 1.32 91/04/06$
  *
- *	@(#)trap.c	7.9 (Berkeley) 10/11/92
+ *	@(#)trap.c	7.10 (Berkeley) 10/24/92
  */
 
 #include <sys/param.h>
@@ -236,7 +236,7 @@ trap(statusReg, causeReg, vadr, pc, args)
 			pa = entry & PG_FRAME;
 			if (!IS_VM_PHYSADDR(pa))
 				panic("trap: kmod");
-			PHYS_TO_VM_PAGE(pa)->clean = FALSE;
+			PHYS_TO_VM_PAGE(pa)->flags &= ~PG_CLEAN;
 #endif
 			return (pc);
 		}
@@ -252,7 +252,8 @@ trap(statusReg, causeReg, vadr, pc, args)
 		extern pmap_hash_t zero_pmap_hash;
 		extern pmap_t cur_pmap;
 
-		if (cur_pmap->pm_hash == zero_pmap_hash)
+		if (cur_pmap->pm_hash == zero_pmap_hash ||
+		    cur_pmap->pm_hash == (pmap_hash_t)0)
 			panic("tlbmod");
 #endif
 		hp = &((pmap_hash_t)PMAP_HASH_UADDR)[PMAP_HASH(vadr)];
@@ -277,7 +278,7 @@ trap(statusReg, causeReg, vadr, pc, args)
 		pa = hp->pmh_pte[i].low & PG_FRAME;
 		if (!IS_VM_PHYSADDR(pa))
 			panic("trap: umod");
-		PHYS_TO_VM_PAGE(pa)->clean = FALSE;
+		PHYS_TO_VM_PAGE(pa)->flags &= ~PG_CLEAN;
 #endif
 		if (!USERMODE(statusReg))
 			return (pc);
@@ -303,8 +304,14 @@ trap(statusReg, causeReg, vadr, pc, args)
 			}
 			goto err;
 		}
+		/*
+		 * It is an error for the kernel to access user space except
+		 * through the copyin/copyout routines.
+		 */
+		if ((i = ((struct pcb *)UADDR)->pcb_onfault) == 0)
+			goto err;
 		/* check for fuswintr() or suswintr() getting a page fault */
-		if ((i = ((struct pcb *)UADDR)->pcb_onfault) == 4)
+		if (i == 4)
 			return (onfault_table[i]);
 		goto dofault;
 
@@ -380,24 +387,6 @@ trap(statusReg, causeReg, vadr, pc, args)
 	    }
 
 	case T_ADDR_ERR_LD+T_USER:	/* misaligned or kseg access */
-		if (vadr == KERNBASE) {
-			struct args {
-				int	i[1];
-			} args;
-			int rval[2];
-
-			/*
-			 * Assume a signal handler is trying to return
-			 * (see sendsig() and sigreturn()). We have to
-			 * pop the sigframe struct to get the address of
-			 * the sigcontext.
-			 */
-			args.i[0] = p->p_md.md_regs[SP] + 4 * sizeof(int);
-			(void) sigreturn(curproc, &args, rval);
-			goto out;
-		}
-		/* FALLTHROUGH */
-
 	case T_ADDR_ERR_ST+T_USER:	/* misaligned or kseg access */
 	case T_BUS_ERR_IFETCH+T_USER:	/* BERR asserted to cpu */
 	case T_BUS_ERR_LD_ST+T_USER:	/* BERR asserted to cpu */
@@ -737,8 +726,11 @@ out:
 	/*
 	 * If profiling, charge system time to the trapped pc.
 	 */
-	if (p->p_flag & SPROFIL)
-		addupc_task(p, pc, (int)(p->p_sticks - sticks));
+	if (p->p_flag & SPROFIL) {
+		extern int psratio;
+
+		addupc_task(p, pc, (int)(p->p_sticks - sticks) * psratio);
+	}
 
 	curpri = p->p_pri;
 	return (pc);
