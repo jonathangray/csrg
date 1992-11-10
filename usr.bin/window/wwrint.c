b5bf78f9f25cc6aac882e732adb4764f4f2ea674
Edward Wang <edward>
1992-11-10 11:49:58 -0800

    checkpointing stuff
    SCCS-vsn: 3.14

--- usr.bin/window/wwrint.c
+++ usr.bin/window/wwrint.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)wwrint.c	3.13 (Berkeley) 08/16/92";
+static char sccsid[] = "@(#)wwrint.c	3.14 (Berkeley) 11/10/92";
 #endif /* not lint */
 
 #include "ww.h"
@@ -73,9 +73,17 @@ wwrint()
 	if (n > 0) {
 		if (tt.tt_rint)
 			n = (*tt.tt_rint)(wwibq, n);
-		wwibq += n;
-		wwnreadc += n;
-		wwsetintr();
+		if (n > 0) {
+			wwibq += n;
+			wwnreadc += n;
+			/*
+			 * Hasten or delay the next checkpoint,
+			 * as the case may be.
+			 */
+			if (tt.tt_checkpoint && !wwdocheckpoint)
+				(void) alarm(1);
+			wwsetintr();
+		}
 	} else if (n == 0)
 		wwnreadz++;
 	else
