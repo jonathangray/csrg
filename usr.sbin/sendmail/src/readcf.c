1332b50415db079af0019b416142f5ea733fb853
Eric Allman <eric>
1992-11-14 11:34:28 -0800

    (partially) ANSI-fy it
    SCCS-vsn: 5.48

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	5.47 (Berkeley) 11/04/92";
+static char sccsid[] = "@(#)readcf.c	5.48 (Berkeley) 11/14/92";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -168,7 +168,7 @@ readcf(cfname)
 					/* delete preceeding white space */
 					while (isspace(*p) && p > buf)
 						p--;
-					if ((e = index(++p, '\n')) != NULL)
+					if ((e = strchr(++p, '\n')) != NULL)
 						(void) strcpy(p, e);
 					else
 						p[0] = p[1] = '\0';
@@ -450,9 +450,9 @@ fileclass(class, filename, fmt, safe)
 		if (sscanf(buf, fmt, wordbuf) != 1)
 			continue;
 		p = wordbuf;
-# else SCANF
+# else /* SCANF */
 		p = buf;
-# endif SCANF
+# endif /* SCANF */
 
 		/*
 		**  Break up the match into words.
@@ -855,7 +855,7 @@ setoption(opt, val, sticky)
 		  case SM_QUEUE:	/* queue only */
 #ifndef QUEUE
 			syserr("need QUEUE to set -odqueue");
-#endif QUEUE
+#endif /* QUEUE */
 			/* fall through..... */
 
 		  case SM_DELIVER:	/* do everything */
