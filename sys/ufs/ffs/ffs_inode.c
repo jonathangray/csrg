1b9230546fb9092346049c38b1384ce1eff9ab3c
Chris Torek <torek>
1992-12-01 20:31:41 -0800

    gotta set ip before you use it, kirk!
    SCCS-vsn: 7.65

--- sys/ufs/ffs/ffs_inode.c
+++ sys/ufs/ffs/ffs_inode.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ffs_inode.c	7.64 (Berkeley) 11/13/92
+ *	@(#)ffs_inode.c	7.65 (Berkeley) 12/01/92
  */
 
 #include <sys/param.h>
@@ -87,11 +87,11 @@ ffs_update(ap)
 	struct dinode *dp;
 	register struct fs *fs;
 
+	ip = VTOI(ap->a_vp);
 	if (ap->a_vp->v_mount->mnt_flag & MNT_RDONLY) {
 		ip->i_flag &= ~(IUPD|IACC|ICHG|IMOD);
 		return (0);
 	}
-	ip = VTOI(ap->a_vp);
 	if ((ip->i_flag & (IUPD|IACC|ICHG|IMOD)) == 0)
 		return (0);
 	if (ip->i_flag&IACC)
