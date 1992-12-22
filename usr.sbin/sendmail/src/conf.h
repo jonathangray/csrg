cdbcb23e266e22eac1fea9c4f109d55a2456e0df
Eric Allman <eric>
1992-12-21 16:08:20 -0800

    Release 6
    SCCS-vsn: 6.1

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)conf.h	5.33 (Berkeley) 12/20/92
+ *	@(#)conf.h	6.1 (Berkeley) 12/21/92
  */
 
 /*
