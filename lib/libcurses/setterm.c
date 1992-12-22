14c2a10d710badf797308fc6fd3401233ac99951
Keith Bostic <bostic>
1992-12-22 15:02:49 -0800

    destcol, destline don't need to be static
    SCCS-vsn: 5.12

--- lib/libcurses/setterm.c
+++ lib/libcurses/setterm.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)setterm.c	5.11 (Berkeley) 09/21/92";
+static char sccsid[] = "@(#)setterm.c	5.12 (Berkeley) 12/22/92";
 #endif /* not lint */
 
 #include <sys/ioctl.h>
@@ -65,8 +65,6 @@ static char	*_PC,
 static char	*aoftspace;		/* Address of _tspace for relocation */
 static char	tspace[2048];		/* Space for capability strings */
 
-static int	destcol, destline;
-
 char *ttytype;
 
 int
@@ -76,6 +74,7 @@ setterm(type)
 	static char genbuf[1024];
 	static char __ttytype[1024];
 	register int unknown;
+	int destcol, destline;
 	struct winsize win;
 	char *p;
 
