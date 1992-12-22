80416d4329d2886cf755b5b7f352430ef0769c4a
Keith Bostic <bostic>
1992-12-22 14:16:47 -0800

    don't include copyright notice
    SCCS-vsn: 5.2

--- lib/libcurses/tscroll.c
+++ lib/libcurses/tscroll.c
@@ -1,7 +1,6 @@
-/*
+/*-
  * Copyright (c) 1992 The Regents of the University of California.
  * All rights reserved.
- *
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,18 +32,12 @@
  */
 
 #ifndef lint
-static char copyright[] =
-"@(#) Copyright (c) 1992 The Regents of the University of California.\n\
- All rights reserved.\n";
-#endif /* not lint */
-
-#ifndef lint
-static char sccsid[] = "@(#)tscroll.c	5.1 (Berkeley) 10/01/92";
+static char sccsid[] = "@(#)tscroll.c	5.2 (Berkeley) 12/22/92";
 #endif /* not lint */
 
 #include <stdio.h>
 
-#define MAXRETURNSIZE 64
+#define	MAXRETURNSIZE	64
 
 /*
  * Routine to perform scrolling.  Derived from tgoto.c in tercamp(3) library.
