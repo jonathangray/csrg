98614b49d59ab73c5c061a045b710e67bcccafd5
Elan Amir <elan>
1992-12-30 14:55:00 -0800

    Changed ERR to CURSES_ERR and OK to CURSES_OK.
    SCCS-vsn: 5.11

--- lib/libcurses/overlay.c
+++ lib/libcurses/overlay.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)overlay.c	5.10 (Berkeley) 10/26/92";
+static char sccsid[] = "@(#)overlay.c	5.11 (Berkeley) 12/30/92";
 #endif	/* not lint */
 
 #include <ctype.h>
@@ -62,7 +62,7 @@ overlay(win1, win2)
 	    starty, startx, endy, endx);
 #endif
 	if (starty >= endy || startx >= endx)
-		return (OK);
+		return (CURSES_OK);
 	y1 = starty - win1->begy;
 	y2 = starty - win2->begy;
 	for (y = starty; y < endy; y++, y1++, y2++) {
@@ -77,5 +77,5 @@ overlay(win1, win2)
 			x++;
 		}
 	}
-	return (OK);
+	return (CURSES_OK);
 }
