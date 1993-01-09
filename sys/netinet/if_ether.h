dbafe1fcf1a18aefd4c64ed441980cd15f6452e9
Andrew Cherenson <andrew>
1993-01-08 18:42:20 -0800

    (By Sklower) checkpoint the current state of Cherenson's work.
    SCCS-vsn: 7.9

--- sys/netinet/if_ether.h
+++ sys/netinet/if_ether.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_ether.h	7.8 (Berkeley) 07/06/92
+ *	@(#)if_ether.h	7.9 (Berkeley) 01/08/93
  */
 
 /*
@@ -149,13 +149,8 @@ struct sockaddr_inarp {
 
 #ifdef	KERNEL
 u_char	etherbroadcastaddr[6];
-#if defined(ISO) && !defined(MULTICAST)
-#define MULTICAST 1
-#endif
-#ifdef MULTICAST
 u_char	ether_ipmulticast_min[6];
 u_char	ether_ipmulticast_max[6];
-#endif
 
 struct	llinfo_arp *arptnew __P((struct in_addr *));
 struct	llinfo_arp llinfo_arp;		/* head of the llinfo queue */
@@ -170,7 +165,6 @@ struct	ifqueue arpintrq;
 void	in_arpinput __P((struct mbuf *));
 void	arpwhohas __P((struct arpcom *, struct in_addr *));
 
-#ifdef MULTICAST
 /*
  * Ethernet multicast address structure.  There is one of these for each
  * multicast address or range of multicast addresses that we are supposed
@@ -237,6 +231,5 @@ struct ether_multistep {
 	(step).e_enm = (ac)->ac_multiaddrs; \
 	ETHER_NEXT_MULTI((step), (enm)); \
 }
-#endif
 #endif
 #endif
