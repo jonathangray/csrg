bd501d7e90d51bb3bc5a18cfe7f9faf3cc6cba2a
Keith Bostic <bostic>
1993-01-11 11:54:00 -0800

    CURSES_{OK,ERR} -> OK,ERR
    SCCS-vsn: 5.10

--- lib/libcurses/scroll.c
+++ lib/libcurses/scroll.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)scroll.c	5.9 (Berkeley) 12/30/92";
+static char sccsid[] = "@(#)scroll.c	5.10 (Berkeley) 01/11/93";
 #endif /* not lint */
 
 #include <curses.h>
@@ -53,7 +53,7 @@ scroll(win)
 #endif
 
 	if (!(win->flags & __SCROLLOK))
-		return (CURSES_ERR);
+		return (ERR);
 
 	getyx(win, oy, ox);
 	wmove(win, 0, 0);
@@ -68,5 +68,5 @@ scroll(win)
 		__TRACE("scroll: win == curscr\n");
 #endif
 	}
-	return (CURSES_OK);
+	return (OK);
 }
