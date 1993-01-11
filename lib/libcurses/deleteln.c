2cb8564508181fc1c10dc98b1ec68db9fcc9c983
Keith Bostic <bostic>
1993-01-11 11:53:45 -0800

    CURSES_{OK,ERR} -> OK,ERR
    SCCS-vsn: 5.15

--- lib/libcurses/deleteln.c
+++ lib/libcurses/deleteln.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deleteln.c	5.14 (Berkeley) 12/30/92";
+static char sccsid[] = "@(#)deleteln.c	5.15 (Berkeley) 01/11/93";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -76,5 +76,5 @@ wdeleteln(win)
 	__touchline(win, y, 0, win->maxx - 1, 0);
 	if (win->orig == NULL)
 		__id_subwins(win);
-	return (CURSES_OK);
+	return (OK);
 }
