539b92e34ef9b07000df21945f68bb3b005afd9d
Keith Bostic <bostic>
1993-01-11 11:53:50 -0800

    CURSES_{OK,ERR} -> OK,ERR
    SCCS-vsn: 5.12

--- lib/libcurses/initscr.c
+++ lib/libcurses/initscr.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)initscr.c	5.11 (Berkeley) 12/30/92";
+static char sccsid[] = "@(#)initscr.c	5.12 (Berkeley) 01/11/93";
 #endif	/* not lint */
 
 #include <curses.h>
@@ -54,7 +54,7 @@ initscr()
 	__echoit = 1;
         __pfast = __rawmode = __noqch = 0;
 
-	if (gettmode() == CURSES_ERR)
+	if (gettmode() == ERR)
 		return (NULL);
 
 	/*
@@ -63,7 +63,7 @@ initscr()
 	 */
 	if (My_term || (sp = getenv("TERM")) == NULL)
 		sp = Def_term;
-	if (setterm(sp) == CURSES_ERR)
+	if (setterm(sp) == ERR)
 		return (NULL);
 
 	/* Need either homing or cursor motion for refreshes */
@@ -75,13 +75,13 @@ initscr()
 
 	if (curscr != NULL)
 		delwin(curscr);
-	if ((curscr = newwin(LINES, COLS, 0, 0)) == CURSES_ERR)
+	if ((curscr = newwin(LINES, COLS, 0, 0)) == ERR)
 		return (NULL);
 	clearok(curscr, 1);
 
 	if (stdscr != NULL)
 		delwin(stdscr);
-	if ((stdscr = newwin(LINES, COLS, 0, 0)) == CURSES_ERR) {
+	if ((stdscr = newwin(LINES, COLS, 0, 0)) == ERR) {
 		delwin(curscr);
 		return (NULL);
 	}
