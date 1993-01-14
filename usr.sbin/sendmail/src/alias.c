8335d7706e565a77470d946f1387e6bcbde3cc10
Eric Allman <eric>
1993-01-14 08:49:47 -0800

    fix compilation problem with -DNDBM only
    SCCS-vsn: 6.4

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -56,12 +56,12 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 
 #ifndef lint
 #ifdef NEWDB
-static char sccsid[] = "@(#)alias.c	6.3 (Berkeley) 01/09/93 (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.4 (Berkeley) 01/14/93 (with NEWDB)";
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.3 (Berkeley) 01/09/93 (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.4 (Berkeley) 01/14/93 (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.3 (Berkeley) 01/09/93 (without NDBM)";
+static char sccsid[] = "@(#)alias.c	6.4 (Berkeley) 01/14/93 (without NDBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -227,6 +227,7 @@ aliaslookup(name)
 		return (rhs.dbm.dptr);
 	}
 # endif /* NDBM */
+	return (NULL);
 # else /* not NEWDB */
 	rhs.dbm = dbm_fetch(AliasDBMptr, lhs.dbm);
 	return (rhs.dbm.dptr);
@@ -235,8 +236,8 @@ aliaslookup(name)
 	s = stab(keybuf, ST_ALIAS, ST_FIND);
 	if (s != NULL)
 		return (s->s_alias);
-# endif
 	return (NULL);
+# endif
 }
 /*
 **  INITALIASES -- initialize for aliasing
