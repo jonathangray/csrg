dd7f3ccc23592a976e33876360dbd2c35130f2f1
Dave Borman <dab>
1993-01-19 12:47:20 -0800

    Fix up the code that deals with the flowcontrol option to properly deal with the RESTART_ANY and RESTART_XON suboptions.
    SCCS-vsn: 5.12

--- libexec/telnetd/state.c
+++ libexec/telnetd/state.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)state.c	5.11 (Berkeley) 12/18/92";
+static char sccsid[] = "@(#)state.c	5.12 (Berkeley) 01/19/93";
 #endif /* not lint */
 
 #include "telnetd.h"
@@ -644,6 +644,9 @@ willoption(option)
 			func = encrypt_send_support;
 			break;
 #endif
+		case TELOPT_LFLOW:
+			func = localstat;
+			break;
 		}
 	    }
 	}
