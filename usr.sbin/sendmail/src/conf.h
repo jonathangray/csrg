eacb709c3b971612c26f9d24d1ace08b2005a516
Eric Allman <eric>
1993-01-20 13:21:52 -0800

    fix problem with address delimitor inside quotes; define $k and $=k to be the name (from the uname call)
    SCCS-vsn: 6.3

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)conf.h	6.2 (Berkeley) 01/13/93
+ *	@(#)conf.h	6.3 (Berkeley) 01/20/93
  */
 
 /*
@@ -103,6 +103,7 @@
 
 # define LOCKF		1	/* use System V lockf instead of flock */
 # define SYS5TZ		1	/* use System V style timezones */
+# define UNAME		1	/* use System V uname system call */
 
 # endif
 
@@ -140,3 +141,16 @@
 # define VA_END		va_end(ap)
 
 # endif
+
+#ifdef UNAME
+# include <sys/utsname.h>
+# ifdef newstr
+#  undef newstr
+# endif
+#else /* ! UNAME */
+# define NODE_LENGTH 32
+struct utsname
+{
+	char nodename[NODE_LENGTH+1];
+};
+#endif /* UNAME */
