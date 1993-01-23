35dcdf33f44dfdd1ef4de2d2314890ecdaa032a8
Keith Bostic <bostic>
1993-01-23 11:13:23 -0800

    date and time created 93/01/23 11:13:23 by bostic
    SCCS-vsn: 5.1

---
+++ contrib/ed/rol.c
@@ -0,0 +1,86 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Rodney Ruddock of the University of Guelph.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)rol.c	5.1 (Berkeley) 01/23/93";
+#endif /* not lint */
+
+#include "ed.h"
+
+/*
+ * After the command check the rest of the line to see nothing illegal
+ * is following. Any single instance of a printsfx suffix is the only
+ * legal thing to follow ( that is, a 'l', 'n', or 'p'). If a suffix
+ * occurs the execution of that suffix occurs back in the cmd_loop
+ * function after the command that called this function finishes
+ * successfully.
+ */
+
+int
+rol(inputt, errnum)
+
+FILE *inputt;
+int *errnum;
+
+{
+
+  ss = getc(inputt);
+  printsfx = 0;
+  /* only one of the suffix is allowed */
+  if (ss == 'p')
+    printsfx = 1;
+  else if (ss == 'n')
+    printsfx = 2;
+  else if (ss == 'l')
+    printsfx = 4;
+  else
+    ungetc(ss, inputt);
+
+  while (1)
+     {
+       ss = getc(inputt);
+       if ((ss != ' ') && (ss != '\n') && (ss != EOF))
+         {
+           *errnum = -1;
+           strcpy(help_msg, "illegal command option");
+           return(1);
+         }
+       if ((ss == '\n') || (ss == EOF))
+         break;
+     }
+
+return(0); /* rest-of-line was okay */
+} /* end-rol */
