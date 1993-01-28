3c208d9d49844aaaab8430b4a89e850960e5b1e3
Eric Allman <eric>
1993-01-28 10:16:02 -0800

    System 5 compatibility, misc fixes
    SCCS-vsn: 6.3

--- usr.sbin/sendmail/src/udb.c
+++ usr.sbin/sendmail/src/udb.c
@@ -34,9 +34,9 @@
 
 #ifndef lint
 #ifdef USERDB
-static char sccsid [] = "@(#)udb.c	6.2 (Berkeley) 01/20/93 (with USERDB)";
+static char sccsid [] = "@(#)udb.c	6.3 (Berkeley) 01/28/93 (with USERDB)";
 #else
-static char sccsid [] = "@(#)udb.c	6.2 (Berkeley) 01/20/93 (without USERDB)";
+static char sccsid [] = "@(#)udb.c	6.3 (Berkeley) 01/28/93 (without USERDB)";
 #endif
 #endif
 
@@ -183,8 +183,6 @@ udbexpand(a, sendq, e)
 	for (up = UdbEnts; !breakout; up++)
 	{
 		char *user;
-		struct timeval timeout;
-		fd_set fdset;
 
 		/*
 		**  Select action based on entry type.
