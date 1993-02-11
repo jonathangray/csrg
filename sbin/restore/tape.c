daa3aa9611a9e8a0344ec59b7b5502db718621e2
Keith Bostic <bostic>
1993-02-11 08:28:43 -0800

    gcc -Wall from Craig Leres (no substantive changes)
    SCCS-vsn: 5.36

--- sbin/restore/tape.c
+++ sbin/restore/tape.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)tape.c	5.35 (Berkeley) 11/11/92";
+static char sccsid[] = "@(#)tape.c	5.36 (Berkeley) 02/11/93";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -176,7 +176,7 @@ setup()
 	vprintf(stdout, "Verify tape and initialize maps\n");
 #ifdef RRESTORE
 	if (host)
-		mt = rmtopen(magtape, O_RDONLY, 0);
+		mt = rmtopen(magtape, 0);
 	else
 #endif
 	if (pipein)
