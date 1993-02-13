128a25d3cf4c43e6f0528f4ebb262962c7e42f9a
Andrew Cherenson <andrew>
1993-02-12 23:04:03 -0800

    for INP_HDRINCL, tell ip_output to not clobber header, so pkts
    SCCS-vsn: 7.12

--- sys/netinet/raw_ip.c
+++ sys/netinet/raw_ip.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)raw_ip.c	7.11 (Berkeley) 01/08/93
+ *	@(#)raw_ip.c	7.12 (Berkeley) 02/12/93
  */
 
 #include <sys/param.h>
