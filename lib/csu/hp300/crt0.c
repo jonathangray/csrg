53fd49b3ca604c45a2bb7979c657ae25759e6a76
Keith Bostic <bostic>
1993-02-12 17:51:26 -0800

    add __progname global variable
    SCCS-vsn: 5.8

--- lib/csu/hp300/crt0.c
+++ lib/csu/hp300/crt0.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)crt0.c	5.7 (Berkeley) 07/10/92";
+static char sccsid[] = "@(#)crt0.c	5.8 (Berkeley) 02/12/93";
 #endif /* not lint */
 
 /*
@@ -55,7 +55,13 @@ static char sccsid[] = "@(#)crt0.c	5.7 (Berkeley) 07/10/92";
  *	allocated.
  */
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
 char **environ = (char **)0;
+static char empty[1];
+char *__progname = empty;
 static int fd;
 
 extern	unsigned char	etext;
@@ -84,9 +90,9 @@ start()
 #ifdef lint
 	kfp = 0;
 	initcode = initcode = 0;
-#else not lint
+#else
 	asm("lea a6@(4),%0" : "=r" (kfp));	/* catch it quick */
-#endif not lint
+#endif
 	for (argv = targv = &kfp->kargv[0]; *targv++; /* void */)
 		/* void */ ;
 	if (targv >= (char **)(*argv))
@@ -107,13 +113,18 @@ asm("eprol:");
 		fd = open("/dev/null", 2);
 	} while (fd >= 0 && fd < 3);
 	close(fd);
-#endif paranoid
+#endif
 
 #ifdef MCRT0
 	atexit(_mcleanup);
 	monstartup(&eprol, &etext);
-#endif MCRT0
+#endif
 	errno = 0;
+	if (argv[0])
+		if ((__progname = strrchr(argv[0], '/')) == NULL)
+			__progname = argv[0];
+		else
+			++__progname;
 	exit(main(kfp->kargc, argv, environ));
 }
 
@@ -126,4 +137,4 @@ moncontrol(val)
 {
 
 }
-#endif /* CRT0 */
+#endif
