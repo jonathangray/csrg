44c4e69ba06fb77eb2198f8df02a281e9b6a25e2
Eric Allman <eric>
1993-03-01 12:43:43 -0800

    changes for ESMTP; mostly better delimiter character pointer passing
    SCCS-vsn: 6.21

--- usr.sbin/sendmail/src/recipient.c
+++ usr.sbin/sendmail/src/recipient.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	6.20 (Berkeley) 03/01/93";
+static char sccsid[] = "@(#)recipient.c	6.21 (Berkeley) 03/01/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -110,14 +110,14 @@ sendto(list, copyf, ctladdr, qflags)
 
 	for (p = list; *p != '\0'; )
 	{
+		auto char *delimptr;
 		register ADDRESS *a;
-		extern char *DelimChar;		/* defined in prescan */
 
 		/* parse the address */
 		while ((isascii(*p) && isspace(*p)) || *p == ',')
 			p++;
-		a = parseaddr(p, (ADDRESS *) NULL, 1, delimiter, e);
-		p = DelimChar;
+		a = parseaddr(p, (ADDRESS *) NULL, 1, delimiter, &delimptr, e);
+		p = delimptr;
 		if (a == NULL)
 			continue;
 		a->q_next = al;
