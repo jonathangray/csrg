67fa0e919abae97038b4d80c54b3fbc05c459ac8
Eric Allman <eric>
1993-03-01 06:53:37 -0800

    header file cleanup; primarily #include <unistd.h> in sendmail.h and delete duplicate cruft
    SCCS-vsn: 6.3

--- usr.sbin/sendmail/src/clock.c
+++ usr.sbin/sendmail/src/clock.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)clock.c	6.2 (Berkeley) 02/23/93";
+static char sccsid[] = "@(#)clock.c	6.3 (Berkeley) 03/01/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -231,6 +231,7 @@ tick()
 
 static bool	SleepDone;
 
+unsigned int
 sleep(intvl)
 	unsigned int intvl;
 {
