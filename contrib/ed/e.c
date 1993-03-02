ee64a249b90bfc0a81da6416aab4537a34a50f83
Keith Bostic <bostic>
1993-03-01 16:56:26 -0800

    STDIO fix
    SCCS-vsn: 5.4

--- contrib/ed/e.c
+++ contrib/ed/e.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)e.c	5.3 (Berkeley) 02/28/93";
+static char sccsid[] = "@(#)e.c	5.4 (Berkeley) 03/01/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -162,6 +162,7 @@ e2(inputt, errnum)
 	if (fhtmp == NULL) {
 		ed_exit(5); /* unable to create buffer */
 	}
+	fwrite("R", sizeof(char), 1, fhtmp);
 	file_seek = 0;
 #endif
 #ifdef DBI
