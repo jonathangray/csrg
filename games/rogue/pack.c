848864c4b95fa74d10e6773009e1f665f473e4b3
Keith Bostic <bostic>
1993-03-05 18:31:11 -0800

    if you throw something away, and type ?, you get an empty list from Rich $alz
    SCCS-vsn: 5.4

--- games/rogue/pack.c
+++ games/rogue/pack.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pack.c	5.3 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)pack.c	5.4 (Berkeley) 03/05/93";
 #endif /* not lint */
 
 /*
@@ -293,6 +293,7 @@ unsigned short mask;
 
 		if (ch == LIST) {
 			check_message();
+			mask = tmask;
 			inventory(&rogue.pack, mask);
 		} else {
 			break;
