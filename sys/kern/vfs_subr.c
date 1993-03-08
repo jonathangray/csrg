ac147a4df28fcb04397baab8005c3fcf9303f93d
Keith Bostic <bostic>
1993-03-08 10:28:06 -0800

    rename va_cflags -> va_vaflags
    SCCS-vsn: 7.97

--- sys/kern/vfs_subr.c
+++ sys/kern/vfs_subr.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vfs_subr.c	7.96 (Berkeley) 03/05/93
+ *	@(#)vfs_subr.c	7.97 (Berkeley) 03/08/93
  */
 
 /*
@@ -216,7 +216,7 @@ void vattr_null(vap)
 		vap->va_mtime.ts_sec = vap->va_mtime.ts_nsec =
 		vap->va_ctime.ts_sec = vap->va_ctime.ts_nsec =
 		vap->va_flags = vap->va_gen = VNOVAL;
-	vap->va_cflags = 0;
+	vap->va_vaflags = 0;
 }
 
 /*
