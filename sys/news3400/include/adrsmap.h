e88f59da042496a32052525e0a3d427aa689779b
Kazumasa Utashiro <utashiro>
1993-03-09 23:44:07 -0800

    cleanup header and machine dependency.
    SCCS-vsn: 7.3

--- sys/news3400/include/adrsmap.h
+++ sys/news3400/include/adrsmap.h
@@ -35,7 +35,7 @@
  *
  * from: $Hdr: adrsmap.h,v 4.300 91/06/09 06:34:29 root Rel41 $ SONY
  *
- *	@(#)adrsmap.h	7.2 (Berkeley) 12/17/92
+ *	@(#)adrsmap.h	7.3 (Berkeley) 03/09/93
  */
 
 /*
@@ -46,8 +46,6 @@
 
 #ifndef __ADRSMAP__
 #define	__ADRSMAP__	1
-
-#include <machine/fix_machine_type.h>
 
 #ifdef news3400
 /*----------------------------------------------------------------------
