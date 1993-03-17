109946ae686187c91070f2918122d3eb7081fe0d
Akito Fujita <akito>
1993-03-17 11:11:12 -0800

    header update
    SCCS-vsn: 7.2

--- sys/luna68k/luna68k/vectors.s
+++ sys/luna68k/luna68k/vectors.s
@@ -30,11 +30,9 @@
 | OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 | SUCH DAMAGE.
 |
-| OMRON:$Id: vectors.s,v 1.1 92/05/29 16:31:21 moti Exp $
+| from: hp300/hp300/vectors.s	7.3 (Berkeley) 6/5/92
 |
-| from: hp300/hp300/vectors.s	7.2 (Berkeley) 5/7/91
-|
-|	@(#)vectors.s	7.1 (Berkeley) 06/15/92
+|	@(#)vectors.s	7.2 (Berkeley) 03/17/93
 |
 
 #define	_fpbsun		_fpfault
