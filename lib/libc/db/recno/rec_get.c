207b46f47ddd82c45cb15d694f9af46cecf4099f
Keith Bostic <bostic>
1993-03-19 14:47:38 -0800

    add BTF_EOF flag to test instead of bt_reof int clean up mmap and file pointer uses in recno interface
    SCCS-vsn: 5.8

--- lib/libc/db/recno/rec_get.c
+++ lib/libc/db/recno/rec_get.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rec_get.c	5.7 (Berkeley) 02/20/93";
+static char sccsid[] = "@(#)rec_get.c	5.8 (Berkeley) 03/19/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -81,9 +81,12 @@ __rec_get(dbp, key, data, flags)
 	 * original file.
 	 */
 	t = dbp->internal;
-	if (nrec > t->bt_nrecs && (ISSET(t, BTF_RINMEM) ||
-	    (status = t->bt_irec(t, nrec)) != RET_SUCCESS))
+	if (nrec > t->bt_nrecs) {
+		if (ISSET(t, BTF_EOF | BTF_RINMEM))
+			return (RET_SPECIAL);
+		if ((status = t->bt_irec(t, nrec)) != RET_SUCCESS)
 			return (status);
+	}
 
 	--nrec;
 	if ((e = __rec_search(t, nrec, SEARCH)) == NULL)
@@ -115,9 +118,6 @@ __rec_fpipe(t, top)
 	int ch;
 	char *p;
 
-	if (t->bt_reof)
-		return (RET_SPECIAL);
-
 	data.data = t->bt_dbuf;
 	data.size = t->bt_reclen;
 
@@ -139,7 +139,7 @@ __rec_fpipe(t, top)
 			break;
 	}
 	if (nrec < top) {
-		t->bt_reof = 1;
+		SET(t, BTF_EOF);
 		return (RET_SPECIAL);
 	}
 	return (RET_SUCCESS);
@@ -167,9 +167,6 @@ __rec_vpipe(t, top)
 	int bval, ch;
 	char *p;
 
-	if (t->bt_reof)
-		return (RET_SPECIAL);
-
 	bval = t->bt_bval;
 	for (nrec = t->bt_nrecs; nrec < top; ++nrec) {
 		for (p = t->bt_dbuf, sz = t->bt_dbufsz;; *p++ = ch, --sz) {
@@ -194,7 +191,7 @@ __rec_vpipe(t, top)
 			break;
 	}
 	if (nrec < top) {
-		t->bt_reof = 1;
+		SET(t, BTF_EOF);
 		return (RET_SPECIAL);
 	}
 	return (RET_SUCCESS);
@@ -221,10 +218,7 @@ __rec_fmap(t, top)
 	size_t len;
 	char *p;
 
-	if (t->bt_reof)
-		return (RET_SPECIAL);
-
-	sp = t->bt_smap;
+	sp = t->bt_cmap;
 	ep = t->bt_emap;
 	data.data = t->bt_dbuf;
 	data.size = t->bt_reclen;
@@ -236,7 +230,7 @@ __rec_fmap(t, top)
 	}
 	for (nrec = t->bt_nrecs; nrec < top; ++nrec) {
 		if (sp >= ep) {
-			t->bt_reof = 1;
+			SET(t, BTF_EOF);
 			return (RET_SPECIAL);
 		}
 		len = t->bt_reclen;
@@ -245,7 +239,7 @@ __rec_fmap(t, top)
 		if (__rec_iput(t, nrec, &data, 0) != RET_SUCCESS)
 			return (RET_ERROR);
 	}
-	t->bt_smap = sp;
+	t->bt_cmap = sp;
 	return (RET_SUCCESS);
 }
 
@@ -269,16 +263,13 @@ __rec_vmap(t, top)
 	recno_t nrec;
 	int bval;
 
-	if (t->bt_reof)
-		return (RET_SPECIAL);
-
-	sp = t->bt_smap;
+	sp = t->bt_cmap;
 	ep = t->bt_emap;
 	bval = t->bt_bval;
 
 	for (nrec = t->bt_nrecs; nrec < top; ++nrec) {
 		if (sp >= ep) {
-			t->bt_reof = 1;
+			SET(t, BTF_EOF);
 			return (RET_SPECIAL);
 		}
 		for (data.data = sp; sp < ep && *sp != bval; ++sp);
@@ -287,6 +278,6 @@ __rec_vmap(t, top)
 			return (RET_ERROR);
 		++sp;
 	}
-	t->bt_smap = sp;
+	t->bt_cmap = sp;
 	return (RET_SUCCESS);
 }
