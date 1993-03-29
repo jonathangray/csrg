35381e6d977d1d8ee06b5af06d5c27737726fed8
Eric Allman <eric>
1993-03-29 11:39:01 -0800

    do recursive evaluation of CNAMEs
    SCCS-vsn: 6.14

--- usr.sbin/sendmail/src/domain.c
+++ usr.sbin/sendmail/src/domain.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef NAMED_BIND
-static char sccsid[] = "@(#)domain.c	6.13 (Berkeley) 03/23/93 (with name server)";
+static char sccsid[] = "@(#)domain.c	6.14 (Berkeley) 03/29/93 (with name server)";
 #else
-static char sccsid[] = "@(#)domain.c	6.13 (Berkeley) 03/23/93 (without name server)";
+static char sccsid[] = "@(#)domain.c	6.14 (Berkeley) 03/29/93 (without name server)";
 #endif
 #endif /* not lint */
 
@@ -378,6 +378,7 @@ getcanonname(host, hbsize)
 	**  Now run through the search list for the name in question.
 	*/
 
+cnameloop:
 	dp = searchlist;
 	mxmatch = NULL;
 	qtype = T_ANY;
@@ -496,7 +497,14 @@ getcanonname(host, hbsize)
 					break;
 				(void)strncpy(host, nbuf, hbsize); /* XXX */
 				host[hbsize - 1] = '\0';
-				return TRUE;
+
+				/*
+				**  RFC 1034 section 3.6 specifies that CNAME
+				**  should point at the canonical name -- but
+				**  urges software to try again anyway.
+				*/
+
+				goto cnameloop;
 
 			  default:
 				/* not a record of interest */
