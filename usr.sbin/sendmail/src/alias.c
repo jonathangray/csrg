1e2bc52e0ef32b182b2cd21acb0e107c804eb6a3
Eric Allman <eric>
1993-03-30 15:50:01 -0800

    hooks for eventual better handling of SMTP recipients
    SCCS-vsn: 6.29

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -52,15 +52,15 @@ ERROR: DBM is no longer supported -- use NDBM instead.
 #ifndef lint
 #ifdef NEWDB
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.28 (Berkeley) 03/29/93 (with NEWDB and NDBM)";
+static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (with NEWDB and NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.28 (Berkeley) 03/29/93 (with NEWDB)";
+static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (with NEWDB)";
 #endif
 #else
 #ifdef NDBM
-static char sccsid[] = "@(#)alias.c	6.28 (Berkeley) 03/29/93 (with NDBM)";
+static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (with NDBM)";
 #else
-static char sccsid[] = "@(#)alias.c	6.28 (Berkeley) 03/29/93 (without NEWDB or NDBM)";
+static char sccsid[] = "@(#)alias.c	6.29 (Berkeley) 03/30/93 (without NEWDB or NDBM)";
 #endif
 #endif
 #endif /* not lint */
@@ -173,6 +173,7 @@ alias(a, sendq, e)
 	if (bitset(EF_VRFYONLY, e->e_flags))
 	{
 		a->q_flags |= QVERIFIED;
+		e->e_nrcpts++;
 		return;
 	}
 	message("aliased to %s", p);
