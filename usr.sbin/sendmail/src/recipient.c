acc949ad1421b8ab39c4d895037962ac29ffcb3c
Eric Allman <eric>
1993-03-30 13:28:41 -0800

    attempt at quick RCPT response
    SCCS-vsn: 6.32.1.1

--- usr.sbin/sendmail/src/recipient.c
+++ usr.sbin/sendmail/src/recipient.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	6.32 (Berkeley) 03/30/93";
+static char sccsid[] = "@(#)recipient.c	6.32.1.1 (Berkeley) 03/30/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -295,6 +295,10 @@ recipient(a, sendq, e)
 	{
 		if (sameaddr(q, a))
 		{
+			/* if this is a reinsertion, just go ahead */
+			if (bitset(QVERIFIED, q->q_flags))
+				break;
+
 			if (tTd(26, 1))
 			{
 				printf("%s in sendq: ", a->q_paddr);
@@ -313,8 +317,13 @@ recipient(a, sendq, e)
 	}
 
 	/* add address on list */
-	*pq = a;
-	a->q_next = NULL;
+	if (*pq != a)
+	{
+		*pq = a;
+		a->q_next = NULL;
+	}
+
+	a->q_flags &= ~QVERIFIED;
 
 	/*
 	**  Alias the name and handle special mailer types.
