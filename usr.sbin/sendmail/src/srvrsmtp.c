3711cf3902acdb38c7f7c75d394872cfb5a329f8
Eric Allman <eric>
1993-04-09 15:55:19 -0700

    set $s after runinchild; runinchild calls newenvelope which clears the macros
    SCCS-vsn: 6.40

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	6.39 (Berkeley) 04/04/93 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.40 (Berkeley) 04/09/93 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	6.39 (Berkeley) 04/04/93 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	6.40 (Berkeley) 04/09/93 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -143,6 +143,7 @@ smtp(e)
 	message("220 %s", inp);
 	SmtpPhase = "startup";
 	protocol = NULL;
+	sendinghost = macvalue('s', e);
 	gothello = FALSE;
 	gotmail = FALSE;
 	for (;;)
@@ -226,7 +227,7 @@ smtp(e)
 				SmtpPhase = "HELO";
 			}
 			setproctitle("%s: %s", CurHostName, inp);
-			define('s', newstr(p), e);
+			sendinghost = newstr(p);
 			if (strcasecmp(p, RealHostName) != 0)
 			{
 				auth_warning(e, "Host %s claimed to be %s",
@@ -234,7 +235,7 @@ smtp(e)
 			}
 			p = macvalue('_', e);
 			if (p == NULL)
-				p = macvalue('s', e);
+				p = RealHostName;
 			message("250", "%s Hello %s, pleased to meet you", HostName, p);
 			break;
 
@@ -245,8 +246,8 @@ smtp(e)
 			if (!gothello)
 			{
 				/* set sending host to our known value */
-				if (macvalue('s', e) == NULL)
-					define('s', RealHostName, e);
+				if (sendinghost == NULL)
+					sendinghost = RealHostName;
 
 				if (bitset(PRIV_NEEDMAILHELO, PrivacyFlags))
 				{
@@ -278,6 +279,7 @@ smtp(e)
 			if (protocol == NULL)
 				protocol = "SMTP";
 			define('r', protocol, e);
+			define('s', sendinghost, e);
 			initsys(e);
 			setproctitle("%s %s: %s", e->e_id, CurHostName, inp);
 
