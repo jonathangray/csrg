8451bb8397642324d6f1a70da591aa4530f4ac80
Eric Allman <eric>
1993-04-12 14:18:20 -0700

    shorten timeout on reverse name lookup to avoid conflict with higher level timeouts
    SCCS-vsn: 6.35

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -39,9 +39,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	6.34 (Berkeley) 04/12/93 (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	6.35 (Berkeley) 04/12/93 (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	6.34 (Berkeley) 04/12/93 (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	6.35 (Berkeley) 04/12/93 (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -50,6 +50,11 @@ static char sccsid[] = "@(#)daemon.c	6.34 (Berkeley) 04/12/93 (without daemon mo
 # include <netdb.h>
 # include <sys/wait.h>
 # include <sys/time.h>
+
+#ifdef NAMED_BIND
+# include <arpa/nameser.h>
+# include <resolv.h>
+#endif
 
 /*
 **  DAEMON.C -- routines to use when running as a daemon.
@@ -841,6 +846,14 @@ hostnamebyanyaddr(sap)
 {
 	register struct hostent *hp;
 
+#ifdef NAMED_BIND
+	int saveretry;
+
+	/* shorten name server timeout to avoid higher level timeouts */
+	saveretry = _res.retry;
+	_res.retry = 3;
+#endif /* NAMED_BIND */
+
 	switch (sap->sa.sa_family)
 	{
 #ifdef NETINET
@@ -865,6 +878,10 @@ hostnamebyanyaddr(sap)
 			   sap->sa.sa_family);
 		break;
 	}
+
+#ifdef NAMED_BIND
+	_res.retry = saveretry;
+#endif /* NAMED_BIND */
 
 	if (hp != NULL)
 		return hp->h_name;
