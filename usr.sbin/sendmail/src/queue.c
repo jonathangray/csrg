67bed480f399385fcd7921c2332c9fc4f933b5bc
Eric Allman <eric>
1993-04-15 07:41:19 -0700

    add e_bodytype and B queue line and -B flag for body type information
    SCCS-vsn: 6.47

--- usr.sbin/sendmail/src/queue.c
+++ usr.sbin/sendmail/src/queue.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	6.46 (Berkeley) 04/04/93 (with queueing)";
+static char sccsid[] = "@(#)queue.c	6.47 (Berkeley) 04/15/93 (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	6.46 (Berkeley) 04/04/93 (without queueing)";
+static char sccsid[] = "@(#)queue.c	6.47 (Berkeley) 04/15/93 (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -179,7 +179,9 @@ notemp:
 	/* output creation time */
 	fprintf(tfp, "T%ld\n", e->e_ctime);
 
-	/* output name of data file */
+	/* output type and name of data file */
+	if (e->e_bodytype != NULL)
+		fprintf(tfp, "B%s\n", e->e_bodytype);
 	fprintf(tfp, "D%s\n", e->e_df);
 
 	/* message from envelope, if it exists */
@@ -1012,6 +1014,10 @@ readqf(e)
 			setsender(newstr(&bp[1]), e, NULL, TRUE);
 			break;
 
+		  case 'B':		/* body type */
+			e->e_bodytype = newstr(&bp[1]);
+			break;
+
 		  case 'D':		/* data file name */
 			e->e_df = newstr(&bp[1]);
 			e->e_dfp = fopen(e->e_df, "r");
@@ -1167,6 +1173,7 @@ printqueue()
 		long dfsize = -1;
 		int flags = 0;
 		char message[MAXLINE];
+		char bodytype[MAXNAME];
 		extern bool shouldqueue();
 		extern bool lockfile();
 
@@ -1185,7 +1192,7 @@ printqueue()
 			printf(" ");
 		errno = 0;
 
-		message[0] = '\0';
+		message[0] = bodytype[0] = '\0';
 		while (fgets(buf, sizeof buf, f) != NULL)
 		{
 			register int i;
@@ -1201,6 +1208,13 @@ printqueue()
 				message[i] = '\0';
 				break;
 
+			  case 'B':	/* body type */
+				if ((i = strlen(&buf[1])) >= sizeof bodytype)
+					i = sizeof bodytype - 1;
+				bcopy(&buf[1], bodytype, i);
+				bodytype[i] = '\0';
+				break;
+
 			  case 'S':	/* sender name */
 				if (Verbose)
 					printf("%8ld %10ld%c%.12s %.38s",
@@ -1212,8 +1226,12 @@ printqueue()
 				else
 					printf("%8ld %.16s %.45s", dfsize,
 					    ctime(&submittime), &buf[1]);
-				if (message[0] != '\0')
-					printf("\n\t\t (%.60s)", message);
+				if (message[0] != '\0' || bodytype[0] != '\0')
+				{
+					printf("\n    %10.10s", bodytype);
+					if (message[0] != '\0')
+						printf("   (%.60s)", message);
+				}
 				break;
 
 			  case 'C':	/* controlling user */
