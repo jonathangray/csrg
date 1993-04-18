e7d23cb394721b930751b29cfe89aef03b6aadf6
Chris Torek <torek>
1993-04-18 04:23:13 -0700

    rm rcs headers (commit to maintenance on vangogh)
    SCCS-vsn: 5.3

--- usr.sbin/config.new/mkioconf.c
+++ usr.sbin/config.new/mkioconf.c
@@ -39,9 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mkioconf.c	5.2 (Berkeley) 01/15/93
- *
- * from: $Header: mkioconf.c,v 1.2 92/12/08 19:34:31 torek Exp $
+ *	@(#)mkioconf.c	5.3 (Berkeley) 04/18/93
  */
 
 #include <sys/param.h>
