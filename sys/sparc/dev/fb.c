30720cc0f70febe2ae747c445d8c6abff6d021bc
Chris Torek <torek>
1993-04-20 05:49:24 -0700

    spelling
    SCCS-vsn: 7.4

--- sys/sparc/dev/fb.c
+++ sys/sparc/dev/fb.c
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fb.c	7.3 (Berkeley) 10/11/92
+ *	@(#)fb.c	7.4 (Berkeley) 04/20/93
  *
- * from: $Header: fb.c,v 1.6 92/06/17 05:35:46 torek Exp $
+ * from: $Header: fb.c,v 1.7 92/11/26 01:12:48 torek Exp $
  */
 
 /*
