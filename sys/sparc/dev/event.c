5348fbaf0bd292e164b5f85151f8273a65e93203
Chris Torek <torek>
1993-04-20 05:48:10 -0700

    spelling
    SCCS-vsn: 7.4

--- sys/sparc/dev/event.c
+++ sys/sparc/dev/event.c
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)event.c	7.3 (Berkeley) 10/11/92
+ *	@(#)event.c	7.4 (Berkeley) 04/20/93
  *
- * from: $Header: event.c,v 1.4 92/06/17 05:35:45 torek Exp $ (LBL)
+ * from: $Header: event.c,v 1.5 92/11/26 01:10:44 torek Exp $ (LBL)
  */
 
 /*
