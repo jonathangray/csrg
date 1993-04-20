ec3460186403d4e2b2524a996c07aa38b40b0460
Chris Torek <torek>
1993-04-20 06:42:29 -0700

    spelling
    SCCS-vsn: 7.3

--- sys/sparc/include/ctlreg.h
+++ sys/sparc/include/ctlreg.h
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ctlreg.h	7.2 (Berkeley) 07/21/92
+ *	@(#)ctlreg.h	7.3 (Berkeley) 04/20/93
  *
- * from: $Header: ctlreg.h,v 1.4 92/06/17 05:22:02 torek Exp $
+ * from: $Header: ctlreg.h,v 1.5 92/11/26 02:04:33 torek Exp $
  */
 
 /*
