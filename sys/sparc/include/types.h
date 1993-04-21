957128945113417630924d9234f943437c0692e9
Chris Torek <torek>
1993-04-20 18:56:40 -0700

    spelling
    SCCS-vsn: 7.4

--- sys/sparc/include/types.h
+++ sys/sparc/include/types.h
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)types.h	7.3 (Berkeley) 07/21/92
+ *	@(#)types.h	7.4 (Berkeley) 04/20/93
  *
- * from: $Header: types.h,v 1.4 92/06/17 06:10:30 torek Exp $ (LBL)
+ * from: $Header: types.h,v 1.5 92/11/26 02:00:07 torek Exp $ (LBL)
  */
 
 #ifndef	_MACHTYPES_H_
