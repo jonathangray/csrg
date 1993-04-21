1c6e12b83ac0016ad73f4d43865fec4fdc3b6459
Chris Torek <torek>
1993-04-20 19:28:59 -0700

    spelling
    SCCS-vsn: 7.4

--- sys/sparc/sbus/bwtwo.c
+++ sys/sparc/sbus/bwtwo.c
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)bwtwo.c	7.3 (Berkeley) 10/11/92
+ *	@(#)bwtwo.c	7.4 (Berkeley) 04/20/93
  *
- * from: $Header: bwtwo.c,v 1.13 92/06/17 06:59:29 torek Exp $
+ * from: $Header: bwtwo.c,v 1.14 92/11/26 02:28:04 torek Exp $
  */
 
 /*
@@ -49,8 +49,6 @@
  *
  * Does not handle interrupts, even though they can occur.
  */
-
-#include "bwtwo.h"
 
 #include <sys/param.h>
 #include <sys/device.h>
