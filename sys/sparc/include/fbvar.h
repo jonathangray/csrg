575b6fe68f1d7a0e8ce4d0a14e99ca083eeb87d4
Chris Torek <torek>
1993-04-20 18:48:50 -0700

    spelling
    SCCS-vsn: 7.3

--- sys/sparc/include/fbvar.h
+++ sys/sparc/include/fbvar.h
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fbvar.h	7.2 (Berkeley) 07/21/92
+ *	@(#)fbvar.h	7.3 (Berkeley) 04/20/93
  *
- * from: $Header: fbvar.h,v 1.14 92/06/17 06:10:16 torek Exp $
+ * from: $Header: fbvar.h,v 1.15 92/11/26 02:04:34 torek Exp $
  */
 
 /*
