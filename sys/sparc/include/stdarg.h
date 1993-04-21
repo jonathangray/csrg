67e87d8843370b193a407a9e565c40fae1fc6fed
Chris Torek <torek>
1993-04-20 18:54:43 -0700

    spelling
    SCCS-vsn: 7.4

--- sys/sparc/include/stdarg.h
+++ sys/sparc/include/stdarg.h
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)stdarg.h	7.3 (Berkeley) 10/15/92
+ *	@(#)stdarg.h	7.4 (Berkeley) 04/20/93
  *
- * from: $Header: stdarg.h,v 1.6 92/10/02 00:08:01 torek Exp $
+ * from: $Header: stdarg.h,v 1.7 92/11/26 02:04:47 torek Exp $
  */
 
 /*
