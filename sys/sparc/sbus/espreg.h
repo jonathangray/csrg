016bec70e6b5d911cddeeef0ad284ee8803826ed
Chris Torek <torek>
1993-04-20 19:29:03 -0700

    spelling
    SCCS-vsn: 7.3

--- sys/sparc/sbus/espreg.h
+++ sys/sparc/sbus/espreg.h
@@ -9,7 +9,7 @@
  * All advertising materials mentioning features or use of this software
  * must display the following acknowledgement:
  *	This product includes software developed by the University of
- *	California, Lawrence Berkeley Laboratories.
+ *	California, Lawrence Berkeley Laboratory.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,9 +39,9 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)espreg.h	7.2 (Berkeley) 07/21/92
+ *	@(#)espreg.h	7.3 (Berkeley) 04/20/93
  *
- * from: $Header: espreg.h,v 1.6 92/06/17 06:59:35 torek Exp $ (LBL)
+ * from: $Header: espreg.h,v 1.7 92/11/26 02:28:10 torek Exp $ (LBL)
  *
  * Derived from Mary Baker's devSCSIC90.c from the Berkeley
  * Sprite project, which is:
