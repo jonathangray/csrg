c20cfcb22e8f5fe0dd6837e7b745020b2efd7f09
Eric Allman <eric>
1993-04-26 16:15:39 -0700

    fix an uninitialized variable problem
    SCCS-vsn: 6.36

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	6.35 (Berkeley) 04/26/93";
+static char sccsid[] = "@(#)savemail.c	6.36 (Berkeley) 04/26/93";
 #endif /* not lint */
 
 # include <pwd.h>
@@ -76,7 +76,7 @@ savemail(e)
 	register struct passwd *pw;
 	register FILE *fp;
 	int state;
-	auto ADDRESS *q;
+	auto ADDRESS *q = NULL;
 	char buf[MAXLINE+1];
 	extern struct passwd *getpwnam();
 	register char *p;
@@ -324,7 +324,6 @@ savemail(e)
 			}
 			if (e->e_dfp != NULL)
 			{
-				auto ADDRESS *q;
 				bool oldverb = Verbose;
 
 				/* we have a home directory; open dead.letter */
