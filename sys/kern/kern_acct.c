4d9ae88a1452eeb2c6f4b7a61e13fb5f385d7213
Kirk McKusick <mckusick>
1993-04-27 16:11:51 -0700

    bug fixes for LEASES from Rick Macklem
    SCCS-vsn: 7.28

--- sys/kern/kern_acct.c
+++ sys/kern/kern_acct.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)kern_acct.c	7.27 (Berkeley) 02/22/93
+ *	@(#)kern_acct.c	7.28 (Berkeley) 04/27/93
  */
 
 #include <sys/param.h>
@@ -166,6 +166,7 @@ acct(p)
 	else
 		ap->ac_tty = NODEV;
 	ap->ac_flag = p->p_acflag;
+	LEASE_CHECK(vp, p, p->p_ucred, LEASE_WRITE);
 	return (vn_rdwr(UIO_WRITE, vp, (caddr_t)ap, sizeof (acctbuf), (off_t)0,
 		UIO_SYSSPACE, IO_UNIT|IO_APPEND, p->p_ucred, (int *)0,
 		(struct proc *)0));
