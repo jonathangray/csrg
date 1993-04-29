6134a2262927ab27ad25b03d91a6a8f7814c5af8
Keith Bostic <bostic>
1993-04-28 18:07:13 -0700

    corrections for echoing the last command when "!!" is given, undo working when 0 or 1 lines were in the buffer before the last buffer modification, a RE interpretation correction; from Rodney Ruddock
    SCCS-vsn: 5.6

--- contrib/ed/q.c
+++ contrib/ed/q.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)q.c	5.5 (Berkeley) 03/18/93";
+static char sccsid[] = "@(#)q.c	5.6 (Berkeley) 04/28/93";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -116,5 +116,5 @@ q(inputt, errnum)
 #ifndef MEMORY
 	unlink(template);
 #endif
-	exit(0);
+	exit(exit_code);
 }
