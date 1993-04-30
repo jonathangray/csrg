ac5a3940015d6b6789415728ec2c021737f373db
Keith Bostic <bostic>
1993-04-30 09:55:51 -0700

    int8: char -> signed char
    SCCS-vsn: 7.8

--- sys/i386/include/types.h
+++ sys/i386/include/types.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)types.h	7.7 (Berkeley) 04/29/93
+ *	@(#)types.h	7.8 (Berkeley) 04/30/93
  */
 
 #ifndef	_MACHTYPES_H_
@@ -47,7 +47,11 @@ typedef struct label_t {
 typedef	u_long	vm_offset_t;
 typedef	u_long	vm_size_t;
 
-typedef	char			   int8;	/* Basic integral types. */
+/*
+ * Basic integral types.  Omit the typedef if
+ * not possible for a machine/compiler combination.
+ */
+typedef	signed char		   int8;
 typedef	unsigned char		 u_int8;
 typedef	short			  int16;
 typedef	unsigned short		u_int16;
