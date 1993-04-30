50459a12d3797b8a9274fbe9df919eb0f124f20e
Eric Allman <eric>
1993-04-30 12:54:03 -0700

    don't change alpha case on header field names
    SCCS-vsn: 6.60

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	6.59 (Berkeley) 04/27/93";
+static char sccsid[] = "@(#)main.c	6.60 (Berkeley) 04/30/93";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -1505,6 +1505,6 @@ auth_warning(e, msg, va_alist)
 		VA_START(msg);
 		vsprintf(p, msg, ap);
 		VA_END;
-		addheader("x-authentication-warning", buf, e);
+		addheader("X-Authentication-Warning", buf, e);
 	}
 }
