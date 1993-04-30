258e9fd71d3ec564953e2c6c7540b99114e8a378
Keith Bostic <bostic>
1993-04-30 14:02:55 -0700

    integral types: one more time, with feeling
    SCCS-vsn: 7.13

--- sys/hp300/include/types.h
+++ sys/hp300/include/types.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)types.h	7.12 (Berkeley) 04/30/93
+ *	@(#)types.h	7.13 (Berkeley) 04/30/93
  */
 
 #ifndef	_MACHTYPES_H_
