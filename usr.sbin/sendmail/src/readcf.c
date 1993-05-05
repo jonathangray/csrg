b0a92bbe499d64e5a0992a8b2a8b6a9d21d3fcab
Eric Allman <eric>
1993-05-04 18:49:03 -0700

    fix a wierdness on handling -o override options
    SCCS-vsn: 6.35

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	6.34 (Berkeley) 05/04/93";
+static char sccsid[] = "@(#)readcf.c	6.35 (Berkeley) 05/04/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -993,7 +993,7 @@ setoption(opt, val, sticky)
 	**  See if this option is preset for us.
 	*/
 
-	if (safe && bitnset(opt, StickyOpt))
+	if (!sticky && bitnset(opt, StickyOpt))
 	{
 		if (tTd(37, 1))
 			printf(" (ignored)\n");
