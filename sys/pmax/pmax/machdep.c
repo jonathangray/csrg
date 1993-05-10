df0b60d7b448857f0af43d60ff33ede0f50c9613
Ralph Campbell <ralph>
1993-05-09 17:39:54 -0700

    new improved pmap changes
    SCCS-vsn: 7.20

--- sys/pmax/pmax/machdep.c
+++ sys/pmax/pmax/machdep.c
@@ -36,7 +36,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)machdep.c	7.19 (Berkeley) 04/05/93
+ *	@(#)machdep.c	7.20 (Berkeley) 05/09/93
  */
 
 /* from: Utah $Hdr: machdep.c 1.63 91/04/24$ */
@@ -62,9 +62,7 @@
 #include <sys/shm.h>
 #endif
 
-#include <vm/vm.h>
 #include <vm/vm_kern.h>
-#include <vm/vm_page.h>
 
 #include <machine/cpu.h>
 #include <machine/reg.h>
@@ -215,9 +213,6 @@ mach_init(argc, argv, code, cv)
 	extern char edata[], end[];
 	extern char MachUTLBMiss[], MachUTLBMissEnd[];
 	extern char MachException[], MachExceptionEnd[];
-#ifdef ATTR
-	extern char *pmap_attributes;
-#endif
 
 	/* clear the BSS segment */
 	v = (caddr_t)pmax_round_page(end);
@@ -303,7 +298,8 @@ mach_init(argc, argv, code, cv)
 	 * This could be used for an idle process.
 	 */
 	nullproc.p_addr = (struct user *)v;
-	nullproc.p_md.md_regs = ((struct user *)v)->u_pcb.pcb_regs;
+	nullproc.p_md.md_regs = nullproc.p_addr->u_pcb.pcb_regs;
+	bcopy("nullproc", nullproc.p_comm, sizeof("nullproc"));
 	for (i = 0; i < UPAGES; i++) {
 		nullproc.p_md.md_upte[i] = firstaddr | PG_V | PG_M;
 		firstaddr += NBPG;
@@ -601,10 +597,6 @@ mach_init(argc, argv, code, cv)
 #ifdef SYSVSHM
 	valloc(shmsegs, struct shmid_ds, shminfo.shmmni);
 #endif
-#ifdef ATTR
-	/* this is allocated here just to save a few bytes */
-	valloc(pmap_attributes, char, physmem);
-#endif
 
 	/*
 	 * Determine how many buffers to allocate.
@@ -631,13 +623,12 @@ mach_init(argc, argv, code, cv)
 	/*
 	 * Clear allocated memory.
 	 */
-	v = (caddr_t)pmax_round_page(v);
 	bzero(start, v - start);
 
 	/*
 	 * Initialize the virtual memory system.
 	 */
-	pmap_bootstrap((vm_offset_t)MACH_CACHED_TO_PHYS(v));
+	pmap_bootstrap((vm_offset_t)v);
 }
 
 /*
@@ -824,16 +815,12 @@ cpu_startup()
 	register unsigned i;
 	register caddr_t v;
 	int base, residual;
-	extern long Usrptsize;
-	extern struct map *useriomap;
+	vm_offset_t minaddr, maxaddr;
+	vm_size_t size;
 #ifdef DEBUG
 	extern int pmapdebug;
 	int opmapdebug = pmapdebug;
-#endif
-	vm_offset_t minaddr, maxaddr;
-	vm_size_t size;
 
-#ifdef DEBUG
 	pmapdebug = 0;
 #endif
 
@@ -878,7 +865,7 @@ cpu_startup()
 	 * limits the number of processes exec'ing at any time.
 	 */
 	exec_map = kmem_suballoc(kernel_map, &minaddr, &maxaddr,
-				 16*NCARGS, TRUE);
+				 16 * NCARGS, TRUE);
 	/*
 	 * Allocate a submap for physio
 	 */
