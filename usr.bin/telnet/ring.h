fe5c342836e45e6267b1674d6596b1661ad213ee
Dave Borman <dab>
1993-05-20 10:37:20 -0700

    Mostly this changes "#if defined(ENCRYPTION)" to "#ifdef ENCRYPTION".  This will allow unifdef to be
    SCCS-vsn: 5.4

--- usr.bin/telnet/ring.h
+++ usr.bin/telnet/ring.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ring.h	5.3 (Berkeley) 12/18/92
+ *	@(#)ring.h	5.4 (Berkeley) 05/20/93
  */
 
 #if defined(P)
