fa1e82e8a2c554d3adcead155bc18e29bdbaac45
Eric Allman <eric>
1993-05-21 18:59:00 -0700

    changes to merge alias and map implementations
    SCCS-vsn: 6.54

--- usr.sbin/sendmail/src/parseaddr.c
+++ usr.sbin/sendmail/src/parseaddr.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	6.53 (Berkeley) 05/17/93";
+static char sccsid[] = "@(#)parseaddr.c	6.54 (Berkeley) 05/21/93";
 #endif /* not lint */
 
 #include "sendmail.h"
