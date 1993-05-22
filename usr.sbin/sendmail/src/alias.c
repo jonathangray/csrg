97cdfcb005458473d0a15232ee5e9dde8b313b74
Eric Allman <eric>
1993-05-22 11:52:45 -0700

    alias maps include null byte; don't add .db if it already exits
    SCCS-vsn: 6.48

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -37,7 +37,7 @@
 # include <pwd.h>
 
 #ifndef lint
-static char sccsid[] = "@(#)alias.c	6.47 (Berkeley) 05/21/93";
+static char sccsid[] = "@(#)alias.c	6.48 (Berkeley) 05/22/93";
 #endif /* not lint */
 
 
@@ -240,7 +240,7 @@ setalias(spec)
 		else
 		{
 			class = "implicit";
-			map->map_mflags = MF_OPTIONAL;
+			map->map_mflags = MF_OPTIONAL|MF_INCLNULL;
 		}
 
 		/* find end of spec */
