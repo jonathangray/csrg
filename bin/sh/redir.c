3beeadca46292c2c1a9ee7cca010a55e470f2e3c
Marc Teitelbaum <marc>
1993-05-24 15:40:02 -0700

    fixes to support old style handling of ` ` (quoting), and "for var do"
    SCCS-vsn: 5.4

--- bin/sh/redir.c
+++ bin/sh/redir.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)redir.c	5.3 (Berkeley) 06/23/92";
+static char sccsid[] = "@(#)redir.c	5.4 (Berkeley) 05/24/93";
 #endif /* not lint */
 
 /*
@@ -70,6 +70,12 @@ struct redirtab {
 
 MKINIT struct redirtab *redirlist;
 
+/* 
+ * We keep track of whether or not fd0 has been redirected.  This is for
+ * background commands, where we want to redirect fd0 to /dev/null only
+ * if it hasn't already been redirected.  
+*/
+int fd0_redirected = 0;
 
 #ifdef __STDC__
 STATIC void openredirect(union node *, char *);
@@ -124,6 +130,8 @@ redirect(redir, flags)
 		} else {
 			close(fd);
 		}
+                if (fd == 0)
+                        fd0_redirected++;
 		openredirect(n, memory);
 	}
 	if (memory[1])
@@ -257,6 +265,8 @@ popredir() {
 
 	for (i = 0 ; i < 10 ; i++) {
 		if (rp->renamed[i] != EMPTY) {
+                        if (i == 0)
+                                fd0_redirected--;
 			close(i);
 			if (rp->renamed[i] >= 0) {
 				copyfd(rp->renamed[i], i);
@@ -269,8 +279,6 @@ popredir() {
 	ckfree(rp);
 	INTON;
 }
-
-
 
 /*
  * Undo all redirections.  Called on error or interrupt.
@@ -291,6 +299,11 @@ SHELLPROC {
 
 #endif
 
+/* Return true if fd 0 has already been redirected at least once.  */
+int
+fd0_redirected_p () {
+        return fd0_redirected != 0;
+}
 
 /*
  * Discard all saved file descriptors.
