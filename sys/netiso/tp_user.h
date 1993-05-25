41f5d743757957cfa785b3ff2f60f12e0f274548
Keith Bostic <bostic>
1993-05-25 11:52:18 -0700

    trailing comment after #else or #endif
    SCCS-vsn: 7.14

--- sys/netiso/tp_user.h
+++ sys/netiso/tp_user.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tp_user.h	7.13 (Berkeley) 10/11/92
+ *	@(#)tp_user.h	7.14 (Berkeley) 05/25/93
  */
 
 /***********************************************************
@@ -159,4 +159,4 @@ struct tp_disc_reason {
  */
 
 
-#endif __TP_USER__
+#endif /* __TP_USER__ */
