0112ccef059d1a9020dfc9c2462fb7306cb11d2a
Keith Bostic <bostic>
1993-05-24 18:26:57 -0700

    merge in rev 5.16->5.17
    SCCS-vsn: 5.19

--- usr.bin/make/job.c
+++ usr.bin/make/job.c
@@ -37,7 +37,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)job.c	5.18 (Berkeley) 05/24/93";
+static char sccsid[] = "@(#)job.c	5.19 (Berkeley) 05/24/93";
 #endif /* not lint */
 
 /*-
@@ -97,16 +97,17 @@ static char sccsid[] = "@(#)job.c	5.18 (Berkeley) 05/24/93";
  */
 
 #include <sys/types.h>
-#include <sys/signal.h>
 #include <sys/stat.h>
 #include <sys/file.h>
 #include <sys/time.h>
 #include <sys/wait.h>
-#include <fcntl.h>
+
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
-#include <signal.h>
+
 #include "make.h"
 #include "hash.h"
 #include "dir.h"
