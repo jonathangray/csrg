0c168c00ee3aaea24f24c8c71cab0a82b2c1ba90
Chris Torek <torek>
1993-05-24 23:15:25 -0700

    pedantically ANSI, i.e., nothing after #else/#endif
    SCCS-vsn: 7.6

--- sys/vm/vm_map.h
+++ sys/vm/vm_map.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_map.h	7.5 (Berkeley) 05/04/92
+ *	@(#)vm_map.h	7.6 (Berkeley) 05/24/93
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -225,4 +225,4 @@ void		 vm_map_startup __P((void));
 int		 vm_map_submap __P((vm_map_t,
 		    vm_offset_t, vm_offset_t, vm_map_t));
 #endif
-#endif	_VM_MAP_
+#endif /* _VM_MAP_ */
