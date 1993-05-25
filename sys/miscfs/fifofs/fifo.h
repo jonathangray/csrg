f9254cc056d751cb04ac2217c19a733858eaebc8
Kirk McKusick <mckusick>
1993-05-25 15:32:17 -0700

    add fifo_pathconf
    SCCS-vsn: 7.8

--- sys/miscfs/fifofs/fifo.h
+++ sys/miscfs/fifofs/fifo.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fifo.h	7.7 (Berkeley) 07/02/92
+ *	@(#)fifo.h	7.8 (Berkeley) 05/25/93
  */
 
 #ifdef FIFO
@@ -72,6 +72,7 @@ int	fifo_bmap __P((struct vop_bmap_args *));
 #define fifo_strategy ((int (*) __P((struct  vop_strategy_args *)))fifo_badop)
 int	fifo_print __P((struct vop_print_args *));
 #define fifo_islocked ((int (*) __P((struct  vop_islocked_args *)))nullop)
+int	fifo_pathconf __P((struct vop_pathconf_args *));
 int	fifo_advlock __P((struct vop_advlock_args *));
 #define fifo_blkatoff ((int (*) __P((struct  vop_blkatoff_args *)))fifo_badop)
 #define fifo_valloc ((int (*) __P((struct  vop_valloc_args *)))fifo_badop)
