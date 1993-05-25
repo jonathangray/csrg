a6ea54aa41baeb0873ebdf03e8e049ad0fedc11a
Chris Torek <torek>
1993-05-24 23:40:33 -0700

    pedantically ANSI, i.e., nothing after #else/#endif
    SCCS-vsn: 7.18

--- sys/vm/vm_fault.c
+++ sys/vm/vm_fault.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_fault.c	7.17 (Berkeley) 03/09/93
+ *	@(#)vm_fault.c	7.18 (Berkeley) 05/24/93
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -959,7 +959,7 @@ void vm_fault_copy_entry(dst_map, src_map, dst_entry, src_entry)
 
 #ifdef	lint
 	src_map++;
-#endif	lint
+#endif
 
 	src_object = src_entry->object.vm_object;
 	src_offset = src_entry->offset;
