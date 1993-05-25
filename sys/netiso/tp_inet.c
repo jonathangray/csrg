74bc4d3201ef0e2c2a97fa2c447aeca2f0edad65
Keith Bostic <bostic>
1993-05-25 11:51:49 -0700

    trailing comment after #else or #endif
    SCCS-vsn: 7.12

--- sys/netiso/tp_inet.c
+++ sys/netiso/tp_inet.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tp_inet.c	7.11 (Berkeley) 10/11/92
+ *	@(#)tp_inet.c	7.12 (Berkeley) 05/25/93
  */
 
 /***********************************************************
@@ -684,5 +684,5 @@ dump_inaddr(addr)
 {
 	printf("INET: port 0x%x; addr 0x%x\n", addr->sin_port, addr->sin_addr);
 }
-#endif ARGO_DEBUG
-#endif INET
+#endif /* ARGO_DEBUG */
+#endif /* INET */
