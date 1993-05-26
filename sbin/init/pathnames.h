934c731987f28964b36ab0c30d881894ffe5d710
Keith Bostic <bostic>
1993-05-26 10:16:29 -0700

    update Donn's organization for general release
    SCCS-vsn: 6.2

--- sbin/init/pathnames.h
+++ sbin/init/pathnames.h
@@ -3,7 +3,7 @@
  * All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Donn Seeley at UUNET Technologies, Inc.
+ * Donn Seeley at Berkeley Software Design, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	6.2 (Berkeley) 03/02/92
+ *	@(#)pathnames.h	6.2 (Berkeley) 05/26/93
  */
 
 #include <paths.h>
