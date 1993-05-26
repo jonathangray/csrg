40fe1d0db464a21654e1a85269a0f69a622329fb
Kirk McKusick <mckusick>
1993-05-26 00:11:03 -0700

    add code for console device (if present)
    SCCS-vsn: 5.12

--- usr.sbin/sysctl/sysctl.c
+++ usr.sbin/sysctl/sysctl.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)sysctl.c	5.11 (Berkeley) 05/20/93";
+static char sccsid[] = "@(#)sysctl.c	5.12 (Berkeley) 05/26/93";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -47,6 +47,7 @@ static char sccsid[] = "@(#)sysctl.c	5.11 (Berkeley) 05/20/93";
 #include <sys/sysctl.h>
 #include <sys/socket.h>
 #include <vm/vm_param.h>
+#include <machine/cpu.h>
 
 #include <netinet/in.h>
 #include <netinet/in_systm.h>
@@ -69,6 +70,9 @@ struct ctlname netname[] = CTL_NET_NAMES;
 struct ctlname hwname[] = CTL_HW_NAMES;
 struct ctlname username[] = CTL_USER_NAMES;
 struct ctlname debugname[CTL_DEBUG_MAXID];
+#ifdef CTL_MACHDEP_NAMES
+struct ctlname machdepname[] = CTL_MACHDEP_NAMES;
+#endif
 char names[BUFSIZ];
 
 struct list {
@@ -84,11 +88,22 @@ struct list secondlevel[] = {
 	{ netname, NET_MAXID },		/* CTL_NET */
 	{ 0, CTL_DEBUG_MAXID },		/* CTL_DEBUG */
 	{ hwname, HW_MAXID },		/* CTL_HW */
+#ifdef CTL_MACHDEP_NAMES
+	{ machdepname, CPU_MAXID },	/* CTL_MACHDEP */
+#else
 	{ 0, 0 },			/* CTL_MACHDEP */
+#endif
 	{ username, USER_MAXID },	/* CTL_USER_NAMES */
 };
 
 int	Aflag, aflag, nflag, wflag;
+
+/*
+ * Variables requiring special processing.
+ */
+#define	CLOCK		0x00000001
+#define	BOOTTIME	0x00000002
+#define	CONSDEV		0x00000004
 
 int
 main(argc, argv)
@@ -171,8 +186,7 @@ parse(string, flags)
 	int flags;
 {
 	int indx, type, state, size, len;
-	int isclockrate = 0;
-	int isboottime = 0;
+	int special = 0;
 	void *newval = 0;
 	int intval, newsize = 0;
 	quad_t quadval;
@@ -248,10 +262,10 @@ parse(string, flags)
 			    "Use ps to view %s information\n", string);
 			return;
 		case KERN_CLOCKRATE:
-			isclockrate = 1;
+			special |= CLOCK;
 			break;
 		case KERN_BOOTTIME:
-			isboottime = 1;
+			special |= BOOTTIME;
 			break;
 		}
 		break;
@@ -293,8 +307,14 @@ parse(string, flags)
 		len = 3;
 		break;
 
-	case CTL_FS:
 	case CTL_MACHDEP:
+#ifdef CPU_CONSDEV
+		if (mib[1] == CPU_CONSDEV)
+			special |= CONSDEV;
+#endif
+		break;
+
+	case CTL_FS:
 	case CTL_USER:
 		break;
 
@@ -343,7 +363,7 @@ parse(string, flags)
 			return;
 		}
 	}
-	if (isclockrate) {
+	if (special & CLOCK) {
 		struct clockinfo *clkp = (struct clockinfo *)buf;
 
 		if (!nflag)
@@ -353,7 +373,7 @@ parse(string, flags)
 		    clkp->hz, clkp->tick, clkp->profhz, clkp->stathz);
 		return;
 	}
-	if (isboottime) {
+	if (special & BOOTTIME) {
 		struct timeval *btp = (struct timeval *)buf;
 
 		if (!nflag)
@@ -361,6 +381,16 @@ parse(string, flags)
 			    ctime(&btp->tv_sec));
 		else
 			fprintf(stdout, "%d\n", btp->tv_sec);
+		return;
+	}
+	if (special & CONSDEV) {
+		dev_t dev = *(dev_t *)buf;
+
+		if (!nflag)
+			fprintf(stdout, "%s = %s\n", string,
+			    devname(dev, S_IFCHR));
+		else
+			fprintf(stdout, "0x%x\n", dev);
 		return;
 	}
 	switch (type) {
