8042161a913029a9799776b41ec62619e06b7440
Keith Bostic <bostic>
1993-05-25 23:03:36 -0700

    declare lseek correctly, so doesn't complain about <sys/types.h>
    SCCS-vsn: 5.22

--- usr.bin/uucp/includes/uucp.h
+++ usr.bin/uucp/includes/uucp.h
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)uucp.h	5.21 (Berkeley) 05/17/93
+ *	@(#)uucp.h	5.22 (Berkeley) 05/25/93
  */
 
 #include <stdio.h>
@@ -401,9 +401,7 @@ extern	char DLocal[], DLocalX[], *subfile(), *subdir();
 extern	char *ttyname(), *strcpy(), *strcat(), *index(), *rindex(),
 		*fgets(), *calloc(), *malloc(), *fdig(), *ttyname(),
 		*cfgets(), *getwd(), *strpbrk(), *strncpy();
-#ifndef lseek		/* XXX */
-extern	long lseek();
-#endif
+extern	off_t lseek();
 extern time_t time();
 
 extern char _FAILED[], CANTOPEN[], DEVNULL[];
