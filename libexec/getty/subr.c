e3a44d6c6c0ec8e4d6a2dfb7b79e875493e9362f
Elan Amir <elan>
1993-05-27 16:08:07 -0700

    deleted getttytab.c; turn off debugging
    SCCS-vsn: 5.13

--- libexec/getty/subr.c
+++ libexec/getty/subr.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)subr.c	5.12 (Berkeley) 05/27/93";
+static char sccsid[] = "@(#)subr.c	5.13 (Berkeley) 05/27/93";
 #endif /* not lint */
 
 /*
@@ -88,7 +88,6 @@ gettable(name, buf)
 			fp->value = 1 ^ fp->invrt;
 		}
 	}
-#define DEBUG
 #ifdef DEBUG
 	printf("name=\"%s\", buf=\"%s\"\n", name, buf);
 	for (sp = gettystrs; sp->field; sp++)
