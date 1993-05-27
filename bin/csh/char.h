bb69164abdf91352f3dc1638b39de89432ee0c85
Christos Zoulas <christos>
1993-05-27 05:06:36 -0700

    changed _Q,_Q1 to _QF,_QB to avoid conflict with new <ctype.h>
    SCCS-vsn: 5.9

--- bin/csh/char.h
+++ bin/csh/char.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)char.h	5.8 (Berkeley) 05/22/93
+ *	@(#)char.h	5.9 (Berkeley) 05/27/93
  */
 
 #include <ctype.h>
@@ -42,8 +42,8 @@ extern unsigned char _cmap_lower[], _cmap_upper[];
 
 #endif
 
-#define	_Q	0x0001		/* '" */
-#define	_Q1	0x0002		/* ` */
+#define	_QF	0x0001		/* '" (Forward quotes) */
+#define	_QB	0x0002		/* ` (Backquote) */
 #define	_SP	0x0004		/* space and tab */
 #define	_NL	0x0008		/* \n */
 #define	_META	0x0010		/* lex meta characters, sp #'`";&<>()|\t\n */
