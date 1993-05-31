18c90f35708f7ee74649c887899a81dda44eee09
Keith Bostic <bostic>
1993-05-31 15:47:55 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- bin/stty/stty.c
+++ bin/stty/stty.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)stty.c	5.33 (Berkeley) 04/29/93";
+static char sccsid[] = "@(#)stty.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 #include <sys/types.h>
