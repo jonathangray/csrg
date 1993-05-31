34f409297ed312941e3fdcbe7541016163f9e0c5
Keith Bostic <bostic>
1993-05-31 15:06:00 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- bin/ps/ps.h
+++ bin/ps/ps.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ps.h	5.5 (Berkeley) 06/05/92
+ *	@(#)ps.h	8.1 (Berkeley) 05/31/93
  */
 
 #define	UNLIMITED	0	/* unlimited terminal width */
