971fccfe0b94f073cd06377ff30b25650fcd54af
Keith Bostic <bostic>
1993-05-31 16:46:52 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- games/ching/ching.sh
+++ games/ching/ching.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -
 #
-# Copyright (c) 1983, 1988 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1983, 1988, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # This module is believed to contain source code proprietary to AT&T.
 # Use and redistribution is subject to the Berkeley Software License
 # Agreement and your Software Agreement with AT&T (Western Electric).
 #
-#	@(#)ching.sh	4.5 (Berkeley) 04/08/91
+#	@(#)ching.sh	8.1 (Berkeley) 05/31/93
 #
 
 cd /usr/games
