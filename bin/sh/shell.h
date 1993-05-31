c29e81b3a1799a3bd323b605a8c6931230398d9f
Keith Bostic <bostic>
1993-05-31 15:20:39 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- bin/sh/shell.h
+++ bin/sh/shell.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Kenneth Almquist.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)shell.h	5.6 (Berkeley) 07/15/92
+ *	@(#)shell.h	8.1 (Berkeley) 05/31/93
  */
 
 /*
