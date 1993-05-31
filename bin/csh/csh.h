8bf733a397d353f4862379e66c6e0f14bd87fa0c
Keith Bostic <bostic>
1993-05-31 16:41:34 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- bin/csh/csh.h
+++ bin/csh/csh.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980, 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)csh.h	5.26 (Berkeley) 05/23/93
+ *	@(#)csh.h	8.1 (Berkeley) 05/31/93
  */
 
 /*
