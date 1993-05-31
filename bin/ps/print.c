048eab914e7d6de3268f73a52f5b668e36b52306
Keith Bostic <bostic>
1993-05-31 15:05:44 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- bin/ps/print.c
+++ bin/ps/print.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)print.c	5.14 (Berkeley) 04/29/93";
+static char sccsid[] = "@(#)print.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 #include <sys/param.h>
