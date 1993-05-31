9565e4d83573ca0058c9ecdac3b989909e9d5f3b
Keith Bostic <bostic>
1993-05-31 16:42:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- bin/csh/init.c
+++ bin/csh/init.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980, 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
 
 #ifndef lint
 /*###9 [cc] warning: `sccsid' defined but not used%%%*/
-static char sccsid[] = "@(#)init.c	5.16 (Berkeley) 05/22/93";
+static char sccsid[] = "@(#)init.c	8.1 (Berkeley) 05/31/93";
 #endif /* not lint */
 
 #if __STDC__
