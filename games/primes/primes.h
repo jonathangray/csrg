ac449dfa8fbeba8823e018b1db98dddb68fe73fd
Keith Bostic <bostic>
1993-05-31 17:43:20 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- games/primes/primes.h
+++ games/primes/primes.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Landon Curt Noll.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)primes.h	5.2 (Berkeley) 06/01/90
+ *	@(#)primes.h	8.1 (Berkeley) 05/31/93
  */
 
 /*
