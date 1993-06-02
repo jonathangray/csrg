01c91f5c0809590d4efa2a48615f5ef4ff939f32
Kirk McKusick <mckusick>
1993-06-01 23:24:29 -0700

    update login copyright string
    SCCS-vsn: 5.79

--- usr.bin/login/login.c
+++ usr.bin/login/login.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)login.c	5.78 (Berkeley) 06/29/92";
+static char sccsid[] = "@(#)login.c	5.79 (Berkeley) 06/01/93";
 #endif /* not lint */
 
 /*
@@ -446,9 +446,10 @@ main(argc, argv)
 #endif
 
 	if (!quietlog) {
-		(void)printf(
-"Copyright (c) 1980,1983,1986,1988,1990,1991 The Regents of the University\n%s",
-"of California.  All rights reserved.\n\n");
+		(void)printf("%s\n\t%s  %s\n\n",
+		    "Copyright (c) 1980, 1983, 1986, 1988, 1990, 1991, 1993",
+		    "The Regents of the University of California. ",
+		    "All rights reserved.");
 		motd();
 		(void)snprintf(tbuf,
 		    sizeof(tbuf), "%s/%s", _PATH_MAILDIR, pwd->pw_name);
