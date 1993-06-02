7bb6e9f36020ab536299a5eb2fa95540590bf8ea
Keith Bostic <bostic>
1993-06-01 23:15:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- local/kerberosIV/register/pathnames.h
+++ local/kerberosIV/register/pathnames.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	5.2 (Berkeley) 07/16/92
+ *	@(#)pathnames.h	8.1 (Berkeley) 06/01/93
  */
 
 #define	SERVER_KEYDIR	"/etc/kerberosIV/register_keys"
