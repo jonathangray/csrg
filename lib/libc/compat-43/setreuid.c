efd06402d0770dbfde1f5ac73c534789ad09cfa3
Keith Bostic <bostic>
1993-06-02 00:04:12 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/compat-43/setreuid.c
+++ lib/libc/compat-43/setreuid.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)setreuid.c	5.1 (Berkeley) 07/09/92";
+static char sccsid[] = "@(#)setreuid.c	8.1 (Berkeley) 06/02/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
