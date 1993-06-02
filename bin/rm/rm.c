b59c1c62c3ca30d54be20155d62eec6725af3e53
Keith Bostic <bostic>
1993-06-02 11:43:51 -0700

    don't quit on FTS_DNR directories; the user entered 'rm -rf a a a b' and fts found the directory, but once it was deleted couldn't read it.
    SCCS-vsn: 8.2

--- bin/rm/rm.c
+++ bin/rm/rm.c
@@ -38,7 +38,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rm.c	8.1 (Berkeley) 05/31/93";
+static char sccsid[] = "@(#)rm.c	8.2 (Berkeley) 06/02/93";
 #endif /* not lint */
 
 #include <sys/types.h>
