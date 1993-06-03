b778e5a6886f2af20cdd11dd5bcefba00e3bc97a
Keith Bostic <bostic>
1993-06-02 19:53:06 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sys/disklabel.h
+++ sys/sys/disklabel.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1987, 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)disklabel.h	7.25 (Berkeley) 05/24/93
+ *	@(#)disklabel.h	8.1 (Berkeley) 06/02/93
  */
 
 /*
