147ee1f2e039d0f65345607fd5e59dce6b421664
Keith Bostic <bostic>
1993-06-02 19:52:56 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sys/device.h
+++ sys/sys/device.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)device.h	7.6 (Berkeley) 02/01/93
+ *	@(#)device.h	8.1 (Berkeley) 06/02/93
  *
  * from: $Header: device.h,v 1.9 93/02/01 19:34:50 torek Exp $ (LBL)
  */
