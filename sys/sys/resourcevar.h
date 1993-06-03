7c8dec0da9f095726a03b02cc7d254fe9a52d1f4
Keith Bostic <bostic>
1993-06-02 19:55:05 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sys/resourcevar.h
+++ sys/sys/resourcevar.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)resourcevar.h	7.3 (Berkeley) 07/08/92
+ *	@(#)resourcevar.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef	_RESOURCEVAR_H_		/* tmp for user.h */
