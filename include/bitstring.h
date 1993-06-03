dae7a64ebe6d807dac24c5b84fd9939bc367885a
Keith Bostic <bostic>
1993-06-02 20:05:01 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/bitstring.h
+++ include/bitstring.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)bitstring.h	5.5 (Berkeley) 04/03/91
+ *	@(#)bitstring.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef _BITSTRING_H_
