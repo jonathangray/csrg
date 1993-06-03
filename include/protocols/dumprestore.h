36bfbfd7dc9c02c365858ffa31ab57ca6fbf5578
Keith Bostic <bostic>
1993-06-02 20:04:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/protocols/dumprestore.h
+++ include/protocols/dumprestore.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)dumprestore.h	5.9 (Berkeley) 03/09/93
+ *	@(#)dumprestore.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef _DUMPRESTORE_H_
