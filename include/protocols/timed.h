66c84f31686790cbf9c22761c19ba5272751d1fc
Keith Bostic <bostic>
1993-06-02 20:04:17 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/protocols/timed.h
+++ include/protocols/timed.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)timed.h	5.1 (Berkeley) 07/19/92
+ *	@(#)timed.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef	_TIMED_H_
