6bdc0e58b9839b8febfb3335394ed30a988f1204
Keith Bostic <bostic>
1993-06-02 20:07:36 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/vis.h
+++ include/vis.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vis.h	5.10 (Berkeley) 02/05/92
+ *	@(#)vis.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef _VIS_H_
