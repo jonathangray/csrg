9c89404b7350690be28912b79f920dbccadc8b3d
Keith Bostic <bostic>
1993-06-02 20:04:55 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/ar.h
+++ include/ar.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ar.h	5.4 (Berkeley) 04/03/91
+ *	@(#)ar.h	8.1 (Berkeley) 06/02/93
  */
 
 #ifndef _AR_H_
