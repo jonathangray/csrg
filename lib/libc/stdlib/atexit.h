b544b77568903e683acef83e0249acd08e822fb0
Keith Bostic <bostic>
1993-06-04 13:04:51 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/stdlib/atexit.h
+++ lib/libc/stdlib/atexit.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)atexit.h	5.1 (Berkeley) 05/15/90
+ *	@(#)atexit.h	8.1 (Berkeley) 06/04/93
  */
 
 /* must be at least 32 to guarantee ANSI conformance */
