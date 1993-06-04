d3c0a7d5b8eade7e6425ed5673d79341d26a7258
Keith Bostic <bostic>
1993-06-04 16:46:53 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libcurses/addbytes.c
+++ lib/libcurses/addbytes.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)addbytes.c	5.22 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)addbytes.c	8.1 (Berkeley) 06/04/93";
 #endif	/* not lint */
 
 #include <curses.h>
