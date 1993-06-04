74be8ea2dbab2d0a7553d140459e5fb13b13b7b7
Keith Bostic <bostic>
1993-06-04 12:05:28 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/gen/sigsetops.c
+++ lib/libc/gen/sigsetops.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,11 +30,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sigsetops.c	5.5 (Berkeley) 01/13/93
+ *	@(#)sigsetops.c	8.1 (Berkeley) 06/04/93
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)sigsetops.c	5.5 (Berkeley) 01/13/93";
+static char sccsid[] = "@(#)sigsetops.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <signal.h>
