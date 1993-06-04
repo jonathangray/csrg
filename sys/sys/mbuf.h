f15fda6363d3788f30b1cc4f093ea892f2245fd0
Keith Bostic <bostic>
1993-06-04 16:47:41 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- sys/sys/mbuf.h
+++ sys/sys/mbuf.h
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mbuf.h	7.19 (Berkeley) 06/04/93
+ *	@(#)mbuf.h	8.1 (Berkeley) 06/04/93
  */
 
 #ifndef M_WAITOK
