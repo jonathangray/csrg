5f2d4f7123ba6c6a75fdd2d1c9ceeb45719b4fc2
Keith Bostic <bostic>
1993-06-04 12:04:42 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/gen/rewinddir.c
+++ lib/libc/gen/rewinddir.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rewinddir.c	5.1 (Berkeley) 05/25/90";
+static char sccsid[] = "@(#)rewinddir.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
