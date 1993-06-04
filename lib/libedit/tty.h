2e8f85d4a0e38b62acc943e26028a3d6229e76c6
Keith Bostic <bostic>
1993-06-04 16:53:04 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libedit/tty.h
+++ lib/libedit/tty.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Christos Zoulas of Cornell University.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tty.h	5.5 (Berkeley) 07/18/92
+ *	@(#)tty.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
