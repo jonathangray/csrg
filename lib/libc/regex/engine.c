4ea55cf9fb070fc1ff55b0b84ba596854a1ab398
Keith Bostic <bostic>
1993-06-04 12:57:47 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/regex/engine.c
+++ lib/libc/regex/engine.c
@@ -1,7 +1,7 @@
 /*-
  * Copyright (c) 1992 Henry Spencer.
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Henry Spencer of the University of Toronto.
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)engine.c	5.5 (Berkeley) 05/21/93
+ *	@(#)engine.c	8.1 (Berkeley) 06/04/93
  */
 
 /*
