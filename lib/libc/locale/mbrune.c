20c58a012cc9bb3dd91e0a6ffc08bf169055e9f0
Keith Bostic <bostic>
1993-06-04 12:43:15 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/locale/mbrune.c
+++ lib/libc/locale/mbrune.c
@@ -35,7 +35,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)mbrune.c	5.1 (Berkeley) 05/26/93";
+static char sccsid[] = "@(#)mbrune.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <limits.h>
