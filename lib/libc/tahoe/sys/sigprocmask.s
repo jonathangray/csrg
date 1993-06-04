187e53b53e284719af632caaab4ec7bf665a9675
Keith Bostic <bostic>
1993-06-04 13:24:30 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/tahoe/sys/sigprocmask.s
+++ lib/libc/tahoe/sys/sigprocmask.s
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)sigprocmask.s	5.2 (Berkeley) 06/01/90"
+	.asciz "@(#)sigprocmask.s	8.1 (Berkeley) 06/04/93"
 #endif /* SYSLIBC_SCCS and not lint */
 
 #include "SYS.h"
