bab8a16cc6d4a13b08d99bf4c2fb295011649a63
Keith Bostic <bostic>
1993-06-04 16:53:22 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libkvm/kvm_file.c
+++ lib/libkvm/kvm_file.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1989, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)kvm_file.c	5.5 (Berkeley) 05/28/93";
+static char sccsid[] = "@(#)kvm_file.c	8.1 (Berkeley) 06/04/93";
 #endif /* LIBC_SCCS and not lint */
 
 /*
