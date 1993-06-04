163aa08769d7bab18e825d68114607bcefe41f6e
Keith Bostic <bostic>
1993-06-04 16:45:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libcurses/PSD.doc/life.c
+++ lib/libcurses/PSD.doc/life.c
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.\"	@(#)life.c	6.3 (Berkeley) 04/17/91
+.\"	@(#)life.c	8.1 (Berkeley) 06/04/93
 .\"
 # include	<curses.h>
 # include	<signal.h>
