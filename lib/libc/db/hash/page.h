65fcc74814deebc733b26d60652e8b069dd08b4c
Keith Bostic <bostic>
1993-06-04 15:22:22 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/db/hash/page.h
+++ lib/libc/db/hash/page.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)page.h	5.2 (Berkeley) 09/04/91
+ *	@(#)page.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
