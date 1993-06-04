efa438839c05636217dbf49cb22d0e18be062eda
Keith Bostic <bostic>
1993-06-04 15:22:26 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/db/hash/search.h
+++ lib/libc/db/hash/search.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Margo Seltzer.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)search.h	5.2 (Berkeley) 09/04/91
+ *	@(#)search.h	8.1 (Berkeley) 06/04/93
  */
 
 /* Backward compatibility to hsearch interface. */
