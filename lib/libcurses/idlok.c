6cdc5c0eaebe3b6ef623d7dc1122f88f4a8e2275
Keith Bostic <bostic>
1993-06-04 16:48:19 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libcurses/idlok.c
+++ lib/libcurses/idlok.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)idlok.c	5.6 (Berkeley) 09/14/92";
+static char sccsid[] = "@(#)idlok.c	8.1 (Berkeley) 06/04/93";
 #endif	/* not lint */
 
 #include <curses.h>
