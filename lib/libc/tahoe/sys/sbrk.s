87078882ff16d129643d5ed6d337f3e564010fd4
Keith Bostic <bostic>
1993-06-04 13:24:21 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/tahoe/sys/sbrk.s
+++ lib/libc/tahoe/sys/sbrk.s
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(SYSLIBC_SCCS) && !defined(lint)
-	.asciz "@(#)sbrk.s	5.4 (Berkeley) 06/01/90"
+	.asciz "@(#)sbrk.s	8.1 (Berkeley) 06/04/93"
 #endif /* SYSLIBC_SCCS and not lint */
 
 #include "SYS.h"
