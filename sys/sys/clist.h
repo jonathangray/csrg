05d3a9614025d2e31b45610f95b961fb5cf796d6
Keith Bostic <bostic>
1993-06-04 16:47:52 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- sys/sys/clist.h
+++ sys/sys/clist.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)clist.h	7.4 (Berkeley) 06/04/93
+ *	@(#)clist.h	8.1 (Berkeley) 06/04/93
  */
 
 struct cblock {
