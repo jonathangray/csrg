197ceeb25562bb5ea9319c5b41e45a74f0c3c6f1
Keith Bostic <bostic>
1993-06-04 16:52:41 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libedit/term.h
+++ lib/libedit/term.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Christos Zoulas of Cornell University.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)term.h	5.4 (Berkeley) 07/16/92
+ *	@(#)term.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
