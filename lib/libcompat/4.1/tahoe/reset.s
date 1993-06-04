0f2f2a4bf305c8f2a0c6d76e4307e93febcf819f
Keith Bostic <bostic>
1993-06-04 16:21:06 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libcompat/4.1/tahoe/reset.s
+++ lib/libcompat/4.1/tahoe/reset.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
@@ -8,7 +8,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-	.asciz "@(#)reset.s	1.3 (Berkeley) 04/12/91"
+	.asciz "@(#)reset.s	8.1 (Berkeley) 06/04/93"
 #endif /* LIBC_SCCS and not lint */
 
 /*
