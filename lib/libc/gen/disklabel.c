b6183f19aab7b9620e08d2053d8f3e0b2ac60c8b
Keith Bostic <bostic>
1993-06-04 12:01:19 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/gen/disklabel.c
+++ lib/libc/gen/disklabel.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983, 1987 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)disklabel.c	5.23 (Berkeley) 10/04/92";
+static char sccsid[] = "@(#)disklabel.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 #include <sys/param.h>
