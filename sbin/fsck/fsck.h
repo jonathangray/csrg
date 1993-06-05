90a1ae86874b41853cd9416ade3de678c4eee859
Keith Bostic <bostic>
1993-06-05 10:56:18 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sbin/fsck/fsck.h
+++ sbin/fsck/fsck.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980, 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fsck.h	5.20 (Berkeley) 06/04/93
+ *	@(#)fsck.h	8.1 (Berkeley) 06/05/93
  */
 
 #define	MAXDUP		10	/* limit on dup blks (per inode) */
