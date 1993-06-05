248253dd1e6b5af9d84be5ad9b54f30c582799af
Keith Bostic <bostic>
1993-06-04 17:04:15 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libm/common_source/pow.c
+++ lib/libm/common_source/pow.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pow.c	5.11 (Berkeley) 05/02/93";
+static char sccsid[] = "@(#)pow.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /* POW(X,Y)  
