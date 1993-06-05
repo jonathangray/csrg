f1d94a32861771984bcaeeb5c8bd8d3641a84336
Keith Bostic <bostic>
1993-06-04 18:46:36 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- libexec/bugfiler/sendbug.sh
+++ libexec/bugfiler/sendbug.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #
-# Copyright (c) 1983 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1983, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,7 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)sendbug.sh	5.12 (Berkeley) 07/25/92
+#	@(#)sendbug.sh	8.1 (Berkeley) 06/04/93
 #
 
 # create a bug report and mail it to '4bsd-bugs'.
