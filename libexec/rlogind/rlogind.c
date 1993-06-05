2971bc9e291dbf9eb969bdbd023a84b3d4f6312c
Keith Bostic <bostic>
1993-06-04 18:59:10 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- libexec/rlogind/rlogind.c
+++ libexec/rlogind/rlogind.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1983, 1988, 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1988, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,13 +32,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1983, 1988, 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1983, 1988, 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)rlogind.c	5.57 (Berkeley) 10/04/92";
+static char sccsid[] = "@(#)rlogind.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /*
