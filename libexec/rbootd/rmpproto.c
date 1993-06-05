5f6dc737a73f45bc2c90c48ede5cde99a986750d
Keith Bostic <bostic>
1993-06-04 18:58:53 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- libexec/rbootd/rmpproto.c
+++ libexec/rbootd/rmpproto.c
@@ -38,14 +38,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rmpproto.c	5.2 (Berkeley) 07/23/92
+ *	@(#)rmpproto.c	8.1 (Berkeley) 06/04/93
  *
  * Utah $Hdr: rmpproto.c 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rmpproto.c	5.2 (Berkeley) 07/23/92";
+static char sccsid[] = "@(#)rmpproto.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 #include <sys/param.h>
