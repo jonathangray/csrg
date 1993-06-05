d84dee03360a6b0ae8327f8a489a87e987019e90
Keith Bostic <bostic>
1993-06-04 17:08:24 -0700

    prototype everything
    SCCS-vsn: 7.9

--- sys/netinet/in_var.h
+++ sys/netinet/in_var.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)in_var.h	7.8 (Berkeley) 04/07/93
+ *	@(#)in_var.h	7.9 (Berkeley) 06/04/93
  */
 
 /*
@@ -191,6 +191,10 @@ struct in_multistep {
 	IN_NEXT_MULTI((step), (inm)); \
 }
 
+int	in_ifinit __P((struct ifnet *,
+	    struct in_ifaddr *, struct sockaddr_in *, int));
 struct	in_multi *in_addmulti __P((struct in_addr *, struct ifnet *));
 int	in_delmulti __P((struct in_multi *));
+void	in_ifscrub __P((struct ifnet *, struct in_ifaddr *));
+int	in_control __P((struct socket *, int, caddr_t, struct ifnet *));
 #endif
