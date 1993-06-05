cff73f84738be43ad50382eea57b94e4b0f4ec97
Keith Bostic <bostic>
1993-06-04 17:44:46 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libplot/imagen/imPcodes.h
+++ lib/libplot/imagen/imPcodes.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)imPcodes.h	5.2 (Berkeley) 04/22/91
+ *	@(#)imPcodes.h	8.1 (Berkeley) 06/04/93
  */
 
 #define	imP_SP	128
