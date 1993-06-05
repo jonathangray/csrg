8bcf4ea011c0b1e84521047e781f1f7d8e7d4546
Keith Bostic <bostic>
1993-06-04 17:03:14 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libm/common_source/jn.c
+++ lib/libm/common_source/jn.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)jn.c	5.5 (Berkeley) 12/16/92";
+static char sccsid[] = "@(#)jn.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /*
