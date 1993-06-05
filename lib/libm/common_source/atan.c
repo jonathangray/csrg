57bcc51619451ef806d024fcd4ef397e161fe19b
Keith Bostic <bostic>
1993-06-04 17:00:23 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libm/common_source/atan.c
+++ lib/libm/common_source/atan.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1985 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)atan.c	5.5 (Berkeley) 10/09/90";
+static char sccsid[] = "@(#)atan.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /* ATAN(X)
