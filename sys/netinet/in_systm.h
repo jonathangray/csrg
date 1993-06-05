3bb7d1eac8eca51e6c427b569e1558f7b890a567
Keith Bostic <bostic>
1993-06-04 17:22:52 -0700

    prototype everything
    SCCS-vsn: 7.5

--- sys/netinet/in_systm.h
+++ sys/netinet/in_systm.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)in_systm.h	7.4 (Berkeley) 06/28/90
+ *	@(#)in_systm.h	7.5 (Berkeley) 06/04/93
  */
 
 /*
@@ -52,5 +52,5 @@ typedef u_long	n_long;			/* long as received from the net */
 typedef	u_long	n_time;			/* ms since 00:00 GMT, byte rev */
 
 #ifdef KERNEL
-n_time	iptime();
+n_time	 iptime __P((void));
 #endif
