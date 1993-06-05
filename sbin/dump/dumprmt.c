8d97d9f0c99859ee8719e6588c144a48c2a287f9
Keith Bostic <bostic>
1993-06-05 10:52:39 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sbin/dump/dumprmt.c
+++ sbin/dump/dumprmt.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)dumprmt.c	5.18 (Berkeley) 05/11/93";
+static char sccsid[] = "@(#)dumprmt.c	8.1 (Berkeley) 06/05/93";
 #endif /* not lint */
 
 #include <sys/param.h>
