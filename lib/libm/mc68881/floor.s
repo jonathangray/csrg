5a7a76b216f7378e572b97dbd1cc5a61d4075fc5
Keith Bostic <bostic>
1993-06-04 17:01:11 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libm/mc68881/floor.s
+++ lib/libm/mc68881/floor.s
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)floor.s	5.1 (Berkeley) 05/17/90
+ *	@(#)floor.s	8.1 (Berkeley) 06/04/93
  */
 
 	.text
