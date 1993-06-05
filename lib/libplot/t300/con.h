12a0034a84c1f6dd43d404050f84466893144fa9
Keith Bostic <bostic>
1993-06-04 17:48:52 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libplot/t300/con.h
+++ lib/libplot/t300/con.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)con.h	4.2 (Berkeley) 04/22/91
+ *	@(#)con.h	8.1 (Berkeley) 06/04/93
  */
 
 #include <sgtty.h>
