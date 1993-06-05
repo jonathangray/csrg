968fbefb09ddb2a7ead7dcc8ce03e05a7d2adc87
Keith Bostic <bostic>
1993-06-04 18:58:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- libexec/rbootd/parseconf.c
+++ libexec/rbootd/parseconf.c
@@ -38,14 +38,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)parseconf.c	5.2 (Berkeley) 07/23/92
+ *	@(#)parseconf.c	8.1 (Berkeley) 06/04/93
  *
  * Utah $Hdr: parseconf.c 3.1 92/07/06$
  * Author: Jeff Forys, University of Utah CSS
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseconf.c	5.2 (Berkeley) 07/23/92";
+static char sccsid[] = "@(#)parseconf.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 #include <sys/param.h>
