c1547e8a4aec20c2eaf6cd29fb62d59067158a7f
Keith Bostic <bostic>
1993-06-04 17:04:10 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libm/common_source/mathimpl.h
+++ lib/libm/common_source/mathimpl.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mathimpl.h	5.6 (Berkeley) 01/10/93
+ *	@(#)mathimpl.h	8.1 (Berkeley) 06/04/93
  */
 
 #include <sys/cdefs.h>
