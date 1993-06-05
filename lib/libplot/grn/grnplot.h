4e91abe902d6ff1487dc52f029eec8a7512cc3fe
Keith Bostic <bostic>
1993-06-04 17:41:17 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libplot/grn/grnplot.h
+++ lib/libplot/grn/grnplot.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980, 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)grnplot.h	6.2 (Berkeley) 04/22/91
+ *	@(#)grnplot.h	8.1 (Berkeley) 06/04/93
  */
 
 /*
