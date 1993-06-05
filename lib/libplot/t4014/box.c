e76acdef8d4a534796307fbe584caa33b78278b5
Keith Bostic <bostic>
1993-06-04 17:52:00 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libplot/t4014/box.c
+++ lib/libplot/t4014/box.c
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)box.c	4.2 (Berkeley) 04/22/91";
+static char sccsid[] = "@(#)box.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 box(x0, y0, x1, y1)
