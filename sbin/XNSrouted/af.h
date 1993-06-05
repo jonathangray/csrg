49ec32fac2d7952f70b6da417d1ccb152d28f19b
Keith Bostic <bostic>
1993-06-05 10:47:18 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sbin/XNSrouted/af.h
+++ sbin/XNSrouted/af.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  *
  *	@(#)af.h	5.1 (Berkeley) 6/4/85 (routed/af.h)
  *
- *	@(#)af.h	5.2 (Berkeley) 06/01/90
+ *	@(#)af.h	8.1 (Berkeley) 06/05/93
  */
 
 /*
