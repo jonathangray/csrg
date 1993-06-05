2e8e9ae43b7d31de3c2ec24d35a7d6e84e3081b0
Keith Bostic <bostic>
1993-06-04 18:48:47 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- libexec/ftpd/vers.c
+++ libexec/ftpd/vers.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)vers.c	5.2 (Berkeley) 06/27/92";
+static char sccsid[] = "@(#)vers.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 char version[] = "Version 5.70";
