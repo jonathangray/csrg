600f4997f5914bdda039793fab9e4adf37065160
Keith Bostic <bostic>
1993-06-04 17:08:27 -0700

    prototype everything
    SCCS-vsn: 7.5

--- sys/netinet/udp.h
+++ sys/netinet/udp.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)udp.h	7.4 (Berkeley) 06/28/90
+ *	@(#)udp.h	7.5 (Berkeley) 06/04/93
  */
 
 /*
