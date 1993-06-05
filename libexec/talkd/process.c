2f571f77de03934524a19514d2f7a19d742238c0
Keith Bostic <bostic>
1993-06-04 19:06:28 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- libexec/talkd/process.c
+++ libexec/talkd/process.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)process.c	5.10 (Berkeley) 02/26/91";
+static char sccsid[] = "@(#)process.c	8.1 (Berkeley) 06/04/93";
 #endif /* not lint */
 
 /*
