74e3484c2942b30d66e2eb9482d1d04428e86915
Keith Bostic <bostic>
1993-06-06 15:16:19 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/pascal/libpc/FSAV.c
+++ usr.bin/pascal/libpc/FSAV.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)FSAV.c	1.5 (Berkeley) 04/09/90";
+static char sccsid[] = "@(#)FSAV.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "h00vars.h"
