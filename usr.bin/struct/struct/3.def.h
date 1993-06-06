8c419a1a6dfd3f83ad405cf417e783779787918a
Keith Bostic <bostic>
1993-06-06 16:29:42 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- usr.bin/struct/struct/3.def.h
+++ usr.bin/struct/struct/3.def.h
@@ -3,7 +3,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)3.def.h	4.3 (Berkeley) 04/16/91
+ *	@(#)3.def.h	8.1 (Berkeley) 06/06/93
  */
 
 #define RECURSE(p,v,r)	{ for (r = 0; r < CHILDNUM(v); ++r) if (DEFINED(LCHILD(v,r))) p(LCHILD(v,r)); if (DEFINED(RSIB(v))) p(RSIB(v)); }
