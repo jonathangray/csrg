4753c373ae54dec621d6bcca1cae28c3207fe7be
Keith Bostic <bostic>
1993-06-06 15:13:29 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/pascal/libpc/libpc.h
+++ usr.bin/pascal/libpc/libpc.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)libpc.h	1.9 (Berkeley) 04/09/90
+ *	@(#)libpc.h	8.1 (Berkeley) 06/06/93
  */
 
 extern FILE *ACTFILE();
