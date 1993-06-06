2047ed3b4c1149e2c7be1f3f65be0147cd4bed2e
Keith Bostic <bostic>
1993-06-06 14:30:28 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/dc/dc.h
+++ usr.bin/dc/dc.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)dc.h	5.2 (Berkeley) 02/23/93
+ *	@(#)dc.h	8.1 (Berkeley) 06/06/93
  */
 
 #define FATAL 0
