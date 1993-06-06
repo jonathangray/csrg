532eeefcb5b5c58dd1b0908e89c02620ccda8f9f
Keith Bostic <bostic>
1993-06-06 15:23:23 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/man/config.c
+++ usr.bin/man/config.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)config.c	5.8 (Berkeley) 04/21/93";
+static char sccsid[] = "@(#)config.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <sys/param.h>
