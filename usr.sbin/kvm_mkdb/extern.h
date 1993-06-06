66901ba089b76be257abdd1305c8afa6bf279b91
Keith Bostic <bostic>
1993-06-06 13:58:21 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/kvm_mkdb/extern.h
+++ usr.sbin/kvm_mkdb/extern.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	5.1 (Berkeley) 07/21/92
+ *	@(#)extern.h	8.1 (Berkeley) 06/06/93
  */
 
 void	create_knlist __P((char *, DB *));
