09de8456890f09ef3c69e2e09d29498f3e9a7e6c
Keith Bostic <bostic>
1993-06-06 16:34:14 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/tip/cmds.c
+++ usr.bin/tip/cmds.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	5.17 (Berkeley) 07/30/92";
+static char sccsid[] = "@(#)cmds.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "tip.h"
