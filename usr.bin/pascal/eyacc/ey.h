c0286cc31f1b2817425b2d19888eb8f19d6b6073
Keith Bostic <bostic>
1993-06-06 15:11:00 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/pascal/eyacc/ey.h
+++ usr.bin/pascal/eyacc/ey.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)ey.h	5.3 (Berkeley) 04/12/91
+ *	@(#)ey.h	8.1 (Berkeley) 06/06/93
  */
 
 #include <stdio.h>
