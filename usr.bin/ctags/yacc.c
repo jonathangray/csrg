80244c97822c5760383c8c15cf4dd42986183d3b
Keith Bostic <bostic>
1993-06-06 14:30:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/ctags/yacc.c
+++ usr.bin/ctags/yacc.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1987 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)yacc.c	5.6 (Berkeley) 02/26/91";
+static char sccsid[] = "@(#)yacc.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <stdio.h>
