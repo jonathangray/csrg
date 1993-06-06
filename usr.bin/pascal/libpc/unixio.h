a43f59d64593101f6bee6a38b65c71354fb4cf0b
Keith Bostic <bostic>
1993-06-06 15:13:32 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/pascal/libpc/unixio.h
+++ usr.bin/pascal/libpc/unixio.h
@@ -1,6 +1,6 @@
 (*
- * Copyright (c) 1979 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1979, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,11 +30,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)unixio.h	1.2 (Berkeley) 04/12/91
+ *	@(#)unixio.h	8.1 (Berkeley) 06/06/93
  *)
 
 const
-sccsid = '@(#)unixio.h 1.2 04/12/91';
+sccsid = '@(#)unixio.h 8.1 06/06/93';
 
 type
 fileptr = record
