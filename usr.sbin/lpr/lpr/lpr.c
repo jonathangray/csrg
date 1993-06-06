32729a6d018bad943a0ec8bc6ccf0786b6760f9b
Keith Bostic <bostic>
1993-06-06 14:01:34 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/lpr/lpr/lpr.c
+++ usr.sbin/lpr/lpr/lpr.c
@@ -56,8 +56,8 @@
 char lpr_id[] = "~|^`lpr.c:\t4.2\t1 May 1981\n";
 
 /*
- * Copyright (c) 1983, 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  *
  * Redistribution and use in source and binary forms, with or without
@@ -91,12 +91,12 @@ char lpr_id[] = "~|^`lpr.c:\t4.2\t1 May 1981\n";
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1983, 1989 The Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1983, 1989, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)lpr.c	5.14 (Berkeley) 09/16/92";
+static char sccsid[] = "@(#)lpr.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
