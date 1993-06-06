9d3c87c333c4451b08b26233c31d9ba2ca0f619e
Keith Bostic <bostic>
1993-06-06 14:29:45 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/ctags/ctags.h
+++ usr.bin/ctags/ctags.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1987 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ctags.h	5.4 (Berkeley) 02/26/91
+ *	@(#)ctags.h	8.1 (Berkeley) 06/06/93
  */
 
 #define	bool	char
