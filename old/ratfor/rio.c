44aedca12db1decec3f0224862f79d8b055116c1
Keith Bostic <bostic>
1993-06-06 16:08:34 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- old/ratfor/rio.c
+++ old/ratfor/rio.c
@@ -6,7 +6,7 @@ Original BTL Ratfor System for 4.2
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)rio.c	1.3 (Berkeley) 04/16/91";
+static char sccsid[] = "@(#)rio.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 
