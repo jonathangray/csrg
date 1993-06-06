49d1786f0e91e4f28f0f91c8ac05470f9cedddec
Keith Bostic <bostic>
1993-06-06 15:47:00 -0700

    add 1993 to copyright
    SCCS-vsn: 5.3

--- usr.bin/pascal/pdx/tahoe/cerror.s
+++ usr.bin/pascal/pdx/tahoe/cerror.s
@@ -1,5 +1,5 @@
-# Copyright (c) 1987 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1987, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -29,7 +29,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)cerror.s	5.2 (Berkeley) 04/16/91
+#	@(#)cerror.s	5.3 (Berkeley) 06/06/93
 #
 
 # modified version of cerror
