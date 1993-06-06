a308905cb2a23212e8300686f73804943faeb2c1
Keith Bostic <bostic>
1993-06-06 14:56:33 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/learn/learn/lrnref.h
+++ usr.bin/learn/learn/lrnref.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1986 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)lrnref.h	4.5 (Berkeley) 04/17/91
+ *	@(#)lrnref.h	8.1 (Berkeley) 06/06/93
  */
 
 #define	READY	0
