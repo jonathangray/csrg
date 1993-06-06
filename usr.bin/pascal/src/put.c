0f1b4b27f0221762745bb0d264f2b37ed30a8353
Keith Bostic <bostic>
1993-06-06 16:11:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/pascal/src/put.c
+++ usr.bin/pascal/src/put.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)put.c	5.4 (Berkeley) 04/16/91";
+static char sccsid[] = "@(#)put.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "whoami.h"
