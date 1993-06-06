e2b2099cba127e98ea96a6c7412daed1f0dbf7fe
Keith Bostic <bostic>
1993-06-06 16:17:34 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/shar/shar.sh
+++ usr.bin/shar/shar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #
-# Copyright (c) 1990 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1990, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,7 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)shar.sh	5.3 (Berkeley) 03/04/93
+#	@(#)shar.sh	8.1 (Berkeley) 06/06/93
 #
 
 if [ $# -eq 0 ]; then
