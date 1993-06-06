b90ca2bf4eb6f4e28032c8dd5ae38530fe8efc22
Keith Bostic <bostic>
1993-06-06 13:53:00 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/config/mkmakefile.c
+++ usr.sbin/config/mkmakefile.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980,1990 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993, 19801990
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)mkmakefile.c	5.39 (Berkeley) 07/31/92";
+static char sccsid[] = "@(#)mkmakefile.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
