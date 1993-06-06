42be200816c0b528e7b97dbd4cfc3eeec6b6dc45
Keith Bostic <bostic>
1993-06-06 16:41:40 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/tn3270/distribution/arpa/telnet.h
+++ usr.bin/tn3270/distribution/arpa/telnet.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1983 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)telnet.h	4.5 (Berkeley) 04/26/91
+ *	@(#)telnet.h	8.1 (Berkeley) 06/06/93
  */
 
 /*
