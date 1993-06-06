e8107c641cb70b3ed32f534cc25636a962451cc5
Keith Bostic <bostic>
1993-06-06 14:33:32 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- usr.bin/diction/style/style.sh
+++ usr.bin/diction/style/style.sh
@@ -4,7 +4,7 @@
 # Use and redistribution is subject to the Berkeley Software License
 # Agreement and your Software Agreement with AT&T (Western Electric).
 #
-#	@(#)style.sh	4.9 (Berkeley) 07/31/91
+#	@(#)style.sh	8.1 (Berkeley) 06/06/93
 #
 
 L=/usr/libexec
