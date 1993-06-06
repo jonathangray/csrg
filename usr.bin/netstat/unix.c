1e40cf4be2a72d16d4f0f8472882e26e7fae182d
Keith Bostic <bostic>
1993-06-06 15:44:22 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/netstat/unix.c
+++ usr.bin/netstat/unix.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1983, 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)unix.c	5.16 (Berkeley) 04/01/93";
+static char sccsid[] = "@(#)unix.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
