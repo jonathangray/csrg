c26ded773646759351f4f4dc8f079f8587685346
Keith Bostic <bostic>
1993-06-06 11:11:49 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/unistd.h
+++ include/unistd.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)unistd.h	5.32 (Berkeley) 06/06/93
+ *	@(#)unistd.h	8.1 (Berkeley) 06/06/93
  */
 
 #ifndef _UNISTD_H_
