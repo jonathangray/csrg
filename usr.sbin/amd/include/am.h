ee23c251482a5343ef4ce557fd77a110d045df8f
Keith Bostic <bostic>
1993-06-06 11:58:19 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/amd/include/am.h
+++ usr.sbin/amd/include/am.h
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)am.h	5.5 (Berkeley) 08/01/92
+ *	@(#)am.h	8.1 (Berkeley) 06/06/93
  *
  * $Id: am.h,v 5.2.2.1 1992/02/09 15:09:54 jsp beta $
  *
