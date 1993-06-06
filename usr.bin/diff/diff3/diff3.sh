b521a77d6c0345cf5798fe2af3b3ed0e7552dd11
Keith Bostic <bostic>
1993-06-06 14:37:36 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/diff/diff3/diff3.sh
+++ usr.bin/diff/diff3/diff3.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # This module is believed to contain source code proprietary to AT&T.
 # Use and redistribution is subject to the Berkeley Software License
 # Agreement and your Software Agreement with AT&T (Western Electric).
 #
-#	@(#)diff3.sh	4.3 (Berkeley) 04/17/91
+#	@(#)diff3.sh	8.1 (Berkeley) 06/06/93
 #
 
 e=
