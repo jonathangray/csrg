90240f0b2bcd1aff34d290297f9cc0efef8ad8fc
Keith Bostic <bostic>
1993-06-06 11:39:37 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/amd/amd/amd.c
+++ usr.sbin/amd/amd/amd.c
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)amd.c	5.4 (Berkeley) 02/09/92
+ *	@(#)amd.c	8.1 (Berkeley) 06/06/93
  *
  * $Id: amd.c,v 5.2.2.1 1992/02/09 15:08:15 jsp beta $
  *
