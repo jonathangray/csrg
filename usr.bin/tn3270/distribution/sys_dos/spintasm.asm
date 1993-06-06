fbd6d937c828b3e4b76ba00b600421625d1b6f79
Keith Bostic <bostic>
1993-06-06 16:45:49 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/tn3270/distribution/sys_dos/spintasm.asm
+++ usr.bin/tn3270/distribution/sys_dos/spintasm.asm
@@ -1,5 +1,5 @@
-; Copyright (c) 1988 The Regents of the University of California.
-; All rights reserved.
+; Copyright (c) 1988, 1993
+;	The Regents of the University of California.  All rights reserved.
 ;
 ; Redistribution and use in source and binary forms, with or without
 ; modification, are permitted provided that the following conditions
@@ -29,7 +29,7 @@
 ; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 ; SUCH DAMAGE.
 ;
-;	@(#)spintasm.asm	4.3 (Berkeley) 06/06/93
+;	@(#)spintasm.asm	8.1 (Berkeley) 06/06/93
 ;
 
 ; The code in this file complete the spint calls
