34a4f495c19958719f404ab7a5e63f2f792f3acf
Keith Bostic <bostic>
1993-06-06 14:40:35 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/ftp/cmds.c
+++ usr.bin/ftp/cmds.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1985, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmds.c	5.29 (Berkeley) 07/06/92";
+static char sccsid[] = "@(#)cmds.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
