b7f8309a299e2179a53c75ad58a6dd69c7c4e2e1
Keith Bostic <bostic>
1993-06-06 16:29:46 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/systat/systat.h
+++ usr.bin/systat/systat.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1980, 1989, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1989, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)systat.h	5.10 (Berkeley) 05/21/93
+ *	@(#)systat.h	8.1 (Berkeley) 06/06/93
  */
 
 #include <curses.h>
