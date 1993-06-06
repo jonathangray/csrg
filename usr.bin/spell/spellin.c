280b6fba1d7accb105827f1a8332eb0792347f6d
Keith Bostic <bostic>
1993-06-06 16:24:32 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/spell/spellin.c
+++ usr.bin/spell/spellin.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
@@ -8,13 +8,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1991 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1991, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)spellin.c	4.3 (Berkeley) 04/18/91";
+static char sccsid[] = "@(#)spellin.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "spell.h"
