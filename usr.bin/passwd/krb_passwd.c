1cde0f1ea84fe5eb3b123ddd71f170e58f3abf54
Keith Bostic <bostic>
1993-06-06 15:49:14 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/passwd/krb_passwd.c
+++ usr.bin/passwd/krb_passwd.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)krb_passwd.c	5.4 (Berkeley) 03/01/91";
+static char sccsid[] = "@(#)krb_passwd.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #ifdef KERBEROS
