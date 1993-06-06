3c8b8abf10a10722eb096d21ab9d53df30578349
Keith Bostic <bostic>
1993-06-06 16:32:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/telnet/commands.c
+++ usr.bin/telnet/commands.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988, 1990 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)commands.c	5.13 (Berkeley) 06/01/93";
+static char sccsid[] = "@(#)commands.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #if	defined(unix)
