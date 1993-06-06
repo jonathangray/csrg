5b73ef812d3716b405bb961151352512a18f30f3
Keith Bostic <bostic>
1993-06-06 14:19:12 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/ar/archive.h
+++ usr.bin/ar/archive.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Hugh Smith at The University of Guelph.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)archive.h	5.9 (Berkeley) 06/19/92
+ *	@(#)archive.h	8.1 (Berkeley) 06/06/93
  */
 
 /* Ar(1) options. */
