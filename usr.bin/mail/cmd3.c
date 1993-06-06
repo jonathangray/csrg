33f860167cf6439eed79131e6bcf300301f4d363
Keith Bostic <bostic>
1993-06-06 15:07:15 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/mail/cmd3.c
+++ usr.bin/mail/cmd3.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)cmd3.c	5.26 (Berkeley) 01/22/93";
+static char sccsid[] = "@(#)cmd3.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "rcv.h"
