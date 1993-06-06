9f6a4ee257959dc09a470f8fbe203a136bfa3755
Keith Bostic <bostic>
1993-06-06 15:04:43 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/m4/stdd.h
+++ usr.bin/m4/stdd.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ozan Yigit at York University.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)stdd.h	5.2 (Berkeley) 11/20/91
+ *	@(#)stdd.h	8.1 (Berkeley) 06/06/93
  */
 
 /*
