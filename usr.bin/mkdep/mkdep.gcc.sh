1c42d6d9d628e24728d448b662883ad198ddb91e
Keith Bostic <bostic>
1993-06-06 15:31:09 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/mkdep/mkdep.gcc.sh
+++ usr.bin/mkdep/mkdep.gcc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #
-# Copyright (c) 1991 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1991, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,7 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)mkdep.gcc.sh	5.6 (Berkeley) 01/23/93
+#	@(#)mkdep.gcc.sh	8.1 (Berkeley) 06/06/93
 #
 
 PATH=/bin:/usr/bin:/usr/ucb
