0c75bce605cc4d18e4423c566c398cb9c9ebced2
Keith Bostic <bostic>
1993-06-06 14:34:57 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- usr.bin/diction/style2/ehash.c
+++ usr.bin/diction/style2/ehash.c
@@ -3,7 +3,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)ehash.c	4.5 (Berkeley) 04/27/91
+ *	@(#)ehash.c	8.1 (Berkeley) 06/06/93
  */
 
 struct hnode {
