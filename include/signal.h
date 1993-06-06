7fe0af83f58b692544beea92d46c8486bfbb1a02
Keith Bostic <bostic>
1993-06-06 11:11:55 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/signal.h
+++ include/signal.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)signal.h	5.7 (Berkeley) 06/06/93
+ *	@(#)signal.h	8.1 (Berkeley) 06/06/93
  */
 
 #ifndef _USER_SIGNAL_H
