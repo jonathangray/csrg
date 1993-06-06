33d9015e94e35f1c85e7c8d133ce473baad4ecff
Keith Bostic <bostic>
1993-06-06 14:31:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/diction/NOTUSED/suggest.sh
+++ usr.bin/diction/NOTUSED/suggest.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -
 #
-# Copyright (c) 1988 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1988, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # This module is believed to contain source code proprietary to AT&T.
 # Use and redistribution is subject to the Berkeley Software License
 # Agreement and your Software Agreement with AT&T (Western Electric).
 #
-#	@(#)suggest.sh	5.3 (Berkeley) 04/17/91
+#	@(#)suggest.sh	8.1 (Berkeley) 06/06/93
 #
 
 trap 'rm $$; exit' 1 2 3 15
