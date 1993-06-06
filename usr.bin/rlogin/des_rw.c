006e7848b82a84e402481fa920e5aeca3ec510ed
Keith Bostic <bostic>
1993-06-06 16:09:28 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/rlogin/des_rw.c
+++ usr.bin/rlogin/des_rw.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,6 +32,6 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)des_rw.c	5.9 (Berkeley) 03/04/93";
+static char sccsid[] = "@(#)des_rw.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
