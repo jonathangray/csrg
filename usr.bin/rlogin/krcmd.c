9e58de1234ad816457e9a4dcf947fb91ea683343
Keith Bostic <bostic>
1993-06-06 16:09:43 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/rlogin/krcmd.c
+++ usr.bin/rlogin/krcmd.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)krcmd.c	1.7 (Berkeley) 03/04/93";
+static char sccsid[] = "@(#)krcmd.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
