8371c3857eba9ded4031d51aa6a66750fda2f54e
Keith Bostic <bostic>
1993-06-06 14:34:11 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- usr.bin/diction/style1/names.h
+++ usr.bin/diction/style1/names.h
@@ -3,7 +3,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)names.h	4.4 (Berkeley) 04/17/91
+ *	@(#)names.h	8.1 (Berkeley) 06/06/93
  */
 
 #define ART	'A'
