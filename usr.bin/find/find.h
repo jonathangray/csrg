c13e71a2f39cf1190b7d656e064b7a96ae1b2199
Keith Bostic <bostic>
1993-06-06 14:38:54 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/find/find.h
+++ usr.bin/find/find.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Cimarron D. Taylor of the University of California, Berkeley.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)find.h	5.10 (Berkeley) 05/01/93
+ *	@(#)find.h	8.1 (Berkeley) 06/06/93
  */
 
 /* node type */
