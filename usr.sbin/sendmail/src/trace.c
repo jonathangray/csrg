8ab57ad59c286e3a00eaac44421d45aee143417e
Keith Bostic <bostic>
1993-06-07 10:32:29 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/sendmail/src/trace.c
+++ usr.sbin/sendmail/src/trace.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1983 Eric P. Allman
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)trace.c	6.1 (Berkeley) 12/21/92";
+static char sccsid[] = "@(#)trace.c	8.1 (Berkeley) 06/07/93";
 #endif /* not lint */
 
 # include "sendmail.h"
