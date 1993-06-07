0f928eda3e19df3a3872d5513559dd8f4cc45da7
Keith Bostic <bostic>
1993-06-06 22:14:00 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/uucp/uuclean/uuclean.c
+++ usr.bin/uucp/uuclean/uuclean.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
@@ -8,13 +8,13 @@
  */
 
 #ifndef lint
-char copyright[] =
-"@(#) Copyright (c) 1985 The Regents of the University of California.\n\
- All rights reserved.\n";
+static char copyright[] =
+"@(#) Copyright (c) 1985, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)uuclean.c	5.9 (Berkeley) 04/24/91";
+static char sccsid[] = "@(#)uuclean.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <signal.h>
