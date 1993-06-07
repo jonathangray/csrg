f57c79a406336f18a48f73528ef446f36c329dad
Keith Bostic <bostic>
1993-06-06 22:30:41 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/window/value.h
+++ usr.bin/window/value.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)value.h	3.9 (Berkeley) 06/06/90
+ *	@(#)value.h	8.1 (Berkeley) 06/06/93
  */
 
 struct value {
