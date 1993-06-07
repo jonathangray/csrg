c73f098e6d118ea586cffb440e0865ba5385970a
Keith Bostic <bostic>
1993-06-06 22:20:28 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/vgrind/regexp.c
+++ usr.bin/vgrind/regexp.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,12 +34,12 @@
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1980 The Regents of the University of California.\n\
- All rights reserved.\n";
+"@(#) Copyright (c) 1980, 1993\n\
+	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)regexp.c	5.6 (Berkeley) 09/16/92";
+static char sccsid[] = "@(#)regexp.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include <ctype.h>
