5af53c3b5dc66c0f2ebb2c539c7bcde971446c6c
Keith Bostic <bostic>
1993-06-06 22:23:08 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/w/proc_compare.c
+++ usr.bin/w/proc_compare.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)proc_compare.c	5.5 (Berkeley) 04/27/93";
+static char sccsid[] = "@(#)proc_compare.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 /*
