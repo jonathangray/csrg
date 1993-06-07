45d03323446a42ceda9f0c38d79943d955fd6d64
Keith Bostic <bostic>
1993-06-06 22:11:04 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/uucp/libuu/logent.c
+++ usr.bin/uucp/libuu/logent.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)logent.c	5.11 (Berkeley) 05/17/93";
+static char sccsid[] = "@(#)logent.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #include "uucp.h"
