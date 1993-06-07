458eb3c84a7b371d4692adc37d54d8983a58587c
Keith Bostic <bostic>
1993-06-06 22:20:34 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/vgrind/vgrindefs.c
+++ usr.bin/vgrind/vgrindefs.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)vgrindefs.c	5.3 (Berkeley) 06/01/90";
+static char sccsid[] = "@(#)vgrindefs.c	8.1 (Berkeley) 06/06/93";
 #endif /* not lint */
 
 #define	BUFSIZ	1024
