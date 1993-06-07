a5e0ea6f6d3ce433503bf927b6b49015718b3d60
Keith Bostic <bostic>
1993-06-06 22:13:34 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/uucp/uucico/pk.h
+++ usr.bin/uucp/uucico/pk.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)pk.h	5.7 (Berkeley) 04/24/91
+ *	@(#)pk.h	8.1 (Berkeley) 06/06/93
  */
 
 struct header {
