9f4505a12872d046918365a06e9f5caf0231ea24
Keith Bostic <bostic>
1993-06-06 22:33:35 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/window/xx.h
+++ usr.bin/window/xx.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Edward Wang at The University of California, Berkeley.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)xx.h	3.5 (Berkeley) 06/06/90
+ *	@(#)xx.h	8.1 (Berkeley) 06/06/93
  */
 
 struct xx {
