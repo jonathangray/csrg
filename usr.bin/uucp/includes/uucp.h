51e67b4fe18aef61c7d370e8229a819830ef5f58
Keith Bostic <bostic>
1993-06-06 22:08:15 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/uucp/includes/uucp.h
+++ usr.bin/uucp/includes/uucp.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)uucp.h	5.22 (Berkeley) 05/25/93
+ *	@(#)uucp.h	8.1 (Berkeley) 06/06/93
  */
 
 #include <stdio.h>
