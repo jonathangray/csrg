c5689d7899311dd84884ed7e69b840af8a6bbfe7
Keith Bostic <bostic>
1993-06-07 20:40:07 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libplot/t4013/space.c
+++ lib/libplot/t4013/space.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1985 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)space.c	8.1 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)space.c	8.1 (Berkeley) 06/07/93";
 #endif /* not lint */
 
 extern float botx;
