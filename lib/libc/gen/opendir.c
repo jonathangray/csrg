235566f1462a0ba15a26fd0bee60a103f02a5b94
Keith Bostic <bostic>
1993-06-08 17:57:26 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/gen/opendir.c
+++ lib/libc/gen/opendir.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)opendir.c	5.14 (Berkeley) 06/08/93";
+static char sccsid[] = "@(#)opendir.c	8.1 (Berkeley) 06/08/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/param.h>
