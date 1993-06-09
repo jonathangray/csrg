9fa3d8d42b64d6644a1946e1f3bcd4417788c9c3
Keith Bostic <bostic>
1993-06-08 18:32:13 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- lib/libcurses/PSD.doc/life.c
+++ lib/libcurses/PSD.doc/life.c
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.\"	@(#)life.c	6.4 (Berkeley) 06/08/93
+.\"	@(#)life.c	8.1 (Berkeley) 06/08/93
 .\"
 # include	<curses.h>
 # include	<signal.h>
