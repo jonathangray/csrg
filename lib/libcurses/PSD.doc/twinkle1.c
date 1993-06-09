ac36965b1e62bef3f9e23a6bc67a673850ae0730
Keith Bostic <bostic>
1993-06-08 18:32:14 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- lib/libcurses/PSD.doc/twinkle1.c
+++ lib/libcurses/PSD.doc/twinkle1.c
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.\"	@(#)twinkle1.c	6.4 (Berkeley) 06/08/93
+.\"	@(#)twinkle1.c	8.1 (Berkeley) 06/08/93
 .\"
 # include	<curses.h>
 # include	<signal.h>
