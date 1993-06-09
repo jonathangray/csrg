2042673d7d655003467fa5f6578e0c5e9006a930
Keith Bostic <bostic>
1993-06-08 18:32:17 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- lib/libcurses/PSD.doc/win_st.c
+++ lib/libcurses/PSD.doc/win_st.c
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.\"	@(#)win_st.c	6.4 (Berkeley) 06/08/93
+.\"	@(#)win_st.c	8.1 (Berkeley) 06/08/93
 .\"
 # define	WINDOW	struct _win_st
 
