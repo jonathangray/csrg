b35f20eb95817b26abe92266203064a23c92f8ab
Keith Bostic <bostic>
1993-06-08 18:32:11 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 8.1

--- lib/libcurses/PSD.doc/ex2.c
+++ lib/libcurses/PSD.doc/ex2.c
@@ -29,7 +29,7 @@
 .\" OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 .\" SUCH DAMAGE.
 .\"
-.\"     @(#)ex2.c	1.2 (Berkeley) 06/08/93
+.\"     @(#)ex2.c	8.1 (Berkeley) 06/08/93
 .\"
 #include <curses.h>
 #include <stdio.h>
