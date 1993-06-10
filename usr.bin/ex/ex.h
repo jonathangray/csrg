f53fc59500e01221f324482761a20312e13a38c3
Keith Bostic <bostic>
1993-06-09 21:42:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/ex/ex.h
+++ usr.bin/ex/ex.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)ex.h	7.13.1.1 (Berkeley) 12/03/92
+ *	@(#)ex.h	8.1 (Berkeley) 06/09/93
  */
 
 #ifdef V6
