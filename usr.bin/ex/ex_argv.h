23e64d187138a45beb38b477eaa2fd22b8c6fcd5
Keith Bostic <bostic>
1993-06-09 21:42:13 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/ex/ex_argv.h
+++ usr.bin/ex/ex_argv.h
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1980 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)ex_argv.h	7.4 (Berkeley) 04/17/91
+ *	@(#)ex_argv.h	8.1 (Berkeley) 06/09/93
  */
 
 /*
