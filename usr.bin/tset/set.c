d839a9f24998dae1682f57320fd190992b82890c
Keith Bostic <bostic>
1993-06-09 22:54:04 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.bin/tset/set.c
+++ usr.bin/tset/set.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)set.c	5.3 (Berkeley) 12/01/92";
+static char sccsid[] = "@(#)set.c	8.1 (Berkeley) 06/09/93";
 #endif /* not lint */
 
 #include <termios.h>
