7e6b5e8462af8d0ee0b8d65f0ab9c257a7e756a4
Keith Bostic <bostic>
1993-06-10 21:57:31 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/kern/kern_physio.c
+++ sys/kern/kern_physio.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)kern_physio.c	7.28 (Berkeley) 12/27/92
+ *	@(#)kern_physio.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
