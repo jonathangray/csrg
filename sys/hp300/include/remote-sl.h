7e0f3f8cd4f90bfab4b0fb51ba429e87cdcaf885
Keith Bostic <bostic>
1993-06-10 21:45:17 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/include/remote-sl.h
+++ sys/hp300/include/remote-sl.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Steven McCanne of Lawrence Berkeley Laboratory.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)remote-sl.h	7.4 (Berkeley) 07/24/92
+ *	@(#)remote-sl.h	8.1 (Berkeley) 06/10/93
  *
  * $Header: remote-sl.h,v 1.2 92/07/23 19:38:20 mccanne Exp $ (LBL)
  */
