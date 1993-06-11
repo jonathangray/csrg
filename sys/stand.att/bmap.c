b7fc0588550ef4203c730c4d587bf11bf933d618
Keith Bostic <bostic>
1993-06-11 16:01:18 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/stand.att/bmap.c
+++ sys/stand.att/bmap.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1982, 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)bmap.c	7.4 (Berkeley) 05/24/93
+ *	@(#)bmap.c	8.1 (Berkeley) 06/11/93
  */
 
 #include <sys/param.h>
