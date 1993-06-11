1d26b6548fc0602a17328e03aeb8b9798fa2c447
Keith Bostic <bostic>
1993-06-10 23:06:03 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/pmax/pmax/asic.h
+++ sys/pmax/pmax/asic.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * The Mach Operating System project at Carnegie-Mellon University,
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)asic.h	7.3 (Berkeley) 05/09/93
+ *	@(#)asic.h	8.1 (Berkeley) 06/10/93
  */
 
 /* 
