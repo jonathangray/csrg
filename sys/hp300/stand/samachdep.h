70301dd59e9764cbaaa7ee5b1fded35ade62e586
Keith Bostic <bostic>
1993-06-10 21:47:42 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/stand/samachdep.h
+++ sys/hp300/stand/samachdep.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)samachdep.h	7.4 (Berkeley) 12/26/92
+ *	@(#)samachdep.h	8.1 (Berkeley) 06/10/93
  */
 
 #define	NHPIB		4
