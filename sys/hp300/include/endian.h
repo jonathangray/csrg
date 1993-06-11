71635e3182fd325a06a463c2716c95b98c1fe58f
Keith Bostic <bostic>
1993-06-10 21:44:36 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/include/endian.h
+++ sys/hp300/include/endian.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1987, 1991 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1987, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)endian.h	7.10 (Berkeley) 05/07/93
+ *	@(#)endian.h	8.1 (Berkeley) 06/10/93
  */
 
 #ifndef _ENDIAN_H_
