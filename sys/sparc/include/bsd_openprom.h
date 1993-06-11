55ca86c5666b82d959704fa7218448ee71ece808
Keith Bostic <bostic>
1993-06-11 15:11:42 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sparc/include/bsd_openprom.h
+++ sys/sparc/include/bsd_openprom.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Jan-Simon Pendry.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)bsd_openprom.h	7.2 (Berkeley) 04/20/93
+ *	@(#)bsd_openprom.h	8.1 (Berkeley) 06/11/93
  *
  * from: $Header: bsd_openprom.h,v 1.3 92/09/09 00:41:33 leres Exp $
  */
