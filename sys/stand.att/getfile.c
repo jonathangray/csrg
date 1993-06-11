c56e844d3dec17fe9a1c0a54b9a9cda0c0340704
Keith Bostic <bostic>
1993-06-11 16:01:44 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/stand.att/getfile.c
+++ sys/stand.att/getfile.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)getfile.c	7.8 (Berkeley) 05/24/93
+ *	@(#)getfile.c	8.1 (Berkeley) 06/11/93
  */
 
 #include <sys/param.h>
