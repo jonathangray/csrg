1c249ca572a91437cdd54975f8551ae83bc0fcb1
Keith Bostic <bostic>
1993-06-10 21:41:34 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/hp300/swapgeneric.c
+++ sys/hp300/hp300/swapgeneric.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)swapgeneric.c	7.8 (Berkeley) 10/11/92
+ *	@(#)swapgeneric.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
