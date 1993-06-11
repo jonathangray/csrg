d5bce50fe328bf9b72c404aa97d6d7b37df1d4eb
Keith Bostic <bostic>
1993-06-11 15:48:49 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/i386/include/types.h
+++ sys/i386/include/types.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)types.h	7.9 (Berkeley) 04/30/93
+ *	@(#)types.h	8.1 (Berkeley) 06/11/93
  */
 
 #ifndef	_MACHTYPES_H_
