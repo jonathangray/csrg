a20db745cfaf75c671e3cba60e35f1fa473884d5
Keith Bostic <bostic>
1993-06-10 23:02:17 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/pmax/include/mips_opcode.h
+++ sys/pmax/include/mips_opcode.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mips_opcode.h	7.1 (Berkeley) 03/19/92
+ *	@(#)mips_opcode.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
