f5b3751f0fdefde06c9b3520b5a5148af6179b53
Keith Bostic <bostic>
1993-06-11 16:01:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/stand.att/cat.c
+++ sys/stand.att/cat.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)cat.c	7.5 (Berkeley) 06/28/90
+ *	@(#)cat.c	8.1 (Berkeley) 06/11/93
  */
 
 main()
