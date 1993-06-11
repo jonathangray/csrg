04153366ff4a55ed2c673db5b93cc24fbc19a78c
Keith Bostic <bostic>
1993-06-10 23:31:11 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/pmax/stand/boot.c
+++ sys/pmax/stand/boot.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)boot.c	7.9 (Berkeley) 05/09/93
+ *	@(#)boot.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
