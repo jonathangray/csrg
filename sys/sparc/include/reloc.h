326ef0508b922df4700c3ab8a0452362003b6fcb
Keith Bostic <bostic>
1993-06-11 15:12:39 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sparc/include/reloc.h
+++ sys/sparc/include/reloc.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)reloc.h	7.3 (Berkeley) 04/20/93
+ *	@(#)reloc.h	8.1 (Berkeley) 06/11/93
  *
  * from: $Header: reloc.h,v 1.7 92/11/26 02:04:45 torek Exp $
  */
