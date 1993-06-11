7d4f802d417508b14012026228e0e3ba7ce60678
Keith Bostic <bostic>
1993-06-10 23:04:16 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netinet/in.h
+++ sys/netinet/in.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982,1986,1990,1993 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993, 1982198619901993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)in.h	7.15 (Berkeley) 06/04/93
+ *	@(#)in.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
