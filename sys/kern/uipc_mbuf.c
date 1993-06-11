e19f85e730fff606f32e2c0158960defffd8787b
Keith Bostic <bostic>
1993-06-10 21:59:37 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/kern/uipc_mbuf.c
+++ sys/kern/uipc_mbuf.c
@@ -29,7 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)uipc_mbuf.c	7.25 (Berkeley) 10/11/92
+ *	@(#)uipc_mbuf.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
