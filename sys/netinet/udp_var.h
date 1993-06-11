fe771af530c3dff695edc077f7f9e39ab0f395f7
Keith Bostic <bostic>
1993-06-10 23:06:35 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netinet/udp_var.h
+++ sys/netinet/udp_var.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986, 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)udp_var.h	7.12 (Berkeley) 06/04/93
+ *	@(#)udp_var.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
