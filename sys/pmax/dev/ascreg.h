399cbac2869a1448758662eea6ca97bdd70034d3
Keith Bostic <bostic>
1993-06-10 22:38:01 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/pmax/dev/ascreg.h
+++ sys/pmax/dev/ascreg.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell and Rick Macklem.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ascreg.h	7.3 (Berkeley) 12/20/92
+ *	@(#)ascreg.h	8.1 (Berkeley) 06/10/93
  */
 
 /* 
