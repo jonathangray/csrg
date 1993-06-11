a90d25eeb95c3318c8562b5e538703c790655e74
Keith Bostic <bostic>
1993-06-10 23:05:44 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netinet/tcp_output.c
+++ sys/netinet/tcp_output.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1988, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tcp_output.c	7.29 (Berkeley) 06/04/93
+ *	@(#)tcp_output.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
