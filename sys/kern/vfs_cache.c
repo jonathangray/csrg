2828d01b74dbed220bbe5853a975de95714b1a90
Keith Bostic <bostic>
1993-06-10 22:00:19 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/kern/vfs_cache.c
+++ sys/kern/vfs_cache.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vfs_cache.c	7.16 (Berkeley) 10/11/92
+ *	@(#)vfs_cache.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
