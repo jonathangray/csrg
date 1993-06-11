abda21c4f5a6dcaa9e12a972626d6de80f3a3f5e
Keith Bostic <bostic>
1993-06-10 23:21:39 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netiso/tuba_table.c
+++ sys/netiso/tuba_table.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tuba_table.c	7.13 (Berkeley) 06/04/93
+ *	@(#)tuba_table.c	8.1 (Berkeley) 06/10/93
  */
 #include <sys/param.h>
 #include <sys/systm.h>
