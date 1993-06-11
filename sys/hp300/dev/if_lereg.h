78dd906461bbd25c745b455cd7fcd08934e54d78
Keith Bostic <bostic>
1993-06-10 21:33:18 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/dev/if_lereg.h
+++ sys/hp300/dev/if_lereg.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_lereg.h	7.5 (Berkeley) 10/11/92
+ *	@(#)if_lereg.h	8.1 (Berkeley) 06/10/93
  */
 
 #include <hp/dev/iotypes.h>	/* XXX */
