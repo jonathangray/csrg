a8efffc122cf9db492cb7de5506cbc06b7b7ea3a
Keith Bostic <bostic>
1993-06-11 13:37:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libcurses/scroll.c
+++ lib/libcurses/scroll.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1981 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1981, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)scroll.c	5.14 (Berkeley) 06/11/93";
+static char sccsid[] = "@(#)scroll.c	8.1 (Berkeley) 06/11/93";
 #endif /* not lint */
 
 #include <curses.h>
