899e8fab60ba305cbcd6951ce5a837f92f24bc01
Keith Bostic <bostic>
1993-06-11 15:35:06 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/i386/i386/swapgeneric.c
+++ sys/i386/i386/swapgeneric.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)swapgeneric.c	5.6 (Berkeley) 10/11/92
+ *	@(#)swapgeneric.c	8.1 (Berkeley) 06/11/93
  */
 
 #include <machine/pte.h>
