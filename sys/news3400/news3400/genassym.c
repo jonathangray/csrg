e4c8d5b9960327e608175280988d515d2ad63c50
Keith Bostic <bostic>
1993-06-11 15:02:38 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/news3400/news3400/genassym.c
+++ sys/news3400/news3400/genassym.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)genassym.c	7.7 (Berkeley) 05/13/93
+ *	@(#)genassym.c	8.1 (Berkeley) 06/11/93
  */
 
 #define KERNEL
