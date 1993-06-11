3c688b78b0bb707e17a410af07c0dc7249bd90e3
Keith Bostic <bostic>
1993-06-10 21:16:36 -0700

    4.4BSD snapshot (revision 8.1)
    SCCS-vsn: 7.8

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
 #
-# Copyright (c) 1984,1986,1990,1993 The Regents of the University of California.
-# All rights reserved.
+# Copyright (c) 1984, 1986, 1990, 1993
+#	The Regents of the University of California.  All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -31,8 +31,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-#	@(#)newvers.sh	8.1 (Berkeley) 06/10/93
-#
+#	@(#)newvers.sh	7.8 (Berkeley) 06/10/93
 
 if [ ! -r version ]
 then
