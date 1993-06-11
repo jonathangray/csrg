3344e323fa5f1d20e8d2b2b12d0fb8e29735fa84
Keith Bostic <bostic>
1993-06-10 23:04:41 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netinet/in_var.h
+++ sys/netinet/in_var.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1985, 1986 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1985, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)in_var.h	7.9 (Berkeley) 06/04/93
+ *	@(#)in_var.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
