6374aafbe4d71b0d3cac221cb0d9b060a7a55e52
Keith Bostic <bostic>
1993-06-10 23:21:32 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netiso/tp_usrreq.c
+++ sys/netiso/tp_usrreq.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tp_usrreq.c	7.29 (Berkeley) 06/10/93
+ *	@(#)tp_usrreq.c	8.1 (Berkeley) 06/10/93
  */
 
 /***********************************************************
