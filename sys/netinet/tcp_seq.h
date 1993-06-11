43df25f7f1c7078cbe0e5a931a855d063f3994b7
Keith Bostic <bostic>
1993-06-10 23:05:48 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netinet/tcp_seq.h
+++ sys/netinet/tcp_seq.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tcp_seq.h	7.5 (Berkeley) 06/04/93
+ *	@(#)tcp_seq.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
