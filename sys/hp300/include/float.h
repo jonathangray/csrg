b9754b6c038da893928e73811f7a9f8774d09f50
Keith Bostic <bostic>
1993-06-10 21:44:41 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/include/float.h
+++ sys/hp300/include/float.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)float.h	7.1 (Berkeley) 05/08/90
+ *	@(#)float.h	8.1 (Berkeley) 06/10/93
  */
 
 #define FLT_RADIX	2		/* b */
