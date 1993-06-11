53e6ea827cbd35fe58015fe7d2492271400ab122
Keith Bostic <bostic>
1993-06-11 15:14:56 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sparc/sbus/if_lereg.h
+++ sys/sparc/sbus/if_lereg.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1982, 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_lereg.h	7.2 (Berkeley) 04/20/93
+ *	@(#)if_lereg.h	8.1 (Berkeley) 06/11/93
  *
  * from: $Header: if_lereg.h,v 1.6 92/11/26 02:28:12 torek Exp $
  */
