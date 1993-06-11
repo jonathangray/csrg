a06569a8a93a86339b49c0e6832de1a96876bd78
Keith Bostic <bostic>
1993-06-11 16:02:18 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/stand.att/saio.h
+++ sys/stand.att/saio.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1982, 1988, 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1988, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)saio.h	7.20 (Berkeley) 05/24/93
+ *	@(#)saio.h	8.1 (Berkeley) 06/11/93
  */
 
 #include <sys/time.h>
