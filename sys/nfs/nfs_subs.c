63e8924fb3a1465adf18e999b4ecf13a940f844b
Keith Bostic <bostic>
1993-06-10 23:39:11 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/nfs/nfs_subs.c
+++ sys/nfs/nfs_subs.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_subs.c	7.70 (Berkeley) 03/30/93
+ *	@(#)nfs_subs.c	8.1 (Berkeley) 06/10/93
  */
 
 /*
