b2eb9d2bb3926c5b1e91f158711936490011ebe9
Keith Bostic <bostic>
1993-06-10 23:34:54 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netns/ns_error.h
+++ sys/netns/ns_error.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1984, 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1984, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ns_error.h	7.5 (Berkeley) 06/28/90
+ *	@(#)ns_error.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
