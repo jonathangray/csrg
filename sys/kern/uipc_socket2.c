5b6aeff7553a63760444328060f8e66b5ba0eed0
Keith Bostic <bostic>
1993-06-10 21:59:54 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/kern/uipc_socket2.c
+++ sys/kern/uipc_socket2.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986, 1988, 1990 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1988, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)uipc_socket2.c	7.19 (Berkeley) 10/11/92
+ *	@(#)uipc_socket2.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
