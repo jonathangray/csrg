6dcf7d4e8fdd3461906ec4394f93a43957e5fdc8
Keith Bostic <bostic>
1993-06-11 14:51:01 -0700

    integrate into 4.4BDS
    SCCS-vsn: 5.2

--- games/boggle/mkindex/mkindex.c
+++ games/boggle/mkindex/mkindex.c
@@ -1,20 +1,67 @@
-/* vi: set tabstop=4 : */
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Barry Brachman.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef lint
+char copyright[] =
+"@(#) Copyright (c) 1993 The Regents of the University of California.\n\
+ All rights reserved.\n";
+#endif /* not lint */
+
+#ifndef lint
+static char sccsid[] = "@(#)mkindex.c	5.2 (Berkeley) 06/11/93";
+#endif /* not lint */
 
 #include <stdio.h>
 
 #include "bog.h"
 
+char *nextword __P((FILE *, char *, int *, int *));
+
+int
 main(argc, argv)
-int argc;
-char **argv;
+	int argc;
+	char *argv[];
 {
 	int clen, rlen, prev;
 	long off, start;
-	char buf[MAXWORDLEN + 1], *p, *nextword();
+	char buf[MAXWORDLEN + 1];
 
 	prev = '\0';
 	off = start = 0L;
-	while (nextword(stdin, buf, &clen, &rlen) != (char *) NULL) {
+	while (nextword(stdin, buf, &clen, &rlen) != NULL) {
 		if (*buf != prev) {
 			if (prev != '\0')
 				printf("%c %6ld %6ld\n", prev, start, off - 1);
@@ -35,30 +82,30 @@ char **argv;
  */
 char *
 nextword(fp, buffer, clen, rlen)
-FILE *fp;
-char *buffer;
-int *clen, *rlen;
+	FILE *fp;
+	char *buffer;
+	int *clen, *rlen;
 {
-    register int ch, pcount;
+	register int ch, pcount;
 	register char *p, *q;
 	static char buf[MAXWORDLEN + 1];
 	static int first = 1;
 	static int lastch = 0;
 
    	if (first) {
-        if ((pcount = getc(fp)) == EOF)
-			return((char *) NULL);
+		if ((pcount = getc(fp)) == EOF)
+			return (NULL);
 		first = 0;
 	}
 	else if ((pcount = lastch) == EOF)
-		return((char *) NULL);
+		return (NULL);
 
 	p = buf + (*clen = pcount);
  
-    while ((ch = getc(fp)) != EOF && ch >= 'a')
+	while ((ch = getc(fp)) != EOF && ch >= 'a')
 			*p++ = ch;
-	    lastch = ch;
-    *p = '\0';
+		lastch = ch;
+	*p = '\0';
 
 	*rlen = (int) (p - buf);
 	*clen = *rlen - *clen;
@@ -69,6 +116,5 @@ int *clen, *rlen;
 		if (*p++ == 'q')
 			*q++ = 'u';
 	}
-    return(buffer);
+	return (buffer);
 }
- 
