592c3e8d46646c8dd623ebb03e657035ad9fd1fb
Keith Bostic <bostic>
1993-06-10 23:19:00 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netiso/cons.h
+++ sys/netiso/cons.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)cons.h	7.5 (Berkeley) 06/04/93
+ *	@(#)cons.h	8.1 (Berkeley) 06/10/93
  */
 
 /***********************************************************
