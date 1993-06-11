84754509e231de2ff351e07120cf5c8b23aaa2f2
Keith Bostic <bostic>
1993-06-10 21:41:20 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/hp300/pmap.c
+++ sys/hp300/hp300/pmap.c
@@ -1,6 +1,6 @@
 /* 
- * Copyright (c) 1991 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pmap.c	7.16 (Berkeley) 05/20/93
+ *	@(#)pmap.c	8.1 (Berkeley) 06/10/93
  */
 
 /*
