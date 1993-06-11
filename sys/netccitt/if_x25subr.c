6768bd3ac44cd6c9a8fc28f489bb6eb4316012bd
Keith Bostic <bostic>
1993-06-10 22:53:15 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netccitt/if_x25subr.c
+++ sys/netccitt/if_x25subr.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_x25subr.c	7.21 (Berkeley) 06/05/93
+ *	@(#)if_x25subr.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
