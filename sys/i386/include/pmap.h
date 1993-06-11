b6a5cf08e3919cceebced1515fee4a766053d91c
Keith Bostic <bostic>
1993-06-11 15:48:14 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/i386/include/pmap.h
+++ sys/i386/include/pmap.h
@@ -1,6 +1,6 @@
 /* 
- * Copyright (c) 1991 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pmap.h	7.6 (Berkeley) 06/02/93
+ *	@(#)pmap.h	8.1 (Berkeley) 06/11/93
  */
 
 /*
