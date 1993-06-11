571cd83ab7a6404593fcb8d2ada86974fd0dbed7
Keith Bostic <bostic>
1993-06-10 21:15:52 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/conf/param.c
+++ sys/conf/param.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)param.c	7.23 (Berkeley) 10/11/92
+ *	@(#)param.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
