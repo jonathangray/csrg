1861b2cb6454832c77ffb3420006c6ca85a3a002
Keith Bostic <bostic>
1993-06-10 22:45:22 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/net/raw_cb.h
+++ sys/net/raw_cb.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1980, 1986 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1980, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)raw_cb.h	7.7 (Berkeley) 06/04/93
+ *	@(#)raw_cb.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
