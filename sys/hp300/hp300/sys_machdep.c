7e0c194183f508435df610edd16f5aba3bff0025
Keith Bostic <bostic>
1993-06-10 21:41:37 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/hp300/sys_machdep.c
+++ sys/hp300/hp300/sys_machdep.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sys_machdep.c	7.11 (Berkeley) 12/27/92
+ *	@(#)sys_machdep.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
