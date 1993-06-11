ce6d650562cf5e6a53ae741bc57d893fdcf71c6a
Keith Bostic <bostic>
1993-06-10 23:01:54 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/pmax/include/cpu.h
+++ sys/pmax/include/cpu.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell and Rick Macklem.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)cpu.h	7.8 (Berkeley) 05/24/93
+ *	@(#)cpu.h	8.1 (Berkeley) 06/10/93
  */
 
 #ifndef _CPU_H_
