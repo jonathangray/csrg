1ad34f24613f12de797398ba6bbd08f41f171c74
Keith Bostic <bostic>
1993-06-11 14:43:59 -0700

    integrate into 4.4BSD
    SCCS-vsn: 5.2

--- games/boggle/boggle/prtable.c
+++ games/boggle/boggle/prtable.c
@@ -1,10 +1,48 @@
-/* vi: set tabstop=4 : */
+/*-
+ * Copyright (c) 1993 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Barry Brachman.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)prtable.c	5.2 (Berkeley) 06/11/93
+ */
 
 #include <curses.h>
 
+#include "extern.h"
+
 #define NCOLS	5
 
-static int get_maxlen();
+static int	get_maxlen __P((char *[], int, int (*)(char **, int)));
 
 /*
  * Routine to print a table
@@ -21,10 +59,12 @@ static int get_maxlen();
  * prtable and length are called with the the address of the base and
  * an index
  */
+void
 prtable(base, num, d_cols, width, prentry, length)
-char *base;
-int num, d_cols;
-int (*prentry)(), (*length)();
+	char *base[];
+	int num, d_cols, width;
+	void (*prentry) __P((char *[], int));
+	int (*length) __P((char *[], int));
 {
         register int c, j;
         register int a, b, cols, loc, maxlen, nrows, z;
@@ -72,9 +112,9 @@ int (*prentry)(), (*length)();
 
 static int
 get_maxlen(base, num, length)
-char *base;
-int num;
-int (*length)();
+	char *base[];
+	int num;
+	int (*length) __P((char **, int));
 {
 	register int i, len, max;
 
@@ -85,4 +125,3 @@ int (*length)();
 	}
 	return(max);
 }
-
