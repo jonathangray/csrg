341e687da4bf8e7af197f5ad82071f60b6583511
Keith Bostic <bostic>
1993-06-11 14:58:43 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/news3400/include/exec.h
+++ sys/news3400/include/exec.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)exec.h	7.2 (Berkeley) 03/09/93
+ *	@(#)exec.h	8.1 (Berkeley) 06/11/93
  */
 
 /*
