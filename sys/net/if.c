e2ad4cfdeb69b1e3688bfe912fcd0c1ffbbdd0ce
Keith Bostic <bostic>
1993-06-10 22:44:28 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/net/if.c
+++ sys/net/if.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if.c	7.27 (Berkeley) 06/04/93
+ *	@(#)if.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
