ed188132fadf0e021bc6804b24e41d864f8e3da4
Keith Bostic <bostic>
1993-06-10 20:50:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/gen/closedir.c
+++ lib/libc/gen/closedir.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1983 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1983, 1993
+ *	Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)closedir.c	5.10 (Berkeley) 06/10/93";
+static char sccsid[] = "@(#)closedir.c	8.1 (Berkeley) 06/10/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
