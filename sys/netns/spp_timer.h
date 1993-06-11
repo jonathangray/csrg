6de6799c284b364219080161fc28f03414087e23
Keith Bostic <bostic>
1993-06-10 23:35:33 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/netns/spp_timer.h
+++ sys/netns/spp_timer.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986, 1988 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)spp_timer.h	7.3 (Berkeley) 06/28/90
+ *	@(#)spp_timer.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
