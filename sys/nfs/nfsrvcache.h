7c5f6846553d4f60565e16877433249b554982ee
Keith Bostic <bostic>
1993-06-10 23:39:53 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/nfs/nfsrvcache.h
+++ sys/nfs/nfsrvcache.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Rick Macklem at The University of Guelph.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfsrvcache.h	7.7 (Berkeley) 05/20/93
+ *	@(#)nfsrvcache.h	8.1 (Berkeley) 06/10/93
  */
 
 /*
