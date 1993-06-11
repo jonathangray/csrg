d2548dafdb2563f2d0ccdd3df60380125df79ecc
Keith Bostic <bostic>
1993-06-10 21:20:49 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/dev/scsi/scsivar.h
+++ sys/dev/scsi/scsivar.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)scsivar.h	5.3 (Berkeley) 02/01/93
+ *	@(#)scsivar.h	8.1 (Berkeley) 06/10/93
  *
  * from: $Header: scsivar.h,v 1.7 92/12/02 03:54:05 torek Exp $ (LBL)
  */
