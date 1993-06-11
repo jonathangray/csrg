71796d0a599d5fe52f94c8bf8ec271bccbe34074
Keith Bostic <bostic>
1993-06-11 16:36:40 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/vm/device_pager.c
+++ sys/vm/device_pager.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 1990 University of Utah.
- * Copyright (c) 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * the Systems Programming Group of the University of Utah Computer
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)device_pager.c	7.8 (Berkeley) 04/17/93
+ *	@(#)device_pager.c	8.1 (Berkeley) 06/11/93
  */
 
 /*
