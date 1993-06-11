a0df04d9bc792a7b1781e4bcf4b562b002a28b0c
Keith Bostic <bostic>
1993-06-10 21:57:44 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/kern/kern_resource.c
+++ sys/kern/kern_resource.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1982, 1986, 1991 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1991, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kern_resource.c	7.26 (Berkeley) 05/25/93
+ *	@(#)kern_resource.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
