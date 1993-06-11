baaf2360ca01026d18acc8feae2fe14678e894b0
Keith Bostic <bostic>
1993-06-10 23:35:46 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/miscfs/fdesc/fdesc_vfsops.c
+++ sys/miscfs/fdesc/fdesc_vfsops.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California
- * Copyright (c) 1990, 1992 Jan-Simon Pendry
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  * All rights reserved.
  *
  * This code is derived from software donated to Berkeley by
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fdesc_vfsops.c	7.4 (Berkeley) 04/29/93
+ *	@(#)fdesc_vfsops.c	8.1 (Berkeley) 06/10/93
  *
  * $Id: fdesc_vfsops.c,v 1.9 1993/04/06 15:28:33 jsp Exp $
  */
