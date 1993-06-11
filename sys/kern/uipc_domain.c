bf88ea5e313c918e71a48bb695df8918997e10ad
Keith Bostic <bostic>
1993-06-10 21:59:33 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/kern/uipc_domain.c
+++ sys/kern/uipc_domain.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)uipc_domain.c	7.16 (Berkeley) 04/27/93
+ *	@(#)uipc_domain.c	8.1 (Berkeley) 06/10/93
  */
 
 #include <sys/param.h>
