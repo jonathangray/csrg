c09649a34296daeed79e446901fb649f763a54dd
Keith Bostic <bostic>
1993-06-10 23:03:02 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/pmax/include/stdarg.h
+++ sys/pmax/include/stdarg.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)stdarg.h	7.3 (Berkeley) 01/21/93
+ *	@(#)stdarg.h	8.1 (Berkeley) 06/10/93
  */
 
 #ifndef _STDARG_H_
