66cd37ef7855c46623c1c1a1b977dd274ceb4333
Keith Bostic <bostic>
1993-06-10 21:23:12 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp/dev/dca.c
+++ sys/hp/dev/dca.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1986, 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1986, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)dca.c	7.18 (Berkeley) 12/27/92
+ *	@(#)dca.c	8.1 (Berkeley) 06/10/93
  */
 
 #include "dca.h"
