32dc629a59090ce03ae22c471b5c0e46bfa096f3
Keith Bostic <bostic>
1993-06-11 15:12:47 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/sparc/include/stdarg.h
+++ sys/sparc/include/stdarg.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)stdarg.h	7.5 (Berkeley) 05/07/93
+ *	@(#)stdarg.h	8.1 (Berkeley) 06/11/93
  *
  * from: $Header: stdarg.h,v 1.8 93/05/07 18:10:14 torek Exp $
  */
