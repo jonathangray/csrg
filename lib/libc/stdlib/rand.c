9abf819e8f99287cbebf86b4ff5092a6795cad4d
Keith Bostic <bostic>
1993-06-14 15:45:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/stdlib/rand.c
+++ lib/libc/stdlib/rand.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)rand.c	5.7 (Berkeley) 06/14/93";
+static char sccsid[] = "@(#)rand.c	8.1 (Berkeley) 06/14/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
