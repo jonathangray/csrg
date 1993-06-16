0675d8c58e7eb3f2ca4bff1360431f28346d5d59
Kirk McKusick <mckusick>
1993-06-16 16:29:10 -0700

    __indir => __syscall
    SCCS-vsn: 5.3

--- lib/libc/sys/ftruncate.c
+++ lib/libc/sys/ftruncate.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ftruncate.c	8.1 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)ftruncate.c	5.3 (Berkeley) 06/16/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
@@ -48,5 +48,5 @@ ftruncate(fd, length)
 	off_t	length;
 {
 
-	return(__indir((quad_t)SYS_ftruncate, fd, 0, length));
+	return(__syscall((quad_t)SYS_ftruncate, fd, 0, length));
 }
