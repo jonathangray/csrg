ca8481065594354fb4c2a19e827e6c6034d53bac
Keith Bostic <bostic>
1993-06-17 07:17:24 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/sys/ftruncate.c
+++ lib/libc/sys/ftruncate.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)ftruncate.c	5.3 (Berkeley) 06/16/93";
+static char sccsid[] = "@(#)ftruncate.c	8.1 (Berkeley) 06/17/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
