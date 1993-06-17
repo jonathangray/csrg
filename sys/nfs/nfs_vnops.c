2ebd65a9fc367b94ac1a55a6428bf0a59e50a618
Kirk McKusick <mckusick>
1993-06-16 17:16:01 -0700

    fix for trashed TCP/IP connections from Macklem
    SCCS-vsn: 7.109

--- sys/nfs/nfs_vnops.c
+++ sys/nfs/nfs_vnops.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_vnops.c	8.1 (Berkeley) 06/10/93
+ *	@(#)nfs_vnops.c	7.109 (Berkeley) 06/16/93
  */
 
 /*
@@ -1629,7 +1629,7 @@ nfs_readdirrpc(vp, uiop, cred)
 		while (more_dirs && siz < uiop->uio_resid) {
 			savoff = off;		/* Hold onto offset and dp */
 			savdp = dp;
-			nfsm_dissecton(tl, u_long *, 2 * NFSX_UNSIGNED);
+			nfsm_dissect(tl, u_long *, 2 * NFSX_UNSIGNED);
 			dp = (struct dirent *)tl;
 			dp->d_fileno = fxdr_unsigned(u_long, *tl++);
 			len = fxdr_unsigned(int, *tl);
@@ -1651,7 +1651,7 @@ nfs_readdirrpc(vp, uiop, cred)
 				nfsm_adv(tlen - len);
 				len = tlen;
 			}
-			nfsm_dissecton(tl, u_long *, 2 * NFSX_UNSIGNED);
+			nfsm_dissect(tl, u_long *, 2 * NFSX_UNSIGNED);
 			off = fxdr_unsigned(u_long, *tl);
 			*tl++ = 0;	/* Ensures null termination of name */
 			more_dirs = fxdr_unsigned(int, *tl);
@@ -1662,7 +1662,7 @@ nfs_readdirrpc(vp, uiop, cred)
 		 * If at end of rpc data, get the eof boolean
 		 */
 		if (!more_dirs) {
-			nfsm_dissecton(tl, u_long *, NFSX_UNSIGNED);
+			nfsm_dissect(tl, u_long *, NFSX_UNSIGNED);
 			more_dirs = (fxdr_unsigned(int, *tl) == 0);
 
 			/*
