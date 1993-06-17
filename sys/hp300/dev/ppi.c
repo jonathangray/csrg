ae361d28c3b7a4a41fb208e37110a50f268335e4
Keith Bostic <bostic>
1993-06-16 17:47:58 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/hp300/dev/ppi.c
+++ sys/hp300/dev/ppi.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1982, 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ppi.c	7.7 (Berkeley) 06/16/93
+ *	@(#)ppi.c	8.1 (Berkeley) 06/16/93
  */
 
 /*
