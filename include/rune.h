103248229aeae1d34d7e50abf867202765bd5ca0
Keith Bostic <bostic>
1993-06-27 19:24:06 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/rune.h
+++ include/rune.h
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Paul Borman at Krystal Technologies.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rune.h	5.3 (Berkeley) 06/27/93
+ *	@(#)rune.h	8.1 (Berkeley) 06/27/93
  */
 
 #ifndef	_RUNE_H_
