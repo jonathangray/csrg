f0fa3ea35cd5bf1e26a0d166c4526d4840f2bb7e
Keith Bostic <bostic>
1993-06-27 19:22:38 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- lib/libc/locale/setlocale.c
+++ lib/libc/locale/setlocale.c
@@ -35,7 +35,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)setlocale.c	8.1 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)setlocale.c	8.1 (Berkeley) 06/27/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <limits.h>
