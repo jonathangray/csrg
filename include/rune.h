d3ab8dc461dc51de47b7b1dc353ede1761405d19
Keith Bostic <bostic>
1993-06-27 18:05:38 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/rune.h
+++ include/rune.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rune.h	5.2 (Berkeley) 06/27/93
+ *	@(#)rune.h	8.1 (Berkeley) 06/27/93
  */
 
 #ifndef	_RUNE_H_
