a5c860c53184477e2652f81eb106a4ec2bd17192
Kirk McKusick <mckusick>
1993-06-29 14:57:59 -0700

    don't read past end of string if_name
    SCCS-vsn: 7.28

--- sys/net/if.c
+++ sys/net/if.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if.c	8.1 (Berkeley) 06/10/93
+ *	@(#)if.c	7.28 (Berkeley) 06/29/93
  */
 
 #include <sys/param.h>
@@ -602,13 +602,14 @@ ifconf(cmd, data)
 	ifrp = ifc->ifc_req;
 	ep = ifr.ifr_name + sizeof (ifr.ifr_name) - 2;
 	for (; space > sizeof (ifr) && ifp; ifp = ifp->if_next) {
-		bcopy(ifp->if_name, ifr.ifr_name, sizeof (ifr.ifr_name) - 2);
+		strncpy(ifr.ifr_name, ifp->if_name, sizeof (ifr.ifr_name) - 2);
 		for (cp = ifr.ifr_name; cp < ep && *cp; cp++)
 			continue;
 		*cp++ = '0' + ifp->if_unit; *cp = '\0';
 		if ((ifa = ifp->if_addrlist) == 0) {
 			bzero((caddr_t)&ifr.ifr_addr, sizeof(ifr.ifr_addr));
-			error = copyout((caddr_t)&ifr, (caddr_t)ifrp, sizeof (ifr));
+			error = copyout((caddr_t)&ifr, (caddr_t)ifrp,
+			    sizeof (ifr));
 			if (error)
 				break;
 			space -= sizeof (ifr), ifrp++;
