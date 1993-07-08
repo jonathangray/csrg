3f1e3c14fd2235f8360499f5ef91f441cd45838e
Keith Bostic <bostic>
1993-07-07 17:16:06 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/stand.att/open.c
+++ sys/stand.att/open.c
@@ -1,12 +1,12 @@
 /*-
- * Copyright (c) 1982, 1988 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1982, 1988, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)open.c	7.8 (Berkeley) 07/07/93
+ *	@(#)open.c	8.1 (Berkeley) 07/07/93
  */
 
 #include <sys/param.h>
