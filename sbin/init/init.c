bc2f08677525f3ea7b280b64653ceee9fe5df71a
Kirk McKusick <mckusick>
1993-07-15 17:39:22 -0700

    do setlogin("root") to establish the single-user user
    SCCS-vsn: 6.24

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -41,7 +41,7 @@ char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)init.c	8.1 (Berkeley) 06/05/93";
+static char sccsid[] = "@(#)init.c	6.24 (Berkeley) 07/15/93";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -194,6 +194,13 @@ main(argc, argv)
 	 */
 	if (setsid() < 0)
 		warning("initial setsid() failed: %m");
+
+	/*
+	 * Establish an initial user so that programs running
+	 * single user do not freak out and die (like passwd).
+	 */
+	if (setlogin("root") < 0)
+		warning("setlogin() failed: %m");
 
 	/*
 	 * This code assumes that we always get arguments through flags,
