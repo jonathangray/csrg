2ddb4cbc457af21174d760d15177346b0d1e1728
Eric Allman <eric>
1993-07-17 13:46:53 -0700

    detect self-destructing alias loop more generally for better diagnostics
    SCCS-vsn: 8.5

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.4 (Berkeley) 07/16/93 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.5 (Berkeley) 07/17/93 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.4 (Berkeley) 07/16/93 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.5 (Berkeley) 07/17/93 (without SMTP)";
 #endif
 #endif /* not lint */
 
