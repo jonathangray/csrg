35d4acce963d139c51fe12d8ea9901dd23a1e8d3
Keith Bostic <bostic>
1993-07-19 11:10:54 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- include/bitstring.h
+++ include/bitstring.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1989 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Paul Vixie.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)bitstring.h	5.6 (Berkeley) 07/19/93
+ *	@(#)bitstring.h	8.1 (Berkeley) 07/19/93
  */
 
 #ifndef _BITSTRING_H_
