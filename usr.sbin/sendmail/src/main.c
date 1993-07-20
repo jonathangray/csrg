7dfe49c6c92a0522191f62c112a0115c8233d03a
Eric Allman <eric>
1993-07-20 15:35:39 -0700

    more portability changes, including ConvexOS support
    SCCS-vsn: 8.8

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.7 (Berkeley) 07/19/93";
+static char sccsid[] = "@(#)main.c	8.8 (Berkeley) 07/20/93";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -173,11 +173,6 @@ main(argc, argv, envp)
 	reenter = TRUE;
 	extern ADDRESS *recipient();
 	bool canrename;
-
-#ifndef SYS5TZ
-	/* enforce use of kernel-supplied time zone information */
-	unsetenv("TZ");
-#endif
 
 	/* in 4.4BSD, the table can be huge; impose a reasonable limit */
 	DtableSize = getdtsize();
@@ -636,18 +631,11 @@ main(argc, argv, envp)
 	if (OpMode == MD_FREEZE || readconfig)
 		readcf(ConfFile, safecf, CurEnv);
 
-#ifdef SYS5TZ
 	/* Enforce use of local time (null string overrides this) */
 	if (TimeZoneSpec == NULL)
 		unsetenv("TZ");
 	else if (TimeZoneSpec[0] != '\0')
-	{
-		p = xalloc(strlen(TimeZoneSpec) + 4);
-		(void) strcpy(p, "TZ=");
-		(void) strcat(p, TimeZoneSpec);
-		putenv(p);
-	}
-#endif
+		setenv("TZ", TimeZoneSpec, 1);
 
 	if (ConfigLevel > MAXCONFIGLEVEL)
 	{
