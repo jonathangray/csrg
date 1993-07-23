3c1fcbfb7e80b3fab3ff685aa930a4af6144dd05
Keith Bostic <bostic>
1993-07-23 08:28:19 -0700

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- usr.sbin/vipw/pw_util.c
+++ usr.sbin/vipw/pw_util.c
@@ -1,6 +1,6 @@
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)pw_util.c	5.5 (Berkeley) 07/23/93";
+static char sccsid[] = "@(#)pw_util.c	8.1 (Berkeley) 07/23/93";
 #endif /* not lint */
 
 /*
