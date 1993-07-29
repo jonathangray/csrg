4606964ab3545e0bd1f488a5e7ec7b6fbcc13e23
Eric Allman <eric>
1993-07-29 08:31:01 -0700

    make certain that $j is part of $=w
    SCCS-vsn: 8.13

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.12 (Berkeley) 07/27/93";
+static char sccsid[] = "@(#)main.c	8.13 (Berkeley) 07/29/93";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -363,6 +363,7 @@ main(argc, argv, envp)
 			printf("canonical name: %s\n", jbuf);
 		p = newstr(jbuf);
 		define('w', p, CurEnv);
+		define('j', p, CurEnv);
 		setclass('w', p);
 
 		q = strchr(jbuf, '.');
@@ -658,7 +659,6 @@ main(argc, argv, envp)
 			ConfigLevel, MAXCONFIGLEVEL);
 	}
 
-
 # ifdef QUEUE
 	if (queuemode && RealUid != 0)
 	{
@@ -724,6 +724,9 @@ main(argc, argv, envp)
 	/* our name for SMTP codes */
 	expand("\201j", jbuf, &jbuf[sizeof jbuf - 1], CurEnv);
 	MyHostName = jbuf;
+
+	/* make certain that this name is part of the $=w class */
+	setclass('w', MyHostName);
 
 	/* the indices of built-in mailers */
 	st = stab("local", ST_MAILER, ST_FIND);
