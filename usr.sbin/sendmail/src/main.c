061979838bda9d1139a6c0d5c9c9996608782b7c
Eric Allman <eric>
1993-08-06 21:16:46 -0700

    fix problem causing slow delivery warning to fail if OP set; fstat can return -1 on some bizarre networks
    SCCS-vsn: 8.14

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.13 (Berkeley) 07/29/93";
+static char sccsid[] = "@(#)main.c	8.14 (Berkeley) 08/06/93";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -184,11 +184,11 @@ main(argc, argv, envp)
 	*/
 
 	i = open("/dev/null", O_RDWR);
-	if (fstat(STDIN_FILENO, &stb) < 0)
+	if (fstat(STDIN_FILENO, &stb) < 0 && errno != EOPNOTSUPP)
 		(void) dup2(i, STDIN_FILENO);
-	if (fstat(STDOUT_FILENO, &stb) < 0)
+	if (fstat(STDOUT_FILENO, &stb) < 0 && errno != EOPNOTSUPP)
 		(void) dup2(i, STDOUT_FILENO);
-	if (fstat(STDERR_FILENO, &stb) < 0)
+	if (fstat(STDERR_FILENO, &stb) < 0 && errno != EOPNOTSUPP)
 		(void) dup2(i, STDERR_FILENO);
 	(void) close(i);
 
@@ -658,6 +658,9 @@ main(argc, argv, envp)
 		syserr("Warning: .cf version level (%d) exceeds program functionality (%d)",
 			ConfigLevel, MAXCONFIGLEVEL);
 	}
+
+	if (MeToo)
+		BlankEnvelope.e_flags |= EF_METOO;
 
 # ifdef QUEUE
 	if (queuemode && RealUid != 0)
