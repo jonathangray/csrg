78ba25ac3087a955a6ddcbd1c0ce6f478c20159c
Eric Allman <eric>
1993-08-06 21:16:19 -0700

    fix problem causing slow delivery warning to fail if OP set
    SCCS-vsn: 8.10

--- usr.sbin/sendmail/src/deliver.c
+++ usr.sbin/sendmail/src/deliver.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)deliver.c	8.9 (Berkeley) 07/29/93";
+static char sccsid[] = "@(#)deliver.c	8.10 (Berkeley) 08/06/93";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -138,7 +138,8 @@ sendall(e, mode)
 	**	is instantiated in the queue as a recipient.
 	*/
 
-	if (!MeToo && !bitset(QQUEUEUP, e->e_from.q_flags))
+	if (!bitset(EF_METOO, e->e_flags) &&
+	    !bitset(QQUEUEUP, e->e_from.q_flags))
 	{
 		if (tTd(13, 5))
 		{
@@ -1816,7 +1817,7 @@ putbody(fp, m, e, separator)
 			e->e_dfp = fopen(e->e_df, "r");
 			if (e->e_dfp == NULL)
 				syserr("putbody: Cannot open %s for %s from %s",
-				e->e_df, e->e_to, e->e_from);
+				e->e_df, e->e_to, e->e_from.q_paddr);
 		}
 		else
 			putline("<<< No Message Collected >>>", fp, m);
@@ -1948,7 +1949,7 @@ mailfile(filename, ctladdr, e)
 			if (e->e_dfp == NULL)
 			{
 				syserr("mailfile: Cannot open %s for %s from %s",
-					e->e_df, e->e_to, e->e_from);
+					e->e_df, e->e_to, e->e_from.q_paddr);
 			}
 		}
 
