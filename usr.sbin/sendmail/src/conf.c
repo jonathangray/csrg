3279e07a648fd4d13317ccc00e3340d1e1634027
Eric Allman <eric>
1993-08-14 14:54:45 -0700

    react reasonably with negative e_msgsize and SIZE parameter
    SCCS-vsn: 8.18

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.17 (Berkeley) 08/14/93";
+static char sccsid[] = "@(#)conf.c	8.18 (Berkeley) 08/14/93";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1313,6 +1313,9 @@ enoughspace(msize)
 		return TRUE;
 	}
 
+	if (msize < 0)
+		msize = 0;
+
 # if defined(HASUSTAT)
 	if (stat(QueueDir, &statbuf) == 0 && ustat(statbuf.st_dev, &fs) == 0)
 # else
@@ -1340,8 +1343,10 @@ enoughspace(msize)
 		{
 #ifdef LOG
 			if (LogLevel > 0)
-				syslog(LOG_ALERT, "%s: low on space (have %ld, need %ld)",
-					QueueDir, fs.f_bavail, msize);
+				syslog(LOG_ALERT,
+					"%s: low on space (have %ld, %s needs %ld in %s)",
+					CurEnv->e_id, fs.f_bavail,
+					CurHostName, msize, QueueDir);
 #endif
 			return FALSE;
 		}
