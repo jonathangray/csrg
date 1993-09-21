49c44e9b6d5ac7661f8d97842754fbdd984f0d46
Keith Bostic <bostic>
1993-09-21 07:24:18 -0700

    changes for 4.4BSD-Lite requested by USL
    SCCS-vsn: 8.2

--- sys/ufs/ufs/ufs_readwrite.c
+++ sys/ufs/ufs/ufs_readwrite.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ufs_readwrite.c	8.1 (Berkeley) 09/05/93
+ *	@(#)ufs_readwrite.c	8.2 (Berkeley) 09/21/93
  */
 
 #ifdef LFS_READWRITE
@@ -141,7 +141,7 @@ READ(ap)
 			xfersize = size;
 		}
 		if (error =
-		    uiomove(bp->b_un.b_addr + blkoffset, (int)xfersize, uio))
+		    uiomove((char *)bp->b_data + blkoffset, (int)xfersize, uio))
 			break;
 
 		if (S_ISREG(mode) && (xfersize + blkoffset == fs->fs_bsize ||
@@ -151,7 +151,7 @@ READ(ap)
 	}
 	if (bp != NULL)
 		brelse(bp);
-	ip->i_flag |= IACC;
+	ip->i_flag |= IACCESS;
 	return (error);
 }
 
@@ -254,7 +254,7 @@ WRITE(ap)
 			xfersize = size;
 
 		error =
-		    uiomove(bp->b_un.b_addr + blkoffset, (int)xfersize, uio);
+		    uiomove((char *)bp->b_data + blkoffset, (int)xfersize, uio);
 #ifdef LFS_READWRITE
 		(void)VOP_BWRITE(bp);
 #else
@@ -272,7 +272,7 @@ WRITE(ap)
 #endif
 		if (error || xfersize == 0)
 			break;
-		ip->i_flag |= IUPD | ICHG;
+		ip->i_flag |= IUPDATE | ICHANGE;
 	}
 	/*
 	 * If we successfully wrote any data, and we are not the superuser
