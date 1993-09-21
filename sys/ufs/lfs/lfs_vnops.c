7d4db6e088f079cca65bc64e0d4ffd5ffa2262ec
Keith Bostic <bostic>
1993-09-21 07:25:50 -0700

    changes for 4.4BSD-Lite requested by USL
    SCCS-vsn: 8.3

--- sys/ufs/lfs/lfs_vnops.c
+++ sys/ufs/lfs/lfs_vnops.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs_vnops.c	8.2 (Berkeley) 09/05/93
+ *	@(#)lfs_vnops.c	8.3 (Berkeley) 09/21/93
  */
 
 #include <sys/param.h>
@@ -461,9 +461,9 @@ lfs_close(ap)
 	int mod;
 
 	if (vp->v_usecount > 1 && !(ip->i_flag & ILOCKED)) {
-		mod = ip->i_flag & IMOD;
+		mod = ip->i_flag & IMODIFIED;
 		ITIMES(ip, &time, &time);
-		if (!mod && ip->i_flag & IMOD)
+		if (!mod && ip->i_flag & IMODIFIED)
 			ip->i_lfs->lfs_uinodes++;
 	}
 	return (0);
