0d16de46d2d61af6c705f4922a109ceaaa90a18a
Keith Bostic <bostic>
1993-09-23 16:40:43 -0700

    changes for 4.4BSD-Lite requested by USL
    SCCS-vsn: 8.4

--- sys/vm/vm_glue.c
+++ sys/vm/vm_glue.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_glue.c	8.3 (Berkeley) 09/21/93
+ *	@(#)vm_glue.c	8.4 (Berkeley) 09/23/93
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
@@ -320,9 +320,9 @@ loop:
 #endif
 	pp = NULL;
 	ppri = INT_MIN;
-	for (p = (struct proc *)allproc; p != NULL; p = p->p_nxt) {
-		if (p->p_stat == SRUN && (p->p_flag & SLOAD) == 0) {
-			pri = p->p_time + p->p_slptime - p->p_nice * 8;
+	for (p = (struct proc *)allproc; p != NULL; p = p->p_next) {
+		if (p->p_stat == SRUN && (p->p_flag & P_INMEM) == 0) {
+			pri = p->p_swtime + p->p_slptime - p->p_nice * 8;
 			if (pri > ppri) {
 				pp = p;
 				ppri = pri;
@@ -359,9 +359,9 @@ loop:
 		(void) splstatclock();
 		if (p->p_stat == SRUN)
 			setrunqueue(p);
-		p->p_flag |= SLOAD;
+		p->p_flag |= P_INMEM;
 		(void) spl0();
-		p->p_time = 0;
+		p->p_swtime = 0;
 		goto loop;
 	}
 	/*
@@ -383,8 +383,9 @@ loop:
 	goto loop;
 }
 
-#define	swappable(p) \
-	(((p)->p_flag & (SSYS|SLOAD|SKEEP|SWEXIT|SPHYSIO)) == SLOAD)
+#define	swappable(p)							\
+	(((p)->p_flag &							\
+	    (P_SYSTEM | P_INMEM | P_NOSWAP | P_WEXIT | P_PHYSIO)) == P_INMEM)
 
 /*
  * Swapout is driven by the pageout daemon.  Very simple, we find eligible
@@ -409,14 +410,14 @@ swapout_threads()
 #endif
 	outp = outp2 = NULL;
 	outpri = outpri2 = 0;
-	for (p = (struct proc *)allproc; p != NULL; p = p->p_nxt) {
+	for (p = (struct proc *)allproc; p != NULL; p = p->p_next) {
 		if (!swappable(p))
 			continue;
 		switch (p->p_stat) {
 		case SRUN:
-			if (p->p_time > outpri2) {
+			if (p->p_swtime > outpri2) {
 				outp2 = p;
-				outpri2 = p->p_time;
+				outpri2 = p->p_swtime;
 			}
 			continue;
 			
@@ -505,11 +506,11 @@ swapout(p)
 	pmap_collect(vm_map_pmap(&p->p_vmspace->vm_map));
 #endif
 	(void) splhigh();
-	p->p_flag &= ~SLOAD;
+	p->p_flag &= ~P_INMEM;
 	if (p->p_stat == SRUN)
 		remrq(p);
 	(void) spl0();
-	p->p_time = 0;
+	p->p_swtime = 0;
 }
 
 /*
