fbcac67b425c0f20394dec2e331d07cbdd770366
Eric Allman <eric>
1993-09-25 07:01:57 -0700

    add MF_ALIASWAIT flag
    SCCS-vsn: 8.23

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sendmail.h	8.22 (Berkeley) 09/24/93
+ *	@(#)sendmail.h	8.23 (Berkeley) 09/25/93
  */
 
 /*
@@ -41,7 +41,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.22		09/24/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.23		09/25/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -547,6 +547,7 @@ MAP
 # define MF_TRY0NULL	0x0100		/* try with no null byte */
 # define MF_TRY1NULL	0x0200		/* try with the null byte */
 # define MF_LOCKED	0x0400		/* this map is currently locked */
+# define MF_ALIASWAIT	0x0800		/* alias map in aliaswait state */
 # define MF_IMPL_HASH	0x1000		/* implicit: underlying hash database */
 # define MF_IMPL_NDBM	0x2000		/* implicit: underlying NDBM database */
 
