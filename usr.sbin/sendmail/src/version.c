b1dd57d702f7a45975d584525dec1193826243e6
Eric Allman <eric>
1993-10-05 11:00:00 -0700

    many changes -- see RELEASE_NOTES
    SCCS-vsn: 8.6

--- usr.sbin/sendmail/src/version.c
+++ usr.sbin/sendmail/src/version.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)version.c	8.5 (Berkeley) 07/23/93";
+static char sccsid[] = "@(#)version.c	8.6 (Berkeley) 10/05/93";
 #endif /* not lint */
 
-char	Version[] = "8.5";
+char	Version[] = "8.6";
