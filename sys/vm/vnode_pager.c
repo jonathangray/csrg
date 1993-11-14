4bfd8429930bf6d6554e5b298f8b1167e2ddcfe4
Mike Hibler <hibler>
1993-11-14 13:54:44 -0800

    eliminate unneeded cast
    SCCS-vsn: 8.3

--- sys/vm/vnode_pager.c
+++ sys/vm/vnode_pager.c
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vnode_pager.c	8.2 (Berkeley) 11/10/93
+ *	@(#)vnode_pager.c	8.3 (Berkeley) 11/14/93
  */
 
 /*
@@ -170,7 +170,7 @@ vnode_pager_alloc(handle, size, prot, foff)
 		pager->pg_handle = handle;
 		pager->pg_type = PG_VNODE;
 		pager->pg_ops = &vnodepagerops;
-		pager->pg_data = (caddr_t)vnp;
+		pager->pg_data = vnp;
 		vp->v_vmdata = (caddr_t)pager;
 	} else {
 		/*
