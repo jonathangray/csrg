3415fd62d3d19b070998d9524f43a46522246867
Mike Hibler <hibler>
1993-11-14 13:32:49 -0800

    add iszerodev predicate
    SCCS-vsn: 8.2

--- sys/news3400/news3400/conf.c
+++ sys/news3400/news3400/conf.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)conf.c	8.1 (Berkeley) 06/11/93
+ *	@(#)conf.c	8.2 (Berkeley) 11/14/93
  */
 
 #include <sys/param.h>
@@ -392,6 +392,12 @@ iskmemdev(dev)
 	if (major(dev) == 3 && (minor(dev) == 0 || minor(dev) == 1))
 		return (1);
 	return (0);
+}
+
+iszerodev(dev)
+	dev_t dev;
+{
+	return (major(dev) == 3 && minor(dev) == 12);
 }
 
 /*
