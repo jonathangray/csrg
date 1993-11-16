eb0bf23e9f8765253294b91924594bdb43543a66
Kirk McKusick <mckusick>
1993-11-16 14:13:12 -0800

    af.h is gone
    SCCS-vsn: 8.2

--- sys/net/if.c
+++ sys/net/if.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if.c	8.1 (Berkeley) 06/29/93
+ *	@(#)if.c	8.2 (Berkeley) 11/16/93
  */
 
 #include <sys/param.h>
@@ -44,7 +44,6 @@
 #include <sys/ioctl.h>
 
 #include <net/if.h>
-#include <net/af.h>
 #include <net/if_dl.h>
 #include <net/if_types.h>
 
