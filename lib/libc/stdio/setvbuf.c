f1f89d9c80058d3e1901402f2794b2a7a027efc2
Chris Torek <torek>
1993-11-16 20:03:22 -0800

    well, looks like we have to get all the fields right after all -- otherwise problems occur for __SWR mode files.  I suppose we could
    SCCS-vsn: 8.2

--- lib/libc/stdio/setvbuf.c
+++ lib/libc/stdio/setvbuf.c
@@ -35,7 +35,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)setvbuf.c	8.1 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)setvbuf.c	8.2 (Berkeley) 11/16/93";
 #endif /* LIBC_SCCS and not lint */
 
 #include <stdio.h>
