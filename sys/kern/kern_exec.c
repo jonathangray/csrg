c25e50eaa79fdcfca53bd571551a7d219b70b4f0
Jan-Simon Pendry <pendry>
1993-12-13 12:22:36 -0800

    add p_textvp to track vnode of executable (for procfs)
    SCCS-vsn: 8.4

--- sys/kern/kern_exec.c
+++ sys/kern/kern_exec.c
@@ -6,7 +6,7 @@
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)kern_exec.c	8.3 (Berkeley) 09/23/93
+ *	@(#)kern_exec.c	8.4 (Berkeley) 12/13/93
  */
 
 #include <sys/param.h>
@@ -399,6 +399,13 @@ execve(p, uap, retval)
 	error = getxfile(p, vp, &exdata.ex_exec, paged, ssize, uid, gid);
 	if (error)
 		goto bad;
+
+	/* take a reference to the new text vnode (for procfs) */
+	if (p->p_textvp)
+		vrele(p->p_textvp);
+	VREF(vp);
+	p->p_textvp = vp;
+
 	vput(vp);
 	vp = NULL;
 
