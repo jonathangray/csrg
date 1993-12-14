6659ed79c8a3485a8a74d363e07a648a63c7630a
Eric Allman <eric>
1993-12-14 07:42:21 -0800

    add SFF_ROOTOK flag
    SCCS-vsn: 8.35

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sendmail.h	8.34 (Berkeley) 12/11/93
+ *	@(#)sendmail.h	8.35 (Berkeley) 12/14/93
  */
 
 /*
@@ -41,7 +41,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.34		12/11/93";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.35		12/14/93";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -747,6 +747,7 @@ struct prival
 #define SFF_ANYFILE		0	/* no special restrictions */
 #define SFF_MUSTOWN		0x0001	/* user must own this file */
 #define SFF_NOSLINK		0x0002	/* file cannot be a symbolic link */
+#define SFF_ROOTOK		0x0004	/* ok for root to own this file */
 
 
 /*
