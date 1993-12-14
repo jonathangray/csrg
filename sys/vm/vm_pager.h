9244c9300fb7c2ff1a9b0f75e18d08ba11ae236f
Kirk McKusick <mckusick>
1993-12-13 23:54:52 -0800

    convert pager list to use <sys/queue.h>
    SCCS-vsn: 8.3

--- sys/vm/vm_pager.h
+++ sys/vm/vm_pager.h
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_pager.h	8.2 (Berkeley) 11/10/93
+ *	@(#)vm_pager.h	8.3 (Berkeley) 12/13/93
  */
 
 /*
@@ -46,12 +46,14 @@
 #ifndef	_VM_PAGER_
 #define	_VM_PAGER_
 
+TAILQ_HEAD(pagerlst, pager_struct);
+
 struct	pager_struct {
-	queue_head_t	pg_list;	/* links for list management */
-	caddr_t		pg_handle;	/* external handle (vp, dev, fp) */
-	int		pg_type;	/* type of pager */
-	struct pagerops	*pg_ops;	/* pager operations */
-	void		*pg_data;	/* private pager data */
+	TAILQ_ENTRY(pager_struct) pg_list;	/* links for list management */
+	caddr_t			  pg_handle;	/* ext. handle (vp, dev, fp) */
+	int			  pg_type;	/* type of pager */
+	struct pagerops		  *pg_ops;	/* pager operations */
+	void			  *pg_data;	/* private pager data */
 };
 
 /* pager types */
@@ -104,7 +106,7 @@ void		 vm_pager_deallocate __P((vm_pager_t));
 int		 vm_pager_get __P((vm_pager_t, vm_page_t, boolean_t));
 boolean_t	 vm_pager_has_page __P((vm_pager_t, vm_offset_t));
 void		 vm_pager_init __P((void));
-vm_pager_t	 vm_pager_lookup __P((queue_head_t *, caddr_t));
+vm_pager_t	 vm_pager_lookup __P((struct pagerlst *, caddr_t));
 vm_offset_t	 vm_pager_map_page __P((vm_page_t));
 int		 vm_pager_put __P((vm_pager_t, vm_page_t, boolean_t));
 void		 vm_pager_sync __P((void));
