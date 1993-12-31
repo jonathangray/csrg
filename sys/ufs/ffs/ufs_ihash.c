1733d2c0ca12b74246df57c5d11eaa43cfb7055e
Kirk McKusick <mckusick>
1993-12-30 16:12:05 -0800

    vget now locks optionally
    SCCS-vsn: 8.4

--- sys/ufs/ffs/ufs_ihash.c
+++ sys/ufs/ffs/ufs_ihash.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ufs_ihash.c	8.3 (Berkeley) 09/23/93
+ *	@(#)ufs_ihash.c	8.4 (Berkeley) 12/30/93
  */
 
 #include <sys/param.h>
@@ -103,7 +103,7 @@ ufs_ihashget(device, inum)
 					break;
 				}
 				vp = ITOV(ip);
-				if (!vget(vp))
+				if (!vget(vp, 1))
 					return (vp);
 				break;
 			}
