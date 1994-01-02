8d48309f267bd8da923adc9f809d49da0b713a53
Keith Bostic <bostic>
1994-01-02 13:41:01 -0800

    clear PENDIN if ICANON is cleared retain the PENDIN flag if set when ICANON is set From: karels@BSDI.COM (Mike Karels)
    SCCS-vsn: 8.6

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tty.c	8.5 (Berkeley) 11/15/93
+ *	@(#)tty.c	8.6 (Berkeley) 01/02/94
  */
 
 #include <sys/param.h>
@@ -651,6 +651,7 @@ ttioctl(tp, cmd, data, flag)
 					tq = tp->t_rawq;
 					tp->t_rawq = tp->t_canq;
 					tp->t_canq = tq;
+					CLR(tp->t_lflag, PENDIN);
 				}
 		}
 		tp->t_iflag = t->c_iflag;
@@ -662,7 +663,7 @@ ttioctl(tp, cmd, data, flag)
 			SET(t->c_lflag, EXTPROC);
 		else
 			CLR(t->c_lflag, EXTPROC);
-		tp->t_lflag = t->c_lflag;
+		tp->t_lflag = t->c_lflag | ISSET(tp->t_lflag, PENDIN);
 		bcopy(t->c_cc, tp->t_cc, sizeof(t->c_cc));
 		splx(s);
 		break;
