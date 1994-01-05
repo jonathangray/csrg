800cd3523a2d5eab25d56d7e8a0e357dd1a2edcb
Jan-Simon Pendry <pendry>
1994-01-04 17:13:56 -0800

    use new VT_ value
    SCCS-vsn: 8.3

--- sys/miscfs/kernfs/kernfs.h
+++ sys/miscfs/kernfs/kernfs.h
@@ -34,17 +34,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kernfs.h	8.2 (Berkeley) 01/04/94
+ *	@(#)kernfs.h	8.3 (Berkeley) 01/04/94
  */
 
 #define	_PATH_KERNFS	"/kern"		/* Default mountpoint */
 
 #ifdef KERNEL
-
-#ifndef VT_KERNFS
-#define VT_KERNFS VT_UFS
-#endif
-
 struct kernfs_mount {
 	struct vnode	*kf_root;	/* Root node */
 };
