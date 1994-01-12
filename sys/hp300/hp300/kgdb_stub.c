0a611422f2be8d66af8a036d7519a79663191a59
Keith Bostic <bostic>
1994-01-12 10:27:53 -0800

    typo
    SCCS-vsn: 8.3

--- sys/hp300/hp300/kgdb_stub.c
+++ sys/hp300/hp300/kgdb_stub.c
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)kgdb_stub.c	8.2 (Berkeley) 01/12/94
+ *	@(#)kgdb_stub.c	8.3 (Berkeley) 01/12/94
  */
 
 /*
@@ -320,7 +320,7 @@ regs_to_gdb(fp, regs)
 static inline void
 gdb_to_regs(fp, regs)
 	struct frame *fp;
-	u_long regs;
+	u_long *regs;
 {
 	kgdb_copy((u_char *)regs, (u_char *)fp->f_regs, 16*4);
 	kgdb_copy((u_char *)&regs[GDB_SR], (u_char *)&fp->f_stackadj, 2*4);
