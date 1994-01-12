c1d4a0c0568c37e6c1834db8d672933a873ac5d2
Kirk McKusick <mckusick>
1994-01-11 16:14:29 -0800

    4.4BSD snapshot (revision 8.1); add 1993 to copyright
    SCCS-vsn: 8.1

--- sys/news3400/include/cpu.h
+++ sys/news3400/include/cpu.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1992 Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1992, 1993
+ *	Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell and Kazumasa Utashiro of Software Research
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)cpu.h	7.6 (Berkeley) 01/05/94
+ *	@(#)cpu.h	8.1 (Berkeley) 01/11/94
  */
 
 #ifndef _CPU_H_
