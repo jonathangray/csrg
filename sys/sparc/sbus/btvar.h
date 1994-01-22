f16927e80956543b0561243688fa4b6ebe1ee2f2
Keith Bostic <bostic>
1994-01-21 19:49:19 -0800

    copyright typo
    SCCS-vsn: 8.2

--- sys/sparc/sbus/btvar.h
+++ sys/sparc/sbus/btvar.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1993 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This software was developed by the Computer Systems Engineering group
  * at Lawrence Berkeley Laboratory under DARPA contract BG 91-66 and
@@ -39,7 +39,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)btvar.h	8.1 (Berkeley) 10/30/93
+ *	@(#)btvar.h	8.2 (Berkeley) 01/21/94
  *
  * from: $Header: btvar.h,v 1.1 93/10/12 15:29:01 torek Exp $
  */
