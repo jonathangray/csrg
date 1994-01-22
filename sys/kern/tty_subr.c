18bef22639f505c0a6d7668f54c7aa95832b55dd
Keith Bostic <bostic>
1994-01-21 17:18:03 -0800

    add USL's copyright notice
    SCCS-vsn: 8.3

--- sys/kern/tty_subr.c
+++ sys/kern/tty_subr.c
@@ -1,12 +1,17 @@
 /*-
  * Copyright (c) 1982, 1986, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * This module is believed to contain source code proprietary to AT&T.
  * Use and redistribution is subject to the Berkeley Software License
  * Agreement and your Software Agreement with AT&T (Western Electric).
  *
- *	@(#)tty_subr.c	8.2 (Berkeley) 09/05/93
+ *	@(#)tty_subr.c	8.3 (Berkeley) 01/21/94
  */
 
 #include <sys/param.h>
