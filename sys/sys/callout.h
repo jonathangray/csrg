d172b083306977123215d1692ab7639a38a54285
Keith Bostic <bostic>
1994-01-21 17:25:04 -0800

    add USL's copyright notice
    SCCS-vsn: 8.2

--- sys/sys/callout.h
+++ sys/sys/callout.h
@@ -1,6 +1,11 @@
 /*-
  * Copyright (c) 1990, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)callout.h	8.1 (Berkeley) 06/02/93
+ *	@(#)callout.h	8.2 (Berkeley) 01/21/94
  */
 
 struct callout {
