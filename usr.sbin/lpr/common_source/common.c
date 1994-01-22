81c6b9ed21057a9c63ff70edf4bd848a0f8fb2f6
Keith Bostic <bostic>
1994-01-21 17:37:37 -0800

    add USL's copyright notice
    SCCS-vsn: 8.2

--- usr.sbin/lpr/common_source/common.c
+++ usr.sbin/lpr/common_source/common.c
@@ -1,6 +1,11 @@
 /*
  * Copyright (c) 1983, 1993
  *	The Regents of the University of California.  All rights reserved.
+ * (c) UNIX System Laboratories, Inc.
+ * All or some portions of this file are derived from material licensed
+ * to the University of California by American Telephone and Telegraph
+ * Co. or Unix System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +37,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)common.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)common.c	8.2 (Berkeley) 01/21/94";
 #endif /* not lint */
 
 #include <sys/param.h>
