b0cda87a266d096c888aa1a0deca901907bacc70
Keith Bostic <bostic>
1994-01-21 19:53:28 -0800

    copyright typo
    SCCS-vsn: 8.6

--- sys/miscfs/portal/portal_vfsops.c
+++ sys/miscfs/portal/portal_vfsops.c
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
- * All rights reserved.
  *
  * This code is derived from software donated to Berkeley by
  * Jan-Simon Pendry.
@@ -34,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)portal_vfsops.c	8.5 (Berkeley) 01/05/94
+ *	@(#)portal_vfsops.c	8.6 (Berkeley) 01/21/94
  *
  * $Id: portal_vfsops.c,v 1.5 1992/05/30 10:25:27 jsp Exp jsp $
  */
