75292c20b90c2c49db105f3ded942015007b2c4b
Keith Bostic <bostic>
1994-01-21 20:05:11 -0800

    copyright typo
    SCCS-vsn: 8.2

--- sys/pmax/stand/start.s
+++ sys/pmax/stand/start.s
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 1992, 1993
- *	Regents of the University of California.  All rights reserved.
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
  * Ralph Campbell.
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)start.s	8.1 (Berkeley) 06/10/93
+ *	@(#)start.s	8.2 (Berkeley) 01/21/94
  */
 
 /*
