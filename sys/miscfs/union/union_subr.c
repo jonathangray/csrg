da5067777d677f606b308fea5f7d4b99dcd6741d
Jan-Simon Pendry <pendry>
1994-01-28 09:42:43 -0800

    date and time created 94/01/28 09:42:43 by pendry
    SCCS-vsn: 1.1

---
+++ sys/miscfs/union/union_subr.c
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) 1994 Jan-Simon Pendry
+ * Copyright (c) 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Jan-Simon Pendry.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *	@(#)union_subr.c	1.1 (Berkeley) 01/28/94
+ */
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/time.h>
+#include <sys/kernel.h>
+#include <sys/vnode.h>
+#include <sys/namei.h>
+#include <sys/malloc.h>
+#include "union.h" /*<miscfs/union/union.h>*/
+
+static struct union_node *unhead;
+static int unvplock;
+
+int
+union_init()
+{
+
+	unhead = 0;
+	unvplock = 0;
+}
+
+/*
+ * allocate a union_node/vnode pair.  the vnode is
+ * referenced and locked.
+ *
+ * all union_nodes are maintained on a singly-linked
+ * list.  new nodes are only allocated when they cannot
+ * be found on this list.  entries on the list are
+ * removed when the vfs reclaim entry is called.
+ *
+ * a single lock is kept for the entire list.  this is
+ * needed because the getnewvnode() function can block
+ * waiting for a vnode to become free, in which case there
+ * may be more than one process trying to get the same
+ * vnode.  this lock is only taken if we are going to
+ * call getnewvnode, since the kernel itself is single-threaded.
+ *
+ * if an entry is found on the list, then call vget() to
+ * take a reference.  this is done because there may be
+ * zero references to it and so it needs to removed from
+ * the vnode free list.
+ */
+int
+union_allocvp(vpp, mp, dvp, cnp, uppervp, lowervp)
+	struct vnode **vpp;
+	struct mount *mp;
+	struct vnode *dvp;		/* may be null */
+	struct componentname *cnp;	/* may be null */
+	struct vnode *uppervp;		/* may be null */
+	struct vnode *lowervp;		/* may be null */
+{
+	int error;
+	struct union_node *un;
+	struct union_node **pp;
+
+loop:
+	for (un = unhead; un != 0; un = un->un_next) {
+		if ((un->un_lowervp == lowervp ||
+		     un->un_lowervp == 0) &&
+		    (un->un_uppervp == uppervp ||
+		     un->un_uppervp == 0) &&
+		    (UNIONTOV(un)->v_mount == mp)) {
+			if (vget(un->un_vnode, 1))
+				goto loop;
+			un->un_lowervp = lowervp;
+			un->un_uppervp = uppervp;
+			*vpp = un->un_vnode;
+			return (0);
+		}
+	}
+
+	/*
+	 * otherwise lock the vp list while we call getnewvnode
+	 * since that can block.
+	 */ 
+	if (unvplock & UN_LOCKED) {
+		unvplock |= UN_WANT;
+		sleep((caddr_t) &unvplock, PINOD);
+		goto loop;
+	}
+	unvplock |= UN_LOCKED;
+
+	error = getnewvnode(VT_UNION, mp, union_vnodeop_p, vpp);
+	if (error)
+		goto out;
+
+	MALLOC((*vpp)->v_data, void *, sizeof(struct union_node),
+		M_TEMP, M_WAITOK);
+
+	un = VTOUNION(*vpp);
+	un->un_next = 0;
+	un->un_dirvp = dvp;
+	un->un_uppervp = uppervp;
+	un->un_lowervp = lowervp;
+	un->un_vnode = *vpp;
+	un->un_flags = 0;
+	if (cnp) {
+		un->un_path = malloc(cnp->cn_namelen+1, M_TEMP, M_WAITOK);
+		bcopy(cnp->cn_nameptr, un->un_path, cnp->cn_namelen);
+		un->un_path[cnp->cn_namelen] = '\0';
+	} else {
+		un->un_path = 0;
+	}
+
+#ifdef DIAGNOSTIC
+	un->un_pid = 0;
+#endif
+
+	/* add to union vnode list */
+	for (pp = &unhead; *pp; pp = &(*pp)->un_next)
+		continue;
+	*pp = un;
+
+out:
+	unvplock &= ~UN_LOCKED;
+
+	if (unvplock & UN_WANT) {
+		unvplock &= ~UN_WANT;
+		wakeup((caddr_t) &unvplock);
+	}
+
+	if (un)
+		VOP_LOCK(UNIONTOV(un));
+
+	return (error);
+}
+
+int
+union_freevp(vp)
+	struct vnode *vp;
+{
+	struct union_node **unpp;
+	struct union_node *un = VTOUNION(vp);
+
+	for (unpp = &unhead; *unpp != 0; unpp = &(*unpp)->un_next) {
+		if (*unpp == un) {
+			*unpp = un->un_next;
+			break;
+		}
+	}
+
+	if (un->un_path)
+		FREE(un->un_path, M_TEMP);
+
+	FREE(vp->v_data, M_TEMP);
+	vp->v_data = 0;
+	return (0);
+}
