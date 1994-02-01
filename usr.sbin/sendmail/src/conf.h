a9c53ac7bf6362cf0deb709d3d18041e8a011e81
Eric Allman <eric>
1994-01-31 19:56:58 -0800

    default SCANF on if we are going to link it anyway
    SCCS-vsn: 8.82

--- usr.sbin/sendmail/src/conf.h
+++ usr.sbin/sendmail/src/conf.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)conf.h	8.81 (Berkeley) 01/31/94
+ *	@(#)conf.h	8.82 (Berkeley) 01/31/94
  */
 
 /*
@@ -1017,3 +1017,11 @@ typedef void		(*sigfunc_t) __P((int));
 # ifndef FORK
 # define FORK		vfork		/* function to call to fork mailer */
 # endif
+
+/*
+**  If we are going to link scanf anyway, use it in readcf
+*/
+
+#if !defined(HASUNAME) && !defined(SCANF)
+# define SCANF		1
+#endif
