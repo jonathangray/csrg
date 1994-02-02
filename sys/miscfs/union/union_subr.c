efc873973c94c2d6388ae7165770774e37f9ec23
Jan-Simon Pendry <pendry>
1994-02-01 17:11:09 -0800

    checkpoint
    SCCS-vsn: 1.2

--- sys/miscfs/union/union_subr.c
+++ sys/miscfs/union/union_subr.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)union_subr.c	1.1 (Berkeley) 01/28/94
+ *	@(#)union_subr.c	1.2 (Berkeley) 02/01/94
  */
 
 #include <sys/param.h>
