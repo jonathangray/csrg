c023f0f16e3269c1d37e1205e818153deee54690
Jan-Simon Pendry <pendry>
1994-02-04 10:51:52 -0800

    added VT_UNION
    SCCS-vsn: 8.7

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vnode.h	8.6 (Berkeley) 01/12/94
+ *	@(#)vnode.h	8.7 (Berkeley) 02/04/94
  */
 
 #include <sys/queue.h>
@@ -53,7 +53,8 @@ enum vtype	{ VNON, VREG, VDIR, VBLK, VCHR, VLNK, VSOCK, VFIFO, VBAD };
  */
 enum vtagtype	{
 	VT_NON, VT_UFS, VT_NFS, VT_MFS, VT_PC, VT_LFS, VT_LOFS, VT_FDESC,
-	VT_PORTAL, VT_NULL, VT_UMAP, VT_KERNFS, VT_PROCFS, VT_AFS, VT_ISOFS
+	VT_PORTAL, VT_NULL, VT_UMAP, VT_KERNFS, VT_PROCFS, VT_AFS, VT_ISOFS,
+	VT_UNION
 };
 
 /*
