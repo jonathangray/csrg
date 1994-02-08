4b377fd09ea9fe7ed18c35283184355b7cc1657e
Eric Allman <eric>
1994-02-08 08:53:48 -0800

    give specific error message about Fx|... syntax
    SCCS-vsn: 8.20

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.19 (Berkeley) 01/22/94";
+static char sccsid[] = "@(#)readcf.c	8.20 (Berkeley) 02/08/94";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -600,6 +600,12 @@ fileclass(class, filename, fmt, safe, optional)
 	struct stat stbuf;
 	char buf[MAXLINE];
 
+	if (filename[0] == '|')
+	{
+		syserr("fileclass: pipes (F%c%s) not supported due to security problems",
+			class, filename);
+		return;
+	}
 	if (stat(filename, &stbuf) < 0)
 	{
 		if (!optional)
