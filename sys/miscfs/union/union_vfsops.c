1b29d024fae6a1145d61008cf9f8d570c7905271
Jan-Simon Pendry <pendry>
1994-02-10 18:50:26 -0800

    up to v8
    SCCS-vsn: 8.1

--- sys/miscfs/union/union_vfsops.c
+++ sys/miscfs/union/union_vfsops.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)union_vfsops.c	2.2 (Berkeley) 02/10/94
+ *	@(#)union_vfsops.c	8.1 (Berkeley) 02/10/94
  */
 
 /*
