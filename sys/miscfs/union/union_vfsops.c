8c00372da798c954016a1e0bca1d0f9289aa06eb
Jan-Simon Pendry <pendry>
1994-02-10 18:52:29 -0800

    use miscfs include path
    SCCS-vsn: 8.2

--- sys/miscfs/union/union_vfsops.c
+++ sys/miscfs/union/union_vfsops.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)union_vfsops.c	8.1 (Berkeley) 02/10/94
+ *	@(#)union_vfsops.c	8.2 (Berkeley) 02/10/94
  */
 
 /*
@@ -52,7 +52,7 @@
 #include <sys/malloc.h>
 #include <sys/filedesc.h>
 #include <sys/queue.h>
-#include "union.h"
+#include <miscfs/union/union.h>
 
 /*
  * Mount union filesystem
