ae4b7a79e1155f810714a373e7613f7db9c21bf8
Mike Hibler <hibler>
1994-02-13 11:52:17 -0800

    add sysctl debug variable to enable/disable reallocblks
    SCCS-vsn: 8.7

--- sys/kern/vfs_cluster.c
+++ sys/kern/vfs_cluster.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vfs_cluster.c	8.6 (Berkeley) 02/05/94
+ *	@(#)vfs_cluster.c	8.7 (Berkeley) 02/13/94
  */
 
 #include <sys/param.h>
@@ -44,6 +44,16 @@
 #include <libkern/libkern.h>
 #include <ufs/ufs/quota.h>
 #include <ufs/ufs/inode.h>
+
+#ifdef DEBUG
+#include <vm/vm.h>
+#include <sys/sysctl.h>
+int doreallocblks = 1;
+struct ctldebug debug13 = { "doreallocblks", &doreallocblks };
+#else
+/* XXX for cluster_write */
+#define doreallocblks 1
+#endif
 
 /*
  * Local declarations
@@ -500,7 +510,8 @@ cluster_write(bp, filesize)
 			 * Otherwise try reallocating to make it sequential.
 			 */
 			cursize = vp->v_lastw - vp->v_cstart + 1;
-			if ((lbn + 1) * bp->b_bcount != filesize ||
+			if (!doreallocblks ||
+			    (lbn + 1) * bp->b_bcount != filesize ||
 			    lbn != vp->v_lastw + 1 || vp->v_clen <= cursize) {
 				cluster_wbuild(vp, NULL, bp->b_bcount,
 				    vp->v_cstart, cursize, lbn);
