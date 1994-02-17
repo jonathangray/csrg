b24e18c96b622ed1c33fa65197419e84c4d49ef1
Jan-Simon Pendry <pendry>
1994-02-17 06:34:31 -0800

    add subdirectory check
    SCCS-vsn: 8.2

--- old/mount_lofs/mount_lofs.c
+++ old/mount_lofs/mount_lofs.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)mount_lofs.c	8.1 (Berkeley) 06/05/93
+ *	@(#)mount_lofs.c	8.2 (Berkeley) 02/17/94
  */
 
 #include <sys/param.h>
