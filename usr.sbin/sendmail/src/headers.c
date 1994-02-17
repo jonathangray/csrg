c7e168f4d4db0add46ef16119f4074caded20cd2
Eric Allman <eric>
1994-02-16 16:34:06 -0800

    better patch of MAYBENEXTRELEASE problem
    SCCS-vsn: 8.28

--- usr.sbin/sendmail/src/headers.c
+++ usr.sbin/sendmail/src/headers.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.27 (Berkeley) 02/16/94";
+static char sccsid[] = "@(#)headers.c	8.28 (Berkeley) 02/16/94";
 #endif /* not lint */
 
 # include <errno.h>
@@ -164,7 +164,7 @@ chompheader(line, def, e)
 		{
 			auto ADDRESS a;
 			char *fancy;
-			bool oldHoldErrs = HoldErrs;
+			bool oldSuprErrs = SuprErrs;
 			extern char *crackaddr();
 			extern char *udbsender();
 
@@ -176,12 +176,12 @@ chompheader(line, def, e)
 			** XXX	This code doesn't belong here -- parsing should
 			** XXX	not be done during collect() phase because
 			** XXX	error messages can confuse the SMTP phase.
-			** XXX	Setting HoldErrs is a crude hack around this
+			** XXX	Setting SuprErrs is a crude hack around this
 			** XXX	problem.
 			*/
 
 			if (OpMode == MD_SMTP || OpMode == MD_ARPAFTP)
-				HoldErrs = TRUE;
+				SuprErrs = TRUE;
 			fancy = crackaddr(fvalue);
 			if (parseaddr(fvalue, &a, RF_COPYNONE, '\0', NULL, e) != NULL &&
 			    a.q_mailer == LocalMailer &&
@@ -194,7 +194,7 @@ chompheader(line, def, e)
 				define('g', oldg, e);
 				fvalue = buf;
 			}
-			HoldErrs = oldHoldErrs;
+			SuprErrs = oldSuprErrs;
 		}
 #endif
 #endif
