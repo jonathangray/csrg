f70e8423bb56dd8c6e345b3ff8efb776284e762a
Keith Bostic <bostic>
1994-02-16 18:15:58 -0800

    add T_AFSDB/18, T_NSAP/22, T_NSAP_PTR/23 From: Paul Vixie <vixie@vangogh.CS.Berkeley.EDU>
    SCCS-vsn: 8.2

--- include/arpa/nameser.h
+++ include/arpa/nameser.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *      @(#)nameser.h	8.1 (Berkeley) 06/02/93
+ *      @(#)nameser.h	8.2 (Berkeley) 02/16/94
  * -
  * Portions Copyright (c) 1993 by Digital Equipment Corporation.
  * 
@@ -124,6 +124,9 @@
 #define T_MX		15		/* mail routing information */
 #define T_TXT		16		/* text strings */
 #define	T_RP		17		/* responsible person */
+#define	T_AFSDB		18		/* AFS cell database */
+#define	T_NSAP		22		/* NSAP address */
+#define	T_NSAP_PTR	23		/* reverse lookup for NSAP */
 	/* non standard */
 #define T_UINFO		100		/* user (finger) information */
 #define T_UID		101		/* user ID */
