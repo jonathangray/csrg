cde867dee8c9d3604859f893a4f8792215a70a2a
Eric Allman <eric>
1994-02-20 11:41:33 -0800

    use __progname (from crt0) as default syslog tag if none specified
    SCCS-vsn: 8.2

--- lib/libc/gen/syslog.c
+++ lib/libc/gen/syslog.c
@@ -32,7 +32,7 @@
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)syslog.c	8.1 (Berkeley) 06/04/93";
+static char sccsid[] = "@(#)syslog.c	8.2 (Berkeley) 02/20/94";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
