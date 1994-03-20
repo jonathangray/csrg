f029a22d51b9e23222cc4167297114d212670731
Keith Bostic <bostic>
1994-03-20 14:35:10 -0800

    delete UofT; requested change from Henry Spencer <henry@zoo.toronto.edu>
    SCCS-vsn: 8.3

--- lib/libc/regex/regfree.c
+++ lib/libc/regex/regfree.c
@@ -4,7 +4,7 @@
  *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Henry Spencer of the University of Toronto.
+ * Henry Spencer.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,11 +34,11 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)regfree.c	8.2 (Berkeley) 03/16/94
+ *	@(#)regfree.c	8.3 (Berkeley) 03/20/94
  */
 
 #if defined(LIBC_SCCS) && !defined(lint)
-static char sccsid[] = "@(#)regfree.c	8.2 (Berkeley) 03/16/94";
+static char sccsid[] = "@(#)regfree.c	8.3 (Berkeley) 03/20/94";
 #endif /* LIBC_SCCS and not lint */
 
 #include <sys/types.h>
