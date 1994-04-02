ca682debf68671ecd11c8eae362249eb7b252be9
Jan-Simon Pendry <pendry>
1994-04-02 10:08:32 -0800

    add 1994 copyright
    SCCS-vsn: 8.3

--- usr.bin/ctags/ctags.h
+++ usr.bin/ctags/ctags.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1987, 1993
+ * Copyright (c) 1987, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)ctags.h	8.2 (Berkeley) 04/01/94
+ *	@(#)ctags.h	8.3 (Berkeley) 04/02/94
  */
 
 #define	bool	char
