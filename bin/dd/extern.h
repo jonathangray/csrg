b0963929bbe62887b4c55f6f18c2dc4fb63afb00
Jan-Simon Pendry <pendry>
1994-04-02 09:55:28 -0800

    add 1994 copyright
    SCCS-vsn: 8.3

--- bin/dd/extern.h
+++ bin/dd/extern.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	8.2 (Berkeley) 04/02/94
+ *	@(#)extern.h	8.3 (Berkeley) 04/02/94
  */
 
 #include <sys/cdefs.h>
