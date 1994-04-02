edcbc7f28e91d9e75c48a24e2ca5e0755a720f67
Jan-Simon Pendry <pendry>
1994-04-02 07:39:21 -0800

    prettyness police
    SCCS-vsn: 8.2

--- bin/dd/extern.h
+++ bin/dd/extern.h
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)extern.h	8.1 (Berkeley) 05/31/93
+ *	@(#)extern.h	8.2 (Berkeley) 04/02/94
  */
 
 #include <sys/cdefs.h>
@@ -44,15 +44,14 @@ void block_close __P((void));
 void dd_out __P((int));
 void def __P((void));
 void def_close __P((void));
-void err __P((const char *, ...));
 void jcl __P((char **));
 void pos_in __P((void));
 void pos_out __P((void));
-void summary __P((int));
+void summary __P((void));
+void summaryx __P((int));
 void terminate __P((int));
 void unblock __P((void));
 void unblock_close __P((void));
-void warn __P((const char *, ...));
 
 extern IO in, out;
 extern STAT st;
