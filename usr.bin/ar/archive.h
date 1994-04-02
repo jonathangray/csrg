0b0d4cac32ac3f96c2e7910efca03cc2fd679dcd
Jan-Simon Pendry <pendry>
1994-04-02 10:03:38 -0800

    add 1994 copyright
    SCCS-vsn: 8.3

--- usr.bin/ar/archive.h
+++ usr.bin/ar/archive.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)archive.h	8.2 (Berkeley) 04/01/94
+ *	@(#)archive.h	8.3 (Berkeley) 04/02/94
  */
 
 /* Ar(1) options. */
