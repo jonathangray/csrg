fbaa840add7474c753b418adaa7685b296a21436
Keith Bostic <bostic>
1994-04-03 16:31:05 -0700

    prettiness police
    SCCS-vsn: 8.2

--- usr.bin/logname/logname.c
+++ usr.bin/logname/logname.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 1991, 1993
+ * Copyright (c) 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -33,20 +33,24 @@
 
 #ifndef lint
 static char copyright[] =
-"@(#) Copyright (c) 1991, 1993\n\
+"@(#) Copyright (c) 1991, 1993, 1994\n\
 	The Regents of the University of California.  All rights reserved.\n";
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)logname.c	8.1 (Berkeley) 06/09/93";
+static char sccsid[] = "@(#)logname.c	8.2 (Berkeley) 04/03/94";
 #endif /* not lint */
 
+#include <err.h>
 #include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+void usage __P((void));
+
+int
 main(argc, argv)
 	int argc;
 	char *argv[];
@@ -55,7 +59,7 @@ main(argc, argv)
 	char *p;
 
 	while ((ch = getopt(argc, argv, "")) != EOF)
-		switch(ch) {
+		switch (ch) {
 		case '?':
 		default:
 			usage();
@@ -63,14 +67,13 @@ main(argc, argv)
 	argc -= optind;
 	argv += optind;
 
-	if ((p = getlogin()) == NULL) {
-		(void)fprintf(stderr, "logname: %s\n", strerror(errno));
-		exit(1);
-	}
+	if ((p = getlogin()) == NULL)
+		err(1, NULL);
 	(void)printf("%s\n", p);
 	exit(0);
 }
 
+void
 usage()
 {
 	(void)fprintf(stderr, "usage: logname\n");
