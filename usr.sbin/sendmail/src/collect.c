ffb60acfd53e5b1fe05610759abc5fd430d77337
Eric Allman <eric>
1994-04-18 15:17:10 -0700

    when creating the df file, be sure you truncate any existing file
    SCCS-vsn: 8.14

--- usr.sbin/sendmail/src/collect.c
+++ usr.sbin/sendmail/src/collect.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)collect.c	8.13 (Berkeley) 04/18/94";
+static char sccsid[] = "@(#)collect.c	8.14 (Berkeley) 04/18/94";
 #endif /* not lint */
 
 # include <errno.h>
@@ -85,7 +85,7 @@ maketemp(from)
 
 	e->e_df = queuename(e, 'd');
 	e->e_df = newstr(e->e_df);
-	if ((tf = dfopen(e->e_df, O_WRONLY|O_CREAT, FileMode)) == NULL)
+	if ((tf = dfopen(e->e_df, O_WRONLY|O_CREAT|O_TRUNC, FileMode)) == NULL)
 	{
 		syserr("Cannot create %s", e->e_df);
 		NoReturn = TRUE;
