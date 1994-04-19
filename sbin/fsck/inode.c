beaf15ade16bcd927ee4108447012165eaa09b32
Kirk McKusick <mckusick>
1994-04-18 19:10:31 -0700

    count the size of holes as well as blocks
    SCCS-vsn: 8.4

--- sbin/fsck/inode.c
+++ sbin/fsck/inode.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)inode.c	8.3 (Berkeley) 02/27/94";
+static char sccsid[] = "@(#)inode.c	8.4 (Berkeley) 04/18/94";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -145,17 +145,16 @@ iblock(idesc, ilevel, isize)
 	for (ap = bp->b_un.b_indir; ap < aplim; ap++) {
 		if (*ap) {
 			idesc->id_blkno = *ap;
-			if (ilevel == 0) {
+			if (ilevel == 0)
 				n = (*func)(idesc);
-			} else {
+			else
 				n = iblock(idesc, ilevel, isize);
-				isize -= sizepb;
-			}
 			if (n & STOP) {
 				bp->b_flags &= ~B_INUSE;
 				return (n);
 			}
 		}
+		isize -= sizepb;
 	}
 	bp->b_flags &= ~B_INUSE;
 	return (KEEPON);
