7fb8a43e2513480b1dcc26c4ba2dfb6f7f806654
Edward Wang <edward>
1994-04-19 14:39:14 -0700

    backslash and ^ handling in getrawlist() fixed
    SCCS-vsn: 8.2

--- usr.bin/mail/list.c
+++ usr.bin/mail/list.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)list.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)list.c	8.2 (Berkeley) 04/19/94";
 #endif /* not lint */
 
 #include "rcv.h"
@@ -415,7 +415,8 @@ getrawlist(line, argv, argc)
 				else if (c == '\\')
 					switch (c = *cp++) {
 					case '\0':
-						*cp2++ = *--cp;
+						*cp2++ = '\\';
+						cp--;
 						break;
 					case '0': case '1': case '2': case '3':
 					case '4': case '5': case '6': case '7':
@@ -444,6 +445,8 @@ getrawlist(line, argv, argc)
 					case 'v':
 						*cp2++ = '\v';
 						break;
+					default:
+						*cp2++ = c;
 					}
 				else if (c == '^') {
 					c = *cp++;
@@ -452,9 +455,11 @@ getrawlist(line, argv, argc)
 					/* null doesn't show up anyway */
 					else if (c >= 'A' && c <= '_' ||
 						 c >= 'a' && c <= 'z')
-						*cp2++ &= 037;
-					else
-						*cp2++ = *--cp;
+						*cp2++ = c & 037;
+					else {
+						*cp2++ = '^';
+						cp--;
+					}
 				} else
 					*cp2++ = c;
 			} else if (c == '"' || c == '\'')
