926501a6fff7b74f0122e9dd38691e10520ce19d
Eric Allman <eric>
1994-05-15 07:08:02 -0700

    print numeric a.k.a. info
    SCCS-vsn: 8.49

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -38,9 +38,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.48 (Berkeley) 04/18/94 (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.49 (Berkeley) 05/15/94 (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.48 (Berkeley) 04/18/94 (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.49 (Berkeley) 05/15/94 (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -539,6 +539,8 @@ myhostname(hostbuf, size)
 
 			sprintf(ipbuf, "[%s]",
 				inet_ntoa(*((struct in_addr *) hp->h_addr_list[i])));
+			if (tTd(0, 4))
+				printf("\ta.k.a.: %s\n", ipbuf);
 			setclass('w', ipbuf);
 		}
 	}
