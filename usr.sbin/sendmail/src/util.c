cd5b1c8766d4a69d6bba18e762408b243e63972d
Eric Allman <eric>
1994-06-17 11:12:59 -0700

    Cray fixes from Eric Wassenaar
    SCCS-vsn: 8.40

--- usr.sbin/sendmail/src/util.c
+++ usr.sbin/sendmail/src/util.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.39 (Berkeley) 04/14/94";
+static char sccsid[] = "@(#)util.c	8.40 (Berkeley) 06/17/94";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1403,7 +1403,8 @@ dumpfd(fd, printclosed, logit)
 			sprintf(p, "(badsock)");
 		else
 		{
-			hp = gethostbyaddr((char *) &sin.sin_addr, slen, AF_INET);
+			hp = gethostbyaddr((char *) &sin.sin_addr,
+					   IPADDRSIZE, AF_INET);
 			sprintf(p, "%s/%d", hp == NULL ? inet_ntoa(sin.sin_addr)
 						   : hp->h_name, ntohs(sin.sin_port));
 		}
@@ -1415,7 +1416,8 @@ dumpfd(fd, printclosed, logit)
 			sprintf(p, "(badsock)");
 		else
 		{
-			hp = gethostbyaddr((char *) &sin.sin_addr, slen, AF_INET);
+			hp = gethostbyaddr((char *) &sin.sin_addr,
+					   IPADDRSIZE, AF_INET);
 			sprintf(p, "%s/%d", hp == NULL ? inet_ntoa(sin.sin_addr)
 						   : hp->h_name, ntohs(sin.sin_port));
 		}
