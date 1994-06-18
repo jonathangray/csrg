8707b5c5cb5de8193164aa784a096ad5553c41bb
Eric Allman <eric>
1994-06-17 17:40:00 -0700

    compatibility with BIND 4.9.3 (just cleanup)
    SCCS-vsn: 8.54

--- usr.sbin/sendmail/src/daemon.c
+++ usr.sbin/sendmail/src/daemon.c
@@ -38,9 +38,9 @@
 
 #ifndef lint
 #ifdef DAEMON
-static char sccsid[] = "@(#)daemon.c	8.53 (Berkeley) 06/17/94 (with daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.54 (Berkeley) 06/17/94 (with daemon mode)";
 #else
-static char sccsid[] = "@(#)daemon.c	8.53 (Berkeley) 06/17/94 (without daemon mode)";
+static char sccsid[] = "@(#)daemon.c	8.54 (Berkeley) 06/17/94 (without daemon mode)";
 #endif
 #endif /* not lint */
 
@@ -50,7 +50,6 @@ static char sccsid[] = "@(#)daemon.c	8.53 (Berkeley) 06/17/94 (without daemon mo
 # include <arpa/inet.h>
 
 #if NAMED_BIND
-# include <arpa/nameser.h>
 # include <resolv.h>
 #endif
 
@@ -323,7 +322,7 @@ gothostent:
 		  case AF_INET:
 			bcopy(hp->h_addr,
 				&addr.sin.sin_addr,
-				IPADDRSIZE);
+				INADDRSZ);
 			break;
 #endif
 
@@ -453,7 +452,7 @@ gothostent:
 			  case AF_INET:
 				bcopy(hp->h_addr_list[i++],
 				      &addr.sin.sin_addr,
-				      IPADDRSIZE);
+				      INADDRSZ);
 				break;
 #endif
 
@@ -895,7 +894,7 @@ host_map_lookup(map, name, av, statp)
 	in_addr.s_addr = inet_addr(&name[1]);
 
 	/* nope -- ask the name server */
-	hp = gethostbyaddr((char *)&in_addr, IPADDRSIZE, AF_INET);
+	hp = gethostbyaddr((char *)&in_addr, INADDRSZ, AF_INET);
 	s->s_namecanon.nc_errno = errno;
 #if NAMED_BIND
 	s->s_namecanon.nc_herrno = h_errno;
@@ -1004,7 +1003,7 @@ hostnamebyanyaddr(sap)
 #ifdef NETINET
 	  case AF_INET:
 		hp = gethostbyaddr((char *) &sap->sin.sin_addr,
-			IPADDRSIZE,
+			INADDRSZ,
 			AF_INET);
 		break;
 #endif
