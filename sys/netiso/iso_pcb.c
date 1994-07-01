318867d83b9e528d7245b7d0459bc06e676b85c8
Keith Sklower <sklower>
1994-06-30 20:05:22 -0700

    fix bugs about binding to a specific NSAP ignoring NSEL
    SCCS-vsn: 8.2

--- sys/netiso/iso_pcb.c
+++ sys/netiso/iso_pcb.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)iso_pcb.c	8.1 (Berkeley) 06/10/93
+ *	@(#)iso_pcb.c	8.2 (Berkeley) 06/30/94
  */
 
 /***********************************************************
@@ -200,7 +200,7 @@ iso_pcbbind(isop, nam)
 			printf("iso_pcbbind: bind to NOT zeroisoaddr\n");
 		ENDDEBUG
 		for (ia = iso_ifaddr; ia; ia = ia->ia_next) 
-			if (SAME_ISOADDR(siso, &ia->ia_addr))
+			if (SAME_ISOIFADDR(siso, &ia->ia_addr))
 				break;
 		if (ia == 0)
 			return EADDRNOTAVAIL;
@@ -228,6 +228,7 @@ iso_pcbbind(isop, nam)
 		register char *cp;
 noname:
 		cp = TSEL(isop->isop_laddr);
+		isop->isop_laddr.siso_tlen = 2;
 	IFDEBUG(D_ISO)
 		printf("iso_pcbbind noname\n");
 	ENDDEBUG
