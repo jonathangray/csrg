0df4e0c356f10f1373d38013bb1e0c07c77b3a92
Eric Allman <eric>
1994-08-20 07:01:24 -0700

    Generalize ReadTimeout option and call it Timeouts; create queuereturn, queuewarn, and fileopen timeouts
    SCCS-vsn: 8.59

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sendmail.h	8.58 (Berkeley) 08/17/94
+ *	@(#)sendmail.h	8.59 (Berkeley) 08/20/94
  */
 
 /*
@@ -41,7 +41,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.58		08/17/94";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.59		08/20/94";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -930,6 +930,7 @@ EXTERN struct
 	time_t	to_quit;	/* QUIT command */
 	time_t	to_miscshort;	/* misc short commands (NOOP, VERB, etc) */
 	time_t	to_ident;	/* IDENT protocol requests */
+	time_t	to_fileopen;	/* opening :include: and .forward files */
 			/* following are per message */
 	time_t	to_q_return;	/* queue return timeout */
 	time_t	to_q_warning;	/* queue warning timeout */
