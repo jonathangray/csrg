8be5312e73b07eb21ff442688d26892ff8111fc8
Kirk McKusick <mckusick>
1994-08-20 16:02:59 -0700

    update to use LIST macros for hash table
    SCCS-vsn: 8.7

--- sys/miscfs/umapfs/umap_subr.c
+++ sys/miscfs/umapfs/umap_subr.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)umap_subr.c	8.6 (Berkeley) 01/26/94
+ *	@(#)umap_subr.c	8.7 (Berkeley) 08/20/94
  *
  * $Id: lofs_subr.c, v 1.11 1992/05/30 10:05:43 jsp Exp jsp $
  */
@@ -50,7 +50,6 @@
 
 #define LOG2_SIZEVNODE 7		/* log2(sizeof struct vnode) */
 #define	NUMAPNODECACHE 16
-#define	UMAP_NHASH(vp) ((((u_long) vp)>>LOG2_SIZEVNODE) & (NUMAPNODECACHE-1))
 
 /*
  * Null layer cache:
@@ -60,39 +59,21 @@
  * alias is removed the target vnode is vrele'd.
  */
 
-/*
- * Cache head
- */
-struct umap_node_cache {
-	struct umap_node	*ac_forw;
-	struct umap_node	*ac_back;
-};
-
-static struct umap_node_cache umap_node_cache[NUMAPNODECACHE];
+#define	UMAP_NHASH(vp) \
+	(&umap_node_hashtbl[(((u_long)vp)>>LOG2_SIZEVNODE) & umap_node_hash])
+LIST_HEAD(umap_node_hashhead, umap_node) *umap_node_hashtbl;
+u_long umap_node_hash;
 
 /*
  * Initialise cache headers
  */
 umapfs_init()
 {
-	struct umap_node_cache *ac;
+
 #ifdef UMAPFS_DIAGNOSTIC
 	printf("umapfs_init\n");		/* printed during system boot */
 #endif
-
-	for (ac = umap_node_cache; ac < umap_node_cache + NUMAPNODECACHE; ac++)
-		ac->ac_forw = ac->ac_back = (struct umap_node *) ac;
-}
-
-/*
- * Compute hash list for given target vnode
- */
-static struct umap_node_cache *
-umap_node_hash(targetvp)
-	struct vnode *targetvp;
-{
-
-	return (&umap_node_cache[UMAP_NHASH(targetvp)]);
+	umap_node_hashtbl = hashinit(NUMAPNODECACHE, M_CACHE, &umap_node_hash);
 }
 
 /*
@@ -151,7 +132,7 @@ umap_node_find(mp, targetvp)
 	struct mount *mp;
 	struct vnode *targetvp;
 {
-	struct umap_node_cache *hd;
+	struct umap_node_hashhead *hd;
 	struct umap_node *a;
 	struct vnode *vp;
 
@@ -165,10 +146,9 @@ umap_node_find(mp, targetvp)
 	 * the target vnode.  If found, the increment the umap_node
 	 * reference count (but NOT the target vnode's VREF counter).
 	 */
-	hd = umap_node_hash(targetvp);
-
- loop:
-	for (a = hd->ac_forw; a != (struct umap_node *) hd; a = a->umap_forw) {
+	hd = UMAP_NHASH(targetvp);
+loop:
+	for (a = hd->lh_first; a != 0; a = a->umap_hash.le_next) {
 		if (a->umap_lowervp == targetvp &&
 		    a->umap_vnode->v_mount == mp) {
 			vp = UMAPTOV(a);
@@ -205,7 +185,7 @@ umap_node_alloc(mp, lowervp, vpp)
 	struct vnode *lowervp;
 	struct vnode **vpp;
 {
-	struct umap_node_cache *hd;
+	struct umap_node_hashhead *hd;
 	struct umap_node *xp;
 	struct vnode *othervp, *vp;
 	int error;
@@ -233,8 +213,8 @@ umap_node_alloc(mp, lowervp, vpp)
 		return (0);
 	}
 	VREF(lowervp);   /* Extra VREF will be vrele'd in umap_node_create */
-	hd = umap_node_hash(lowervp);
-	insque(xp, hd);
+	hd = UMAP_NHASH(lowervp);
+	LIST_INSERT_HEAD(hd, xp, umap_hash);
 	return (0);
 }
 
