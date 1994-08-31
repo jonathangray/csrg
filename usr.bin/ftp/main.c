d0884569b6b7ef0e2e760b1b21d3e712df27022e
Keith Bostic <bostic>
1994-08-31 16:12:54 -0700

    I'm glad to see this was tested.  Sigh. From: Charles Hannum <mycroft@NetBSD.ORG>
    SCCS-vsn: 8.5

--- usr.bin/ftp/main.c
+++ usr.bin/ftp/main.c
@@ -38,7 +38,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.4 (Berkeley) 04/03/94";
+static char sccsid[] = "@(#)main.c	8.5 (Berkeley) 08/31/94";
 #endif /* not lint */
 
 /*
@@ -78,7 +78,7 @@ main(argc, argv)
 	autologin = 1;
 
 	while ((ch = getopt(argc, argv, "dgintv")) != EOF) {
-		switch (*cp) {
+		switch (ch) {
 		case 'd':
 			options |= SO_DEBUG;
 			debug++;
