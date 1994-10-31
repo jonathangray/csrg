c22acb954f0d46a665a3e6027e88c2005b826881
Keith Sklower <sklower>
1994-10-31 13:42:56 -0800

    compromise fixes for duped keys
    SCCS-vsn: 8.4

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rtsock.c	8.3.1.1 (Berkeley) 10/09/94
+ *	@(#)rtsock.c	8.4 (Berkeley) 10/31/94
  */
 
 #include <sys/param.h>
@@ -132,6 +132,7 @@ route_output(m, so)
 	register struct rtentry *rt = 0;
 	struct rtentry *saved_nrt = 0;
 	struct radix_node_head *rnh;
+	struct radix_node_head *rnh;
 	struct rt_addrinfo info;
 	int len, error = 0;
 	struct ifnet *ifp = 0;
@@ -189,7 +190,12 @@ route_output(m, so)
 
 	case RTM_DELETE:
 		error = rtrequest(RTM_DELETE, dst, gate, netmask,
-				rtm->rtm_flags, (struct rtentry **)0);
+				rtm->rtm_flags, &saved_nrt);
+		if (error == 0) {
+			if ((rt = saved_nrt)->rt_refcnt <= 0)
+				rt->rt_refcnt++;
+			goto report;
+		}
 		break;
 
 	case RTM_GET:
@@ -205,6 +211,7 @@ route_output(m, so)
 		switch(rtm->rtm_type) {
 
 		case RTM_GET:
+		report:
 			dst = rt_key(rt);
 			gate = rt->rt_gateway;
 			netmask = rt_mask(rt);
@@ -219,7 +226,7 @@ route_output(m, so)
 					ifaaddr = 0;
 			    }
 			}
-			len = rt_msg2(RTM_GET, &info, (caddr_t)0,
+			len = rt_msg2(rtm->rtm_type, &info, (caddr_t)0,
 				(struct walkarg *)0);
 			if (len > rtm->rtm_msglen) {
 				struct rt_msghdr *new_rtm;
@@ -229,7 +236,7 @@ route_output(m, so)
 				Bcopy(rtm, new_rtm, rtm->rtm_msglen);
 				Free(rtm); rtm = new_rtm;
 			}
-			(void)rt_msg2(RTM_GET, &info, (caddr_t)rtm,
+			(void)rt_msg2(rtm->rtm_type, &info, (caddr_t)rtm,
 				(struct walkarg *)0);
 			rtm->rtm_flags = rt->rt_flags;
 			rtm->rtm_rmx = rt->rt_rmx;
