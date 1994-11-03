aa7024a5c49a7c7a677f65fdd054e056d7af7052
Keith Sklower <sklower>
1994-11-02 18:23:48 -0800

    this one actually compiles . . .
    SCCS-vsn: 8.5

--- sys/net/rtsock.c
+++ sys/net/rtsock.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)rtsock.c	8.4 (Berkeley) 10/31/94
+ *	@(#)rtsock.c	8.5 (Berkeley) 11/02/94
  */
 
 #include <sys/param.h>
