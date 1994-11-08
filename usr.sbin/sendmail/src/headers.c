5bc6541107233155874ac70f4fae7ebc71b02026
Eric Allman <eric>
1994-11-08 11:48:53 -0800

    allow per-mailer default character set (applies to INCOMING mailer)
    SCCS-vsn: 8.40

--- usr.sbin/sendmail/src/headers.c
+++ usr.sbin/sendmail/src/headers.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)headers.c	8.39 (Berkeley) 11/06/94";
+static char sccsid[] = "@(#)headers.c	8.40 (Berkeley) 11/08/94";
 #endif /* not lint */
 
 # include <errno.h>
@@ -1069,8 +1069,7 @@ putheader(mci, h, e)
 		if (hvalue("Content-Type", e->e_header) == NULL)
 		{
 			sprintf(obuf, "Content-Type: text/plain; charset=%s",
-				DefaultCharSet != NULL ? DefaultCharSet
-						       : "unknown-8bit");
+				defcharset(e));
 			putline(obuf, mci);
 		}
 		if (hvalue("Content-Transfer-Encoding", e->e_header) == NULL)
