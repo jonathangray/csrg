6d3b50eb5e75e0e6f5bc1a702a4dc71ea3891f0f
Eric Allman <eric>
1994-11-25 09:05:54 -0800

    improve DSN handling somewhat + some bug fixes
    SCCS-vsn: 8.70

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.69 (Berkeley) 10/25/94";
+static char sccsid[] = "@(#)main.c	8.70 (Berkeley) 11/25/94";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -872,8 +872,12 @@ main(argc, argv, envp)
 			for (j = '\0'; j <= '\177'; j++)
 				if (bitnset(j, m->m_flags))
 					(void) putchar(j);
-			printf(" E=");
+			printf("L=%d E=", m->m_linelimit);
 			xputs(m->m_eol);
+			if (m->m_defcharset != NULL)
+				printf(" C=%s", m->m_defcharset);
+			if (m->m_mtstype != NULL)
+				printf(" T=%s", m->m_mtstype);
 			if (m->m_argv != NULL)
 			{
 				char **a = m->m_argv;
