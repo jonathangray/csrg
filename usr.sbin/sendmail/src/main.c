699761248b88ff4d5e0e3e706bede2c28c88a2f7
Eric Allman <eric>
1994-11-27 11:20:18 -0800

    improved debugging
    SCCS-vsn: 8.71

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.70 (Berkeley) 11/25/94";
+static char sccsid[] = "@(#)main.c	8.71 (Berkeley) 11/27/94";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -864,33 +864,7 @@ main(argc, argv, envp)
 
 			if (m == NULL)
 				continue;
-			printf("mailer %d (%s): P=%s S=%d/%d R=%d/%d M=%ld U=%d:%d F=",
-				i, m->m_name,
-				m->m_mailer, m->m_se_rwset, m->m_sh_rwset,
-				m->m_re_rwset, m->m_rh_rwset, m->m_maxsize,
-				m->m_uid, m->m_gid);
-			for (j = '\0'; j <= '\177'; j++)
-				if (bitnset(j, m->m_flags))
-					(void) putchar(j);
-			printf("L=%d E=", m->m_linelimit);
-			xputs(m->m_eol);
-			if (m->m_defcharset != NULL)
-				printf(" C=%s", m->m_defcharset);
-			if (m->m_mtstype != NULL)
-				printf(" T=%s", m->m_mtstype);
-			if (m->m_argv != NULL)
-			{
-				char **a = m->m_argv;
-
-				printf(" A=");
-				while (*a != NULL)
-				{
-					if (a != m->m_argv)
-						printf(" ");
-					xputs(*a++);
-				}
-			}
-			printf("\n");
+			printmailer(m);
 		}
 	}
 
