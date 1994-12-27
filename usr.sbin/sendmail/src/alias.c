c39458576c9297a802240efcf9fff2d5667711f4
Eric Allman <eric>
1994-12-27 14:44:34 -0800

    allow multiple OA/file/name lines
    SCCS-vsn: 8.34

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -36,7 +36,7 @@
 # include <pwd.h>
 
 #ifndef lint
-static char sccsid[] = "@(#)alias.c	8.33 (Berkeley) 11/28/94";
+static char sccsid[] = "@(#)alias.c	8.34 (Berkeley) 12/27/94";
 #endif /* not lint */
 
 
@@ -217,6 +217,7 @@ setalias(spec)
 	register MAP *map;
 	char *class;
 	STAB *s;
+	static bool first_unqual = TRUE;
 
 	if (tTd(27, 8))
 		printf("setalias(%s)\n", spec);
@@ -234,10 +235,11 @@ setalias(spec)
 		**  for the files implementation, not necessarily in order.
 		*/
 
-		if (spec[0] == '/')
+		if (spec[0] == '/' && first_unqual)
 		{
 			s = stab("aliases.files", ST_MAP, ST_ENTER);
 			map = &s->s_map;
+			first_unqual = FALSE;
 		}
 		else
 		{
