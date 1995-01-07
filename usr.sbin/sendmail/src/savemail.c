4e6d9247cf14f38470e9740a8990dcc166216f9f
Eric Allman <eric>
1995-01-07 08:43:38 -0800

    avoid bogus dups
    SCCS-vsn: 8.50

--- usr.sbin/sendmail/src/savemail.c
+++ usr.sbin/sendmail/src/savemail.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)savemail.c	8.49 (Berkeley) 12/12/94";
+static char sccsid[] = "@(#)savemail.c	8.50 (Berkeley) 01/07/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -512,11 +512,20 @@ returntosender(msg, returnq, sendbody, e)
 		if (bitset(QBADADDR, q->q_flags))
 			continue;
 
+		if (!DontPruneRoutes && pruneroute(q->q_paddr))
+		{
+			register ADDRESS *p;
+
+			parseaddr(q->q_paddr, q, RF_COPYPARSE, '\0', NULL, e);
+			for (p = returnq; p != NULL; p = p->q_next)
+			{
+				if (p != q && sameaddr(p, q))
+					q->q_flags |= QDONTSEND;
+			}
+		}
+
 		if (!bitset(QDONTSEND, q->q_flags))
 			ee->e_nrcpts++;
-
-		if (!DontPruneRoutes && pruneroute(q->q_paddr))
-			parseaddr(q->q_paddr, q, RF_COPYPARSE, '\0', NULL, e);
 
 		if (q->q_alias == NULL)
 			addheader("To", q->q_paddr, &ee->e_header);
