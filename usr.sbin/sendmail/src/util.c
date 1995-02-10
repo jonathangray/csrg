9d79c58daa1946c7816964d70740a13d6f68e5bf
Eric Allman <eric>
1995-02-10 07:37:51 -0800

    fix security problem allowing bogus lines to be written to qf file from command line or IDENT protocol by embedding newlines
    SCCS-vsn: 8.39.1.1

--- usr.sbin/sendmail/src/util.c
+++ usr.sbin/sendmail/src/util.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.47 (Berkeley) 02/10/95";
+static char sccsid[] = "@(#)util.c	8.39.1.1 (Berkeley) 02/10/95";
 #endif /* not lint */
 
 # include "sendmail.h"
