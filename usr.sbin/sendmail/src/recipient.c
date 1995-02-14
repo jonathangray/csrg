071d5bb8afd1b37a5a46b02b209d70094a0ea5c0
Eric Allman <eric>
1995-02-14 06:11:29 -0800

    fix bug -- should have been testing bufp == NULL instead of buf == NULL
    SCCS-vsn: 8.64

--- usr.sbin/sendmail/src/recipient.c
+++ usr.sbin/sendmail/src/recipient.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.44.1.3 (Berkeley) 02/14/95";
+static char sccsid[] = "@(#)recipient.c	8.64 (Berkeley) 02/14/95";
 #endif /* not lint */
 
 # include "sendmail.h"
