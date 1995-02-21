4a59b786b37f562890552eb137d778f81e7bbd67
Eric Allman <eric>
1995-02-20 17:32:35 -0800

    fix sendtolist() buffer copying -- confusion of global v. local
    SCCS-vsn: 8.65

--- usr.sbin/sendmail/src/recipient.c
+++ usr.sbin/sendmail/src/recipient.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)recipient.c	8.44.1.4 (Berkeley) 02/20/95";
+static char sccsid[] = "@(#)recipient.c	8.65 (Berkeley) 02/20/95";
 #endif /* not lint */
 
 # include "sendmail.h"
