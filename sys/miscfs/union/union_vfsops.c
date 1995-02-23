edb947decae5e881cee775467d30a79c2bc4aae6
Kirk McKusick <mckusick>
1995-02-22 19:26:14 -0800

    vgone => VOP_REVOKE
    SCCS-vsn: 8.15

--- sys/miscfs/union/union_vfsops.c
+++ sys/miscfs/union/union_vfsops.c
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)union_vfsops.c	8.14 (Berkeley) 12/30/94
+ *	@(#)union_vfsops.c	8.15 (Berkeley) 02/22/95
  */
 
 /*
