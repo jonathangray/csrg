ca00087f2557cdf6af8ca1ea837b923dbcfa4946
Kirk McKusick <mckusick>
1995-02-22 19:19:09 -0800

    vgone => VOP_REVOKE
    SCCS-vsn: 8.5

--- sys/miscfs/fdesc/fdesc_vfsops.c
+++ sys/miscfs/fdesc/fdesc_vfsops.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fdesc_vfsops.c	8.4 (Berkeley) 01/21/94
+ *	@(#)fdesc_vfsops.c	8.5 (Berkeley) 02/22/95
  *
  * $Id: fdesc_vfsops.c,v 1.9 1993/04/06 15:28:33 jsp Exp $
  */
