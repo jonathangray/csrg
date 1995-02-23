264f23ef4843814cd21d8d276d2692c835d397d3
Kirk McKusick <mckusick>
1995-02-22 20:14:42 -0800

    vgone => VOP_REVOKE
    SCCS-vsn: 8.5

--- sys/nfs/nfs_vfsops.c
+++ sys/nfs/nfs_vfsops.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)nfs_vfsops.c	8.4 (Berkeley) 08/18/94
+ *	@(#)nfs_vfsops.c	8.5 (Berkeley) 02/22/95
  */
 
 #include <sys/param.h>
