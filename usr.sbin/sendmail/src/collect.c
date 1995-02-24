8a36fd8ce62ce35c8e9026a8419e4dfa8c0ba23a
Eric Allman <eric>
1995-02-24 08:58:32 -0800

    delete Apparently-To: code
    SCCS-vsn: 8.28

--- usr.sbin/sendmail/src/collect.c
+++ usr.sbin/sendmail/src/collect.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)collect.c	8.27 (Berkeley) 02/23/95";
+static char sccsid[] = "@(#)collect.c	8.28 (Berkeley) 02/24/95";
 #endif /* not lint */
 
 # include <errno.h>
