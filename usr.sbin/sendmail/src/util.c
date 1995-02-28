f17b4e02eec75d22a2324b4edbecb46cfbafe7d4
Eric Allman <eric>
1995-02-28 15:11:19 -0800

    prototypes
    SCCS-vsn: 8.39.1.4

--- usr.sbin/sendmail/src/util.c
+++ usr.sbin/sendmail/src/util.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.52 (Berkeley) 02/28/95";
+static char sccsid[] = "@(#)util.c	8.39.1.4 (Berkeley) 02/28/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1635,7 +1635,7 @@ cleanstrcpy(t, f, l)
 char *
 denlstring(s, logattacks)
 	char *s;
-	bool logattacks;
+	int logattacks;
 {
 	register char *p;
 	int l;
