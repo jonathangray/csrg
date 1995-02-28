7dbce6c01fc03da749cadb77ac0ae5032b5b30aa
Eric Allman <eric>
1995-02-28 13:25:38 -0800

    make fdopen() mode compatible with open() mode
    SCCS-vsn: 8.34.1.1

--- usr.sbin/sendmail/src/envelope.c
+++ usr.sbin/sendmail/src/envelope.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)envelope.c	8.50 (Berkeley) 02/17/95";
+static char sccsid[] = "@(#)envelope.c	8.34.1.1 (Berkeley) 02/28/95";
 #endif /* not lint */
 
 #include "sendmail.h"
