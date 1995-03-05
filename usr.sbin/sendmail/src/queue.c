a237f0b692a1f124b38cec7a6b7b9f0db98d2172
Eric Allman <eric>
1995-03-05 14:34:13 -0800

    changes suggested by *Hobbit*; SafeFileEnvironment option
    SCCS-vsn: 8.66

--- usr.sbin/sendmail/src/queue.c
+++ usr.sbin/sendmail/src/queue.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.65 (Berkeley) 03/05/95 (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.66 (Berkeley) 03/05/95 (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.65 (Berkeley) 03/05/95 (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.66 (Berkeley) 03/05/95 (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -1199,6 +1199,7 @@ readqf(e)
 	char buf[MAXLINE];
 	extern long atol();
 	extern ADDRESS *setctluser();
+	extern void loseqfile();
 	extern ADDRESS *sendto();
 
 	/*
@@ -1255,7 +1256,7 @@ readqf(e)
 # endif /* LOG */
 		if (tTd(40, 8))
 			printf("readqf(%s): bogus file\n", qf);
-		rename(qf, queuename(e, 'Q'));
+		loseqfile(e, "bogus file uid in mqueue");
 		fclose(qfp);
 		return FALSE;
 	}
@@ -1415,7 +1416,7 @@ readqf(e)
 			syserr("readqf: %s: line %d: bad line \"%s\"",
 				qf, LineNumber, bp);
 			fclose(qfp);
-			rename(qf, queuename(e, 'Q'));
+			loseqfile(e, "unrecognized line");
 			return FALSE;
 		}
 
@@ -1851,4 +1852,32 @@ setctluser(user)
 	else
 		a->q_paddr = newstr(p);
 	return a;
+}
+/*
+**  LOSEQFILE -- save the qf as Qf and try to let someone know
+**
+**	Parameters:
+**		e -- the envelope (e->e_id will be used).
+**		why -- reported to whomever can hear.
+**
+**	Returns:
+**		none.
+*/
+
+void
+loseqfile(e, why)
+	register ENVELOPE *e;
+	char *why;
+{
+	char buf[40];
+
+	if (e == NULL || e->e_id == NULL)
+		return;
+	if (strlen(e->e_id) > sizeof buf - 4)
+		return;
+	strcpy(buf, queuename(e, 'q'));
+	rename(buf, queuename(e, 'Q'));
+#ifdef LOG
+	syslog(LOG_ALERT, "Losing %s: %s", buf, why);
+#endif
 }
