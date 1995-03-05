136fe88f76a2824023240ad2870369358d811b56
Eric Allman <eric>
1995-03-05 11:57:18 -0800

    add "strict" parameter to denlstring to allow continuations
    SCCS-vsn: 8.65

--- usr.sbin/sendmail/src/queue.c
+++ usr.sbin/sendmail/src/queue.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.41.1.3 (Berkeley) 03/05/95 (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.65 (Berkeley) 03/05/95 (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.41.1.3 (Berkeley) 03/05/95 (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.65 (Berkeley) 03/05/95 (without queueing)";
 #endif
 #endif /* not lint */
 
@@ -265,7 +265,7 @@ queueup(e, queueall, announce)
 		fprintf(tfp, "$_%s\n", denlstring(p, TRUE, FALSE));
 
 	/* output name of sender */
-	fprintf(tfp, "S%s\n", denlstring(p, FALSE));
+	fprintf(tfp, "S%s\n", denlstring(p, TRUE, FALSE));
 
 	/* output ESMTP-supplied "original" information */
 	if (e->e_envid != NULL)
@@ -289,7 +289,7 @@ queueup(e, queueall, announce)
 		{
 			printctladdr(q, tfp);
 			if (q->q_orcpt != NULL)
-			fprintf(tfp, "%s\n", denlstring(q->q_paddr, FALSE));
+			fprintf(tfp, "%s\n", denlstring(q->q_paddr, TRUE, FALSE));
 			if (announce)
 			{
 				e->e_to = q->q_paddr;
