fb54a2fc0694ff96922efcc6739b5cd2ba7a20bb
Eric Allman <eric>
1995-03-06 16:02:59 -0800

    avoid possible attacks against hidden files; this isn't perfect
    SCCS-vsn: 8.19.1.1

--- usr.sbin/sendmail/src/domain.c
+++ usr.sbin/sendmail/src/domain.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #if NAMED_BIND
-static char sccsid[] = "@(#)domain.c	8.32 (Berkeley) 03/06/95 (with name server)";
+static char sccsid[] = "@(#)domain.c	8.19.1.1 (Berkeley) 03/06/95 (with name server)";
 #else
-static char sccsid[] = "@(#)domain.c	8.32 (Berkeley) 03/06/95 (without name server)";
+static char sccsid[] = "@(#)domain.c	8.19.1.1 (Berkeley) 03/06/95 (without name server)";
 #endif
 #endif /* not lint */
 
@@ -724,6 +724,7 @@ gethostalias(host)
 	if (fname == NULL ||
 	    (fp = safefopen(fname, O_RDONLY, 0, SFF_ANYFILE)) == NULL)
 		return NULL;
+	setbuf(fp, NULL);
 	while (fgets(buf, sizeof buf, fp) != NULL)
 	{
 		for (p = buf; p != '\0' && !(isascii(*p) && isspace(*p)); p++)
