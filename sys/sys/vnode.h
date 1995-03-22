c1d8c98cb2fa5ff8bb57b8c8390ef1df64c7c9d2
Kirk McKusick <mckusick>
1995-03-22 14:55:46 -0800

    add exclusive create flag for passing to VOP_CREATE
    SCCS-vsn: 8.15

--- sys/sys/vnode.h
+++ sys/sys/vnode.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vnode.h	8.14 (Berkeley) 02/23/95
+ *	@(#)vnode.h	8.15 (Berkeley) 03/22/95
  */
 
 #include <sys/queue.h>
@@ -142,9 +142,10 @@ struct vattr {
 };
 
 /*
- * Flags for va_cflags.
+ * Flags for va_vaflags.
  */
 #define	VA_UTIMES_NULL	0x01		/* utimes argument was NULL */
+#define VA_EXCLUSIVE	0x02		/* exclusive create request */
 
 /*
  * Flags for ioflag.
