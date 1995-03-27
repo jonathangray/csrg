5918949f0ea338106564454d346e19fe9ca004c2
Eric Allman <eric>
1995-03-27 11:43:27 -0800

    make DSN info match draft numbers
    SCCS-vsn: 8.63

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.62 (Berkeley) 03/27/95 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.63 (Berkeley) 03/27/95 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.62 (Berkeley) 03/27/95 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.63 (Berkeley) 03/27/95 (without SMTP)";
 #endif
 #endif /* not lint */
 
