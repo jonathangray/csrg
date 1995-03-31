9e65c958b641932a5909310fbb5465dd0bab2a3b
Eric Allman <eric>
1995-03-31 10:52:19 -0800

    pull getpwXXX and gethostbyXXX calls out into conf.c so they can be tweaked
    SCCS-vsn: 8.75

--- usr.sbin/sendmail/src/queue.c
+++ usr.sbin/sendmail/src/queue.c
@@ -36,14 +36,13 @@
 
 #ifndef lint
 #ifdef QUEUE
-static char sccsid[] = "@(#)queue.c	8.74 (Berkeley) 03/27/95 (with queueing)";
+static char sccsid[] = "@(#)queue.c	8.75 (Berkeley) 03/31/95 (with queueing)";
 #else
-static char sccsid[] = "@(#)queue.c	8.74 (Berkeley) 03/27/95 (without queueing)";
+static char sccsid[] = "@(#)queue.c	8.75 (Berkeley) 03/31/95 (without queueing)";
 #endif
 #endif /* not lint */
 
 # include <errno.h>
-# include <pwd.h>
 # include <dirent.h>
 
 # ifdef QUEUE
@@ -458,7 +457,7 @@ printctladdr(a, tfp)
 	lastuid = uid;
 	lastctladdr = a;
 
-	if (uid == 0 || (pw = getpwuid(uid)) == NULL)
+	if (uid == 0 || (pw = sm_getpwuid(uid)) == NULL)
 		uname = "";
 	else
 		uname = pw->pw_name;
@@ -1840,7 +1839,7 @@ setctluser(user)
 	p = strchr(user, ':');
 	if (p != NULL)
 		*p++ = '\0';
-	if (*user != '\0' && (pw = getpwnam(user)) != NULL)
+	if (*user != '\0' && (pw = sm_getpwnam(user)) != NULL)
 	{
 		if (strcmp(pw->pw_dir, "/") == 0)
 			a->q_home = "";
