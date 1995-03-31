af9263b167bdc811d6cb4b8559e8577fdaac6d08
Eric Allman <eric>
1995-03-31 15:22:58 -0800

    change "bool" to be an int -- prototypes just don't grok it otherwise
    SCCS-vsn: 8.60

--- usr.sbin/sendmail/src/util.c
+++ usr.sbin/sendmail/src/util.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)util.c	8.59 (Berkeley) 03/31/95";
+static char sccsid[] = "@(#)util.c	8.60 (Berkeley) 03/31/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -1790,8 +1790,8 @@ cleanstrcpy(t, f, l)
 char *
 denlstring(s, strict, logattacks)
 	char *s;
-	int strict;
-	int logattacks;
+	bool strict;
+	bool logattacks;
 {
 	register char *p;
 	int l;
