5031f02a76004b97f6881dfa726c3d650dbdc889
Eric Allman <eric>
1995-03-31 16:54:09 -0800

    merge a bit with Sun version
    SCCS-vsn: 8.42

--- usr.sbin/sendmail/src/alias.c
+++ usr.sbin/sendmail/src/alias.c
@@ -35,7 +35,7 @@
 # include "sendmail.h"
 
 #ifndef lint
-static char sccsid[] = "@(#)alias.c	8.41 (Berkeley) 03/31/95";
+static char sccsid[] = "@(#)alias.c	8.42 (Berkeley) 03/31/95";
 #endif /* not lint */
 
 
@@ -76,6 +76,7 @@ alias(a, sendq, aliaslevel, e)
 	register char *p;
 	int naliases;
 	char *owner;
+	auto int stat = EX_OK;
 	char obuf[MAXNAME + 6];
 	extern ADDRESS *sendto();
 	extern char *aliaslookup();
@@ -93,10 +94,21 @@ alias(a, sendq, aliaslevel, e)
 	e->e_to = a->q_paddr;
 
 	/*
-	**  Look up this name
+	**  Look up this name.
+	**
+	**	If the map was unavailable, we will queue this message
+	**	until the map becomes available; otherwise, we could
+	**	bounce messages inappropriately.
 	*/
 
-	p = aliaslookup(a->q_user, e);
+	p = aliaslookup(a->q_user, &stat, e);
+	if (stat == EX_TEMPFAIL || stat == EX_UNAVAILABLE)
+	{
+		a->q_flags |= QQUEUEUP;
+		if (e->e_message == NULL)
+			e->e_message = "alias database unavailable";
+		return;
+	}
 	if (p == NULL)
 		return;
 
@@ -143,7 +155,7 @@ alias(a, sendq, aliaslevel, e)
 		(void) strcat(obuf, a->q_user);
 	if (!bitnset(M_USR_UPPER, a->q_mailer->m_flags))
 		makelower(obuf);
-	owner = aliaslookup(obuf, e);
+	owner = aliaslookup(obuf, &stat, e);
 	if (owner == NULL)
 		return;
 
@@ -164,6 +176,8 @@ alias(a, sendq, aliaslevel, e)
 **
 **	Parameters:
 **		name -- the name to look up.
+**		pstat -- a pointer to a place to put the status.
+**		e -- the current envelope.
 **
 **	Returns:
 **		the value of name.
@@ -177,8 +191,9 @@ alias(a, sendq, aliaslevel, e)
 */
 
 char *
-aliaslookup(name, e)
+aliaslookup(name, pstat, e)
 	char *name;
+	int *pstat;
 	ENVELOPE *e;
 {
 	register int dbno;
@@ -192,7 +207,7 @@ aliaslookup(name, e)
 		map = AliasDB[dbno];
 		if (!bitset(MF_OPEN, map->map_mflags))
 			continue;
-		p = (*map->map_class->map_lookup)(map, name, NULL, &stat);
+		p = (*map->map_class->map_lookup)(map, name, NULL, pstat);
 		if (p != NULL)
 			return p;
 	}
