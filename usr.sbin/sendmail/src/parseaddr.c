ba39f6b0f10d29d24d6d445595fbe7f6e9b379fe
Eric Allman <eric>
1995-03-31 16:59:35 -0800

    fix compile glitch
    SCCS-vsn: 8.63

--- usr.sbin/sendmail/src/parseaddr.c
+++ usr.sbin/sendmail/src/parseaddr.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)parseaddr.c	8.62 (Berkeley) 03/31/95";
+static char sccsid[] = "@(#)parseaddr.c	8.63 (Berkeley) 03/31/95";
 #endif /* not lint */
 
 #include "sendmail.h"
