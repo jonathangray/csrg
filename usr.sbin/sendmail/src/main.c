f79f2e15ac2daec4fdb65c5874e77e7db1cf450e
Eric Allman <eric>
1995-04-09 17:41:27 -0700

    clean up group handling for sender & envelope addresses
    SCCS-vsn: 8.101

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.100 (Berkeley) 04/09/95";
+static char sccsid[] = "@(#)main.c	8.101 (Berkeley) 04/09/95";
 #endif /* not lint */
 
 #define	_DEFINE
