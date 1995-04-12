80fc31e624e31101d5c0bacf1c22db9ae0768712
Kirk McKusick <mckusick>
1995-04-12 14:29:48 -0700

    redefine lock_* calls to use lockmgr
    SCCS-vsn: 8.5

--- sys/vm/vm_map.c
+++ sys/vm/vm_map.c
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)vm_map.c	8.4 (Berkeley) 01/09/95
+ *	@(#)vm_map.c	8.5 (Berkeley) 04/12/95
  *
  *
  * Copyright (c) 1987, 1990 Carnegie-Mellon University.
