4450bf9c5419d1dd627eb040bb6118d64b695dc9
Thomas Ferrin <tef>
1995-04-27 21:38:41 -0700

    Bug fixes.
    SCCS-vsn: 8.2

--- usr.sbin/lpr/lpd/recvjob.c
+++ usr.sbin/lpr/lpd/recvjob.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)recvjob.c	8.1 (Berkeley) 06/06/93";
+static char sccsid[] = "@(#)recvjob.c	8.2 (Berkeley) 04/27/95";
 #endif /* not lint */
 
 /*
@@ -342,7 +342,7 @@ frecverr(msg, va_alist)
         va_dcl
 #endif
 {
-	extern char *fromb;
+	extern char fromb[];
 	va_list ap;
 #if __STDC__
 	va_start(ap, msg);
