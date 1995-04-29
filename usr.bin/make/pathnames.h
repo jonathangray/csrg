62934e8c02edef5c90a745a6619cde72ebdcec6e
Christos Zoulas <christos>
1995-04-28 17:08:24 -0700

    Updated to the latest version from the NetBSD source
    SCCS-vsn: 8.2

--- usr.bin/make/pathnames.h
+++ usr.bin/make/pathnames.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)pathnames.h	8.1 (Berkeley) 06/06/93
+ *	@(#)pathnames.h	8.2 (Berkeley) 04/28/95
  */
 
 #define	_PATH_OBJDIR		"obj"
