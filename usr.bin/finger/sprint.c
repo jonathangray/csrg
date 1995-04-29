9ee09249de46f106166c846d90e5e335fa98c217
Keith Bostic <bostic>
1995-04-28 17:36:39 -0700

    use memmove, not memcpy
    SCCS-vsn: 8.3

--- usr.bin/finger/sprint.c
+++ usr.bin/finger/sprint.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)sprint.c	8.2 (Berkeley) 04/28/95";
+static char sccsid[] = "@(#)sprint.c	8.3 (Berkeley) 04/28/95";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -85,7 +85,7 @@ sflag_print()
 			err(1, "db seq");
 		if (r == 1)
 			break;
-		memcpy(&tmp, data.data, sizeof tmp);
+		memmove(&tmp, data.data, sizeof tmp);
 		pn = tmp;
 
 		for (w = pn->whead; w != NULL; w = w->next) {
