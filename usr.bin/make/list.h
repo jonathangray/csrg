b78c9df5acc73aa350d3426cc935876aa70cd7e0
Christos Zoulas <christos>
1995-04-28 17:08:11 -0700

    Updated to the latest version from the NetBSD source
    SCCS-vsn: 8.2

--- usr.bin/make/list.h
+++ usr.bin/make/list.h
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)list.h	8.1 (Berkeley) 06/06/93
+ *	@(#)list.h	8.2 (Berkeley) 04/28/95
  */
 
 /*
