60bfc1feb5ddacf47e39f89a6f5c920f87707ca5
Keith Bostic <bostic>
1995-05-04 15:50:34 -0700

    optarg/optind moved to unistd.h
    SCCS-vsn: 8.3

--- usr.sbin/kvm_mkdb/kvm_mkdb.c
+++ usr.sbin/kvm_mkdb/kvm_mkdb.c
@@ -38,7 +38,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)kvm_mkdb.c	8.2 (Berkeley) 04/27/95";
+static char sccsid[] = "@(#)kvm_mkdb.c	8.3 (Berkeley) 05/04/95";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -52,6 +52,7 @@ static char sccsid[] = "@(#)kvm_mkdb.c	8.2 (Berkeley) 04/27/95";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "extern.h"
 
