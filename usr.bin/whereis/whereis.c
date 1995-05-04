49a42da787d7b763129955d24e33c1a3ede665f5
Keith Bostic <bostic>
1995-05-04 15:48:52 -0700

    optarg/optind moved to unistd.h
    SCCS-vsn: 8.3

--- usr.bin/whereis/whereis.c
+++ usr.bin/whereis/whereis.c
@@ -38,7 +38,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)whereis.c	8.2 (Berkeley) 04/27/95";
+static char sccsid[] = "@(#)whereis.c	8.3 (Berkeley) 05/04/95";
 #endif /* not lint */
 
 #include <sys/param.h>
@@ -50,6 +50,7 @@ static char sccsid[] = "@(#)whereis.c	8.2 (Berkeley) 04/27/95";
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void usage __P((void));
 
