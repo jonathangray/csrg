e943fc088f4ca83fa17af800c9ddd8b5bc4e0bed
Christos Zoulas <christos>
1995-05-04 18:03:55 -0700

    misc cleanup
    SCCS-vsn: 8.2

--- bin/sh/bltin/bltin.h
+++ bin/sh/bltin/bltin.h
@@ -33,7 +33,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)bltin.h	8.1 (Berkeley) 05/31/93
+ *	@(#)bltin.h	8.2 (Berkeley) 05/04/95
  */
 
 /*
