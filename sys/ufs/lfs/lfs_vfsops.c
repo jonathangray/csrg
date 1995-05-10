0dd12a62cec14c2161923c4f103dc34ca082db3f
Kirk McKusick <mckusick>
1995-05-10 13:21:30 -0700

    mountroot should fail, not panic
    SCCS-vsn: 8.17

--- sys/ufs/lfs/lfs_vfsops.c
+++ sys/ufs/lfs/lfs_vfsops.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)lfs_vfsops.c	8.16 (Berkeley) 05/10/95
+ *	@(#)lfs_vfsops.c	8.17 (Berkeley) 05/10/95
  */
 
 #include <sys/param.h>
@@ -93,9 +93,11 @@ lfs_mountroot()
 	/*
 	 * Get vnodes for swapdev and rootdev.
 	 */
-	if (bdevvp(swapdev, &swapdev_vp) || bdevvp(rootdev, &rootvp))
-		panic("ffs_mountroot: can't setup bdevvp's");
-
+	if ((error = bdevvp(swapdev, &swapdev_vp)) ||
+	    (error = bdevvp(rootdev, &rootvp))) {
+		printf("lfs_mountroot: can't setup bdevvp's");
+		return (error);
+	}
 	if (error = vfs_rootmountalloc("lfs", "root_device", &mp))
 		return (error);
 	if (error = lfs_mountfs(rootvp, mp, p)) {
