4034aca035317b35aa3b5b174a8afe028e817e75
Eric Allman <eric>
1995-05-16 21:08:04 -0700

    vendor hooks (for Sun)
    SCCS-vsn: 8.110

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.109 (Berkeley) 05/15/95";
+static char sccsid[] = "@(#)main.c	8.110 (Berkeley) 05/16/95";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -691,7 +691,9 @@ main(argc, argv, envp)
 #ifdef XDEBUG
 	checkfd012("before readcf");
 #endif
+	vendor_pre_defaults(CurEnv);
 	readcf(getcfname(), safecf, CurEnv);
+	vendor_post_defaults(CurEnv);
 
 	if (tTd(0, 1))
 	{
