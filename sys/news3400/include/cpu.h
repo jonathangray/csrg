50c4b3f0e4e249d73f3c8b38376095f0f0b99ef0
Kirk McKusick <mckusick>
1995-05-17 21:33:36 -0700

    add definition for ability to produce a backtrace
    SCCS-vsn: 8.3

--- sys/news3400/include/cpu.h
+++ sys/news3400/include/cpu.h
@@ -34,7 +34,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)cpu.h	8.2 (Berkeley) 01/21/94
+ *	@(#)cpu.h	8.3 (Berkeley) 05/17/95
  */
 
 #ifndef _CPU_H_
@@ -57,6 +57,7 @@
 #define	cpu_wait(p)		/* nothing */
 #define cpu_setstack(p, ap)	(p)->p_md.md_regs[SP] = ap
 #define cpu_set_init_frame(p, fp) (p)->p_md.md_regs = fp
+#define	BACKTRACE(p)		/* not implemented */
 
 /*
  * Arguments to hardclock and gatherstats encapsulate the previous
