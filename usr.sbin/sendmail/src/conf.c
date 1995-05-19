21e4adc952fbe11cfab442d4e915691e1938f96c
Eric Allman <eric>
1995-05-19 16:33:28 -0700

    forward declarations to fix compiler problems
    SCCS-vsn: 8.172

--- usr.sbin/sendmail/src/conf.c
+++ usr.sbin/sendmail/src/conf.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)conf.c	8.171 (Berkeley) 05/19/95";
+static char sccsid[] = "@(#)conf.c	8.172 (Berkeley) 05/19/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -3009,6 +3009,8 @@ sm_getpwuid(uid)
 */
 
 #ifdef SIOCGIFCONF
+struct rtentry;
+struct mbuf;
 # include <arpa/inet.h>
 # include <sys/time.h>
 # include <net/if.h>
