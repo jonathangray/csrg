ac8a737750b4fd35f8af85ce3257bc3400323233
Eric Allman <eric>
1995-05-19 17:18:25 -0700

    re-word /mx output to be more precise
    SCCS-vsn: 8.113

--- usr.sbin/sendmail/src/main.c
+++ usr.sbin/sendmail/src/main.c
@@ -39,7 +39,7 @@ static char copyright[] =
 #endif /* not lint */
 
 #ifndef lint
-static char sccsid[] = "@(#)main.c	8.112 (Berkeley) 05/19/95";
+static char sccsid[] = "@(#)main.c	8.113 (Berkeley) 05/19/95";
 #endif /* not lint */
 
 #define	_DEFINE
@@ -1765,7 +1765,7 @@ testmodeline(line, e)
 			char *mxhosts[MAXMXHOSTS + 1];
 
 			nmx = getmxrr(p, mxhosts, FALSE, &rcode);
-			printf("%d MX records:\n", nmx);
+			printf("getmxrr(%s) returns %d value(s):\n", p, nmx);
 			for (i = 0; i < nmx; i++)
 				printf("\t%s\n", mxhosts[i]);
 #else
