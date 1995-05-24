e3558dd266cd46d8076e9271f81ee2ff7170d2ac
Mike Karels <karels>
1995-05-24 10:50:55 -0700

    changes to randomize iss (somewhat); use a random component in the increment. Increase rate of periodic increment, but leave the per-connection increment about the same.  Small cleanup in code to handle bad (e.g. spoofed) SYN: remove redundant code, add counter (perhaps should count only if SYN is set), and simplify slightly for RST.
    SCCS-vsn: 8.12

--- sys/netinet/tcp_input.c
+++ sys/netinet/tcp_input.c
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)tcp_input.c	8.11 (Berkeley) 05/24/95
+ *	@(#)tcp_input.c	8.12 (Berkeley) 05/24/95
  */
 
 #ifndef TUBA_INCLUDE
