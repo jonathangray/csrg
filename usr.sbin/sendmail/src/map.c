f5ecd1944cbc312a3c57c91098ed8f6f5c812769
Eric Allman <eric>
1995-05-26 07:50:03 -0700

    add -q map flag to avoid quote stripping; clean up domain addition in nis_getcanonname and nisplus_getcanonname
    SCCS-vsn: 8.68

--- usr.sbin/sendmail/src/map.c
+++ usr.sbin/sendmail/src/map.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)map.c	8.67 (Berkeley) 05/25/95";
+static char sccsid[] = "@(#)map.c	8.68 (Berkeley) 05/26/95";
 #endif /* not lint */
 
 #include "sendmail.h"
@@ -148,6 +148,10 @@ map_parseargs(map, ap)
 
 		  case 'A':
 			map->map_mflags |= MF_APPEND;
+			break;
+
+		  case 'q':
+			map->map_mflags |= MF_KEEPQUOTES;
 			break;
 
 		  case 'a':
@@ -1280,9 +1284,22 @@ nis_getcanonname(name, hbsize, statp)
 		return FALSE;
 	}
 
-	if (hbsize >= strlen(cname))
+	if (strchr(cname, '.') != NULL)
 	{
-		strcpy(name, cname);
+		domain = "";
+	}
+	else
+	{
+		domain = macvalue('m', CurEnv);
+		if (domain == NULL)
+			domain = "";
+	}
+	if (hbsize >= strlen(cname) + strlen(domain) + 1)
+	{
+		if (domain[0] == '\0')
+			strcpy(name, vp);
+		else
+			sprintf(name, "%s.%s", vp, domain);
 		*statp = EX_OK;
 		return TRUE;
 	}
@@ -1633,9 +1650,16 @@ nisplus_getcanonname(name, hbsize, statp)
 		if (tTd(38, 20))
 			printf("nisplus_getcanonname(%s), found %s\n",
 				name, vp);
-		domain = macvalue('m', CurEnv);
-		if (domain == NULL)
+		if (strchr(vp, '.') != NULL)
+		{
 			domain = "";
+		}
+		else
+		{
+			domain = macvalue('m', CurEnv);
+			if (domain == NULL)
+				domain = "";
+		}
 		if (hbsize > vsize + (int) strlen(domain) + 1)
 		{
 			if (domain[0] == '\0')
