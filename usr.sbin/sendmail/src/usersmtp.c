2e1a84c7d4d9e3946e226adc643e5d780d46777f
Eric Allman <eric>
1995-05-28 11:49:05 -0700

    lint
    SCCS-vsn: 8.54

--- usr.sbin/sendmail/src/usersmtp.c
+++ usr.sbin/sendmail/src/usersmtp.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)usersmtp.c	8.53 (Berkeley) 05/24/95 (with SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.54 (Berkeley) 05/28/95 (with SMTP)";
 #else
-static char sccsid[] = "@(#)usersmtp.c	8.53 (Berkeley) 05/24/95 (without SMTP)";
+static char sccsid[] = "@(#)usersmtp.c	8.54 (Berkeley) 05/28/95 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -63,9 +63,7 @@ char	SmtpError[MAXLINE] = "";	/* save failure error messages */
 int	SmtpPid;			/* pid of mailer */
 bool	SmtpNeedIntro;			/* need "while talking" in transcript */
 
-#ifdef __STDC__
-extern	smtpmessage(char *f, MAILER *m, MCI *mci, ...);
-#endif
+extern void	smtpmessage __P((char *f, MAILER *m, MCI *mci, ...));
 /*
 **  SMTPINIT -- initialize SMTP.
 **
@@ -83,6 +81,7 @@ extern	smtpmessage(char *f, MAILER *m, MCI *mci, ...);
 **		creates connection and sends initial protocol.
 */
 
+void
 smtpinit(m, mci, e)
 	struct mailer *m;
 	register MCI *mci;
@@ -326,6 +325,7 @@ helo_options(line, firstline, m, mci, e)
 **		e -- the envelope (including the sender to specify).
 */
 
+int
 smtpmailfrom(m, mci, e)
 	struct mailer *m;
 	MCI *mci;
@@ -507,6 +507,7 @@ smtpmailfrom(m, mci, e)
 **		Sends the mail via SMTP.
 */
 
+int
 smtprcpt(to, m, mci, e)
 	ADDRESS *to;
 	register MAILER *m;
@@ -601,6 +602,7 @@ smtprcpt(to, m, mci, e)
 static jmp_buf	CtxDataTimeout;
 static void	datatimeout();
 
+int
 smtpdata(m, mci, e)
 	struct mailer *m;
 	register MCI *mci;
@@ -744,6 +746,7 @@ datatimeout()
 **		sends the final protocol and closes the connection.
 */
 
+void
 smtpquit(m, mci, e)
 	register MAILER *m;
 	register MCI *mci;
@@ -783,6 +786,7 @@ smtpquit(m, mci, e)
 **  SMTPRSET -- send a RSET (reset) command
 */
 
+void
 smtprset(m, mci, e)
 	register MAILER *m;
 	register MCI *mci;
@@ -806,6 +810,7 @@ smtprset(m, mci, e)
 **  SMTPPROBE -- check the connection state
 */
 
+int
 smtpprobe(mci)
 	register MCI *mci;
 {
@@ -839,6 +844,7 @@ smtpprobe(mci)
 **		flushes the mail file.
 */
 
+int
 reply(m, mci, e, timeout, pfunc)
 	MAILER *m;
 	MCI *mci;
@@ -884,7 +890,6 @@ reply(m, mci, e, timeout, pfunc)
 		if (p == NULL)
 		{
 			bool oldholderrs;
-			extern char MsgBuf[];		/* err.c */
 
 			/* if the remote end closed early, fake an error */
 			if (errno == 0)
@@ -1006,6 +1011,7 @@ reply(m, mci, e, timeout, pfunc)
 */
 
 /*VARARGS1*/
+void
 #ifdef __STDC__
 smtpmessage(char *f, MAILER *m, MCI *mci, ...)
 #else
