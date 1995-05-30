a28e522b8f4feab03d4c6c8420be7388420e2aa8
Eric Allman <eric>
1995-05-29 18:37:42 -0700

    prototype makemapentry to return a MAP pointer
    SCCS-vsn: 8.137

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sendmail.h	8.136 (Berkeley) 05/28/95
+ *	@(#)sendmail.h	8.137 (Berkeley) 05/29/95
  */
 
 /*
@@ -41,7 +41,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.136		05/28/95";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.137		05/29/95";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
