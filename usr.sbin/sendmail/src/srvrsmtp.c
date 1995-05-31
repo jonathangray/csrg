b3220d502bb4de0dcc0a9b7d32d570211251cc61
Eric Allman <eric>
1995-05-31 08:31:27 -0700

    log more info in several places
    SCCS-vsn: 8.78

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.77 (Berkeley) 05/30/95 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.78 (Berkeley) 05/31/95 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.77 (Berkeley) 05/30/95 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.78 (Berkeley) 05/31/95 (without SMTP)";
 #endif
 #endif /* not lint */
 
@@ -206,7 +206,7 @@ smtp(e)
 
 		/* read the input line */
 		SmtpPhase = "server cmd read";
-		setproctitle("server %s cmd read", CurHostName);
+		setproctitle("server %s cmd read", CurSmtpClient);
 		p = sfgets(inp, sizeof inp, InChannel, TimeOuts.to_nextcommand,
 				SmtpPhase);
 
@@ -347,7 +347,7 @@ smtp(e)
 			{
 				auth_warning(e,
 					"Host %s didn't use HELO protocol",
-					peerhostname);
+					CurSmtpClient);
 			}
 #ifdef PICKY_HELO_CHECK
 			if (strcasecmp(sendinghost, peerhostname) != 0 &&
@@ -355,7 +355,7 @@ smtp(e)
 			     strcasecmp(sendinghost, MyHostName) != 0))
 			{
 				auth_warning(e, "Host %s claimed to be %s",
-					peerhostname, sendinghost);
+					CurSmtpClient, sendinghost);
 			}
 #endif
 
@@ -724,7 +724,7 @@ doquit:
 			if (LogLevel > 0)
 				syslog(LOG_CRIT,
 				    "\"%s\" command from %s (%s)",
-				    c->cmdname, peerhostname,
+				    c->cmdname, CurSmtpClient,
 				    anynet_ntoa(&RealHostAddr));
 # endif
 			/* FALL THROUGH */
@@ -1123,7 +1123,7 @@ runinchild(label, e)
 			auto int st;
 
 			/* parent -- wait for child to complete */
-			setproctitle("server %s child wait", CurHostName);
+			setproctitle("server %s child wait", CurSmtpClient);
 			st = waitfor(childpid);
 			if (st == -1)
 				syserr("451 %s: lost child", label);
