f7c0d6a5d974a05ce7954fee8cfc80feeacaf554
Eric Allman <eric>
1995-06-11 06:47:08 -0700

    updates for new DSN drafts
    SCCS-vsn: 8.81

--- usr.sbin/sendmail/src/srvrsmtp.c
+++ usr.sbin/sendmail/src/srvrsmtp.c
@@ -36,9 +36,9 @@
 
 #ifndef lint
 #ifdef SMTP
-static char sccsid[] = "@(#)srvrsmtp.c	8.80 (Berkeley) 06/05/95 (with SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.81 (Berkeley) 06/11/95 (with SMTP)";
 #else
-static char sccsid[] = "@(#)srvrsmtp.c	8.80 (Berkeley) 06/05/95 (without SMTP)";
+static char sccsid[] = "@(#)srvrsmtp.c	8.81 (Berkeley) 06/11/95 (without SMTP)";
 #endif
 #endif /* not lint */
 
