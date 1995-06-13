12f8982b138773674282ef5b524e2a61ee4b8f98
Christos Zoulas <christos>
1995-06-13 15:40:30 -0700

    - posix compliance: Don't create .[A-Z]* targets - support for .PHONY targets
    SCCS-vsn: 8.3

--- usr.bin/make/make.h
+++ usr.bin/make/make.h
@@ -35,7 +35,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)make.h	8.2 (Berkeley) 04/28/95
+ *	@(#)make.h	8.3 (Berkeley) 06/13/95
  */
 
 /*-
@@ -192,6 +192,7 @@ typedef struct GNode {
 				     * local variables. */
 #define OP_NOTMAIN	0x00008000  /* The node is exempt from normal 'main
 				     * target' processing in parse.c */
+#define OP_PHONY	0x00010000  /* Not a file target; run always */
 /* Attributes applied by PMake */
 #define OP_TRANSFORM	0x80000000  /* The node is a transformation rule */
 #define OP_MEMBER 	0x40000000  /* Target is a member of an archive */
