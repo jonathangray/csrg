f1ab709d7c096ae4a789eab0b9f6090d8bcf108a
Eric Allman <eric>
1995-06-17 10:23:43 -0700

    improved debugging
    SCCS-vsn: 8.12

--- usr.sbin/sendmail/src/macro.c
+++ usr.sbin/sendmail/src/macro.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)macro.c	8.11 (Berkeley) 04/21/95";
+static char sccsid[] = "@(#)macro.c	8.12 (Berkeley) 06/17/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -340,7 +340,11 @@ macid(p, ep)
 	char mbuf[21];
 
 	if (tTd(35, 14))
-		printf("macid(%s) => ", p);
+	{
+		printf("macid(");
+		xputs(p);
+		printf(") => ");
+	}
 
 	if (*p == '\0' || (p[0] == '{' && p[1] == '}'))
 	{
