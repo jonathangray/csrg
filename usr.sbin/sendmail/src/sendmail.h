46d6984d44b55aa9ab11a24dab53ae0e5a78ab5b
Eric Allman <eric>
1995-06-18 12:04:08 -0700

    add EF_DONT_MIME flag
    SCCS-vsn: 8.144

--- usr.sbin/sendmail/src/sendmail.h
+++ usr.sbin/sendmail/src/sendmail.h
@@ -31,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)sendmail.h	8.143 (Berkeley) 06/15/95
+ *	@(#)sendmail.h	8.144 (Berkeley) 06/18/95
  */
 
 /*
@@ -41,7 +41,7 @@
 # ifdef _DEFINE
 # define EXTERN
 # ifndef lint
-static char SmailSccsId[] =	"@(#)sendmail.h	8.143		06/15/95";
+static char SmailSccsId[] =	"@(#)sendmail.h	8.144		06/18/95";
 # endif
 # else /*  _DEFINE */
 # define EXTERN extern
@@ -468,6 +468,7 @@ struct envelope
 #define EF_RET_PARAM	0x0100000	/* RCPT command had RET argument */
 #define EF_HAS_DF	0x0200000	/* set when df file is instantiated */
 #define EF_IS_MIME	0x0400000	/* really is a MIME message */
+#define EF_DONT_MIME	0x0800000	/* never MIME this message */
 
 EXTERN ENVELOPE	*CurEnv;	/* envelope currently being processed */
 
