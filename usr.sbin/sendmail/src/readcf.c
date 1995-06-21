530902a3501e5a2692d35d300b1ba6fcc2fe3710
Eric Allman <eric>
1995-06-21 07:09:54 -0700

    avoid bogus returns in symbolic ruleset names
    SCCS-vsn: 8.108

--- usr.sbin/sendmail/src/readcf.c
+++ usr.sbin/sendmail/src/readcf.c
@@ -33,7 +33,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)readcf.c	8.107 (Berkeley) 06/20/95";
+static char sccsid[] = "@(#)readcf.c	8.108 (Berkeley) 06/21/95";
 #endif /* not lint */
 
 # include "sendmail.h"
@@ -2269,7 +2269,7 @@ strtorwset(p, endp, stabmode)
 
 		if (stabmode == ST_ENTER && delim == '=')
 		{
-			ruleset = strtol(p, endp, 10);
+			ruleset = strtol(++p, endp, 10);
 			if (ruleset >= MAXRWSETS / 2 || ruleset < 0)
 			{
 				syserr("bad ruleset %s = %d (%d max)",
